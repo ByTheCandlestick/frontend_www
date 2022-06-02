76395e4d9b7469c32938849603ee14634a7c7cfd
RSmith <master_schmadoodle@outlook.com>
2022-06-02 23:46:58 +0100

    Minor changes to Dashboard design

--- Pages/index.php
+++ Pages/index.php
@@ -1,16 +1,36 @@
 <section>
 	<div class="row">
 		<div class="col-12 col-md-6 col-lg-3 p-2">
-			YoY SALES
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">YOY Sales</h5>
+					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+				</div>
+			</div>
 		</div>
 		<div class="col-12 col-md-6 col-lg-3 p-2">
-			GROSS PROFIT YoY
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">GROSS PROFIT YoY</h5>
+					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+				</div>
+			</div>
 		</div>
 		<div class="col-12 col-md-6 col-lg-3 p-2">
-			SALES TODAY
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">SALES TODAY</h5>
+					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+				</div>
+			</div>
 		</div>
 		<div class="col-12 col-md-6 col-lg-3 p-2">
-			GROSS PROFIT TODAY
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">GROSS PROFIT TODAY</h5>
+					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
