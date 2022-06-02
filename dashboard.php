c406252f9a3bb8eeb0960d8e92c4c2362438e5c8
Robert Smith <master_schmadoodle@outlook.com>
2022-06-02 17:17:42 +0100

    Minor changes

--- dashboard.php
+++ dashboard.php
@@ -66,7 +66,7 @@
 									# @todo generate all page links
 									foreach(mysqli_fetch_assoc(DB_Query("SELECT * FROM `admin_pages` WHERE `Active`=1")) as $item) {
 										print_r(sprintf('
-											<a href="%s" class="app-sidebar-link active">
+											<a href="" class="app-sidebar-link active">
 												<svg
 													xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
 													<path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
