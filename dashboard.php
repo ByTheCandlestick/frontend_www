3be4ad82d42548e25c841cda6258b5fabf66224c
Robert Smith <master_schmadoodle@outlook.com>
2022-06-02 17:21:29 +0100

    Minor changes

--- dashboard.php
+++ dashboard.php
@@ -64,18 +64,16 @@
 						<div class="app-sidebar">
 							<?
 									$items = DB_Query("SELECT * FROM `admin_pages` WHERE `Active`=1");
-									print_r($items);
 									foreach($items as $item) {
-										print_r($item);
 										print(sprintf('
-											<a href="" class="app-sidebar-link">
+											<a href="%s" class="app-sidebar-link">
 												<svg
 													xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
 													<path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
 													<polyline points="9 22 9 12 15 12 15 22" />
 												</svg>
 											</a>
-										'));
+										', $item['URL']));
 									}
 								?>
 							<a href="" class="app-sidebar-link">
