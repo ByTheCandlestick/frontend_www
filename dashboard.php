72a485186aabca6792a4de4679ddc89b3265a0ae
Robert Smith <master_schmadoodle@outlook.com>
2022-06-02 17:16:34 +0100

    Minor changes

--- dashboard.php
+++ dashboard.php
@@ -64,16 +64,18 @@
 						<div class="app-sidebar">
 							<?
 									# @todo generate all page links
-									$menu_items = mysqli_fetch_assoc(DB_Query("SELECT * FROM `admin_pages` WHERE `Active`=1"));
-									print_r($menu_items);
+									foreach(mysqli_fetch_assoc(DB_Query("SELECT * FROM `admin_pages` WHERE `Active`=1")) as $item) {
+										print_r(sprintf('
+											<a href="%s" class="app-sidebar-link active">
+												<svg
+													xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
+													<path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+													<polyline points="9 22 9 12 15 12 15 22" />
+												</svg>
+											</a>
+										', $item['URL']));
+									}
 								?>
-							<a href="" class="app-sidebar-link active">
-								<svg
-									xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
-									<path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-									<polyline points="9 22 9 12 15 12 15 22" />
-								</svg>
-							</a>
 							<a href="" class="app-sidebar-link">
 								<svg class="link-icon"
 									xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-pie-chart" viewBox="0 0 24 24">
