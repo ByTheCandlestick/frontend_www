2ed9b721f2376db94be982e13c039eae1e6ea825
RSmith <master_schmadoodle@outlook.com>
2022-06-02 23:45:21 +0100

    Minor changes to Dashboard design

--- Pages/index.php
+++ Pages/index.php
@@ -1,15 +1,15 @@
 <section>
 	<div class="row">
-		<div class="col-12 col-md-6 col-lg-4 p-2">
+		<div class="col-12 col-md-6 col-lg-3 p-2">
 			YoY SALES
 		</div>
-		<div class="col-12 col-md-6 col-lg-4 p-2">
+		<div class="col-12 col-md-6 col-lg-3 p-2">
 			GROSS PROFIT YoY
 		</div>
-		<div class="col-12 col-md-6 col-lg-4 p-2">
+		<div class="col-12 col-md-6 col-lg-3 p-2">
 			SALES TODAY
 		</div>
-		<div class="col-12 col-md-6 col-lg-4 p-2">
+		<div class="col-12 col-md-6 col-lg-3 p-2">
 			GROSS PROFIT TODAY
 		</div>
 	</div>
