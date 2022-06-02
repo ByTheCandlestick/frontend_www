636e9c4e94f41b5d46a4523dd6a3d2c897cf8c89
RSmith <master_schmadoodle@outlook.com>
2022-06-02 22:50:15 +0100

    Minor changes

--- Classes/vars.php
+++ Classes/vars.php
@@ -57,6 +57,21 @@
 		//
 	}
 	if($user_ok) {
-		header ('Connection: close');
+		// buffer all upcoming output
+		ob_start();
+
+		// get the size of the output
+		$size = ob_get_length();
+
+		// send headers to tell the browser to close the connection
+		header("Content-Length: $size");
+		header('Connection: close');
+
+		// flush all output
+		ob_end_flush();
+		ob_flush();
+		flush();
+
+		// RUN ANY REQUIRED FUNCTIONS
 	}
 ?>
