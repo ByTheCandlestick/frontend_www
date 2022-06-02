006bdb5a7e2c3bfe62514369f5c25c35e829014c
RSmith <master_schmadoodle@outlook.com>
2022-06-02 23:31:50 +0100

    Minor changes to the acces script

--- Classes/vars.php
+++ Classes/vars.php
@@ -30,7 +30,7 @@
 			if(isset($_COOKIE["session_code"])) {
 				$log_session	= $_COOKIE['session_code'];
 				$query = DB_Query("SELECT * FROM `Users_sessions` WHERE `Session_code`='$log_session' AND `Active`='1' LIMIT 1");
-				$log_id = mysqli_fetch_row($query)['ID']
+				$log_id = mysqli_fetch_row($query)['ID'];
 				$numrows = mysqli_num_rows($query);
 				if($numrows > 0){
 					$user_ok = true;
