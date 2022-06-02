4c47351362ba4385a7a27697f1cfea1e8e8d06aa
RSmith <master_schmadoodle@outlook.com>
2022-06-02 23:59:38 +0100

    Minor changes to Dashboard design

--- Pages/index.php
+++ Pages/index.php
@@ -1,31 +1,31 @@
 <section>
 	<div class="row">
-		<div class="col-12 col-md-6 col-lg-3 p-2 h-100">
-			<div class="card">
+		<div class="col-12 col-md-6 col-lg-3 p-2">
+			<div class="card h-100">
 				<div class="card-body">
 					<h5 class="card-title">YOY Sales</h5>
 					<p class="card-text">With supporting text beloW.</p>
 				</div>
 			</div>
 		</div>
-		<div class="col-12 col-md-6 col-lg-3 p-2 h-100">
-			<div class="card">
+		<div class="col-12 col-md-6 col-lg-3 p-2">
+			<div class="card h-100">
 				<div class="card-body">
 					<h5 class="card-title">GROSS PROFIT YoY</h5>
 					<p class="card-text">With supporting text below as a natural lead-in.</p>
 				</div>
 			</div>
 		</div>
-		<div class="col-12 col-md-6 col-lg-3 p-2 h-100">
-			<div class="card">
+		<div class="col-12 col-md-6 col-lg-3 p-2">
+			<div class="card h-100">
 				<div class="card-body">
 					<h5 class="card-title">SALES TODAY</h5>
 					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
 				</div>
 			</div>
 		</div>
-		<div class="col-12 col-md-6 col-lg-3 p-2 h-100">
-			<div class="card">
+		<div class="col-12 col-md-6 col-lg-3 p-2">
+			<div class="card h-100">
 				<div class="card-body">
 					<h5 class="card-title">GROSS PROFIT TODAY</h5>
 					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
