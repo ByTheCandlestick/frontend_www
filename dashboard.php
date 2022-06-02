100315ab34dfa46ab2937e84c67594186c5d881b
Robert Smith <master_schmadoodle@outlook.com>
2022-06-02 17:42:58 +0100

    Minor changes

--- dashboard.php
+++ dashboard.php
@@ -58,14 +58,12 @@
 								foreach($items as $item) {
 									print(sprintf('
 										<a href="%s" class="app-sidebar-link">
-											<svg
-												xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
-												<path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-												<polyline points="9 22 9 12 15 12 15 22" />
-											</svg>
+											<i class="fa fa-%s"></i>
 										</a>
-									',
-									$item['URL']));
+										',
+										$item['URL'],
+										$item['Icon']
+									));
 								}
 							?>
 						</div>
