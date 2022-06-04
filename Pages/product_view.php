9298df8c6a9db1e3d1d099b69ea65b018a946090
Robert Smith <master_schmadoodle@outlook.com>
2022-06-04 10:34:09 +0100

    Minor changes to products page

--- Pages/product_view.php
+++ Pages/product_view.php
@@ -1,1 +1,11 @@
-product_view <? print(QS) ?>
+<?
+	// @todo Get Product Info
+?>
+<div class="row">
+	<div class="col-12 col-md-4 col-lg-3">
+		<input type="text" />
+	</div>
+</div>
+<div class="row">
+	
+</div>
