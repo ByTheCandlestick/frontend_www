250487c0b7a0bc275b12931798d21d87d5c9ee4a
Robert Smith <master_schmadoodle@outlook.com>
2022-06-04 12:17:28 +0100

    Minor changes to CSS

--- Pages/product_view.php
+++ Pages/product_view.php
@@ -8,7 +8,7 @@
 			<h1>Product Info</h1>
 		</div>
 		<div class="col-12 col-md-6 col-lg-4 offset-lg-4">
-			<h1>Product Info</h1>
+			<a href="#" class="btn btn-primary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Primary link</a>
 		</div>
 	</div>
 	<hr>
