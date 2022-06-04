3167c6a1539a01d0235898cf3636f2c34e5cd40f
Robert Smith <master_schmadoodle@outlook.com>
2022-06-04 10:59:12 +0100

    Minor changes to products page

--- Pages/product_view.php
+++ Pages/product_view.php
@@ -2,9 +2,34 @@
 	// @todo Get Product Info
 ?>
 <div class="row" name="ProductInfo">
-	<div class="col-12 col-md-4 col-lg-2"> <input type="text" class="w-100" name="SKU"/> </div>
-	<div class="col-12 col-md-4 col-lg-4"> <input type="text" class="w-100" name="Title"/> </div>
-	<div class="col-12 col-md-4 col-lg-2"> <input type="text" class="w-100" name="Category"/> </div>
-	<div class="col-12 col-md-4 col-lg-2"> <input type="text" class="w-100" name="Subcategory"/> </div>
-	<div class="col-12 col-md-4 col-lg-2"> <input type="text" class="w-100" name="Range"/> </div>
+	<div class="col-12 col-md-4 col-lg-2">
+		<div class="form-floating mb-3">
+			<input type="text" class="form-control" id="floatingInput" placeholder="">
+			<label for="floatingInput">SKU</label>
+		</div>
+	</div>
+	<div class="col-12 col-md-4 col-lg-4">
+		<div class="form-floating mb-3">
+			<input type="text" class="form-control" id="floatingInput" placeholder="">
+			<label for="floatingInput">Title</label>
+		</div>
+	</div>
+	<div class="col-12 col-md-4 col-lg-2">
+		<div class="form-floating mb-3">
+			<input type="text" class="form-control" id="floatingInput" placeholder="">
+			<label for="floatingInput">Category</label>
+		</div>
+	</div>
+	<div class="col-12 col-md-4 col-lg-2">
+		<div class="form-floating mb-3">
+			<input type="text" class="form-control" id="floatingInput" placeholder="">
+			<label for="floatingInput">Subcategory</label>
+		</div>
+	</div>
+	<div class="col-12 col-md-4 col-lg-2">
+		<div class="form-floating mb-3">
+			<input type="text" class="form-control" id="floatingInput" placeholder="">
+			<label for="floatingInput">Range</label>
+		</div>
+	</div>
 </div>
