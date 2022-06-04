fb0580a7bc4a61e2a483aa83b3e19bcfe8ac8321
Robert Smith <master_schmadoodle@outlook.com>
2022-06-04 11:40:34 +0100

    Minor changes to products page

--- Pages/product_view.php
+++ Pages/product_view.php
@@ -2,19 +2,19 @@
 	$prod = mysqli_fetch_assoc(DB_Query(sprintf("SELECT  * FROM `products` WHERE `SKU`=%s", QS)));
 ?>
 <div class="row" name="ProductInfo">
-	<div class="col-12 col-md-4 col-lg-2">
+	<div class="col-12 col-lg-2">
 		<div class="form-floating mb-3">
 			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['SKU'])?>">
 			<label for="floatingInput">SKU</label>
 		</div>
 	</div>
-	<div class="col-12 col-md-4 col-lg-4">
+	<div class="col-12 col-lg-4">
 		<div class="form-floating mb-3">
 			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Title'])?>">
 			<label for="floatingInput">Title</label>
 		</div>
 	</div>
-	<div class="col-12 col-md-4 col-lg-2">
+	<div class="col-12 col-lg-2">
 		<div class="form-floating mb-3">
 			<select class="form-select" id="floatingSelect">
 				<option selected>Please select</option>
@@ -22,13 +22,13 @@
 			<label for="floatingInput">Range</label>
 		</div>
 	</div>
-	<div class="col-12 col-md-6 col-lg-4">
+	<div class="col-12 col-lg-4">
 		<div class="form-floating mb-3">
 			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['images'])?>">
 			<label for="floatingInput">IMAGES</label>
 		</div>
 	</div>
-	<div class="col-12 col-md-4 col-lg-2">
+	<div class="col-12 col-lg-2">
 		<div class="form-floating mb-3">
 			<select class="form-select" id="floatingSelect">
 				<option selected>Please select</option>
@@ -36,7 +36,7 @@
 			<label for="floatingInput">Category</label>
 		</div>
 	</div>
-	<div class="col-12 col-md-4 col-lg-2">
+	<div class="col-12 col-lg-2">
 		<div class="form-floating mb-3">
 			<select class="form-select" id="floatingSelect">
 				<option selected>Please select</option>
@@ -44,7 +44,7 @@
 			<label for="floatingInput">Subcategory</label>
 		</div>
 	</div>
-	<div class="col-12 col-md-4 col-lg-2">
+	<div class="col-12 col-lg-2">
 		<div class="form-floating mb-3">
 			<div class="form-check form-switch">
 				<input class="form-check-input" type="checkbox" value="" id="flexCheckDisabled" disabled>
@@ -56,44 +56,44 @@
 			</div>
 		</div>
 	</div>
-	<div class="col-3 col-md-2 col-lg-1">
+	<div class="col-3 col-lg-1">
 		<div class="form-floating mb-3">
 			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Currency'])?>">
 			<label for="floatingInput">Currency</label>
 		</div>
 	</div>
-	<div class="col-9 col-md-10 col-lg-1">
+	<div class="col-9 col-lg-1">
 		<div class="form-floating mb-3">
 			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['GrossProfit'])?>">
 			<label for="floatingInput">Profit</label>
 		</div>
 	</div>
-	<div class="col-9 col-md-10 col-lg-1">
+	<div class="col-9 col-lg-1">
 		<div class="form-floating mb-3">
 			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['RetailPrice'])?>">
 			<label for="floatingInput">Retail</label>
 		</div>
 	</div>
-	<div class="col-12 col-md-3 col-lg-1">
+	<div class="col-12 col-lg-1">
 		<div class="form-floating mb-3">
 			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['NetPrice'])?>">
 			<label for="floatingInput">Net</label>
 		</div>
 	</div>
-	<div class="col-12 col-md-3 col-lg-1">
+	<div class="col-12 col-lg-1">
 		<div class="form-floating mb-3">
 			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['GrossPrice'])?>">
 			<label for="floatingInput">Gross</label>
 		</div>
 	</div>
-	<div class="col-9 col-md-10 col-lg-1">
+	<div class="col-9 col-lg-1">
 		<div class="form-floating mb-3 input-group">
 			<input type="text" class="form-control" id="floatingInput" placeholder="" aria-describedby="percentage" value="<? print($prod['ProfitMargin'])?>">
 			<label for="floatingInput">Markup</label>
 			<span class="input-group-text" id="percentage">%</span>
 		</div>
 	</div>
-	<div class="col-12 col-md-4 col-lg-2">
+	<div class="col-12 col-lg-2">
 		<div class="form-floating mb-3">
 			<div class="form-check form-switch">
 				<input class="form-check-input" type="checkbox" value="" id="flexCheckDisabled" disabled>
@@ -101,7 +101,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="col-12 col-md-4 col-lg-2">
+	<div class="col-12 col-lg-2">
 		<div class="form-floating mb-3">
 			<select class="form-select" id="floatingSelect">
 				<option selected>Please select</option>
@@ -111,7 +111,7 @@
 			<label for="floatingInput">Discount type</label>
 		</div>
 	</div>
-	<div class="col-12 col-md-4 col-lg-2">
+	<div class="col-12 col-lg-2">
 		<div class="form-floating mb-3">
 			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['DiscountAmount'])?>">
 			<label for="floatingInput">Discount</label>
