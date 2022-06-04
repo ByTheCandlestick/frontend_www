128dfba4a377c40df214861c16cfdec0d8fbcd32
Robert Smith <master_schmadoodle@outlook.com>
2022-06-04 11:08:10 +0100

    Minor changes to products page

--- Pages/product_view.php
+++ Pages/product_view.php
@@ -48,7 +48,15 @@
 		<div class="form-floating mb-3">
 			<div class="form-check">
 				<input class="form-check-input" type="checkbox" value="" id="flexCheckDisabled" disabled>
-				<label class="form-check-label" for="flexCheckDisabled"> Disabled checkbox </label>
+				<label class="form-check-label" for="flexCheckDisabled"> Discontinued? </label>
+			</div>
+		</div>
+	</div>
+	<div class="col-12 col-md-4 col-lg-2">
+		<div class="form-floating mb-3">
+			<div class="form-check">
+				<input class="form-check-input" type="checkbox" value="" id="flexCheckDisabled" disabled>
+				<label class="form-check-label" for="flexCheckDisabled"> Active? </label>
 			</div>
 		</div>
 	</div>
