b5c07d09157a44f5fd9d3a389ed7c46d43b78b08
Robert Smith <master_schmadoodle@outlook.com>
2022-06-04 10:37:52 +0100

    Minor changes to products page

--- Pages/product_view.php
+++ Pages/product_view.php
@@ -2,7 +2,8 @@
 	// @todo Get Product Info
 ?>
 <div class="row" name="ProductInfo">
-	<div class="col-12 col-md-4 col-lg-3"> <input type="text" class="w-100" name=""/> </div>
-	<div class="col-12 col-md-4 col-lg-3"> <input type="text" class="w-100" name=""/> </div>
-	<div class="col-12 col-md-4 col-lg-3"> <input type="text" class="w-100" name=""/> </div>
+	<div class="col-12 col-md-4 col-lg-2"> <input type="text" class="w-100" name="SKU"/> </div>
+	<div class="col-12 col-md-4 col-lg-4"> <input type="text" class="w-100" name="Title"/> </div>
+	<div class="col-12 col-md-4 col-lg-3"> <input type="text" class="w-100" name="Category"/> </div>
+	<div class="col-12 col-md-4 col-lg-3"> <input type="text" class="w-100" name="Range"/> </div>
 </div>
