d7e027c85f50a7e48ffaf08df8adaabb7a5459a9
Robert Smith <master_schmadoodle@outlook.com>
2022-06-04 11:33:26 +0100

    Minor changes to products page

--- Pages/product_view.php
+++ Pages/product_view.php
@@ -56,28 +56,28 @@
 			</div>
 		</div>
 	</div>
-	<div class="col-12 col-md-3 col-lg-1">
-		<div class="form-floating mb-3">
-			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['NetPrice'])?>">
-			<label for="floatingInput">Net Price</label>
-		</div>
-	</div>
-	<div class="col-12 col-md-3 col-lg-1">
-		<div class="form-floating mb-3">
-			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['GrossPrice'])?>">
-			<label for="floatingInput">Gross Price</label>
-		</div>
-	</div>
 	<div class="col-3 col-md-2 col-lg-1">
 		<div class="form-floating mb-3">
 			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Currency'])?>">
 			<label for="floatingInput">Currency</label>
 		</div>
 	</div>
+	<div class="col-12 col-md-3 col-lg-1">
+		<div class="form-floating mb-3">
+			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['NetPrice'])?>">
+			<label for="floatingInput">Net</label>
+		</div>
+	</div>
+	<div class="col-12 col-md-3 col-lg-1">
+		<div class="form-floating mb-3">
+			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['GrossPrice'])?>">
+			<label for="floatingInput">Gross</label>
+		</div>
+	</div>
 	<div class="col-9 col-md-10 col-lg-1">
 		<div class="form-floating mb-3">
 			<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['RetailPrice'])?>">
-			<label for="floatingInput">Retail Price</label>
+			<label for="floatingInput">Retail</label>
 		</div>
 	</div>
 	<div class="col-9 col-md-10 col-lg-1">
