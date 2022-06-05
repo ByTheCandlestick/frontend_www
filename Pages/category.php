26803d5c913bdfe6a15718f395515ac8f8bde6ce
Robert Smith <master_schmadoodle@outlook.com>
2022-06-05 13:10:29 +0100

    Minor changes to Product.php and user.php

--- Pages/category.php
+++ Pages/category.php
@@ -1,15 +1,41 @@
-<section>
-	<!-- Section Header -->
-	<div class="row">
-		<div class="col-12 col-md-6 col-lg-8">
-			<h1>Product Category</h1>
+<?
+	$query = DB_Query(sprintf("SELECT * FROM `product_categories` WHERE `ID`=%s", QS));
+	if(mysqli_num_rows($query) > 0) {
+		$user = mysqli_fetch_assoc($query);
+?>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6 col-lg-8">
+				<h1>Product Category</h1>
+			</div>
+			<div class="col-12 col-md-6 col-lg-4 text-md-end">
+			</div>
 		</div>
-		<div class="col-12 col-md-6 col-lg-4 text-md-end">
-		</div>
-	</div>
-	<hr>
-	<!-- Section Body -->
-	<div class="row">
+		<hr>
+		<!-- Section Body -->
+		<div class="row">
 
-	</div>
-</section>
+		</div>
+	</section>
+<?
+	} else {
+?>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6 col-lg-8">
+				<h1>Category not found.</h1>
+			</div>
+			<div class="col-12 col-md-6 col-lg-4 text-md-end">
+			</div>
+		</div>
+		<hr>
+		<!-- Section Body -->
+		<div class="row">
+			<button class="btn btn-outline-primary col-12 col-md-3 col-lg-1" onclick="history.go(-1)">Go back</buton>
+		</div>
+	</section>
+<?
+	}
+?>
