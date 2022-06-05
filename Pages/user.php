532e964c86b64ea6c6e7060b0faf24caf1bba20b
Robert Smith <master_schmadoodle@outlook.com>
2022-06-05 11:21:07 +0100

    Minor changes to users/user titles

---
+++ Pages/user.php
@@ -0,0 +1,15 @@
+<section>
+	<!-- Section Header -->
+	<div class="row">
+		<div class="col-12 col-md-6 col-lg-4">
+			<h1>User info</h1>
+		</div>
+		<div class="col-12 col-md-6 col-lg-8 text-md-end">
+		</div>
+	</div>
+	<hr>
+	<!-- Section Body -->
+	<div class="row">
+
+	</div>
+</section>
--- Pages/users.php
+++ Pages/users.php
@@ -2,7 +2,7 @@
 	<!-- Section Header -->
 	<div class="row">
 		<div class="col-12 col-md-6 col-lg-4">
-			<h1>Products</h1>
+			<h1>Users</h1>
 		</div>
 		<div class="col-12 col-md-6 col-lg-8 text-md-end">
 		</div>
