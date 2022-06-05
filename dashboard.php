24db152074b3e73a468caa9d6d578330024efecf
Robert Smith <master_schmadoodle@outlook.com>
2022-06-05 13:24:05 +0100

    Added name on admin page

--- dashboard.php
+++ dashboard.php
@@ -90,7 +90,7 @@
 							</button>
 							<button class="profile-btn">
 								<img src="https://assets.codepen.io/3306515/IMG_2025.jpg" />
-								<span>Aybüke C.</span>
+								<span><?print($userdata['First_name'].''.$userdata['Last_name'])?></span>
 							</button>
 						</div>
 					</div>
