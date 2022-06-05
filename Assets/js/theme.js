93cd545028ae459e93626ac1b0763370f9500b2e
Robert Smith <master_schmadoodle@outlook.com>
2022-06-05 12:02:53 +0100

    Preloader CSS

--- Assets/js/theme.js
+++ Assets/js/theme.js
@@ -1,18 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-	var modeSwitch = document.querySelector('.mode-switch');
-
-	modeSwitch.addEventListener('click', function () {                     document.documentElement.classList.toggle('dark');
-		modeSwitch.classList.toggle('active');
-	});
-	
-	var listView = document.querySelector('.list-view');
-	var gridView = document.querySelector('.grid-view');
-	
-	document.querySelector('.messages-btn').addEventListener('click', function () {
-		document.querySelector('.messages-section').classList.add('show');
-	});
-	
-	document.querySelector('.messages-close').addEventListener('click', function() {
-		document.querySelector('.messages-section').classList.remove('show');
-	});
+$( document ).ready(function() {
+	// -----========== Dark mode toggle ==========----- //
+		var modeSwitch = $('.mode-switch');
+		modeSwitch.click(function () {
+			$( document ).toggleClass('dark');
+			modeSwitch.toggleClass('active');
+		});
+	// -----========== PRELOADER ==========----- //
+	$('.app-preloader').fadeOut()
 });
--- dashboard.php
+++ dashboard.php
@@ -40,6 +40,24 @@
 			<script src="/Assets/js/theme.js"></script>
 		</head>
 		<body>
+			<div class="app-preloader">
+				<svg class="pl" viewBox="0 0 128 256" width="128px" height="256px" xmlns="http://www.w3.org/2000/svg">
+					<defs>
+						<linearGradient id="pl-grad1" x1="0" y1="0" x2="0" y2="1">
+							<stop offset="0%" stop-color="hsl(223,90%,55%)" />
+							<stop offset="100%" stop-color="hsl(253,90%,55%)" />
+						</linearGradient>
+						<linearGradient id="pl-grad2" x1="0" y1="0" x2="0" y2="1">
+							<stop offset="0%" stop-color="hsl(193,90%,55%)" />
+							<stop offset="50%" stop-color="hsl(223,90%,55%)" />
+							<stop offset="100%" stop-color="hsl(253,90%,55%)" />
+						</linearGradient>
+					</defs>
+					<circle class="pl_ring" r="56" cx="64" cy="192" fill="none" stroke="#ddd" stroke-width="16" stroke-linecap="round" />
+					<circle class="pl_worm1" r="56" cx="64" cy="192" fill="none" stroke="url(#pl-grad1)" stroke-width="16" stroke-linecap="round" stroke-dasharray="87.96 263.89" />
+					<path class="pl_worm2" d="M120,192A56,56,0,0,1,8,192C8,161.07,16,8,64,8S120,161.07,120,192Z" fill="none" stroke="url(#pl-grad2)" stroke-width="16" stroke-linecap="round" stroke-dasharray="87.96 494" />
+				</svg>
+			</div>
 			<div class="app-container">
 				<div class="app-header">
 					<div class="app-header-left">
