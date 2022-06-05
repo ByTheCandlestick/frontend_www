27479f429a940ab72920e3047c5121d4de657c97
Robert Smith <master_schmadoodle@outlook.com>
2022-06-05 11:34:37 +0100

    Minor changes to sidebar

--- Pages/category.php
+++ Pages/category.php
@@ -2,7 +2,7 @@
 	<!-- Section Header -->
 	<div class="row">
 		<div class="col-12 col-md-6 col-lg-4">
-			<h1>Product Collections</h1>
+			<h1>Product Category</h1>
 		</div>
 		<div class="col-12 col-md-6 col-lg-8 text-md-end">
 		</div>
--- Pages/tags.php
+++ Pages/tags.php
@@ -2,7 +2,7 @@
 	<!-- Section Header -->
 	<div class="row">
 		<div class="col-12 col-md-6 col-lg-4">
-			<h1>Product Categories</h1>
+			<h1>Product Categories, Collections and Tags</h1>
 		</div>
 		<div class="col-12 col-md-6 col-lg-8 text-md-end">
 		</div>
