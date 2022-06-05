83aae876ca7f96ec3cf2fab68396fd3ae966a72b
Robert Smith <master_schmadoodle@outlook.com>
2022-06-05 13:15:24 +0100

    Minor changes to error.php

---
+++ Pages/error.php
@@ -0,0 +1,41 @@
+<?
+	if(QS_SUBPAGE !== "") {
+?>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6 col-lg-8">
+				<h1>Error <?print(QS_SUBPAGE)?>.</h1>
+			</div>
+			<div class="col-12 col-md-6 col-lg-4 text-md-end">
+			</div>
+		</div>
+		<hr>
+		<!-- Section Body -->
+		<div class="row">
+			An error has occurred, Please try again later.
+			<button class="btn btn-outline-primary col-12 col-md-3 col-lg-1" onclick="history.go(-1)">Go back</buton>
+		</div>
+	</section>
+<?
+	} else {
+?>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6 col-lg-8">
+				<h1>An error occured, no error code was given.</h1>
+			</div>
+			<div class="col-12 col-md-6 col-lg-4 text-md-end">
+			</div>
+		</div>
+		<hr>
+		<!-- Section Body -->
+		<div class="row">
+			An error has occurred, Please try again later.
+			<button class="btn btn-outline-primary col-12 col-md-3 col-lg-1" onclick="history.go(-1)">Go back</buton>
+		</div>
+	</section>
+<?
+	}
+?>
