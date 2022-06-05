c7f9b26fa381db77ef38c2949375a542d91cc93a
Robert Smith <master_schmadoodle@outlook.com>
2022-06-05 13:54:17 +0100

    Minor changes to dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -68,12 +68,15 @@
 						<p class="app-name">The Candlestick</p>
 					</div>
 					<!-- Search -->
-					<div class="app-header-center search-wrapper">
+					<div class="search-wrapper">
 						<input class="search-input" type="text" placeholder="Search">
 						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-search" viewBox="0 0 24 24">
 							<circle cx="11" cy="11" r="8"></circle>
 							<path d="M21 21l-4.35-4.35"></path>
 						</svg>
+						<div class="search-results">
+							
+						</div>
 					</div>
 					<!-- Profile, Notifications and Dark mode -->
 					<div class="app-header-right">
