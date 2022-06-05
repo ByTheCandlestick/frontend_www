62a2edb1fc72600cf7db441c4e6d9299d3492eba
Robert Smith <master_schmadoodle@outlook.com>
2022-06-05 15:25:48 +0100

    Minor changes to search

--- Assets/js/temp.js
+++ Assets/js/temp.js
@@ -1,21 +1,20 @@
-$( document ).load( function() {
+$( document ).on( "load", function() {
 	var codesEl = document.getElementById("codes");
 	$.get('/Assets/search.json', function(data) { var jsonData = data; });
+});
 
-	function printData(Arr) {
-		for(var i=0; i<Arr.length; i++) {
-			codesEl.innerText += `\n${Arr[i].name} code: ${Arr[i].code}`;
-		}
+function printData(Arr) {
+	for(var i=0; i<Arr.length; i++) {
+		codesEl.innerText += `\n${Arr[i].name} code: ${Arr[i].code}`;
 	}
+}
 
-	function search(ev) {
-		var key = ev.target.value;
-		codesEl.innerText = null;
-		
-		printData(jsonData.filter((data)=>{
-			var regex = new RegExp(key, "i");
-			return data.name.match(regex) || data.code.match(regex);
-		}));
-	}
-
+function search(ev) {
+	var key = ev.target.value;
+	codesEl.innerText = null;
+	
+	printData(jsonData.filter((data)=>{
+		var regex = new RegExp(key, "i");
+		return data.name.match(regex) || data.code.match(regex);
+	}));
 }
