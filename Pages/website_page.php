b9a5d460d18e1b2afc81570fe32c31e43850e99f
RSmith <master_schmadoodle@outlook.com>
2022-06-06 09:38:28 +0100

    Minor changes

---
+++ Pages/website_page.php
@@ -0,0 +1,56 @@
+<section>
+	<!-- Section Header -->
+	<div class="row">
+		<div class="col-12 col-md-6 col-lg-8">
+			<h1>Website</h1>
+		</div>
+		<div class="col-12 col-md-6 col-lg-4 text-md-end">
+			<input type="text" class="tableFilter"> </input>
+		</div>
+	</div>
+	<hr>
+	<!-- Section Body -->
+	<div class="row">
+		<table class="productsTable table table-striped table-hover">
+			<thead class="sticky-top" style="background: var(--section); z-index: unset;">
+				<tr>
+					<th scope="col">ID</th>
+					<th scope="col">URL</th>
+					<th scope="col">Sub URL</th>
+					<th scope="col">Page Title</th>
+					<th scope="col"></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?
+					$query = DB_Query(sprintf("SELECT * FROM `page_layouts` WHERE `domain_id`=%s ORDER BY `id`", QS));
+					while ($row = mysqli_fetch_array($query)) {
+						print('
+							<tr>
+								<th scope="row">'.$row['ID'].'</th>
+								<td>'.$row['page_url'].'</td>
+								<td>'.$row['subpage_url'].'</td>
+								<td>'.$row['page_title'].'</td>
+								<td>
+									<a href="/Website/Page/'.$row['ID'].'">
+										<i class="fa fa-ellipsis"></i>
+									</a>
+								</td>
+							</tr>
+						');
+					}
+				?>
+			</tbody>
+		</table>
+	</div>
+</section>
+<script>
+	$(document).ready(function(){
+		$(".tableFilter").on("keyup", function() {
+			var value = $(this).val().toLowerCase();
+			$(".productsTable tr").filter(function() {
+				$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+			});
+		});
+	});
+</script>
