1267a0f67cc02df12d3127b6f1006ea5451f426d
RSmith <master_schmadoodle@outlook.com>
2022-06-06 13:09:26 +0100

    Minor changes to website edit

--- Pages/website_page.php
+++ Pages/website_page.php
@@ -17,54 +17,63 @@
 		<!-- Section Body -->
 		<div class="row">
 			<div class="col-12 col-lg-6">
-				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($site['name'])?>">
-					<label for="floatingInput">Name</label>
+				<div class="row">
+					<div class="col-12 col-lg-6">
+						<div class="form-floating mb-3">
+							<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($site['name'])?>">
+							<label for="floatingInput">Name</label>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 col-lg-3">
+						<div class="form-floating mb-3">
+							<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($site['page_url'])?>">
+							<label for="floatingInput">Page URL</label>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 col-lg-3">
+						<div class="form-floating mb-3">
+							<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($site['subpage_url'])?>">
+							<label for="floatingInput">Subpage URL</label>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div class="col-12 col-md-6 col-lg-3">
-				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($site['page_url'])?>">
-					<label for="floatingInput">Page URL</label>
+			<div class="col-12 col-md-6">
+				<div class="row">
+					<div class="col-12 col-md-6 col-lg-3">
+						<?
+							$query = DB_Query("SELECT * FROM `page_styles`");
+							while($row = mysqli_fetch_array($query)) {
+								(in_array($row['id'], $styles))?$checked=" checked":$checked="";
+								print('
+									<div class="form-check">
+										<input class="form-check-input" type="checkbox" value="' . $row['id'] . '" id="StyleCheckboxes-'.$row['id'].'"'.$checked.'>
+										<label class="form-check-label" for="StyleCheckboxes-'.$row['id'].'">
+											' . $row['name'] . '
+										</label>
+									</div>
+								');
+							}
+						?>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 col-lg-3">
+						<?
+							$query = DB_Query("SELECT * FROM `page_scripts`");
+							while($row = mysqli_fetch_array($query)) {
+								(in_array($row['id'], $scripts))?$checked=" checked":$checked="";
+								print('
+									<div class="form-check">
+										<input class="form-check-input" type="checkbox" value="'.$row['id'].'" id="ScriptCheckboxes-'.$row['id'].'"'.$checked.'>
+										<label class="form-check-label" for="ScriptCheckboxes-'.$row['id'].'">
+											' . $row['name'] . '
+										</label>
+									</div>
+								');
+							}
+						?>
+					</div>
 				</div>
-			</div>
-			<div class="col-12 col-md-6 col-lg-3">
-				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($site['subpage_url'])?>">
-					<label for="floatingInput">Subpage URL</label>
-				</div>
-			</div>
-			<div class="col-12 col-md-6 col-lg-3">
-				<?
-					$query = DB_Query("SELECT * FROM `page_styles`");
-					while($row = mysqli_fetch_array($query)) {
-						(in_array($row['id'], $styles))?$checked=" checked":$checked="";
-						print('
-							<div class="form-check">
-								<input class="form-check-input" type="checkbox" value="' . $row['id'] . '" id="StyleCheckboxes-'.$row['id'].'"'.$checked.'>
-								<label class="form-check-label" for="StyleCheckboxes-'.$row['id'].'">
-									' . $row['name'] . '
-								</label>
-							</div>
-						');
-					}
-				?>
-			</div>
-			<div class="col-12 col-md-6 col-lg-3">
-				<?
-					$query = DB_Query("SELECT * FROM `page_scripts`");
-					while($row = mysqli_fetch_array($query)) {
-						(in_array($row['id'], $scripts))?$checked=" checked":$checked="";
-						print('
-							<div class="form-check">
-								<input class="form-check-input" type="checkbox" value="'.$row['id'].'" id="ScriptCheckboxes-'.$row['id'].'"'.$checked.'>
-								<label class="form-check-label" for="ScriptCheckboxes-'.$row['id'].'">
-									' . $row['name'] . '
-								</label>
-							</div>
-						');
-					}
-				?>
 			</div>
 		</div>
 	</section>
