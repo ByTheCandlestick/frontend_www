086e96681377b99dea8676ccce0c3b81fe67e8e9
RSmith <master_schmadoodle@outlook.com>
2022-06-07 00:00:57 +0100

    Minor changes to oxygen builder

--- Classes/funcs.php
+++ Classes/funcs.php
@@ -20,24 +20,24 @@
 		global $userdata, $info, $user_ok, $conn;
 		$columns = explode("#", $string);
 		$seccode = $secext = NULL;
-		print('<main><div class="row">');
+		print('<div class="row">');
 		array_shift($columns);
 		foreach($columns as $column) {
 			[$width, $section_string] = explode(';', $column);
-			print("<div class=\"col-md-$width\">");
+			print("<div style=\"backgroung: blue;\" class=\"col-md-$width\">");
 				$sections = explode(',', $section_string);
 				foreach($sections as $section) {
 					[$seccode, $secext] = explode(':', $section);
 					if($result = DB_Query("SELECT * FROM `page_sections` WHERE `id`='$seccode'")) {
 						if(mysqli_num_rows($result) == 1) {
 							$row = mysqli_fetch_array($result);
-							print('<div>'.$seccode.'<br>'.$secext.'</div>');
+							print('<div style="background: green;">'.$seccode.'<br>'.$secext.'</div>');
 							unset($secext);
 						}
 					}
 				}
 			print("</div>");
 		}
-		print('</div></main>');
+		print('</div>');
 	}
 ?>
