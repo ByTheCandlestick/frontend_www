8ad6563b3eb0ae9f2d8fe7ad899e5738d7e31123
RSmith <master_schmadoodle@outlook.com>
2022-06-06 15:38:56 +0100

    Minor changes test

--- Assets/js/theme.js
+++ Assets/js/theme.js
@@ -17,6 +17,7 @@ $( document ).ready(function() {
 	$(".search-area input").focusin( function(){
 		search.suggestions.show();
 	});
+	console.log($.get('/Assets/search.json'))
 	// -----========== Nestled functions ==========----- //
 	website = {
 		save: function() {
@@ -25,7 +26,7 @@ $( document ).ready(function() {
 	};
 	search = {
 		suggestions: $(".search-suggestions"),
-		jsonData: $.get('/Assets/search.json'),
+		jsonData: "",
 		process: function(ev) {
 			var key = ev.target.value;
 			search.suggestions.html("");
