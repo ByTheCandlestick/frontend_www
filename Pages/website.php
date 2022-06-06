114c4463d891a3f01dae8177531d1ba1135f31db
RSmith <master_schmadoodle@outlook.com>
2022-06-06 09:22:35 +0100

    Minor changes

--- Pages/website.php
+++ Pages/website.php
@@ -10,24 +10,36 @@
 	<hr>
 	<!-- Section Body -->
 	<div class="row">
-		<div class="col-12 col-lg-10">
-			<div class="row">
-				<div class="col-6 col-lg-2">
-					ID
-				</div>
-				<div class="col-6 col-lg-2">
-					Page URL
-				</div>
-				<div class="col-6 col-lg-2">
-					Sub Page URL
-				</div>
-				<div class="col-6 col-lg-2">
-					Page title
-				</div>
-			</div>
-		</div>
-		<div class="col-12 col-lg-2">
-
-		</div>
+		<table class="productsTable table table-striped table-hover">
+			<thead class="sticky-top" style="background: var(--section); z-index: unset;">
+				<tr>
+					<th scope="col">ID</th>
+					<th scope="col">URL</th>
+					<th scope="col">Sub URL</th>
+					<th scope="col">Page Title</th>
+					<th scope="col"></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?
+					$query = DB_Query("SELECT * FROM `misc_websites`");
+					while ($row = mysqli_fetch_array($query)) {
+						print('
+							<tr>
+								<th scope="row">'.$row['ID'].'</th>
+								<td>'.$row['page_url'].'</td>
+								<td>'.$row['subpage_url'].'</td>
+								<td>'.$row['page_title'].'</td>
+								<td>
+									<a href="/Website/View/'.$row['ID'].'">
+										<i class="fa fa-ellipsis"></i>
+									</a>
+								</td>
+							</tr>
+						');
+					}
+				?>
+			</tbody>
+		</table>
 	</div>
 </section>
