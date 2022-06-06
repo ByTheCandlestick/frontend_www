f0c6679ffbb950974a61ecf9a807e686029d7d3b
RSmith <master_schmadoodle@outlook.com>
2022-06-06 11:41:56 +0100

    Minor changes

--- Pages/website_page.php
+++ Pages/website_page.php
@@ -2,6 +2,8 @@
 	$query = DB_Query(sprintf("SELECT * FROM `page_layouts` WHERE `ID`=%s", QS));
 	if(mysqli_num_rows($query) > 0) {
 		$site = mysqli_fetch_assoc($query);
+		$styles = explode(',', $site['style_ids']);
+		$scripts = explode(',', $site['script_ids']);
 ?>
 <section>
 	<!-- Section Header -->
@@ -16,13 +18,13 @@
 	<div class="row">
 		<div class="col-12 col-md-6 col-lg-3">
 			<div class="form-floating mb-3">
-				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($site['images'])?>">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($site['page_url'])?>">
 				<label for="floatingInput">Page URL</label>
 			</div>
 		</div>
 		<div class="col-12 col-md-6 col-lg-3">
 			<div class="form-floating mb-3">
-				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($site['images'])?>">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($site['subpage_url'])?>">
 				<label for="floatingInput">Subpage URL</label>
 			</div>
 		</div>
@@ -30,10 +32,10 @@
 			<?
 				$query = DB_Query("SELECT * FROM `page_styles`");
 				while($row = mysqli_fetch_array($query)) {
-					()?$checked=:;
+					(in_array($row['id'], $styles))?$checked=" checked":$checked="";
 					print('
 						<div class="form-check">
-							<input class="form-check-input" type="checkbox" value="' . $row['id'] . '" id="flexCheckDefault">
+							<input class="form-check-input" type="checkbox" value="' . $row['id'] . '" id="flexCheckDefault"'.$checked.'>
 							<label class="form-check-label" for="flexCheckDefault">
 								' . $row['name'] . '
 							</label>
