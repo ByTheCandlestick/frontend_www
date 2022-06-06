14551fb4bfa0124532c61384592024f3159f89b4
RSmith <master_schmadoodle@outlook.com>
2022-06-06 13:10:32 +0100

    Minor changes to website edit

--- Pages/website_page.php
+++ Pages/website_page.php
@@ -18,19 +18,19 @@
 		<div class="row">
 			<div class="col-12 col-lg-6">
 				<div class="row">
-					<div class="col-12 col-lg-6">
+					<div class="col-12">
 						<div class="form-floating mb-3">
 							<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($site['name'])?>">
 							<label for="floatingInput">Name</label>
 						</div>
 					</div>
-					<div class="col-12 col-md-6 col-lg-3">
+					<div class="col-12 col-md-6">
 						<div class="form-floating mb-3">
 							<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($site['page_url'])?>">
 							<label for="floatingInput">Page URL</label>
 						</div>
 					</div>
-					<div class="col-12 col-md-6 col-lg-3">
+					<div class="col-12 col-md-6">
 						<div class="form-floating mb-3">
 							<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($site['subpage_url'])?>">
 							<label for="floatingInput">Subpage URL</label>
