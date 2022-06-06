c5d2c4b1e3bb7d5e16ec67499b03f56457ef9b6e
RSmith <master_schmadoodle@outlook.com>
2022-06-06 16:46:26 +0100

    Restructure js

--- Assets/js/theme.js
+++ Assets/js/theme.js
@@ -31,7 +31,7 @@ $( document ).ready(function() {
 		jsonData: null,
 		process: function(ev) {
 			if(ev.key == 'Enter'){
-				console.log($(search.suggestions.children()[0]).find("a").attr("href"));
+				window.location = $(search.suggestions.children()[0]).find("a").attr("href");
 			} else {
 				var key = ev.target.value;
 				search.suggestions.html("");
