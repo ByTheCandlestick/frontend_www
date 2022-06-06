b9b45891e01faeb93c04487831b1cc440e480179
RSmith <master_schmadoodle@outlook.com>
2022-06-06 23:50:47 +0100

    Minor changes to oxygen builder

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -1,29 +1,32 @@
 <?
 	if(QS_SUBPAGE != "") {
+		$page = mysqli_fetch_row(DB_Query(sprintf("SELECT * FROM `page_layouts` WHERE `ID`=%s", QS)));
 ?>
-<section>
-	<!-- Section Header -->
-	<div class="row">
-		<div class="col-12 col-md-6 col-lg-8">
-			<h1>Oxygen</h1>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6 col-lg-8">
+				<h1>Oxygen</h1>
+			</div>
+			<div class="col-12 col-md-6 col-lg-4 text-md-end">
+				<a href="javascript:website.saveLayout(<?print(QS)?>);" class="btn btn-outline-primary">
+					<i class="fa fa-save"></i>
+				</a>
+			</div>
 		</div>
-		<div class="col-12 col-md-6 col-lg-4 text-md-end">
-			<a href="javascript:website.saveLayout(<?print(QS)?>);" class="btn btn-outline-primary">
-				<i class="fa fa-save"></i>
-			</a>
-		</div>
-	</div>
-	<hr>
-	<!-- Section Body -->
-	<div class="row">
-		<div class="col-lg-3 h-100">
+		<hr>
+		<!-- Section Body -->
+		<div class="row">
+			<div class="col-lg-3 h-100">
 
+			</div>
+			<div class="col-lg-9 h-100">
+				<?
+					print_r($page);
+				?>
+			</div>
 		</div>
-		<div class="col-lg-9 h-100">
-
-		</div>
-	</div>
-</section>
+	</section>
 <?
 	} else {
 ?>
