7873847bd5fa7bd33a73f602df938c977997f505
RSmith <master_schmadoodle@outlook.com>
2022-06-06 11:06:57 +0100

    Minor changes

--- Pages/website_page.php
+++ Pages/website_page.php
@@ -10,10 +10,16 @@
 	<!-- Section Body -->
 	<div class="row">
 		<div class="col-12 col-md-6 col-lg-3">
-			Page URL
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['images'])?>">
+				<label for="floatingInput">Page URL</label>
+			</div>
 		</div>
 		<div class="col-12 col-md-6 col-lg-3">
-			Subpage URL
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['images'])?>">
+				<label for="floatingInput">Subpage URL</label>
+			</div>
 		</div>
 		<div class="col-12 col-md-6 col-lg-3">
 			<?
