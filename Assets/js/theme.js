922458793731e68a0f689863424a231dc14013d7
RSmith <master_schmadoodle@outlook.com>
2022-06-06 15:51:35 +0100

    Minor changes test

--- Assets/js/theme.js
+++ Assets/js/theme.js
@@ -25,7 +25,7 @@ $( document ).ready(function() {
 	};
 	search = {
 		suggestions: $(".search-suggestions"),
-		jsonData: $.get('/Assets/search.json', function(data){ alert( "Data Loaded: " + data )}),
+		jsonData: $.get('/Assets/search.json', function(data){ alert( "Data Loaded: " + data.responseJSON )}),
 		process: function(ev) {
 			var key = ev.target.value;
 			search.suggestions.html("");
