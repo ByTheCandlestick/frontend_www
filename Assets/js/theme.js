5e72f8a076ce74e69a3f2a483ae42f3c52d26344
RSmith <master_schmadoodle@outlook.com>
2022-06-06 15:31:25 +0100

    Minor changes test

--- Assets/js/theme.js
+++ Assets/js/theme.js
@@ -10,7 +10,7 @@ $( document ).ready(function() {
 	$('.app-preloader').fadeOut()
 	// -----========== Search ==========----- //
 	$(".search-area input").focusout( function(){
-		if(search.suggestions.is(':focus')) {
+		if(search.suggestions.not(':hover')) {
 			search.suggestions.hide();
 		}
 	});
