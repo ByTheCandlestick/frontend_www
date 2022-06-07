572387d227a8a23296f05510b77a89ce087e342b
Robert Smith <master_schmadoodle@outlook.com>
2022-06-07 14:22:57 +0100

    Minor changes to product edit page

--- Pages/product.php
+++ Pages/product.php
@@ -220,7 +220,7 @@
 					<select class="form-select" id="floatingSelect">
 						<option selected>Please select</option>
 						<?
-							$query = DB_Query("SELECT * FROM `products_colour` WHERE `Active`=1");
+							$query = DB_Query("SELECT * FROM `products_colours` WHERE `Active`=1");
 							while ($row = mysqli_fetch_array($query)) {
 								($row['ID'] == $prod['Colour_ID'])? $selected=' selected' : $selected='';
 								print_r('<option value="'.$row['ID'].'"'.$selected.'>'.$row['Name'].'</option>');
