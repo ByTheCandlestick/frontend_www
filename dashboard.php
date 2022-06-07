dce5d1fdf2be868ba053d015f357e89caad0e3f5
Robert Smith <master_schmadoodle@outlook.com>
2022-06-07 16:20:14 +0100

    Minor changes to product edit page

--- dashboard.php
+++ dashboard.php
@@ -81,7 +81,9 @@
 								<path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
 							</svg>
 						</button>
-						<span><?print($userdata['First_name'].' '.$userdata['Last_name'])?></span>
+						<button class="profile-btn">
+							<span><?print($userdata['First_name'].' '.$userdata['Last_name'])?></span>
+						</button>
 					</div>
 				</div>
 				<div class="app-content">
