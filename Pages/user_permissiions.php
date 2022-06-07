336e146a8cbb24c583cf8900d809624bc5a0ef4f
Robert Smith <master_schmadoodle@outlook.com>
2022-06-07 11:15:29 +0100

    Minor changes

---
+++ Pages/user_permissiions.php
@@ -0,0 +1,44 @@
+<?
+	$query = DB_Query(sprintf("SELECT * FROM `Users` WHERE `ID`=%s", QS));
+	if(mysqli_num_rows($query) > 0) {
+		$user = mysqli_fetch_assoc($query);
+?>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6 col-lg-8">
+				<h1>User info</h1>
+			</div>
+			<div class="col-12 col-md-6 col-lg-4 text-md-end">
+				<a href="/User/Permissions/<?print(QS)?>" class="btn btn-outline-primary">
+					<i class="fa fa-key"></i>
+				</a>
+			</div>
+		</div>
+		<hr>
+		<!-- Section Body -->
+		<div class="row">
+
+		</div>
+	</section>
+<?
+	} else {
+?>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6 col-lg-8">
+				<h1>User not found.</h1>
+			</div>
+			<div class="col-12 col-md-6 col-lg-4 text-md-end">
+			</div>
+		</div>
+		<hr>
+		<!-- Section Body -->
+		<div class="row">
+			<button class="btn btn-outline-primary col-12 col-md-3 col-lg-1" onclick="history.go(-1)">Go back</buton>
+		</div>
+	</section>
+<?
+	}
+?>
