c00ce60baef238128cb974a15315cdfd0a2174a7
Robert Smith <master_schmadoodle@outlook.com>
2022-06-07 12:00:50 +0100

    Minor changes

--- Pages/user.php
+++ Pages/user.php
@@ -27,48 +27,51 @@
 					<label for="floatingInput">Username</label>
 				</div>
 			</div>
-			
 			<div class="col-12 col-md-6 col-lg-2">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Username'])?>">
-					<label for="floatingInput">Email</label>
-				</div>
-			</div>
-			<div class="col-12 col-md-6 col-lg-2">
-				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Username'])?>">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['First_name'])?>">
 					<label for="floatingInput">First name</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-2">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Username'])?>">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Last_name'])?>">
 					<label for="floatingInput">Last name</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-2">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Username'])?>">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Email'])?>">
+					<label for="floatingInput">Email</label>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-2">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Phone'])?>">
 					<label for="floatingInput">Phone no.</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-3 col-lg-2">
 				<div class="form-floating mb-3">
+					<? ($user['Change_password']==1)$checked="checked":$checked=""; ?>
 					<div class="form-check form-switch">
 						<input class="form-check-input" type="checkbox" value="" id="flexCheckDisabled" disabled>
 						<label class="form-check-label" for="flexCheckDisabled"> Reset password? </label>
 					</div>
+					<? ($user['Disable_analytics']==1)$checked="checked":$checked=""; ?>
 					<div class="form-check form-switch">
-						<input class="form-check-input" type="checkbox" value="" id="flexCheckDisabled" disabled checked >
+						<input class="form-check-input" type="checkbox" value="" id="flexCheckDisabled" disabled>
 						<label class="form-check-label" for="flexCheckDisabled"> Disable analytics? </label>
 					</div>
+					<? ($user['Email_active']==1)$checked="checked":$checked=""; ?>
+					<div class="form-check form-switch">
+						<input class="form-check-input" type="checkbox" value="" id="flexCheckDisabled" disabled>
+						<label class="form-check-label" for="flexCheckDisabled"> Email activated? </label>
+					</div>
+					<? ($user['active']==1)$checked="checked":$checked=""; ?>
 					<div class="form-check form-switch">
 						<input class="form-check-input" type="checkbox" value="" id="flexCheckDisabled" disabled>
 						<label class="form-check-label" for="flexCheckDisabled"> User active? </label>
-					</div>
-					<div class="form-check form-switch">
-						<input class="form-check-input" type="checkbox" value="" id="flexCheckDisabled" disabled checked >
-						<label class="form-check-label" for="flexCheckDisabled"> Email activated? </label>
 					</div>
 				</div>
 			</div>
