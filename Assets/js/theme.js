05154a76406df9968da364cb45e344f20ab2fd1a
Robert Smith <master_schmadoodle@outlook.com>
2022-06-07 11:05:07 +0100

    Minor changes

--- Assets/js/theme.js
+++ Assets/js/theme.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$( document ).ready(async function() {
 	// -----========== VARIABLES ==========----- //
 	const api_url = window.location.protocol+'//api.'+window.location.hostname.slice(6) + '/v1';
 	const api_key = 'iwdk5xYYMyUbyKuHMB8UuA5R2pbqgYLvjzzKQFCeJzKbAkg2qAJGWunzJPZFxvaCvue5xHJEwrhG3b9Ye5mn3UYBT7ZE46crHkgenvY4LaUSgb3Jcj8T67tUuyVtD6nRTQxvurPZ6E96WiQKep7G8kUjJhxHchEZk6KrWqZ2Tf2B9ZgtErZ4UMNNSJWE9DV8gM3YMkzmraACBxd9nPBteJKPx3SFdBMHQGBAL5bzSmJtCfezQJ7Ed3hk4CBnhda3';
