02ff46f95d03d9761d277090b244aa1797bd4c3f
RSmith <master_schmadoodle@outlook.com>
2022-06-08 08:47:41 +0100

    Minor changes to alert styles

--- Pages/product.php
+++ Pages/product.php
@@ -21,13 +21,13 @@
 		<div class="row ProductInfo">
 			<div class="col-12 col-md-6">
 				<div class="row">
-					<div class="col-12 col-md-6 col-lg-5" name="title">
+					<div class="col-12 col-lg-6" name="title">
 						<div class="form-floating mb-3">
 							<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Title'])?>">
 							<label for="floatingInput">Title</label>
 						</div>
 					</div>
-					<div class="col-12 col-md-6 col-lg-2" name="range">
+					<div class="col-12 col-lg-6" name="range">
 						<div class="form-floating mb-3">
 							<select class="form-select" id="floatingSelect">
 								<option value="-1" selected>Please select</option>
