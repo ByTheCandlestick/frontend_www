d05529086a88f888d682841c80fb8f8e60932c9f
Robert Smith <master_schmadoodle@outlook.com>
2022-06-09 09:56:53 +0100

    Minor changes to analytics

--- Pages/analytics.php
+++ Pages/analytics.php
@@ -10,6 +10,13 @@
 	<hr>
 	<!-- Section Body -->
 	<div class="row">
-
+		<h3>Page views</h3>
+		<div class="row col-12 col-md-6">
+			<h5>Page referrers</h5>
+		</div>
+		<div class="row col-12 col-md-6">
+			
+		</div>
+		<h3>Product sales</h3>
 	</div>
 </section>
