f84671ad6195bb27dd1141e55da79c3c858336f7
Robert Smith <master_schmadoodle@outlook.com>
2022-06-10 10:20:31 +0100

    Minor changes for major website restructure

--- themes/mat_ui_admin.php
+++ themes/mat_ui_admin.php
@@ -1,10 +1,12 @@
 <?
 	//CHECK IF THE USER IS ALLOWED TO ACCESS THE WEBSITE
+	/*
 	if($user_ok) {
 		$query = DB_Query("SELECT * FROM `Users_permissions` WHERE `UID`=$log_id LIMIT 1");
 		if(mysqli_fetch_assoc($query)['Access_admin'] != 1)
 			$user_ok = false;
 	}
+	*/
 	require_user_ok();
 	// Determine the required row from the page requested
 	$domainID = domainID();
