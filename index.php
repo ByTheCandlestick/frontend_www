a89e9411172fa6ab7ae92361e2f587157fdde318
Robert Smith <master_schmadoodle@outlook.com>
2022-06-10 11:13:13 +0100

    Minor changes for a major website restructure

--- index.php
+++ index.php
@@ -2,5 +2,4 @@
 	require_once('./Classes/funcs.php');
 	require_once('./Classes/vars.php');
     $theme = getThemepage();
-	define('THEME_ROOT', $_SERVER["DOCUMENT_ROOT"]."/Themes/".$theme."/");
 ?>
