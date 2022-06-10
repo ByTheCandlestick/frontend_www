a2556d011fa872d9a5f525dc5351b919629e23ec
Robert Smith <master_schmadoodle@outlook.com>
2022-06-10 15:36:50 +0100

    Minor changes to website analytics

--- Classes/analytics.php
+++ Classes/analytics.php
@@ -25,7 +25,7 @@
 				echo "<script>console.log('Unable to submit analytics -0')</script>";
 			}
 			if($q = DB_Query("SELECT * FROM `page_views` WHERE `timestamp`='$timestamp' LIMIT 1", ANALYTICS)) {
-				global $analytics_ID = mysqli_fetch_row($q)[0];
+				$analytics_ID = mysqli_fetch_row($q)[0];
 			}
 
 			if(isset($referrer)) {
