eb5dbcfb286e397363ac496c053b231ec4cc29a8
Robert Smith <master_schmadoodle@outlook.com>
2022-06-10 15:15:57 +0100

    Minor changes to website analytics

--- Classes/analytics.php
+++ Classes/analytics.php
@@ -2,7 +2,7 @@
 	if($user_ok && !$userdata['Disable_analytics'] || !$user_ok) {
 		$analytics_startTime = microtime(true);
 		$user_ip = getenv('REMOTE_ADDR');
-		$timestamp = now();
+		$timestamp = time();
 		// Get the users location
 			$geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
 			$country = $geo["geoplugin_countryName"];
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?
+	date_default_timezone_set('Europe/London');
+	//
 	require_once('./Classes/funcs.php');
 	require_once('./Classes/vars.php');
 
