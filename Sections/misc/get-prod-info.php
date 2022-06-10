a2d14a5afb8e074fce984ac82bf4ebe545bd2607
Robert Smith <master_schmadoodle@outlook.com>
2022-06-10 14:33:41 +0100

    Minor changes to websites.php footer

--- Sections/misc/get-prod-info.php
+++ Sections/misc/get-prod-info.php
@@ -1,7 +1,7 @@
 <?
 	$query = DB_Query(sprintf("SELECT * FROM `products` WHERE `Slug`='%s' AND `Active`='1' LIMIT 1", QS));
 	if(mysqli_num_rows($query) > 0) {
-		$product = mysqli_fetch_assoc($query);
+		global $product = mysqli_fetch_assoc($query);
 	} else {
 		print('<script> window.location.replace("/Boutique/") </script>');
 	}
