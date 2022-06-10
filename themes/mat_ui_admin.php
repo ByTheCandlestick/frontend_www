2f7eacf18a94e2c451015780d53e654cd1c2ef76
Robert Smith <master_schmadoodle@outlook.com>
2022-06-10 10:25:43 +0100

    Minor changes for a major website restructure

--- themes/mat_ui_admin.php
+++ themes/mat_ui_admin.php
@@ -15,7 +15,7 @@
 	ignore_user_abort(true); // optional
 	ob_start();
 
-	Header('Location: '.(($_SERVER['HTTPS'])? 'https://' : 'http://').substr($_SERVER['HTTP_HOST'], 6));
+	echo 'here'
 	
 	$size = ob_get_length();
 	header("Content-Length: $size");
