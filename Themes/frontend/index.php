252766317a060f5b924ed4a66baf8782a3339b7c
Robert Smith <master_schmadoodle@outlook.com>
2022-06-10 14:45:39 +0100

    Minor changes to websites.php footer

--- Themes/frontend/index.php
+++ Themes/frontend/index.php
@@ -265,15 +265,15 @@
 											<a href="/Cart/">My Cart</a>
 										</li>
 										<li>
-											<i class="fad fa-user pe-1"></i>
+											<i class="fad fa-cart pe-1"></i>
 											<a href="/my/Addresses/">My Addresses</a>
 										</li>
 										<li>
-											<i class="fad fa-user pe-1"></i>
+											<i class="fad fa-house pe-1"></i>
 											<a href="/my/Cards/">My Cards</a>
 										</li>
 										<li>
-											<i class="fad fa-sign-out pe-1"></i>
+											<i class="fad fa-card pe-1"></i>
 											<a href="/Logout/">Logout</a>
 										</li>
 								<?
