3023677e6d18e05968ab614a40d740f8b9a4d41b
Robert Smith <master_schmadoodle@outlook.com>
2022-06-11 12:39:37 +0100

    Minor changes

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?
 	date_default_timezone_set('Europe/London');
-	//
 	require_once('./Classes/funcs.php');
 	require_once('./Classes/vars.php');
-	//
 	print_r('/'.QS_PAGE.'/'.QS_SUBPAGE.'/'.QS.'/');
+	print_r($_SERVER);
 	if(QS_PAGE == "/manifest.json") {
 
 	} else {
