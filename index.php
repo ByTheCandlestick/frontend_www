19176a98c34c387b8a9b4519a9e3ba098596f580
Robert Smith <master_schmadoodle@outlook.com>
2022-06-11 13:30:14 +0100

    Minor changes

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 	date_default_timezone_set('Europe/London');
 	require_once('./Classes/funcs.php');
 	require_once('./Classes/vars.php');
-	print_r(json_encode($_SERVER));
-	if($_SERVER['SCRIPT_URL']=="/manifest.json") {
+
+	if($_SERVER['REQUEST_URI']=="/manifest.json") {
 		if(file_exists($path = sprintf("/Themes/%s/App/manifest.json", __THEME__))) {
 			print(file_get_contents($path));
 		}
