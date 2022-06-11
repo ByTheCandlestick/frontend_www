e09c05cf442c51d88f9cfaa0170e0dce58b23aaf
Robert Smith <master_schmadoodle@outlook.com>
2022-06-11 13:25:34 +0100

    Minor changes

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 	date_default_timezone_set('Europe/London');
 	require_once('./Classes/funcs.php');
 	require_once('./Classes/vars.php');
-	print($_GET);
+
+	print($_SERVER);
+
 	if(isset($_GET['file']) && isset($_GET['ext'])) {
 		if(file_exists($path = 'Themes/'.__THEME__.'/Assets/'.$_GET['ext'].'/'.$_GET['file'].'.'.$_GET['ext'])) {
 			print(file_get_contents($path));
