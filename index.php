cbe7ec48bf6b5220a89d5f313a317116627af501
Robert Smith <master_schmadoodle@outlook.com>
2022-06-11 14:11:59 +0100

    Minor changes

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 	require_once('./Classes/funcs.php');
 	require_once('./Classes/vars.php');
 	print_r($_GET);
-	if($_SERVER['REQUEST_URI'] == "/manifest.json") {
-		if(file_exists($path = sprintf(__ROOT__."/Themes/%s/App/manifest.json", __THEME__))) {
+	if(isset($_GET['file']) && isset($_GET['ext'])) {
+		if(file_exists($path = sprintf("%s/Themes/%s/%s/%s.%s", __ROOT__, __THEME__, $_GET['ext'], $_GET['file'], $_GET['ext']))) {
 			print(file_get_contents($path));
 		} else {
 			print(json_encode(array("error"=>"File not found")));
