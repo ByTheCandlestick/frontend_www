d146b7eac375003223a41f12a792a20863055065
Robert Smith <master_schmadoodle@outlook.com>
2022-06-11 10:44:22 +0100

    Edit supplier pages

--- Pages/suppliers.php
+++ Pages/suppliers.php
@@ -5,11 +5,56 @@
 			<h1>All suppliers</h1>
 		</div>
 		<div class="col-12 col-md-6 col-lg-4 text-md-end">
+			<input type="text" class="tableFilter"> </input>
 		</div>
 	</div>
 	<hr>
 	<!-- Section Body -->
 	<div class="row">
-
+		<table class="usersTable table table-striped table-hover">
+			<thead class="sticky-top" style="background: var(--section); z-index: unset;">
+				<th scope="col">ID</th>
+				<th scope="col">Name</th>
+				<th scope="col">Reference</th>
+				<th scope="col">Email</th>
+				<th scope="col">Phone</th>
+				<th scope="col">Enabled</th>
+				<th scope="col">Created</th>
+				<th scope="col"></th>
+			</thead>
+			<tbody>
+				<?
+					$query = DB_Query("SELECT * FROM `Suppliers`");
+					while ($row = mysqli_fetch_array($query)) {
+						print('
+							<tr>
+								<th scope="row">'.$row['ID'].'</th>
+								<td>'.$row['Name']'</td>
+								<td>'.$row['Reference'].'</td>
+								<td>'.$row['Email'].'</td>
+								<td>'.$row['Phone'].'</td>
+								<td>'.$row['Active'].'</td>
+								<td>'.$row['Created'].'</td>
+								<td>
+									<a href="/Supplier/Edit/'.$row['ID'].'">
+										<i class="fa fa-pencil"></i>
+									</a>
+								</td>
+							</tr>
+						');
+					}
+				?>
+			</tbody>
+		</table>
 	</div>
 </section>
+<script>
+	$(document).ready(function(){
+		$(".tableFilter").on("keyup", function() {
+			var value = $(this).val().toLowerCase();
+			$(".usersTable tr").filter(function() {
+				$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+			});
+		});
+	});
+</script>
