11e7b7c839ba654e4a5e849941bec143549e473a
Robert Smith <master_schmadoodle@outlook.com>
2022-06-11 13:28:17 +0100

    Minor changes

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 	require_once('./Classes/funcs.php');
 	require_once('./Classes/vars.php');
 
-	if(isset($_SERVER['SCRIPT_URL'])) {
-		if(file_exists($path = sprintf("/Themes/%s/Assets/", __THEME__, ))) {
+	if($_SERVER['SCRIPT_URL']=="/manifest.json") {
+		if(file_exists($path = sprintf("/Themes/%s/App/manifest.json", __THEME__))) {
 			print(file_get_contents($path));
 		}
 	} else {
