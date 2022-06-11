ac6d608f5cb3cb6ec57aa37043a2b1fb819e33d8
Robert Smith <master_schmadoodle@outlook.com>
2022-06-11 14:13:36 +0100

    Minor changes

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 	require_once('./Classes/funcs.php');
 	require_once('./Classes/vars.php');
 	if(isset($_GET['file']) && isset($_GET['ext'])) {
-		echo $path = sprintf("%s/Themes/%s/%s/%s.%s", __ROOT__, __THEME__, $_GET['ext'], $_GET['file'], $_GET['ext']);
-		if(file_exists($path)) {
+		if(file_exists($path = sprintf("%s/Themes/%s/Assets/%s/%s.%s", __ROOT__, __THEME__, $_GET['ext'], $_GET['file'], $_GET['ext']))) {
 			print(file_get_contents($path));
 		} else {
 			print(json_encode(array("error"=>"File not found")));
