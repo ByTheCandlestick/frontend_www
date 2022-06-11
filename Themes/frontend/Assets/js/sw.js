6b7be6bfd09a8f9ce91afee17c6778e1bf35db82
Robert Smith <master_schmadoodle@outlook.com>
2022-06-11 14:41:34 +0100

    Minor changes to fileview

---
+++ Themes/frontend/Assets/js/sw.js
@@ -0,0 +1,46 @@
+//
+// Offline Mode
+// Route: Online => 'offline.html'
+//
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+const CACHE = "CandlestickOffline";
+const offlineFallbackPage = "/app/offline/index.html";
+self.addEventListener("message", (event) => {
+	if (event.data && event.data.type === "SKIP_WAITING") {
+		self.skipWaiting();
+	}
+});
+self.addEventListener('install', async(event) => {
+	event.waitUntil(
+		caches.open(CACHE)
+		.then((cache) => cache.add(offlineFallbackPage))
+	);
+});
+if (workbox.navigationPreload.isSupported()) {
+	workbox.navigationPreload.enable();
+}
+self.addEventListener('fetch', (event) => {
+	if (event.request.mode === 'navigate') {
+		event.respondWith((async() => {
+			try {
+				const preloadResp = await event.preloadResponse;
+				if (preloadResp) {
+					return preloadResp;
+				}
+				const networkResp = await fetch(event.request);
+				return networkResp;
+			} catch (error) {
+				const cache = await caches.open(CACHE);
+				const cachedResp = await cache.match(offlineFallbackPage);
+				return cachedResp;
+			}
+		})());
+	}
+});
+//
+// App icon badges
+//
+
+//
+// App Push Notifications
+//
