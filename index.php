13c459f44aeeb8f315fc8c0c6cc2c916dd5660d0
Robert Smith <master_schmadoodle@outlook.com>
2022-06-11 14:12:24 +0100

    Minor changes

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 	date_default_timezone_set('Europe/London');
 	require_once('./Classes/funcs.php');
 	require_once('./Classes/vars.php');
-	print_r($_GET);
 	if(isset($_GET['file']) && isset($_GET['ext'])) {
-		if(file_exists($path = sprintf("%s/Themes/%s/%s/%s.%s", __ROOT__, __THEME__, $_GET['ext'], $_GET['file'], $_GET['ext']))) {
+		echo $path = sprintf("%s/Themes/%s/%s/%s.%s", __ROOT__, __THEME__, $_GET['ext'], $_GET['file'], $_GET['ext'])
+		if(file_exists($path)) {
 			print(file_get_contents($path));
 		} else {
 			print(json_encode(array("error"=>"File not found")));
