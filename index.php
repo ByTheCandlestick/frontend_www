1bc86ba81881fcd49570d0de6f4553ae696a60ef
Robert Smith <master_schmadoodle@outlook.com>
2022-06-11 12:34:10 +0100

    Minor changes

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?
 	date_default_timezone_set('Europe/London');
 	//
-	require_once('./Classes/funcs.php');
 	require_once('./Classes/vars.php');
 	//
 	print_r(QS_PAGE);
 	if(QS_PAGE == "/manifest.json") {
 
 	} else {
+		require_once('./Classes/funcs.php');
 		if($website_info['Maintenance']) {
 
 		} else {
