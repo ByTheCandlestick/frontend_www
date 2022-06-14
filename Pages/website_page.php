8d710ff89e0eed9741589c4d3071c61f10c91142
Robert Smith <master_schmadoodle@outlook.com>
2022-06-14 19:24:28 +0100

    Minor changes to page_edit

--- Pages/website_page.php
+++ Pages/website_page.php
@@ -52,7 +52,8 @@
 						<select class="form-select" id="floatingSelect">
 							<option value="-1" selected>Please select</option>
 							<?
-								while ($row = mysqli_fetch_array(DB_Query("SELECT * FROM `misc_websites`"))) {
+								$query = DB_Query("SELECT * FROM `misc_websites`");
+								while ($row = mysqli_fetch_array($query)) {
 									print_r('<option value="'.$row['ID'].'">'.$row['Name'].' - '.$row['Domain'].'</option>');
 								}
 							?>
