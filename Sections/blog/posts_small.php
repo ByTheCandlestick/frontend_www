5375670f7ba390a058db83db0469f0a28914e1c3
Robert Smith <master_schmadoodle@outlook.com>
2022-06-14 14:53:11 +0100

    Minor changes to Modern theme

--- Sections/blog/posts_small.php
+++ Sections/blog/posts_small.php
@@ -24,7 +24,7 @@
 								<source srcset="'.__API__.'/Images/fetch/'. $row['Image'] .'/jpg/" type="image/jpg"/>
 								<source srcset="'.__API__.'/Images/fetch/'. $row['Image'] .'/png/" type="image/png"/>
 								<source srcset="'.__API__.'/Images/fetch/'. $row['Image'] .'/jpx/" type="image/jpx"/>
-								<img src="'.__API__.'/Images/fetch/'. $row['Image'] .'/webp/" type="image/webp" width="700px" height="350px">
+								<img src="'.__API__.'/Images/fetch/'. $row['Image'] .'/webp/" type="image/webp" width="414px" height="207px">
 							</picture>
 							<div class="card-body">
 								<div class="small text-muted">'.$row['Timestamp'].'</div>
