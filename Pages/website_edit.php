61c09c3a799a4695736f412e4ba651f59f2cc8fe
Robert Smith <master_schmadoodle@outlook.com>
2022-06-14 18:54:58 +0100

    Minor changes to create a website PHP

---
+++ Pages/website_edit.php
@@ -0,0 +1,60 @@
+<section>
+	<!-- Section Header -->
+	<div class="row">
+		<div class="col-12 col-md-6">
+			<h1>Create a new website</h1>
+		</div>
+		<div class="col-12 col-md-6 text-md-end">
+			<div class="row">
+				<div class="col-12 d-flex justify-content-end align-items-center p-0">
+					<a href="javascript:website.create();" class="btn btn-outline-primary m-1">
+						<i class="fa fa-save"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<hr>
+	<!-- Section Body -->
+	<div class="row">
+		<div class="col-12">
+			<div class="row">
+				<h5>Site info</h5>
+				<div class="col-12 col-md-6 col-lg-3" name="name">
+					<div class="form-floating mb-3">
+						<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+						<label for="floatingInput">Name</label>
+					</div>
+				</div>
+				<div class="col-12 col-md-6 col-lg-3" name="domain">
+					<div class="form-floating mb-3">
+						<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+						<label for="floatingInput">Domain</label>
+					</div>
+				</div>
+				<div class="col-12 col-md-6 col-lg-3" name="page_type">
+					<div class="form-floating mb-3">
+						<select class="form-select" id="floatingSelect">
+							<option value="-1" selected>Please select</option>
+							<?
+								$query = DB_Query("SELECT * FROM `page_types` WHERE `Active`=1");
+								while ($row = mysqli_fetch_array($query)) {
+									print_r('<option value="'.$row['ID'].'">'.$row['Name'].'</option>');
+								}
+							?>
+						</select>
+						<label for="floatingInput">Page type</label>
+					</div>
+				</div>
+				<div class="col-12 col-md-6 col-lg-3" name="status">
+						<div class="form-floating mb-3">
+							<div class="form-check form-switch">
+								<input class="form-check-input" type="checkbox" name="maintenance" id="flexCheck">
+								<label class="form-check-label" for="flexCheck"> Maintenance? </label>
+							</div>
+						</div>
+					</div>
+			</div>
+		</div>
+	</div>
+</section>
