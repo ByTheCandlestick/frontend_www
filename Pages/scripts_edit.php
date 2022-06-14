d976c4efdb10c3d8b80e7ed85b9c75edabbb2b01
Robert Smith <master_schmadoodle@outlook.com>
2022-06-14 17:22:29 +0100

    Minor changes to scripts and style / edit files

--- Pages/scripts_edit.php
+++ Pages/scripts_edit.php
@@ -1,6 +1,62 @@
 <?
-	$query = DB_Query(sprintf("SELECT * FROM `page_scripts` WHERE `ID`=%s", QS));
-	if(mysqli_num_rows($query) > 0) {
+	if(QS=="new") {
+?>
+    <section>
+        <!-- Section Header -->
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h1>Script Edit</h1>
+            </div>
+            <div class="col-12 col-md-6 text-md-end">
+            <div class="row">
+                <div class="col-12 col-lg-6 d-flex justify-content-end align-items-center p-0">
+                    <a href="javascript:website.script.create();history.go(-1);" class="btn btn-outline-primary m-1">
+                        <i class="fa fa-save"></i>
+                    </a>
+                </div>
+                <div class="col-12 col-lg-6">
+                </div>
+            </div>
+            </div>
+        </div>
+        <hr>
+        <!-- Section Body -->
+        <div class="row">
+            <h5>Script info</h5>
+            <div class="col-12 col-md-6 col-lg-4" name="name">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+                    <label for="floatingInput">Name</label>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4" name="location">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+                    <label for="floatingInput">Location</label>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4" name="importance">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+                    <label for="floatingInput">Importance</label>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4" name="status">
+                <div class="form-floating mb-3">
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" name="preload" id="flexCheck">
+                        <label class="form-check-label" for="flexCheck"> Preload? </label>
+                    </div>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" name="available" id="flexCheck">
+                        <label class="form-check-label" for="flexCheck"> Active? </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?
+	} elseif(mysqli_num_rows($query = DB_Query(sprintf("SELECT * FROM `page_scripts` WHERE `ID`=%s", QS))) > 0) {
 		$script = mysqli_fetch_assoc($query);
 ?>
 	<section>
@@ -57,63 +113,6 @@
             </div>
 		</div>
 	</section>
-<?
-	} elseif(QS == "new") {
-?>
-    <section>
-        <!-- Section Header -->
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <h1>Script Edit</h1>
-            </div>
-            <div class="col-12 col-md-6 text-md-end">
-            <div class="row">
-                <div class="col-12 col-lg-6 d-flex justify-content-end align-items-center p-0">
-                    <a href="javascript:website.script.create();history.go(-1);" class="btn btn-outline-primary m-1">
-                        <i class="fa fa-save"></i>
-                    </a>
-                </div>
-                <div class="col-12 col-lg-6">
-                </div>
-            </div>
-            </div>
-        </div>
-        <hr>
-        <!-- Section Body -->
-        <div class="row">
-            <h5>Script info</h5>
-            <div class="col-12 col-md-6 col-lg-4" name="name">
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="" value="">
-                    <label for="floatingInput">Name</label>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4" name="location">
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="" value="">
-                    <label for="floatingInput">Location</label>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4" name="importance">
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="" value="">
-                    <label for="floatingInput">Importance</label>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4" name="status">
-                <div class="form-floating mb-3">
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" name="preload" id="flexCheck">
-                        <label class="form-check-label" for="flexCheck"> Preload? </label>
-                    </div>
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" name="available" id="flexCheck">
-                        <label class="form-check-label" for="flexCheck"> Active? </label>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
 <?
     } else {
 ?>
