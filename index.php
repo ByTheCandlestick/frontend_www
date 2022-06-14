641efaa7d9eff6efa520c728a30b0171854847c5
Robert Smith <master_schmadoodle@outlook.com>
2022-06-14 10:25:26 +0100

    minor changes to striped theme

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 	require_once('./Classes/vars.php');
 	if(isset($_GET['file']) && isset($_GET['ext'])) {
 		if(file_exists($path = sprintf("%s/Themes/%s/Assets/%s/%s.%s", __ROOT__, __THEME__, $_GET['ext'], $_GET['file'], $_GET['ext']))) {
-			print(file_get_contents($path));
 			header('Content-Type: text/'.$_GET['ext']);
+			print(file_get_contents($path));
 		} else {
-			print(json_encode(array("error"=>"File not found")));
 			header('Content-Type: text/json');
+			print(json_encode(array("error"=>"File not found")));
 		}
 	} else {
 		if($website_info['Maintenance']) {
