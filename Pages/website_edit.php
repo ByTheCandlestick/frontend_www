9bc98013d3a0549a3147b3fff6a2446621c66752
Robert Smith <master_schmadoodle@outlook.com>
2022-06-14 19:01:08 +0100

    Minor changes to create a website PHP

--- Pages/website_edit.php
+++ Pages/website_edit.php
@@ -11,8 +11,8 @@
 			<div class="col-12 col-md-6 text-md-end">
 				<div class="row">
 					<div class="col-12 d-flex justify-content-end align-items-center p-0">
-						<a href="javascript:website.domain.delete(<?print(QS)?>);" class="btn btn-outline-primary m-1">
-							<i class="fa fa-save"></i>
+						<a href="javascript:website.domain.delete(<?print(QS)?>);" class="btn btn-outline-danger m-1">
+							<i class="fa fa-trash-alt"></i>
 						</a>
 						<a href="javascript:website.domain.update(<?print(QS)?>);" class="btn btn-outline-primary m-1">
 							<i class="fa fa-save"></i>
