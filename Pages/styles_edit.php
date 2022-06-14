c6451e05006b0f3209ca00c23dfbde3b941d0784
Robert Smith <master_schmadoodle@outlook.com>
2022-06-14 17:21:22 +0100

    Minor changes to scripts and style / edit files

--- Pages/styles_edit.php
+++ Pages/styles_edit.php
@@ -1,60 +1,5 @@
 <?
-	$query = DB_Query(sprintf("SELECT * FROM `page_styles` WHERE `ID`=%s", QS));
-	if(mysqli_num_rows($query) > 0) {
-		$style = mysqli_fetch_assoc($query);
-?>
-	<section>
-		<!-- Section Header -->
-		<div class="row">
-			<div class="col-12 col-md-6">
-				<h1>Style Edit</h1>
-			</div>
-			<div class="col-12 col-md-6 text-md-end">
-			<div class="row">
-				<div class="col-12 col-lg-6 d-flex justify-content-end align-items-center p-0">
-					<a href="javascript:website.style.save(<?print(QS)?>);history.go(-1);" class="btn btn-outline-primary m-1">
-						<i class="fa fa-save"></i>
-					</a>
-				</div>
-				<div class="col-12 col-lg-6">
-				</div>
-			</div>
-			</div>
-		</div>
-		<hr>
-		<!-- Section Body -->
-		<div class="row">
-            <h5>Style info</h5>
-            <div class="col-12 col-md-6 col-lg-4" name="name">
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="<? print(($style['Name']=='')?'No name was set':'')?>" value="<? print(($style['Name']=='')?'':$style['Name'])?>">
-                    <label for="floatingInput">Name</label>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4" name="location">
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="<? print(($style['Location']=='')?'No title was set':'')?>" value="<? print(($style['Location']=='')?'':$style['Location'])?>">
-                    <label for="floatingInput">Location</label>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4" name="page_url">
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="<? print(($style['Importance']=='')?'No base URL was set':'')?>" value="<? print(($style['Importance']=='')?'':$style['Importance'])?>">
-                    <label for="floatingInput">Importance</label>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4" name="status">
-                <div class="form-floating mb-3">
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" name="available" id="flexCheck" <?($style['Active']==1)?print("checked"):print("")?>>
-                        <label class="form-check-label" for="flexCheck"> Active? </label>
-                    </div>
-                </div>
-            </div>
-		</div>
-	</section>
-<?
-	} elseif(QS == "new") {
+	if(QS == "new") {
 ?>
     <section>
         <!-- Section Header -->
@@ -110,6 +55,60 @@
             </div>
         </div>
     </section>
+<?
+	} elseif(mysqli_num_rows($query = DB_Query(sprintf("SELECT * FROM `page_styles` WHERE `ID`=%s", QS))) > 0) {
+		$style = mysqli_fetch_assoc($query);
+?>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6">
+				<h1>Style Edit</h1>
+			</div>
+			<div class="col-12 col-md-6 text-md-end">
+			<div class="row">
+				<div class="col-12 col-lg-6 d-flex justify-content-end align-items-center p-0">
+					<a href="javascript:website.style.save(<?print(QS)?>);history.go(-1);" class="btn btn-outline-primary m-1">
+						<i class="fa fa-save"></i>
+					</a>
+				</div>
+				<div class="col-12 col-lg-6">
+				</div>
+			</div>
+			</div>
+		</div>
+		<hr>
+		<!-- Section Body -->
+		<div class="row">
+            <h5>Style info</h5>
+            <div class="col-12 col-md-6 col-lg-4" name="name">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="<? print(($style['Name']=='')?'No name was set':'')?>" value="<? print(($style['Name']=='')?'':$style['Name'])?>">
+                    <label for="floatingInput">Name</label>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4" name="location">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="<? print(($style['Location']=='')?'No title was set':'')?>" value="<? print(($style['Location']=='')?'':$style['Location'])?>">
+                    <label for="floatingInput">Location</label>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4" name="page_url">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="<? print(($style['Importance']=='')?'No base URL was set':'')?>" value="<? print(($style['Importance']=='')?'':$style['Importance'])?>">
+                    <label for="floatingInput">Importance</label>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4" name="status">
+                <div class="form-floating mb-3">
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" name="available" id="flexCheck" <?($style['Active']==1)?print("checked"):print("")?>>
+                        <label class="form-check-label" for="flexCheck"> Active? </label>
+                    </div>
+                </div>
+            </div>
+		</div>
+	</section>
 <?
 	} else {
 ?>
