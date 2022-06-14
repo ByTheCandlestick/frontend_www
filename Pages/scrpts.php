ceaffd9bb60337a57b137790200e89934d5282ae
Robert Smith <master_schmadoodle@outlook.com>
2022-06-14 16:33:23 +0100

    Added basic script and style pages

---
+++ Pages/scrpts.php
@@ -0,0 +1,83 @@
+<section>
+	<!-- Section Header -->
+	<div class="row">
+		<div class="col-12 col-md-6">
+			<h1>All scripts</h1>
+		</div>
+		<div class="col-12 col-md-6 text-md-end">
+			<div class="row">
+				<div class="col-12 col-lg-6 d-flex justify-content-end align-items-center p-0">
+					<a href="/Scripts/New/" class="btn btn-outline-primary m-1">
+						<i class="fa fa-plus"></i>
+					</a>
+				</div>
+				<div class="col-12 col-lg-6">
+					<div class="form-floating">
+						<input type="text" class="form-control tableFilter" id="tableSearch" placeholder=" ">
+						<label for="tableSearch" class="ps-5">Search</label>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<hr>
+	<!-- Section Body -->
+	<div class="row">
+		<table class="scriptsTable table table-striped table-hover">
+			<thead class="sticky-top" style="background: var(--section); z-index: unset;">
+				<th scope="col">ID</th>
+				<th scope="col">Name</th>
+				<th scope="col">Location</th>
+				<th scope="col">Preload?</th>
+				<th scope="col">Importance</th>
+				<th scope="col"></th>
+			</thead>
+			<tbody>
+				<?
+					$query = DB_Query("SELECT * FROM `page_scripts`");
+					if(mysqli_num_rows($query) > 0) {
+						while ($row = mysqli_fetch_array($query)) {
+							print('
+								<tr>
+									<th scope="row">'.$row['ID'].'</th>
+									<td>'.$row['Name'].'</td>
+									<td>'.$row['Location'].'</td>
+									<td>'.$row['Preload'].'</td>
+									<td>'.$row['Importance'].'</td>
+									<td>
+										<a href="/Scripts/Edit/'.$row['ID'].'">
+											<i class="fa fa-pencil"></i>
+										</a>
+									</td>
+								</tr>
+							');
+						}
+					} else {
+						print('
+							<tr>
+								<th scope="row"></th>
+								<td>No data found</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+						');
+					}
+				?>
+			</tbody>
+		</table>
+	</div>
+</section>
+<script>
+	$(document).ready(function(){
+		$(".tableFilter").on("keyup", function() {
+			var value = $(this).val().toLowerCase();
+			$(".scriptsTable tbody tr").filter(function() {
+				$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+			});
+		});
+	});
+</script>
