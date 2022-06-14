070b0cb1244d9c2b7e561d983a1fae54f4fd52cc
Robert Smith <master_schmadoodle@outlook.com>
2022-06-14 19:00:49 +0100

    Minor changes to create a website PHP

--- Themes/Material Dash/Assets/js/script.js
+++ Themes/Material Dash/Assets/js/script.js
@@ -136,54 +136,72 @@ $( document ).ready(function() {
 			},
 		}
 		website = {
-			create: function() {
-				data = {
-					'api_key': api_key,
-					'name': $("div[name=name]").find("input").val(),
-					'domain': $("div[name=domain]").find("input").val(),
-					'page_type': $("div[name=page_type]").find("option:selected").val(),
-					'maintenance': (($("div[name=status]").find("input[name=maintenance]:checked").length === 0)?0:1),
-				}
-				$.ajax({
-					url: api_url + '/Website/',
-					data: data,
-					type: 'PUT',
-					xhrFields: {
-						withCredentials: true,
-					},
-					success: function(body) {
-						alert.simple("Successfully saved the website", "success");
-					},
-					error: function(body) {
-						alert.simple("An error has occurred. Please try again later", "danger");
+			domain: {
+				create: function() {
+					data = {
+						'api_key': api_key,
+						'name': $("div[name=name]").find("input").val(),
+						'domain': $("div[name=domain]").find("input").val(),
+						'page_type': $("div[name=page_type]").find("option:selected").val(),
+						'maintenance': (($("div[name=status]").find("input[name=maintenance]:checked").length === 0)?0:1),
 					}
-				});
-			},
-			update: function(sid) {
-				data = {
-					'api_key': api_key,
-					'name': $("div[name=name]").find("input").val(),
-					'domain': $("div[name=domain]").find("input").val(),
-					'page_type': $("div[name=page_type]").find("option:selected").val(),
-					'maintenance': (($("div[name=status]").find("input[name=maintenance]:checked").length === 0)?0:1),
-				}
-				$.ajax({
-					url: api_url + '/Website/' + sid + '/',
-					data: data,
-					type: 'POST',
-					xhrFields: {
-						withCredentials: true,
-					},
-					success: function(body) {
-						alert.simple("Successfully saved the website", "success");
-					},
-					error: function(body) {
-						alert.simple("An error has occurred. Please try again later", "danger");
+					$.ajax({
+						url: api_url + '/Website/',
+						data: data,
+						type: 'PUT',
+						xhrFields: {
+							withCredentials: true,
+						},
+						success: function(body) {
+							alert.simple("Successfully saved the website", "success");
+						},
+						error: function(body) {
+							alert.simple("An error has occurred. Please try again later", "danger");
+						}
+					});
+				},
+				update: function(sid) {
+					data = {
+						'api_key': api_key,
+						'name': $("div[name=name]").find("input").val(),
+						'domain': $("div[name=domain]").find("input").val(),
+						'page_type': $("div[name=page_type]").find("option:selected").val(),
+						'maintenance': (($("div[name=status]").find("input[name=maintenance]:checked").length === 0)?0:1),
 					}
-				});
-			},
-			delete: function(sid) {
-				
+					$.ajax({
+						url: api_url + '/Website/' + sid + '/',
+						data: data,
+						type: 'POST',
+						xhrFields: {
+							withCredentials: true,
+						},
+						success: function(body) {
+							alert.simple("Successfully saved the website", "success");
+						},
+						error: function(body) {
+							alert.simple("An error has occurred. Please try again later", "danger");
+						}
+					});
+				},
+				delete: function(sid) {
+					data = {
+						'api_key': api_key,
+					}
+					$.ajax({
+						url: api_url + '/Website/' + sid + '/',
+						data: data,
+						type: 'DELETE',
+						xhrFields: {
+							withCredentials: true,
+						},
+						success: function(body) {
+							alert.simple("Successfully saved the website", "success");
+						},
+						error: function(body) {
+							alert.simple("An error has occurred. Please try again later", "danger");
+						}
+					});
+				},
 			},
 			page: {
 				create: function() {
