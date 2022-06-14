2e5f3ab7df093703ac339cea04654fff36b83ddb
Robert Smith <master_schmadoodle@outlook.com>
2022-06-14 19:20:32 +0100

    Minor changes to page_edit

--- Pages/website_page.php
+++ Pages/website_page.php
@@ -1,6 +1,92 @@
 <?
 	if(strToLower(QS) == "new"){
 ?>
+<section>
+	<!-- Section Header -->
+	<div class="row">
+		<div class="col-12 col-md-6">
+			<h1>Website Edit</h1>
+		</div>
+		<div class="col-12 col-md-6 text-md-end">
+		<div class="row">
+			<div class="col-12 d-flex justify-content-end align-items-center p-0">
+				<a href="javascript:website.page.create();" class="btn btn-outline-primary m-1">
+					<i class="fa fa-save"></i>
+				</a>
+			</div>
+		</div>
+		</div>
+	</div>
+	<hr>
+	<!-- Section Body -->
+	<div class="row">
+		<div class="col-12 col-lg-6">
+			<div class="row">
+				<h5>Site info</h5>
+				<div class="col-12" name="name">
+					<div class="form-floating mb-3">
+						<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+						<label for="floatingInput">Name</label>
+					</div>
+				</div>
+				<div class="col-12" name="title">
+					<div class="form-floating mb-3">
+						<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+						<label for="floatingInput">Title</label>
+					</div>
+				</div>
+				<div class="col-12 col-md-6" name="page_url">
+					<div class="form-floating mb-3">
+						<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+						<label for="floatingInput">Page URL</label>
+					</div>
+				</div>
+				<div class="col-12 col-md-6" name="subpage_url">
+					<div class="form-floating mb-3">
+						<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+						<label for="floatingInput">Subpage URL</label>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-12 col-md-6">
+			<div class="row">
+				<div class="col-12 col-md-6" name="styles">
+					<h5>Styles</h5>
+					<?
+						$query = DB_Query("SELECT * FROM `page_styles` ORDER BY `importance` ASC");
+						while($row = mysqli_fetch_array($query)) {
+							print('
+								<div class="form-check">
+									<input class="form-check-input" type="checkbox" value="' . $row['id'] . '" id="StyleCheckboxes-'.$row['id'].'">
+									<label class="form-check-label" for="StyleCheckboxes-'.$row['id'].'">
+										' . $row['name'] . '
+									</label>
+								</div>
+							');
+						}
+					?>
+				</div>
+				<div class="col-12 col-md-6" name="scripts">
+					<h5>Scripts</h5>
+					<?
+						$query = DB_Query("SELECT * FROM `page_scripts` ORDER BY `importance` ASC");
+						while($row = mysqli_fetch_array($query)) {
+							print('
+								<div class="form-check">
+									<input class="form-check-input" type="checkbox" value="'.$row['id'].'" id="ScriptCheckboxes-'.$row['id'].'">
+									<label class="form-check-label" for="ScriptCheckboxes-'.$row['id'].'">
+										' . $row['name'] . '
+									</label>
+								</div>
+							');
+						}
+					?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 <?
 	} else if($query = DB_Query(sprintf("SELECT * FROM `page_layouts` WHERE `ID`=%s", QS))) {
 		if(mysqli_num_rows($query) > 0) {
@@ -104,22 +190,6 @@
 <?
 		}
 	} else {
-?>
-	<section>
-		<!-- Section Header -->
-		<div class="row">
-			<div class="col-12 col-md-6">
-				<h1>Website page not found.</h1>
-			</div>
-			<div class="col-12 col-md-6 text-md-end">
-			</div>
-		</div>
-		<hr>
-		<!-- Section Body -->
-		<div class="row">
-			<button class="btn btn-outline-primary col-12 col-md-3 col-lg-1" onclick="history.go(-1)">Go back</buton>
-		</div>
-	</section>
-<?
+		header('location: /Error/404/');
 	}
 ?>
