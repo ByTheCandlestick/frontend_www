c5bb5f0b781d67dbf861a3ed6ff63676d32df524
Robert Smith <master_schmadoodle@outlook.com>
2022-06-14 14:54:15 +0100

    Minor changes to Modern theme

--- Sections/blog/posts_small.php
+++ Sections/blog/posts_small.php
@@ -45,10 +45,7 @@
                         <hr class="my-0" />
                         <ul class="pagination justify-content-center my-4">
                             <li class="page-item disabled"><a class="page-link" href="?p='.($page - 1).'" tabindex="-1" aria-disabled="true">Newer</a></li>
-                            <li class="page-item active" aria-current="page"><a class="page-link" href="">1</a></li>
-                            <li class="page-item disabled"><a class="page-link" href="#!">...</a></li>
-                            <li class="page-item"><a class="page-link" href="#!">'.$total_pages.'</a></li>
-                            <li class="page-item"><a class="page-link" href="'. ($page + 1) .'s">Older</a></li>
+                            <li class="page-item"><a class="page-link" href="?p='. ($page + 1) .'s">Older</a></li>
                         </ul>
                     </nav>
 				');
