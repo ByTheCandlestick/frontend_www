6872ee7c93b7b82169bda9226da97256b573c562
Robert Smith <master_schmadoodle@outlook.com>
2022-06-14 17:19:30 +0100

    Minor changes to scripts and style / edit files

--- Pages/styles_edit.php
+++ Pages/styles_edit.php
@@ -54,6 +54,63 @@
 		</div>
 	</section>
 <?
+	} elseif(QS == "new") {
+?>
+    <section>
+        <!-- Section Header -->
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h1>Style Edit</h1>
+            </div>
+            <div class="col-12 col-md-6 text-md-end">
+            <div class="row">
+                <div class="col-12 col-lg-6 d-flex justify-content-end align-items-center p-0">
+                    <a href="javascript:website.style.create();history.go(-1);" class="btn btn-outline-primary m-1">
+                        <i class="fa fa-save"></i>
+                    </a>
+                </div>
+                <div class="col-12 col-lg-6">
+                </div>
+            </div>
+            </div>
+        </div>
+        <hr>
+        <!-- Section Body -->
+        <div class="row">
+            <h5>Style info</h5>
+            <div class="col-12 col-md-6 col-lg-4" name="name">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+                    <label for="floatingInput">Name</label>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4" name="location">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+                    <label for="floatingInput">Location</label>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4" name="importance">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+                    <label for="floatingInput">Importance</label>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4" name="status">
+                <div class="form-floating mb-3">
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" name="preload" id="flexCheck">
+                        <label class="form-check-label" for="flexCheck"> Preload? </label>
+                    </div>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" name="available" id="flexCheck">
+                        <label class="form-check-label" for="flexCheck"> Active? </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?
 	} else {
 ?>
 	<section>
