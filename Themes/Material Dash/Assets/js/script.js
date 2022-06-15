6d9f5aff800760d96d420747f33764c37e074dbf
Robert Smith <master_schmadoodle@outlook.com>
2022-06-15 16:14:56 +0100

    Minor changes and testing auto calculation

--- Themes/Material Dash/Assets/js/script.js
+++ Themes/Material Dash/Assets/js/script.js
@@ -619,19 +619,19 @@ $( document ).ready(function() {
 
 			},
 			calculate: function() {
-				let container_size = $("div[name=container]").find("option:selected").attr('size');
-				let container_price = $("div[name=container]").find("option:selected").attr('price');
-				let wick_price = $("div[name=wick]").find("option:selected").attr('price');
-				let wickStand_price = $("div[name=wick_stand]").find("option:selected").attr('price');
-				let material_price = $("div[name=material]").find("option:selected").attr('price');
-				let fragrance_price = $("div[name=fragrance]").find("option:selected").attr('price');
-				let colour_price = $("div[name=colour]").find("option:selected").attr('price');
-				let packaging_price = $("div[name=packaging]").find("option:selected").attr('price');
-				let shipping_price = $("div[name=shipping]").find("option:selected").attr('price');
-				let Margin = $("div[name=markup]").find("input").val();
+				container_size = $("div[name=container]").find("option:selected").attr('size');
+				container_price = $("div[name=container]").find("option:selected").attr('price');
+				wick_price = $("div[name=wick]").find("option:selected").attr('price');
+				wickStand_price = $("div[name=wick_stand]").find("option:selected").attr('price');
+				material_price = $("div[name=material]").find("option:selected").attr('price');
+				fragrance_price = $("div[name=fragrance]").find("option:selected").attr('price');
+				colour_price = $("div[name=colour]").find("option:selected").attr('price');
+				packaging_price = $("div[name=packaging]").find("option:selected").attr('price');
+				shipping_price = $("div[name=shipping]").find("option:selected").attr('price');
+				margin = $("div[name=markup]").find("input").val();
 
-				netPrice = container_price + wick_price + wickStand_price + (material_price * container_size) + (fragrance_price * container_size) + (colour_price * container_size) + packaging_price + shipping_price;
-				grossPrice = netPrice + (netPrice * (Margin / 100))
+				netPrice = (container_price + wick_price + wickStand_price + (material_price * container_size) + (fragrance_price * container_size) + (colour_price * container_size) + packaging_price + shipping_price);
+				grossPrice = netPrice + (netPrice * (margin / 100))
 				netProfit = grossPrice - netPrice;
 				console.log(netPrice);
 				console.log(grossPrice);
