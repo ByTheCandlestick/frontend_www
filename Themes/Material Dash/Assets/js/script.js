2a2d74b80ce2ce167d87823e7feb29f503dde073
RSmith <master_schmadoodle@outlook.com>
2022-06-15 19:24:53 +0100

    Minor changes to admin page JS

--- Themes/Material Dash/Assets/js/script.js
+++ Themes/Material Dash/Assets/js/script.js
@@ -615,16 +615,16 @@ $( document ).ready(function() {
 			delete(pid) {
 			},
 			calculate() {
-				container_size = Number($("div[name=container]").find("option:selected").attr('size'));
-				container_price = Number($("div[name=container]").find("option:selected").attr('price'));
-				wick_price = Number($("div[name=wick]").find("option:selected").attr('price'));
-				wickStand_price = Number($("div[name=wick_stand]").find("option:selected").attr('price'));
-				material_price = Number($("div[name=material]").find("option:selected").attr('price'));
-				fragrance_price = Number($("div[name=fragrance]").find("option:selected").attr('price'));
-				colour_price = Number($("div[name=colour]").find("option:selected").attr('price'));
-				packaging_price = Number($("div[name=packaging]").find("option:selected").attr('price'));
-				shipping_price = Number($("div[name=shipping]").find("option:selected").attr('price'));
-				margin = Number($("div[name=markup]").find("input").val());
+				container_size	= Number($("div[name=container]").find("option:selected").attr('size'));
+				container_price	= Number($("div[name=container]").find("option:selected").attr('price'));
+				wick_price		= Number($("div[name=wick]").find("option:selected").attr('price'));
+				wickStand_price	= Number($("div[name=wick_stand]").find("option:selected").attr('price'));
+				material_price	= Number($("div[name=material]").find("option:selected").attr('price'));
+				fragrance_price	= Number($("div[name=fragrance]").find("option:selected").attr('price'));
+				colour_price	= Number($("div[name=colour]").find("option:selected").attr('price'));
+				packaging_price	= Number($("div[name=packaging]").find("option:selected").attr('price'));
+				shipping_price	= Number($("div[name=shipping]").find("option:selected").attr('price'));
+				margin			= Number($("div[name=markup]").find("input").val());
 
 				// Calculate the final prices
 				netPrice = container_price + wick_price + wickStand_price + (material_price * container_size) + (fragrance_price * container_size) + (colour_price * container_size) + packaging_price + shipping_price;
