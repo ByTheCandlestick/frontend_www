861489cb2341b2841930920535690778a2b62d04
Robert Smith <master_schmadoodle@outlook.com>
2022-06-15 15:39:50 +0100

    Minor changes to new products page

--- Themes/Material Dash/Assets/js/script.js
+++ Themes/Material Dash/Assets/js/script.js
@@ -682,11 +682,11 @@ $( document ).ready(function() {
 		});
 		$('input[name=discounted]').change(function() {
 			if($('input[name=discounted]:checked').length === 0) {
-				$('div[name=discount_type]').find('input').prop('disabled', true);
-				$('div[name=discount_amount]').find('select').prop('disabled', true);
+				$('div[name=discount_type]').find('select').prop('disabled', true);
+				$('div[name=discount_amount]').find('input').prop('disabled', true);
 			} else {
-				$('div[name=discount_type]').find('input').prop('disabled', false);
-				$('div[name=discount_amount]').find('select').prop('disabled', false);
+				$('div[name=discount_type]').find('select').prop('disabled', false);
+				$('div[name=discount_amount]').find('input').prop('disabled', false);
 			}
 		});
 	// -----========== EOF ==========----- //
