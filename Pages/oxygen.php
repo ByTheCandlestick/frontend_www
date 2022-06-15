32e3d2de0602df9017bcd877dbf6a6201504a1b0
Robert Smith <master_schmadoodle@outlook.com>
2022-06-15 10:56:16 +0100

    Minor changes to oxygen website editor

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -11,10 +11,10 @@
 		<div class="col-12 col-md-6 text-md-end">
 			<div class="row">
 				<div class="col-12 d-flex justify-content-end align-items-center p-0">
-					<div class="form-floating mb-3">
+					<div class="form-floating m-1">
 						<div class="form-check form-switch">
 							<input type="checkbox" class="btn-check" id="display_type" name="display_type" autocomplete="off" <?($page['display_type']==1)?print("checked"):print("")?>>
-							<label class="btn btn-outline-primary m-1" for="display_type"> <?($page['display_type']==1)?print("Sections"):print("Pages")?> </label>
+							<label class="btn btn-outline-primary" for="display_type"> <?($page['display_type']==1)?print("Sections"):print("Pages")?> </label>
 						</div>
 					</div>
 					<a href="javascript:website.saveLayout(<?print(QS)?>);" class="btn btn-outline-primary m-1">
