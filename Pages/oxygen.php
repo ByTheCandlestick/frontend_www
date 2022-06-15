76323c017d91f8b10105340be6cdcf63d09cd479
Robert Smith <master_schmadoodle@outlook.com>
2022-06-15 11:34:35 +0100

    Minor changes to oxygen website editor

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -43,35 +43,33 @@
 		</style>
 		<div class="col-lg-3">
 			<div class="accordion accordion-flush" id="SectionElements">
-				<?
-					$query = DB_Query("SELECT * FROM `page_sections` ORDER BY `section_type`");
-					$type = null;
-					if(mysqli_num_rows($query) > 0) {
-						while ($row = mysqli_fetch_array($query)) {
-							if($type == null) {
-								print('<div class="accordion-item">');
-							} elseif($type != null && $type != $row['section_type']) {
-								print('</div><div class="accordion-item">');
-							}
-							if($type != $row['section_type']) {
+				<div class="accordion-item">
+					<?
+						$query = DB_Query("SELECT * FROM `page_sections` ORDER BY `section_type`");
+						$type = null;
+						if(mysqli_num_rows($query) > 0) {
+							while ($row = mysqli_fetch_array($query)) {
+								if($type != null && $type != $row['section_type']) {
+									print('
+										</div>
+										<div class="accordion-item">
+											<h2 class="accordion-header" id="headingOne">
+												<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#'.$row['section_type'].'" aria-expanded="true" aria-controls="'.$row['section_type'].'">
+													'.$row['section_type'].'
+												</button>
+											</h2>
+									');
+								}
 								print('
-									<h2 class="accordion-header" id="headingOne">
-										<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#'.$row['section_type'].'" aria-expanded="true" aria-controls="'.$row['section_type'].'">
-											'.$row['section_type'].'
-										</button>
-									</h2>
-								');
+									<div id="'.$row['section_type'].'" class="accordion-collapse collapse col-12 col-md-5 element" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
+										<div class="accordion-body">
+											'.$row['short_description'].'
+										</div>
+									</div>');
+								$type = $row['section_type'];
 							}
-							print('
-								<div id="'.$row['section_type'].'" class="accordion-collapse collapse col-12 col-md-5 element" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
-									<div class="accordion-body">
-										'.$row['short_description'].'
-									</div>
-								</div>');
-							$type = $row['section_type'];
 						}
-					}
-				?>
+					?>
 				</div>
 			</div>
 		</div>
