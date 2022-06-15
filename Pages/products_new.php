1e7519b85547e40058536396cd12de9c2094d0d6
Robert Smith <master_schmadoodle@outlook.com>
2022-06-15 15:22:33 +0100

    Minor changes to new products page

--- Pages/products_new.php
+++ Pages/products_new.php
@@ -6,13 +6,13 @@
 			<p></p>
 		</div>
 		<div class="col-12 col-md-6 text-md-end">
-		<div class="row">
-			<div class="col-12 d-flex justify-content-end align-items-center p-0">
-				<a href="javascript:product.create();" class="btn btn-outline-primary m-1">
-					<i class="fa fa-save"></i>
-				</a>
+			<div class="row">
+				<div class="col-12 d-flex justify-content-end align-items-center p-0">
+					<a href="javascript:product.create();" class="btn btn-outline-primary m-1">
+						<i class="fa fa-save"></i>
+					</a>
+				</div>
 			</div>
-		</div>
 		</div>
 	</div>
 	<hr>
