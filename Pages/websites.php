7b5b2f1fadab861271d2d593e5d0ff499d24dcaa
Robert Smith <master_schmadoodle@outlook.com>
2022-06-15 13:27:27 +0100

    Minor changes to oxygen editor

--- Pages/websites.php
+++ Pages/websites.php
@@ -7,6 +7,9 @@
 		<div class="col-12 col-md-6 text-md-end">
 			<div class="row">
 				<div class="col-12 col-lg-6 d-flex justify-content-end align-items-center p-0">
+					<a href="/Websites/Themes/" class="btn btn-outline-primary m-1">
+						<i class="fa fa-brush"></i>
+					</a>
 					<a href="/Websites/styles/" class="btn btn-outline-primary m-1">
 						<i class="fa fa-book-spells"></i>
 					</a>
