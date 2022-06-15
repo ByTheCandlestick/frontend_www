7cba76dad68bfb8dc70453a2808cd61b6eb36c67
Robert Smith <master_schmadoodle@outlook.com>
2022-06-15 10:42:18 +0100

    Minor changes to oxygen website editor

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -10,12 +10,10 @@
 		</div>
 		<div class="col-12 col-md-6 text-md-end">
 			<div class="row">
-				<div class="col-12 col-lg-6 d-flex justify-content-end align-items-center p-0">
+				<div class="col-12 d-flex justify-content-end align-items-center p-0">
 					<a href="javascript:website.saveLayout(<?print(QS)?>);" class="btn btn-outline-primary m-1">
 						<i class="fa fa-save"></i>
 					</a>
-				</div>
-				<div class="col-12 col-lg-6">
 				</div>
 			</div>
 		</div>
