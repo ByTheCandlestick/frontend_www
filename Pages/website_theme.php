707335dd808fbd23b0c2f139bbaffafdd7b6532f
Robert Smith <master_schmadoodle@outlook.com>
2022-06-15 13:35:31 +0100

    Minor changes to implement page themes

--- Pages/website_theme.php
+++ Pages/website_theme.php
@@ -20,7 +20,7 @@
 		<hr>
 		<!-- Section Body -->
 		<div class="row">
-			<h5>Site info</h5>
+			<h5>Theme info</h5>
 			<div class="col-12 col-md-6 col-lg-3" name="name">
 				<div class="form-floating mb-3">
 					<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
@@ -52,6 +52,7 @@
 <?
 	} else if($query = DB_Query(sprintf("SELECT * FROM `page_types` WHERE `ID`=%s", QS))) {
 		if(mysqli_num_rows($query) > 0) {
+			$theme = mysqli_fetch_assoc($query);
 ?>
 	<section>
 		<!-- Section Header -->
@@ -75,85 +76,30 @@
 		<hr>
 		<!-- Section Body -->
 		<div class="row">
-			<div class="col-12 col-lg-6">
-				<div class="row">
-					<h5>Site info</h5>
-					<div class="col-12" name="name">
-						<div class="form-floating mb-3">
-							<input type="text" class="form-control" id="floatingInput" placeholder="<? print(($page['page_name']=='')?'No name was set':'')?>" value="<? print(($page['page_name']=='')?'':$page['page_name'])?>">
-							<label for="floatingInput">Name</label>
-						</div>
-					</div>
-					<div class="col-12" name="title">
-						<div class="form-floating mb-3">
-							<input type="text" class="form-control" id="floatingInput" placeholder="<? print(($page['page_title']=='')?'No title was set':'')?>" value="<? print(($page['page_title']=='')?'':$page['page_title'])?>">
-							<label for="floatingInput">Title</label>
-						</div>
-					</div>
-					<div class="col-12 col-md-6" name="page_url">
-						<div class="form-floating mb-3">
-							<input type="text" class="form-control" id="floatingInput" placeholder="<? print(($page['page_url']=='')?'No base URL was set':'')?>" value="<? print(($page['page_url']=='')?'':$page['page_url'])?>">
-							<label for="floatingInput">Page URL</label>
-						</div>
-					</div>
-					<div class="col-12 col-md-6" name="subpage_url">
-						<div class="form-floating mb-3">
-							<input type="text" class="form-control" id="floatingInput" placeholder="<? print(($page['subpage_url']=='')?'No sub URL was set':'')?>" value="<? print(($page['subpage_url']=='')?'':$page['subpage_url'])?>">
-							<label for="floatingInput">Subpage URL</label>
-						</div>
-					</div>
-				</div>
-				<div class="col-12" name="domain">
-					<div class="form-floating mb-3">
-						<select class="form-select" id="floatingSelect">
-							<option value="-1" selected>Please select</option>
-							<?
-								$query = DB_Query("SELECT * FROM `misc_websites`");
-								while ($row = mysqli_fetch_array($query)) {
-									$row['ID'] == $page['domain_id'] ? $selected="selected" : $selected="";
-									print_r('<option value="'.$row['ID'].'"'.$selected.'>'.$row['Domain'].' - '.$row['Name'].'</option>');
-								}
-							?>
-						</select>
-						<label for="floatingInput">Domain</label>
-					</div>
+			<h5>Theme info</h5>
+			<div class="col-12 col-md-6 col-lg-3" name="name">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="<? print(($theme['name']=='')?'No name was set':'')?>" value="<? print(($theme['name']=='')?'':$theme['name'])?>">
+					<label for="floatingInput">Name</label>
 				</div>
 			</div>
-			<div class="col-12 col-md-6">
-				<div class="row">
-					<div class="col-12 col-md-6" name="styles">
-						<h5>Styles</h5>
-						<?
-							$query = DB_Query("SELECT * FROM `page_styles` ORDER BY `importance` ASC");
-							while($row = mysqli_fetch_array($query)) {
-								(in_array($row['ID'], $styles))?$checked=" checked":$checked="";
-								print('
-									<div class="form-check">
-										<input class="form-check-input" type="checkbox" value="' . $row['ID'] . '" id="StyleCheckboxes-'.$row['ID'].'"'.$checked.'>
-										<label class="form-check-label" for="StyleCheckboxes-'.$row['ID'].'">
-											' . $row['Name'] . '
-										</label>
-									</div>
-								');
-							}
-						?>
-					</div>
-					<div class="col-12 col-md-6" name="scripts">
-						<h5>Scripts</h5>
-						<?
-							$query = DB_Query("SELECT * FROM `page_scripts` ORDER BY `importance` ASC");
-							while($row = mysqli_fetch_array($query)) {
-								(in_array($row['ID'], $scripts))?$checked=" checked":$checked="";
-								print('
-									<div class="form-check">
-										<input class="form-check-input" type="checkbox" value="'.$row['ID'].'" id="ScriptCheckboxes-'.$row['ID'].'"'.$checked.'>
-										<label class="form-check-label" for="ScriptCheckboxes-'.$row['ID'].'">
-											' . $row['Name'] . '
-										</label>
-									</div>
-								');
-							}
-						?>
+			<div class="col-12 col-md-6 col-lg-3" name="Description">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="<? print(($theme['Description']=='')?'No name was set':'')?>" value="<? print(($theme['Description']=='')?'':$theme['Description'])?>">
+					<label for="floatingInput">Description</label>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3" name="Location">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="<? print(($theme['Location']=='')?'No name was set':'')?>" value="<? print(($theme['Location']=='')?'':$theme['Location'])?>">
+					<label for="floatingInput">Location</label>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3" name="status">
+				<div class="form-floating mb-3">
+					<div class="form-check form-switch">
+						<input class="form-check-input" type="checkbox" name="discontinued" id="flexCheck" <?($prod['Active']==1)?print("checked"):print("")?>>
+						<label class="form-check-label" for="flexCheck"> Active </label>
 					</div>
 				</div>
 			</div>
