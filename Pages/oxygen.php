f7f8cef8de636178c9d3862560931f0f2c8d2d7e
Robert Smith <master_schmadoodle@outlook.com>
2022-06-15 11:31:11 +0100

    Minor changes to oxygen website editor

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -50,9 +50,11 @@
 						while ($row = mysqli_fetch_array($query)) {
 							if($type == null) {
 								print('<div class="accordion-item">');
-							} elseif($type != null && $type != $row['section_type']) {
+							}
+							if($type != null && $type != $row['section_type']) {
 								print('</div><div class="accordion-item">');
-							} elseif($type != $row['section_type']) {
+							}
+							if($type != $row['section_type']) {
 								print('
 									<h2 class="accordion-header" id="headingOne">
 										<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#'.$row['section_type'].'" aria-expanded="true" aria-controls="'.$row['section_type'].'">
