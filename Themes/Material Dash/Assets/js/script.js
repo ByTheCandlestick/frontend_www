7151f26c732ce5096e807470aba8a1f928c2d007
Robert Smith <master_schmadoodle@outlook.com>
2022-06-15 17:04:45 +0100

    Minor changes and testing auto calculation

--- Themes/Material Dash/Assets/js/script.js
+++ Themes/Material Dash/Assets/js/script.js
@@ -637,8 +637,8 @@ $( document ).ready(function() {
 
 				// Round to nearest 5 0r 9
 				arr = [
-					Math.ceil(grossPrice / 0.05) * 0.05,
-					Math.ceil(grossPrice / 0.09) * 0.09,
+					(Math.ceil(grossPrice / 0.05) * 0.05) + 0.05,
+					(Math.ceil(grossPrice / 0.09) * 0.09),
 				]
 				retailPrice = arr.reduce((prev, curr) => {
 				  return (Math.abs(curr - grossPrice) < Math.abs(prev - grossPrice) ? curr : prev);
