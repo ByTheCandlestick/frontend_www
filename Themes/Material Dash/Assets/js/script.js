6da5de22a373e7b4fdc25fe470d24e22af12a67a
Robert Smith <master_schmadoodle@outlook.com>
2022-06-15 15:27:28 +0100

    Minor changes to new products page

--- Themes/Material Dash/Assets/js/script.js
+++ Themes/Material Dash/Assets/js/script.js
@@ -669,9 +669,9 @@ $( document ).ready(function() {
 	// -----========== Auto-calculate product ==========----- //
 		$('input[name=auto_calculate]').change(function() {
 			if($('input[name=auto_calculate]:checked').length === 0) {
-				$('div[name-net]').find('input').prop('disabled', false);
+				$('div[name=net]').find('input').prop('disabled', false);
 			} else {
-				$('div[name-net]').find('input').prop('disabled', true);
+				$('div[name=net]').find('input').prop('disabled', true);
 			}
 		});
 	// -----========== EOF ==========----- //
