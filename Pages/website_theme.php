8e07e83b4a257c69ed883be7edba0aa30e0fef27
Robert Smith <master_schmadoodle@outlook.com>
2022-06-15 13:32:56 +0100

    Minor changes to implement page themes

--- Pages/website_theme.php
+++ Pages/website_theme.php
@@ -42,7 +42,7 @@
 			<div class="col-12 col-md-6 col-lg-3" name="status">
 				<div class="form-floating mb-3">
 					<div class="form-check form-switch">
-						<input class="form-check-input" type="checkbox" name="discontinued" id="flexCheck">
+						<input class="form-check-input" type="checkbox" name="discontinued" id="flexCheck" checked>
 						<label class="form-check-label" for="flexCheck"> Active </label>
 					</div>
 				</div>
