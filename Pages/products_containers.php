513e5393d9d6c709560d25b3fe742d0458c81215
Robert Smith <master_schmadoodle@outlook.com>
2022-06-16 13:41:47 +0100

    Minor canges to table styling

--- Pages/products_containers.php
+++ Pages/products_containers.php
@@ -1,5 +1,5 @@
 <?
-	$q = DB_Query("SELECT * FROM `products_categories` WHERE `Active`=1");
+	$q = DB_Query("SELECT * FROM `suppliers` WHERE `Active`=1");
 	while($row = mysqli_fetch_array($q)) { $suppliers[$row['ID']] = $row['Name']; }
 ?>
 <section>
