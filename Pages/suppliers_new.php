0bd3f8fb91f801a525b44421d659320002dc32aa
Robert Smith <master_schmadoodle@outlook.com>
2022-06-16 13:48:03 +0100

    Add supplier basic form

--- Pages/suppliers_new.php
+++ Pages/suppliers_new.php
@@ -17,37 +17,37 @@
 	<hr>
 	<!-- Section Body -->
 	<div class="row SupplierInfo">
-		<div class="col-12 col-lg-6" name="reference">
+		<div class="col-12 col-md-6 col-lg-4" name="reference">
 			<div class="form-floating mb-3">
 				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Reference'])?>">
 				<label for="floatingInput">Reference</label>
 			</div>
 		</div>
-		<div class="col-12 col-lg-6" name="name">
+		<div class="col-12 col-md-6 col-lg-4" name="name">
 			<div class="form-floating mb-3">
 				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Name'])?>">
 				<label for="floatingInput">Name</label>
 			</div>
 		</div>
-		<div class="col-12 col-lg-6" name="email">
+		<div class="col-12 col-md-6 col-lg-4" name="email">
 			<div class="form-floating mb-3">
 				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Email'])?>">
 				<label for="floatingInput">Email</label>
 			</div>
 		</div>
-		<div class="col-12 col-lg-6" name="phone">
+		<div class="col-12 col-md-6 col-lg-4" name="phone">
 			<div class="form-floating mb-3">
 				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Phone'])?>">
 				<label for="floatingInput">Phone</label>
 			</div>
 		</div>
-		<div class="col-12 col-lg-6" name="hours">
+		<div class="col-12 col-md-6 col-lg-4" name="hours">
 			<div class="form-floating mb-3">
 				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Opening Hours'])?>">
 				<label for="floatingInput">Opening hours</label>
 			</div>
 		</div>
-		<div class="col-12 col-lg-3" name="status">
+		<div class="col-12 col-md-6 col-lg-4" name="status">
 			<div class="form-floating mb-3">
 				<div class="form-check form-switch">
 					<input class="form-check-input" type="checkbox" name="available" id="flexCheck" <?($prod['Active']==1)?print("checked"):print("")?>>
