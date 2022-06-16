c713f470103cceed6d07f7f379156f16595ea700
Robert Smith <master_schmadoodle@outlook.com>
2022-06-16 14:27:16 +0100

    Minor changes to create supplier

--- Pages/suppliers_edit.php
+++ Pages/suppliers_edit.php
@@ -24,39 +24,39 @@
 		<div class="row SupplierInfo">
 			<div class="col-12 col-md-6 col-lg-2" name="reference">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($supp['Reference'])?>">
 					<label for="floatingInput">Reference</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="name">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($supp['Name'])?>">
 					<label for="floatingInput">Name</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="email">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($supp['Email'])?>">
 					<label for="floatingInput">Email</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-2" name="phone">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($supp['Phone'])?>">
 					<label for="floatingInput">Phone</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-2" name="status">
 				<div class="form-floating mb-3">
 					<div class="form-check form-switch">
-						<input class="form-check-input" type="checkbox" name="active" id="flexCheck" checked>
+						<input class="form-check-input" type="checkbox" name="active" id="flexCheck" <?($prod['Active']==1)?print("checked"):print("")?>>
 						<label class="form-check-label" for="flexCheck"> Active? </label>
 					</div>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="hours">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($supp['Opening Hours'])?>">
 					<label for="floatingInput">Opening hours</label>
 				</div>
 			</div>
