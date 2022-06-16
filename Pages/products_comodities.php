71031d19d288e8eb21db63dbbe973ab6035b1346
Robert Smith <master_schmadoodle@outlook.com>
2022-06-16 12:40:11 +0100

    minor changes to comodities file

--- Pages/products_comodities.php
+++ Pages/products_comodities.php
@@ -5,7 +5,7 @@
 	<!-- Section Header -->
 	<div class="row">
 		<div class="col-12 col-md-6">
-			<h1>Products</h1>
+			<h1>Comodities</h1>
 		</div>
 		<div class="col-12 col-md-6 text-md-end">
 			<div class="row">
@@ -499,13 +499,3 @@
         </div>
     </div>
 </section>
-<script>
-	$(document).ready(function(){
-		$(".tableFilter").on("keyup", function() {
-			var value = $(this).val().toLowerCase();
-			$(".comoditiesTable tbody tr").filter(function() {
-				$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-			});
-		});
-	});
-</script>
