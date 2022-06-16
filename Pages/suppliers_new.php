51e1ecf911d046e5e8e2c630824ac66a7befbe4c
Robert Smith <master_schmadoodle@outlook.com>
2022-06-16 13:48:56 +0100

    Add supplier basic form

--- Pages/suppliers_new.php
+++ Pages/suppliers_new.php
@@ -17,7 +17,7 @@
 	<hr>
 	<!-- Section Body -->
 	<div class="row SupplierInfo">
-		<div class="col-12 col-md-6 col-lg-3" name="reference">
+		<div class="col-12 col-md-6 col-lg-2" name="reference">
 			<div class="form-floating mb-3">
 				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Reference'])?>">
 				<label for="floatingInput">Reference</label>
@@ -35,24 +35,24 @@
 				<label for="floatingInput">Email</label>
 			</div>
 		</div>
-		<div class="col-12 col-md-6 col-lg-3" name="phone">
+		<div class="col-12 col-md-6 col-lg-2" name="phone">
 			<div class="form-floating mb-3">
 				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Phone'])?>">
 				<label for="floatingInput">Phone</label>
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-2" name="status">
+			<div class="form-floating mb-3">
+				<div class="form-check form-switch">
+					<input class="form-check-input" type="checkbox" name="available" id="flexCheck" <?($prod['Active']==1)?print("checked"):print("")?>>
+					<label class="form-check-label" for="flexCheck"> Active? </label>
+				</div>
 			</div>
 		</div>
 		<div class="col-12 col-md-6 col-lg-3" name="hours">
 			<div class="form-floating mb-3">
 				<input type="text" class="form-control" id="floatingInput" placeholder="" value="<? print($prod['Opening Hours'])?>">
 				<label for="floatingInput">Opening hours</label>
-			</div>
-		</div>
-		<div class="col-12 col-md-6 col-lg-3" name="status">
-			<div class="form-floating mb-3">
-				<div class="form-check form-switch">
-					<input class="form-check-input" type="checkbox" name="available" id="flexCheck" <?($prod['Active']==1)?print("checked"):print("")?>>
-					<label class="form-check-label" for="flexCheck"> Active? </label>
-				</div>
 			</div>
 		</div>
 	</div>
