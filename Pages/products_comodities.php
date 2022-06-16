bc6da8927ab35ce2677d1071a17f89e0bb7f5f0f
Robert Smith <master_schmadoodle@outlook.com>
2022-06-16 12:00:42 +0100

    Minor changes to voxo theme

--- Pages/products_comodities.php
+++ Pages/products_comodities.php
@@ -30,7 +30,7 @@
                     </div>
                 </div>
                 <div class="card-body p-0">
-                    <table class="productsTable table table-striped table-hover">
+                    <table class="containersTable table table-striped table-hover m-0">
                         <thead class="sticky-top" style="background: var(--section); z-index: unset;">
                             <tr>
                                 <th scope="col">id</th>
@@ -72,7 +72,7 @@
                     </table>
                 </div>
                 <div class="card-footer text-muted">
-                    <a href="">
+                    <a href="/Products/Containers">
                         See more
                     </a>
                 </div>
@@ -89,7 +89,7 @@
                     </div>
                 </div>
                 <div class="card-body p-0">
-                    <table class="productsTable table table-striped table-hover">
+                    <table class="wicksTable table table-striped table-hover m-0">
                         <thead class="sticky-top" style="background: var(--section); z-index: unset;">
                             <tr>
                                 <th scope="col">id</th>
@@ -131,7 +131,7 @@
                     </table>
                 </div>
                 <div class="card-footer text-muted">
-                    <a href="">
+                    <a href="/Products/Wicks">
                         See more
                     </a>
                 </div>
@@ -148,7 +148,7 @@
                     </div>
                 </div>
                 <div class="card-body p-0">
-                    <table class="productsTable table table-striped table-hover">
+                    <table class="wickstandsTable table table-striped table-hover m-0">
                         <thead class="sticky-top" style="background: var(--section); z-index: unset;">
                             <tr>
                                 <th scope="col">id</th>
@@ -190,7 +190,66 @@
                     </table>
                 </div>
                 <div class="card-footer text-muted">
-                    <a href="">
+                    <a href="/Products/WickStands">
+                        See more
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 p-2">
+            <div class="card">
+                <div class="card-header row m-0">
+                    <div class="col-8">
+                        Containers
+                    </div>
+                    <div class="col-4">
+                        Containers
+                    </div>
+                </div>
+                <div class="card-body p-0">
+                    <table class="wickstandsTable table table-striped table-hover m-0">
+                        <thead class="sticky-top" style="background: var(--section); z-index: unset;">
+                            <tr>
+                                <th scope="col">id</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Price</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?
+                                $query = DB_Query("SELECT * FROM `products_wickstands` LIMIT 4");
+                                if(mysqli_num_rows($query) > 0) {
+                                    while ($row = mysqli_fetch_array($query)) {
+                                        print('
+                                            <tr>
+                                                <th scope="row">'.$row['ID'].'</th>
+                                                <td>'.$row['Name'].'</td>
+                                                <td>'.$row['Price (ea)'].'</td>
+                                                <td>
+                                                    <a href="/Products/Como/'.$row['ID'].'">
+                                                        <i class="fa fa-pencil"></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        ');
+                                    }
+                                } else {
+                                    print('
+                                        <tr>
+                                            <th scope="row"></th>
+                                            <td>No data found</td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                    ');
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer text-muted">
+                    <a href="/Products/WickStands">
                         See more
                     </a>
                 </div>
