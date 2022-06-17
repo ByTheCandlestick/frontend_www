c69439114a022751650c1a4cbc26a6a654447557
Robert Smith <master_schmadoodle@outlook.com>
2022-06-17 17:05:43 +0100

    Minor changes to api.json

--- Pages/assistance_api.php
+++ Pages/assistance_api.php
@@ -15,6 +15,11 @@
 	<!-- Section Body -->
 	<div class="row">
         <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui.css" />
+        <style>
+            .topbar {
+                display: none!important;
+            }
+        </style>
         <div id="swagger-ui"></div>
         <script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-bundle.js" charset="UTF-8"></script>
         <script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-standalone-preset.js" charset="UTF-8"></script>
