42feb86e2aac72caae4b9742cffd1dbff8473213
Robert Smith <master_schmadoodle@outlook.com>
2022-06-17 17:07:50 +0100

    Minor changes to api.json

--- Pages/assistance_api.php
+++ Pages/assistance_api.php
@@ -6,7 +6,7 @@
 	<!-- Section Header -->
 	<div class="row">
 		<div class="col-12 col-md-6">
-			<h1>Analytics</h1>
+			<h2 class="title">Analytics</h1>
 		</div>
 		<div class="col-12 col-md-6 text-md-end">
 		</div>
