c5f7d8b003efd0bc0b04354cb6e883a6d043e7c4
Robert Smith <master_schmadoodle@outlook.com>
2022-06-17 17:05:02 +0100

    Minor changes to api.json

--- Pages/assistance_api.php
+++ Pages/assistance_api.php
@@ -3,32 +3,39 @@
     $url.= $_SERVER['HTTP_HOST'];   
 ?>
 <section>
-    <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui.css" />
-    <style>
-        .topbar {
-            display: none!important;
-        }
-    </style>
-    <div id="swagger-ui"></div>
-    <script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-bundle.js" charset="UTF-8"></script>
-    <script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-standalone-preset.js" charset="UTF-8"></script>
-    <script>
-        window.onload = function() {
-            const ui = SwaggerUIBundle({
-                url: "<?print($url);?>/api.json",
-                dom_id: '#swagger-ui',
-                deepLinking: true,
-                validatorUrl: null,
-                presets: [
-                    SwaggerUIBundle.presets.apis,
-                    SwaggerUIStandalonePreset
-                ],
-                plugins: [
-                    SwaggerUIBundle.plugins.DownloadUrl
-                ],
-                layout: "StandaloneLayout"
-            });
-            window.ui = ui;
-        };
-    </script>
+	<!-- Section Header -->
+	<div class="row">
+		<div class="col-12 col-md-6">
+			<h1>Analytics</h1>
+		</div>
+		<div class="col-12 col-md-6 text-md-end">
+		</div>
+	</div>
+	<hr>
+	<!-- Section Body -->
+	<div class="row">
+        <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui.css" />
+        <div id="swagger-ui"></div>
+        <script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-bundle.js" charset="UTF-8"></script>
+        <script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-standalone-preset.js" charset="UTF-8"></script>
+        <script>
+            window.onload = function() {
+                const ui = SwaggerUIBundle({
+                    url: "<?print($url);?>/api.json",
+                    dom_id: '#swagger-ui',
+                    deepLinking: true,
+                    validatorUrl: null,
+                    presets: [
+                        SwaggerUIBundle.presets.apis,
+                        SwaggerUIStandalonePreset
+                    ],
+                    plugins: [
+                        SwaggerUIBundle.plugins.DownloadUrl
+                    ],
+                    layout: "StandaloneLayout"
+                });
+                window.ui = ui;
+            };
+        </script>
+    </div>
 </section>
