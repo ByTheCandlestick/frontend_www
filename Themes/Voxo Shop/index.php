61b1dc3c24b9f42d102edc8cbcbfaa877cfa23e9
RSmith <master_schmadoodle@outlook.com>
2022-06-26 20:56:03 +0100

    Minor changes to redirect website functionality

--- Themes/Voxo Shop/index.php
+++ Themes/Voxo Shop/index.php
@@ -98,7 +98,7 @@
 					<?
 						if(isset($_GET['rw']) && $_GET['rw'] != "") {
 							print('
-								<rw data="'.mysqli_fetch_row(DB_QUERY(sprintf("SELECT `Domain` FROM `misc_websites` WHERE `ID`='%s'", $_GET['rw'])))[].'"></rw>
+								<rw data="' . mysqli_fetch_row(DB_QUERY(sprintf("SELECT `Domain` FROM `misc_websites` WHERE `ID`='%s'", $_GET['rw'])))[0] . '"></rw>
 							');
 						}
 						if(isset($_GET['rwp']) && $_GET['rwp'] != "") {
