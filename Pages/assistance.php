4b77264218ecbc07fca8817df73b9c31ab0c50a2
Robert Smith <master_schmadoodle@outlook.com>
2022-07-01 16:41:15 +0100

    Minor changes to assistance CSS

--- Pages/assistance.php
+++ Pages/assistance.php
@@ -12,16 +12,16 @@
 	<hr>
 	<!-- Section Body -->
 	<div class="row" style="height:100%;">
-		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="background: #fff; border-right: solid 1px rgba(0,0,0,0.4);">
+		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="background: #fff; overflow-x: hidden; overflow-y: scroll; border-right: solid 1px rgba(0,0,0,0.4);">
 			
 		</div>
-		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="background: #fff; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)">
+		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="background: #fff; overflow-x: hidden; overflow-y: scroll; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)">
 			
 		</div>
-		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="background: #fff; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)">
+		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="background: #fff; overflow-x: hidden; overflow-y: scroll; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)">
 			
 		</div>
-		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="background: #fff; box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)">
+		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="background: #fff; overflow-x: hidden; overflow-y: scroll; box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)">
 			
 		</div>
 	</div>
