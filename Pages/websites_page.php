6fee5bd5faf2a789d7eba0402f163f6aad98b3d8
Robert Smith <master_schmadoodle@outlook.com>
2022-07-01 11:18:57 +0100

    Minor changes to page edit

--- Pages/websites_page.php
+++ Pages/websites_page.php
@@ -176,25 +176,25 @@
 						</div>
 					</div>
 					<h5>Menu info</h5>
-					<div class="col-12 col-lg-2" name="menu_item">
+					<div class="col-12 col-lg-6" name="menu_item">
 						<div class="form-check">
 							<input class="form-check-input" type="checkbox" value="" id="MenuCheckboxe">
 							<label class="form-check-label" for="MenuCheckboxe">Is menu item?</label>
 						</div>
 					</div>
-					<div class="col-12 col-lg-4" name="menu_icon">
+					<div class="col-12 col-lg-6" name="menu_icon">
 						<div class="form-floating mb-3">
 							<input type="text" class="form-control" id="floatingInput" placeholder="<? print(($page['menu_icon']=='')?'No title was set':'')?>" value="<? print(($page['menu_icon']=='')?'':$page['menu_icon'])?>">
 							<label for="floatingInput">Icon</label>
 						</div>
 					</div>
-					<div class="col-12 col-lg-2" name="menu_order">
+					<div class="col-12 col-lg-4" name="menu_order">
 						<div class="form-floating mb-3">
 							<input type="number" class="form-control" id="floatingInput" placeholder="<? print(($page['menu_order']=='')?'No title was set':'')?>" value="<? print(($page['menu_order']=='')?'':$page['menu_order'])?>">
 							<label for="floatingInput">Menu Order</label>
 						</div>
 					</div>
-					<div class="col-12 col-lg-4" name="menu_url">
+					<div class="col-12 col-lg-8" name="menu_url">
 						<div class="form-floating mb-3">
 							<input type="text" class="form-control" id="floatingInput" placeholder="<? print(($page['menu_url']=='')?'No title was set':'')?>" value="<? print(($page['menu_url']=='')?'':$page['menu_url'])?>">
 							<label for="floatingInput">URL</label>
