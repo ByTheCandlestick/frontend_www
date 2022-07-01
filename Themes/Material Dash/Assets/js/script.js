43590391139461a963c96ac5aa63fca37f8b8c35
Robert Smith <master_schmadoodle@outlook.com>
2022-07-01 11:42:46 +0100

    Minor changes to update page data

--- Themes/Material Dash/Assets/js/script.js
+++ Themes/Material Dash/Assets/js/script.js
@@ -273,9 +273,9 @@ $( document ).ready(() => {
 			},
 			/** @final */
 			page: {
-				styles: $("div[name=styles]").children().find("input[type=checkbox]:checked").each((index, element) => { var styles=[]; styles.push($(element).val()); }),
-				scripts: $("div[name=scripts]").children().find("input[type=checkbox]:checked").each((index, element) => { var scripts=[]; scripts.push($(element).val()); }),
-				data: {
+				styles = [],
+				scripts = [],
+				data = {
 					'api_key': api_key,
 					'style': website.page.styles.join(","),
 					'script': website.page.scripts.join(","),
@@ -289,10 +289,20 @@ $( document ).ready(() => {
 					'menu_icon': $("div[name=menu_icon] input").val(),
 					'menu_url': $("div[name=menu_url] input").val(),
 				},
+				loadScripts() {
+					$("div[name=scripts]").children().find("input[type=checkbox]:checked").each((index, element) => {
+						website.page.scripts.push($(element).val());
+					})
+				},
+				loadStyles() {
+					$("div[name=styles]").children().find("input[type=checkbox]:checked").each((index, element) => {
+						website.page.styles.push($(element).val());
+					})
+				},
 				create() {
 					$.ajax({
 						url: api_url + '/Page/',
-						data: website.page.data,
+						data: data,
 						type: 'PUT',
 						xhrFields: {
 							withCredentials: true,
@@ -310,9 +320,27 @@ $( document ).ready(() => {
 					});
 				},
 				update(pid) {
+					var styles = [];
+					var scripts = [];
+					$("div[name=styles]").children().find("input[type=checkbox]:checked").each((index, element) => { styles.push($(element).val()); });
+					$("div[name=scripts]").children().find("input[type=checkbox]:checked").each((index, element) => { scripts.push($(element).val()); });
+					data = {
+						'api_key': api_key,
+						'style': styles.join(","),
+						'script': scripts.join(","),
+						'name': $("div[name=name] input").val(),
+						'title': $("div[name=title] input").val(),
+						'page_url': $("div[name=page_url] input").val(),
+						'subpage_url': $("div[name=subpage_url] input").val(),
+						'domain_id': $("div[name=domain]").find("option:selected").val(),
+						'menu_item': (($("div[name=menu_item] input:checked").length === 0)?0:1),
+						'menu_order': $("div[name=menu_order] input").val(),
+						'menu_icon': $("div[name=menu_icon] input").val(),
+						'menu_url': $("div[name=menu_url] input").val(),
+					}
 					$.ajax({
 						url: api_url + '/Page/' + pid + '/',
-						data: website.page.data,
+						data: data,
 						type: 'POST',
 						xhrFields: {
 							withCredentials: true,
