235f1ee4f2336cf33ca2829fc90979796b06ae3c
Robert Smith <master_schmadoodle@outlook.com>
2022-07-01 11:33:07 +0100

    Minor changes to update page data

--- Themes/Material Dash/Assets/js/script.js
+++ Themes/Material Dash/Assets/js/script.js
@@ -273,7 +273,7 @@ $( document ).ready(() => {
 			},
 			/** @final */
 			page: {
-				styles: $("div[name=styles]").children().find("input[type=checkbox]:checked").each((index, element) => { push($(element).val()); }),
+				styles: $("div[name=styles]").children().find("input[type=checkbox]:checked").each((index, element) => { this.push($(element).val()); }),
 				scripts: $("div[name=scripts]").children().find("input[type=checkbox]:checked").each((index, element) => { this.push($(element).val()); }),
 				data: {
 					'api_key': api_key,
