99bc7e161c0b9abec3fe7246ea3903b7a79673df
Robert Smith <master_schmadoodle@outlook.com>
2022-07-01 16:49:41 +0100

    Minor changes to assistance CSS

--- Pages/assistance.php
+++ Pages/assistance.php
@@ -12,16 +12,16 @@
 	<hr>
 	<!-- Section Body -->
 	<div class="row" style="height: calc(100% - 141px);">
-		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="overflow-x: hidden; overflow-y: scroll; border-right: solid 1px rgba(0,0,0,0.4);">
+		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="overflow-x: hidden; overflow-y: scroll;height: 100%; border-right: solid 1px rgba(0,0,0,0.4);">
 			gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br> gg <br>
 		</div>
-		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="overflow-x: hidden; overflow-y: scroll; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)">
+		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="overflow-x: hidden; overflow-y: scroll;height: 100%; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)">
 			
 		</div>
-		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="overflow-x: hidden; overflow-y: scroll; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)">
+		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="overflow-x: hidden; overflow-y: scroll;height: 100%; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)">
 			
 		</div>
-		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="overflow-x: hidden; overflow-y: scroll; box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)">
+		<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="overflow-x: hidden; overflow-y: scroll;height: 100%; box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)">
 			
 		</div>
 	</div>
