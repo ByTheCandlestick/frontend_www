1f21d2b5ae5fc1cdcd30f26ad1ab56c2a69bf1ab
Robert Smith <master_schmadoodle@outlook.com>
2022-07-02 12:37:11 +0100

    Minor changes to asistance md

--- Pages/assistance.php
+++ Pages/assistance.php
@@ -12,9 +12,9 @@
 	<hr>
 	<!-- Section Body -->
 	<div class="row assistanceNav" style="height: calc(100% - 141px);">
-		<div name="lv1" class="col-12 col-md-6 col-lg-4 col-xl-3" style="overflow-x: hidden; overflow-y: scroll;height: 100%; border-right: solid 1px rgba(0,0,0,0.4);"></div>
-		<div name="lv2" class="col-12 col-md-6 col-lg-4 col-xl-3" style="overflow-x: hidden; overflow-y: scroll;height: 100%; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)"></div>
-		<div name="lv3" class="col-12 col-md-6 col-lg-4 col-xl-3" style="overflow-x: hidden; overflow-y: scroll;height: 100%; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)"></div>
-		<div name="lv4" class="col-12 col-md-6 col-lg-4 col-xl-3" style="overflow-x: hidden; overflow-y: scroll;height: 100%; box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)"></div>
+		<div name="lv1" class="col-12 col-md-4 col-lg-4 col-xl-2" style="overflow-x: hidden; overflow-y: scroll;height: 100%; border-right: solid 1px rgba(0,0,0,0.4);"></div>
+		<div name="lv2" class="col-12 col-md-4 col-lg-4 col-xl-2" style="overflow-x: hidden; overflow-y: scroll;height: 100%; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)"></div>
+		<div name="lv3" class="col-12 col-md-4 col-lg-4 col-xl-2" style="overflow-x: hidden; overflow-y: scroll;height: 100%; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)"></div>
+		<div name="lv4" class="col-12 col-md-12 col-lg-12 col-xl-6" style="overflow-x: hidden; overflow-y: scroll;height: 100%; box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)"></div>
 	</div>
 </section>
