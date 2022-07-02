3c39b087aadcc3a5bec95fe5a7d7e39e5492363b
Robert Smith <master_schmadoodle@outlook.com>
2022-07-02 15:27:04 +0100

    Minor changes to assistance

--- Pages/assistance.php
+++ Pages/assistance.php
@@ -1,5 +1,26 @@
 <style>
-
+.assistanceNav {
+	height: calc(100% - 141px);
+}
+.assistanceNav div {
+	overflow-x: hidden;
+	overflow-y: scroll;
+	height: 100%;
+}
+.assistanceNav div:nth-child(1) {
+	border-right: solid 1px rgba(0,0,0,0.4);
+}
+.assistanceNav div:nth-child(2) {
+	border-right: solid 1px rgba(0,0,0,0.4);
+	box-shadow: inset 7px 0px 9px -12px rgb(0 0 0);
+}
+.assistanceNav div:nth-child(3) {
+	border-right: solid 1px rgba(0,0,0,0.4);
+	box-shadow: inset 7px 0px 9px -12px rgb(0 0 0);
+}
+.assistanceNav div:nth-child(4) {
+	box-shadow: inset 7px 0px 9px -12px rgb(0 0 0);
+}
 </style>
 <section>
 	<!-- Section Header -->
@@ -11,10 +32,10 @@
 	</div>
 	<hr>
 	<!-- Section Body -->
-	<div class="row assistanceNav" style="height: calc(100% - 141px);">
-		<div name="lv1" class="col-12 col-md-4 col-lg-4 col-xl-2" style="overflow-x: hidden; overflow-y: scroll;height: 100%; border-right: solid 1px rgba(0,0,0,0.4);"></div>
-		<div name="lv2" class="col-12 col-md-4 col-lg-4 col-xl-2" style="overflow-x: hidden; overflow-y: scroll;height: 100%; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)"></div>
-		<div name="lv3" class="col-12 col-md-4 col-lg-4 col-xl-2" style="overflow-x: hidden; overflow-y: scroll;height: 100%; border-right: solid 1px rgba(0,0,0,0.4); box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)"></div>
-		<div name="lv4" class="col-12 col-md-12 col-lg-12 col-xl-6" style="overflow-x: hidden; overflow-y: scroll;height: 100%; box-shadow: inset 7px 0px 9px -12px rgb(0 0 0)"></div>
+	<div class="row assistanceNav">
+		<div name="lv1" class="col-12 col-md-4 col-lg-4 col-xl-2"></div>
+		<div name="lv2" class="col-12 col-md-4 col-lg-4 col-xl-2"></div>
+		<div name="lv3" class="col-12 col-md-4 col-lg-4 col-xl-2"></div>
+		<div name="lv4" class="col-12 col-md-12 col-lg-12 col-xl-6"></div>
 	</div>
 </section>
