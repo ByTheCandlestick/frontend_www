b8930c478ed1710e46ef891213a3a8af7b5b2cc9
Robert Smith <master_schmadoodle@outlook.com>
2022-07-05 17:12:12 +0100

    Minor changes

--- Themes/Material Dash/index.php
+++ Themes/Material Dash/index.php
@@ -128,7 +128,7 @@
 								<span class="app-icon">
 									<img src="http://api.candlestick-indev.co.uk/v1/Images/Fetch/candlestickLogo_20220530162542/" alt="logo" width="60px" height="60px" class="img-fluid" title="The Candlestick Logo">
 								</span>
-								<p class="app-name"> The Candlestick<? DOMAIN_TYPE=='indev': print('Developer mode'); ?> </p>
+								<p class="app-name"> The Candlestick<? DOMAIN_TYPE=='indev': print(' Developer mode'); ?> </p>
 							</div>
 							<!-- Search -->
 							<div class="search-wrapper" rel="/search.json">
