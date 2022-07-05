98923e037a0a7727eb4aabfb790b487fc1d4ca29
Robert Smith <master_schmadoodle@outlook.com>
2022-07-05 19:03:31 +0100

    Minor changes to admin search

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -64,6 +64,10 @@
             <div class="col-12">
                 <h5>Notes</h5>
                 <div class="row">
+                    <div class="col-12 col-md-6 form-floating mb-3">
+                        <textarea type="text" class="form-control" id="floatingInput" style="resize:none;" disabled><? print($invoice['notes'])?></textarea>
+                        <label for="floatingInput">Order notes</label>
+                    </div>
                 </div>
             </div>
 
