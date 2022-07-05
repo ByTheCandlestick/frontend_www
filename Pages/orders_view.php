9f3512ebb06136c42461e5868ecbdec0b018e3f2
Robert Smith <master_schmadoodle@outlook.com>
2022-07-05 17:43:10 +0100

    Minor changes

---
+++ Pages/orders_view.php
@@ -0,0 +1,20 @@
+<section>
+	<!-- Section Header -->
+	<div class="row">
+		<div class="col-12 col-md-6">
+			<h1><?print(QS)?></h1>
+		</div>
+		<div class="col-12 col-md-6 text-md-end">
+			<div class="row">
+				<div class="col-12 col-lg-6 d-block d-md-flex justify-content-end align-items-center p-0">
+				</div>
+				<div class="col-12 col-lg-6">
+    			</div>
+			</div>
+		</div>
+	</div>
+	<hr>
+	<!-- Section Body -->
+	<div class="row">
+	</div>
+</section>
