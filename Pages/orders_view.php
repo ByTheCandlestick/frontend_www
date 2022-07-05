0dc63ed9861636ee43bc7fa42275a116e05a3913
Robert Smith <master_schmadoodle@outlook.com>
2022-07-05 17:43:24 +0100

    Minor changes

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -2,7 +2,7 @@
 	<!-- Section Header -->
 	<div class="row">
 		<div class="col-12 col-md-6">
-			<h1><?print(QS)?></h1>
+			<h1>Invoice: <?print(QS)?></h1>
 		</div>
 		<div class="col-12 col-md-6 text-md-end">
 			<div class="row">
