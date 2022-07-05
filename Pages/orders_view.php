4840bb6ac5a59301d8c70af196a1fd527d1ed0de
Robert Smith <master_schmadoodle@outlook.com>
2022-07-05 18:36:10 +0100

    Minor changes to admin search

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -32,11 +32,11 @@
             </div>
             <div class="col-12 col-lg-9">
                 <div class="row">
-                    <div class="col-12 col-md-6 col-md-4 form-floating mb-3">
+                    <div class="col-12 col-md-6 col-lg-4 form-floating mb-3">
                         <input type="text" class="form-control" id="floatingInput" value="<? print($invoice['name'])?>" disabled>
                         <label for="floatingInput">Deliver to name</label>
                     </div>
-                    <div class="col-12 col-md-6 col-md-4 form-floating mb-3">
+                    <div class="col-12 col-md-6 col-lg-4 form-floating mb-3">
                         <input type="text" class="form-control" id="floatingInput" value="<? print($delivery['Name'])?>" disabled>
                         <label for="floatingInput">Delivery by</label>
                     </div>
