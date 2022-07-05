937b91ff6b98b0096982e1a2a60ddee66604cdcf
Robert Smith <master_schmadoodle@outlook.com>
2022-07-05 18:42:46 +0100

    Minor changes to admin search

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -58,9 +58,108 @@
                     </div>
                 </div>
             </div>
-            <div class="col-12 form-floating mb-3">
-                <textarea type="text" class="form-control" id="floatingInput" style="height:10rem; resize:none;" disabled><? print($address['number_name'].' '.$address['line_1'].','.PHP_EOL.$address['line_2'].','.PHP_EOL.$address['town'].','.PHP_EOL.$address['county'].','.PHP_EOL.$address['country'].','.PHP_EOL.$address['postcode'])?></textarea>
-                <label for="floatingInput">Delivery address</label>
+            <div class="col-12">
+                <?
+                    if($invoiced_items != null) {
+                        $invoiced_items_count = count($invoiced_items);
+                        for($i=0;$i<$invoiced_items_count;$i++) {
+                            $invoiced_item = $invoiced_items[$i];
+                            $invoiced_item_id = $invoiced_item[2];
+                            if($q = DB_Query("SELECT * FROM `products` WHERE `SKU`=$invoiced_item_id AND `Active`=1 LIMIT 1")) {
+                                while($res = mysqli_fetch_array($q)) {
+                                    $currency = $res['Currency'];
+                                    $fmt = new NumberFormatter( locale_get_default()."@currency=$currency", NumberFormatter::CURRENCY );
+                                    $invoiced_item_curr = $fmt->getSymbol(NumberFormatter::CURRENCY_SYMBOL);
+                                    $invoiced_item_image = explode(',', $res['Images'])[0];
+                                    $invoiced_item_title = $res['Title'];
+                                    $invoiced_item_price = $res['RetailPrice'];
+                                    $invoiced_item_slug = $res['Slug'];
+                                    $invoiced_item_quantity = $invoiced_item[3];
+                                    $invoiced_item_total = $invoiced_item_price * $invoiced_item_quantity;
+                                    $cart_total = $cart_total + $invoiced_item_total;
+
+
+                                    $titles=$options=array();
+                                    $x = explode(';', $res['Variants']);
+                                    foreach($x as $y) {
+                                        $z = explode(':', $y);
+                                        if(isset($z[0]) && isset($z[1])) {
+                                            $ttl = $z[0];
+                                            $opt = explode(',', $z[1]);
+                                            if(isset($z[0]) && isset($z[1])) {
+                                                array_push($titles,$ttl);
+                                                array_push($options,$opt);
+                                            }
+                                        }
+                                    }
+
+                                }
+                                print('
+                                    <cart-item raw="'.$invoiced_item_id.','.$invoiced_item_quantity.','.$invoiced_items[$i][4].'" prod-id="'.$invoiced_item_id.'" prod-qty="'.$invoiced_item_quantity.'" prod-opt="'.$invoiced_item[2].'">
+                                        <div class="row border-bottom m-3 p-3 / m-md-0 p-md-2 mx-md-5 px-md-5">
+                                            <div class="row col-12 col-lg-8 pb-2 pb-lg-0">
+                                                <div class="mw-25 mw-md-10 col-3 pe-3 / col-md-3 pe-md-3">
+                                                    <picture>
+                                                        <source srcset="'.__API__.'/Images/fetch/'. $invoiced_item_image .'/jpeg/" type="image/jpeg"/>
+                                                        <source srcset="'.__API__.'/Images/fetch/'. $invoiced_item_image .'/jpg/" type="image/jpg"/>
+                                                        <source srcset="'.__API__.'/Images/fetch/'. $invoiced_item_image .'/png/" type="image/png"/>
+                                                        <source srcset="'.__API__.'/Images/fetch/'. $invoiced_item_image .'/jpx/" type="image/jpx"/>
+                                                        <img src="'.__API__.'/Images/fetch/'. $invoiced_item_image .'/webp/" type="image/webp" width="100%" height="auto">
+                                                    </picture>
+                                                </div>
+                                                <div class="col-9 col-md-8 align-items-center">
+                                                    <div class="top-50 position-relative" style="transform:translatey(-50%);">
+                                                        <p>
+                                                            <a class="link-dark" href="https://www.thecandlestick.co.uk/Boutique/product/'.$invoiced_item_slug.'">
+                                                                '.$invoiced_item_title.'
+                                                            </a>
+                                                        </p>
+                                                        <p class="font-monospace text-muted">
+                                ');
+                                for($n=0; $n<count($titles);$n++) {
+                                    $item_options = explode(':', $invoiced_item[2]);
+                                    print($titles[$n].': '.$options[$n][$item_options[$n]-1].'&nbsp;&nbsp;&nbsp;');
+                                }
+                                print('
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row align-items-center col-12 col-lg-4">
+                                                <div class="col-3">
+                                                    <div class="form-floating">
+                                                        <input class="text-center form-control border-0 bg-transparent" placeholder="Leave a comment here" id="floatingTextarea" value="'.$invoiced_item_curr.$invoiced_item_price.'" disabled>
+                                                        <label for="floatingTextarea">Price</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-12 col-lg-6">
+                                                    <div class="form-floating">
+                                                        <input class="text-center form-control border-0 bg-transparent" placeholder="Leave a comment here" id="floatingTextarea" value="'.$invoiced_item_quantity.'" disabled>
+                                                        <label for="floatingTextarea">Quantity</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-3">
+                                                    <div class="form-floating">
+                                                        <input class="text-center form-control border-0 bg-transparent" placeholder="Leave a comment here" id="floatingTextarea" value="'.$invoiced_item_curr.$invoiced_item_total.'" disabled>
+                                                        <label for="floatingTextarea">Total</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </cart-item>
+                                ');
+                            }
+                        }
+                    } else {
+                        print('
+                            <div class="border-bottom row p-3">
+                                <div class="col-12 p-3 text-center">
+                                    <span> Your cart is empty </span>
+                                </div>
+                            </div>
+                        ');
+                    }
+                ?>
             </div>
         </div>
     </section>
