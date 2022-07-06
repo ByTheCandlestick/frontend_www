f337fe305901d2c9ce1b220bd8e833e8e7139813
Robert Smith <master_schmadoodle@outlook.com>
2022-07-06 14:16:19 +0100

    Minor changes

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -13,11 +13,9 @@
             <div class="col-12 col-md-6 text-md-end">
                 <div class="row">
                     <div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
-					<div class="form-floating m-1">
                         <a href="javascript:order.refund(<?print($invoice['invoice_number'])?>);" class="btn btn-outline-primary m-1">
                             <i class="fa fa-fax"></i>
                         </a>
-					</div>
                     </div>
                 </div>
             </div>
