bf41cafb35f17b40a12ef7c4a4b61b26415fd809
Robert Smith <master_schmadoodle@outlook.com>
2022-07-07 13:05:39 +0100

    Minor changes

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -33,7 +33,7 @@
                         <textarea type="text" class="form-control" id="floatingInput" style="height:10rem; resize:none;" disabled><? print($address['number_name'].' '.$address['line_1'].','.PHP_EOL.$address['line_2'].','.PHP_EOL.$address['town'].','.PHP_EOL.$address['county'].','.PHP_EOL.$address['country'].','.PHP_EOL.$address['postcode'])?></textarea>
                         <label for="floatingInput">Delivery address</label>
                     </div>
-                    </div class="row">
+                    <div class="row">
                         <div class="col-12 col-md-6 col-lg-6 form-floating mb-3">
                             <input type="text" class="form-control" id="floatingInput" value="<? print($invoice['name'])?>" disabled>
                             <label for="floatingInput">Deliver to name</label>
