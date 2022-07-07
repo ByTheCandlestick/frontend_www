9c93200598e6ad58f4319dfd89762a2aa7e1db96
Robert Smith <master_schmadoodle@outlook.com>
2022-07-07 13:06:07 +0100

    Minor changes

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -33,18 +33,20 @@
                         <textarea type="text" class="form-control" id="floatingInput" style="height:10rem; resize:none;" disabled><? print($address['number_name'].' '.$address['line_1'].','.PHP_EOL.$address['line_2'].','.PHP_EOL.$address['town'].','.PHP_EOL.$address['county'].','.PHP_EOL.$address['country'].','.PHP_EOL.$address['postcode'])?></textarea>
                         <label for="floatingInput">Delivery address</label>
                     </div>
-                    <div class="row">
-                        <div class="col-12 col-md-6 col-lg-6 form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingInput" value="<? print($invoice['name'])?>" disabled>
-                            <label for="floatingInput">Deliver to name</label>
-                        </div>
-                        <div class="col-12 col-md-6 col-lg-6 form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingInput" value="<? print($delivery['Name'])?>" disabled>
-                            <label for="floatingInput">Delivery by</label>
+                    <div class="col-8">
+                        <div class="row">
+                            <div class="col-12 col-md-6 col-lg-6 form-floating mb-3">
+                                <input type="text" class="form-control" id="floatingInput" value="<? print($invoice['name'])?>" disabled>
+                                <label for="floatingInput">Deliver to name</label>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-6 form-floating mb-3">
+                                <input type="text" class="form-control" id="floatingInput" value="<? print($delivery['Name'])?>" disabled>
+                                <label for="floatingInput">Delivery by</label>
+                            </div>
                         </div>
                     </div>
                 </div>
-                </div>
+            </div>
 
             <div class="col-12">
                 <h5>Taxonomy</h5>
