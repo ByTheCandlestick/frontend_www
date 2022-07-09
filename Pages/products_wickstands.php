741c87916234897e0eeef94f638645c3cca71e01
Robert Smith <master_schmadoodle@outlook.com>
2022-07-09 14:02:07 +0100

    Minor changes

--- Pages/products_wickstands.php
+++ Pages/products_wickstands.php
@@ -11,7 +11,7 @@
 		<div class="col-12 col-md-6 text-md-end">
 			<div class="row">
 				<div class="col-12 col-lg-6 d-block d-md-flex justify-content-end align-items-center p-0">
-					<a href="/Products/Wick Stand/New/" class="btn btn-outline-primary m-1">
+					<a href="/Products/WickStand/New/" class="btn btn-outline-primary m-1">
 						<i class="fa fa-plus"></i>
 					</a>
 				</div>
@@ -53,7 +53,7 @@
 									<td>'.$row['Price (ea)'].'</td>
 									<td>'.$row['Active'].'</td>
 									<td>
-										<a href="/Products/Wick Stand/'.$row['ID'].'">
+										<a href="/Products/WickStand/'.$row['ID'].'">
 											<i class="fa fa-pencil"></i>
 										</a>
 									</td>
