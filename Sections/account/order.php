9d6eb1cdb231181d9d9f87fd279b149f95397a73
RSmith <master_schmadoodle@outlook.com>
2022-07-09 22:42:58 +0100

    Minor changes

--- Sections/account/order.php
+++ Sections/account/order.php
@@ -141,6 +141,7 @@
 				if($q = DB_Query("SELECT * FROM `products` WHERE `SKU`=$order_item_id AND `active`=1 LIMIT 1")) {
 					while($res = mysqli_fetch_array($q)) {
 						$order_item_image  = explode(',', $res['Images'])[0];
+						$order_item_slug = $res['Slug'];
 						$order_item_title = $res['Title'];
 						$titles=$options=array();
 
