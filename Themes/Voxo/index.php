947c798fd9fe07dbc39c91acdd9a657cc493e413
RSmith <master_schmadoodle@outlook.com>
2022-07-16 10:40:13 +0100

    Minor changes

--- Themes/Voxo/index.php
+++ Themes/Voxo/index.php
@@ -140,7 +140,7 @@
 						<nav id="navbar" class="navbar">
 							<ul>
 								<?
-									$items = DB_Query(sprintf("SELECT * FROM `page_layouts` WHERE `Active`=1 AND `menu_item`=1 AND `ID`=%i ORDER BY `menu_order` ASC", $website_info['ID']));
+									$items = DB_Query(sprintf("SELECT * FROM `page_layouts` WHERE `Active`=1 AND `menu_item`=1 AND `ID`=%s ORDER BY `menu_order` ASC", $website_info['ID']));
 									foreach($items as $item) {
 										if($item['page_url'] == QS_PAGE) {
 											$link = '#';
