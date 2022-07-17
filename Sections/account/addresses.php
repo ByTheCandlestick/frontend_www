709046b9a0aee13934138aa5fc15ea140e83055c
Robert Smith <master_schmadoodle@outlook.com>
2022-07-17 14:17:10 +0100

    Minor changes to .htaccess

--- Sections/account/addresses.php
+++ Sections/account/addresses.php
@@ -6,11 +6,12 @@
 				$l1 = $item['number_name'].' '.$item['line_1'];
 				$l2 = $item['line_2']; $l3 = $item['town'];
 				$l4 = $item['county']; $l5 = $item['country'];
-				$l6 = $item['postcode'];
+				$l6 = $item['postcode']; $name = $item['Name'];
 				print("
 					<div class=\"col-6 col-md-4 col-lg-3\">
 						<div class=\"card\">
 							<div class=\"card-body\">
+							<h5 class=\"card-title\">$name</h5>
 								<p class=\"card-text\">
 									$l1,<br>
 									$l2,<br>
