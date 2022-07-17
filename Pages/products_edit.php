d0e1390ab816c47be22c5f11f3f890c4d56996f4
Robert Smith <master_schmadoodle@outlook.com>
2022-07-17 15:43:04 +0100

    Minor cahnges to order details

--- Pages/products_edit.php
+++ Pages/products_edit.php
@@ -10,13 +10,16 @@
 				<p><? print($prod['SKU'])?> - <? print($prod['Title'])?></p>
 			</div>
 			<div class="col-12 col-md-6 text-md-end">
-			<div class="row">
-				<div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
-					<a href="javascript:product.update(<?print(QS)?>);" class="btn btn-outline-primary m-1">
-						<i class="fa fa-save"></i>
-					</a>
+				<div class="row">
+					<div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
+						<a href="javascript:product.delete(<?print(QS)?>);" class="btn btn-outline-primary m-1">
+							<i class="fa fa-trash-alt"></i>
+						</a>
+						<a href="javascript:product.update(<?print(QS)?>);" class="btn btn-outline-primary m-1">
+							<i class="fa fa-save"></i>
+						</a>
+					</div>
 				</div>
-			</div>
 			</div>
 		</div>
 		<hr>
