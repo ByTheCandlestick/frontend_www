8c3cd216acc3d958de7e1d726785f59ea4cc9121
Robert Smith <master_schmadoodle@outlook.com>
2022-07-17 13:39:48 +0100

    Minor changes to .htaccess

--- Sections/account/addresses.php
+++ Sections/account/addresses.php
@@ -13,7 +13,7 @@
 							<div class=\"card-body\">
 								<h5 class=\"card-title\">Name:</h5>
 								<p class=\"card-text\">
-									$l1\n$l2\n$l3\n$l4\n$l5\n$l6
+									$l1\n, $l2\n, $l3\n, $l4\n, $l5\n, $l6
 								</p>
 								<a href=\"#\" class=\"btn btn-primary\">Go somewhere</a>
 							</div>
