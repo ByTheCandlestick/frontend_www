d2e027d126141edcec3a284f2be37df6f4f61640
Robert Smith <master_schmadoodle@outlook.com>
2022-07-17 13:29:19 +0100

    Minor changes to .htaccess

---
+++ Sections/account/addresses.php
@@ -0,0 +1,29 @@
+<?
+	$items = DB_Query(sprintf("SELECT * FROM `Users_address` WHERE `UID`=%s ORDER BY `ID` ASC", $userdata['ID']));
+	foreach($items as $item) {
+		$l1 = $item['number'].' '.$item['line_1'];
+		$l2 = $item['line_2'];
+		$l3 = $item['town'];
+		$l4 = $item['county'];
+		$l5 = $item['country'];
+		$l6 = $item['postcode'];
+		print("
+			<div class=\"col-sm-6\">
+				<div class=\"card\">
+					<div class=\"card-body\">
+						<h5 class=\"card-title\">Name:</h5>
+						<p class=\"card-text\">
+							$l1
+							$l2
+							$l3
+							$l4
+							$l5
+							$l6
+						</p>
+						<a href=\"#\" class=\"btn btn-primary\">Go somewhere</a>
+					</div>
+				</div>
+			</div>
+		")
+	}
+?>
