16a5e898c1e478d8ceba63528e4131907db9fe39
Robert Smith <master_schmadoodle@outlook.com>
2022-07-23 11:52:24 +0100

    Minor changes to order details

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -247,14 +247,14 @@
 						<div class="col-6">
 							<div class="form-floating mb-3 input-group">
 								<span class="input-group-text" id="">£</span>
-								<input type="text" class="form-control" id="floatingInput" value="0" min="0" max="<?print($depositAfterRefunds)?>" step=".01" name="refundCurrValue" onKeyUp="orders.refunds.check();" onChange="misc.setTwoDecimal();">
+								<input type="number" class="form-control" id="floatingInput" value="0" min="0" max="<?print($depositAfterRefunds)?>" step=".01" name="refundCurrValue" onKeyUp="orders.refunds.check();" onChange="misc.setTwoDecimal();">
 								<label for="floatingInput" class="ps-5">Value</label>
 							</div>
 						</div>
 						<div class="col-6">
 							<div class="form-floating mb-3 input-group">
 								<span class="input-group-text" id="">£</span>
-								<input type="text" class="form-control" id="floatingInput" value="<?print($depositAfterRefunds)?>" step=".01" name="refundMaxValue" disabled>
+								<input type="number" class="form-control" id="floatingInput" value="<?print($depositAfterRefunds)?>" step=".01" name="refundMaxValue" disabled>
 								<label for="floatingInput" class="ps-5">Max</label>
 							</div>
 						</div>
