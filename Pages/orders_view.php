74c54592d2b6fbb067bea74c5ec086919d363284
Robert Smith <master_schmadoodle@outlook.com>
2022-07-23 11:55:39 +0100

    Minor changes to order details

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -5,7 +5,8 @@
 		$address = mysqli_fetch_assoc(DB_Query(sprintf("SELECT * FROM `Users_address` WHERE `id`=%s", $invoice['Billing address'])));
 		$delivery = mysqli_fetch_assoc(DB_Query(sprintf("SELECT * FROM `products_shippings` WHERE `id`=%s", $invoice['Shipping to'])));
 		$refunds = mysqli_fetch_row(DB_Query(sprintf("SELECT * FROM `Sales - refunds` WHERE `id`=%s", $invoice['Shipping to'])));
-		$depositAfterRefunds = (($invoice['Subtotal']-$invoice['Processing Fees'])-$invoice['tax'])-$refunds;
+		$incomeAfterRefunds = (($invoice['Deposit'] - $invoice['Processing Fees']) - $invoice['tax']) - $refunds;
+		$depositAfterRefunds = $invoice['Deposit'] - $refunds;
 ?>
 	<section>
 		<!-- Section Header -->
