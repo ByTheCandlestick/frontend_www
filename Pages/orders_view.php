b1a9178ffe74d6d658e0972bd6e1a89cb7022766
Robert Smith <master_schmadoodle@outlook.com>
2022-07-23 11:00:20 +0100

    Minor cahnges to order details

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -4,7 +4,7 @@
 		$invoice = mysqli_fetch_assoc($query);
 		$address = mysqli_fetch_assoc(DB_Query(sprintf("SELECT * FROM `Users_address` WHERE `id`=%s", $invoice['Billing address'])));
 		$delivery = mysqli_fetch_assoc(DB_Query(sprintf("SELECT * FROM `products_shippings` WHERE `id`=%s", $invoice['Shipping to'])));
-		$refunds = '0.00';
+		$refunds = mysqli_fetch_row(DB_Query(sprintf("SELECT * FROM `Sales - refunds` WHERE `id`=%s", $invoice['Shipping to'])));
 ?>
 	<section>
 		<!-- Section Header -->
