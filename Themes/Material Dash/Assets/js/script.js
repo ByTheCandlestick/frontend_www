6324248885982e0b3c3cb43484e0dd9dc99db5a8
Robert Smith <master_schmadoodle@outlook.com>
2022-07-23 12:58:13 +0100

    Minor changes to order details

--- Themes/Material Dash/Assets/js/script.js
+++ Themes/Material Dash/Assets/js/script.js
@@ -991,7 +991,7 @@ $(document).ready(() => {
 				check() {
 					var currValue = $('input[name=refundCurrValue]'),
 						maxValue = $('input[name=refundMaxValue]');
-					if(currValue.val().match(/\./).length == 1) {
+					if((currValue.val()).match(/\./).length == 1) {
 						val = currValue.val().match(/([0-9]+)\.([0-9]+)/);
 						if(val[2] > 9) {
 							currValue.val(parseFloat(currValue.val()).toFixed(2));
