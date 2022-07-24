481a3003b3bf6755b3dd79af4358793d10032388
RSmith <master_schmadoodle@outlook.com>
2022-07-24 21:39:02 +0100

    Minor changes related to refund

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -4,10 +4,10 @@
 		$invoice = mysqli_fetch_assoc($query);
 		$address = mysqli_fetch_assoc(DB_Query(sprintf("SELECT * FROM `Users_address` WHERE `id`=%s", $invoice['Billing address'])));
 		$delivery = mysqli_fetch_assoc(DB_Query(sprintf("SELECT * FROM `products_shippings` WHERE `id`=%s", $invoice['Shipping to'])));
+		$refundsValue = 0;
 		$refunds = DB_Query(sprintf("SELECT * FROM `Sales - refunds` WHERE `Charge ID`='%s'", $invoice['Charge ID']));
 		$allRefunds = mysqli_fetch_assoc($refunds);
-		$refundsValue = 0;
-		foreach($allRefunds as $refund) {
+		foreach($refund = mysqli_fetch_assoc($refunds)) {
 			$refundsValue += $refund['Subtotal'];
 		}
 		$income = ($invoice['Deposit'] - $invoice['Processing Fees']) - $invoice['tax'];
