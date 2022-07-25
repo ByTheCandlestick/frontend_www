bddb977fa06491b256e878a071992ddb85a57cd6
RSmith <master_schmadoodle@outlook.com>
2022-07-26 00:24:55 +0100

    Minor changes

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -4,7 +4,7 @@
 		$invoice = mysqli_fetch_assoc($query);
 		$address = mysqli_fetch_assoc(DB_Query(sprintf("SELECT * FROM `Users_address` WHERE `id`=%s", $invoice['Billing address'])));
 		$delivery = mysqli_fetch_assoc(DB_Query(sprintf("SELECT * FROM `products_shippings` WHERE `id`=%s", $invoice['Shipping by'])));
-		$refunds = DB_Query(sprintf("SELECT * FROM `Transactions` WHERE `Charge ID`='%s' AND `Type`='Refund' ORDER BY `Created` ASC", $invoice['ID']));
+		$refunds = DB_Query(sprintf("SELECT * FROM `Transactions` WHERE `Charge ID`='%s' AND `Type`='Refund' ORDER BY `Created` ASC", $invoice['Charge ID']));
 		$refundsValue = 0;
 		while($refund = mysqli_fetch_assoc($refunds)) {
 			$refundsValue += $refund['Subtotal'];
