327129f200d130740f8afe6902c64dbb86f6aafa
RSmith <master_schmadoodle@outlook.com>
2022-07-26 00:04:11 +0100

    Minor changes

--- Pages/transactions.php
+++ Pages/transactions.php
@@ -1,6 +1,6 @@
 <?
     $transactions = array();
-    $q = DB_Query("SELECT * FROM `Transactions` LIMIT 50");
+    $q = DB_Query("SELECT * FROM `Transactions` ORDER BY `Modified` ASC LIMIT 50");
 	while($transaction = mysqli_fetch_assoc($q)) { array_push($transactions, $transaction); }
 ?>
 <section>
