f365a59a0b2b569a68952f5ff07b8207b7262087
Robert Smith <master_schmadoodle@outlook.com>
2022-07-25 12:59:15 +0100

    Minor cahnges to transactions page

--- Pages/transactions.php
+++ Pages/transactions.php
@@ -1,11 +1,9 @@
 <?
     $transactions = array();
-	while($order = mysqli_fetch_assoc(DB_Query("SELECT * FROM `Transactions - orders` LIMIT 50"))) {
-        $transactions[$order['Created']] = $order;
-    }
-	while($refund = mysqli_fetch_assoc(DB_Query("SELECT * FROM `Transactions - refunds` LIMIT 50"))) {
-        $transactions[$refund['Created']] = $refund;
-    }
+    $q = DB_Query("SELECT * FROM `Transactions - orders` LIMIT 50");
+	while($order = mysqli_fetch_assoc($q)) { array_push($transactions, $order); }
+    $q = DB_Query("SELECT * FROM `Transactions - refunds` LIMIT 50");
+	while($refund = mysqli_fetch_assoc()) { array_push($transactions, $refund); }
     $createdDate = array_column($transactions, 'Created');
     print_r(array_multisort($createdDate, SORT_DESC, $transactions));
 ?>
