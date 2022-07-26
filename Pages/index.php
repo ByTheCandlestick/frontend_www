77a560cc6c9ce562910d7aba4a343ba2d89332c7
Robert Smith <master_schmadoodle@outlook.com>
2022-07-26 12:28:59 +0100

    Minor changes to SQL DB names

--- Pages/index.php
+++ Pages/index.php
@@ -7,6 +7,8 @@
 	for ($i=count($days_b)-1; $i>=0; $i--) {
 		array_push($days, $days_b[$i]);
 	}
+	echo $currYear = date(strtotime());
+	echo $lastYear = date(strtotime('last year'));
 ?>
 <section>
 	<!-- Section Header -->
