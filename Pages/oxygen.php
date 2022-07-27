2e1e96a6ac40e8ea50ea49928f5e9a476149d31d
Robert Smith <master_schmadoodle@outlook.com>
2022-07-27 14:08:45 +0100

    Minor changes to oxygen builder

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -54,7 +54,7 @@
 								if($type != null && $type != $row['section_type']) {
 									print('
 										</div>
-										<div class="accordion-item container row builderElements">
+										<div class="accordion-item container row templateBuilderElements">
 											<h2 class="accordion-header p-0" id="headingOne">
 												<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#'.$row['section_type'].'" aria-expanded="true" aria-controls="'.$row['section_type'].'">
 													'.$row['section_type'].'
@@ -99,7 +99,7 @@
 	dragula([
 		document.querySelector('.templateBuilder'),
 		document.querySelector('.templateBuilderGrid'),
-		document.querySelector('.builderElements')
+		document.querySelector('.templateBuilderElements')
 	], {
 		isContainer: function (el) {
 			return false; // only elements in drake.containers will be taken into account
