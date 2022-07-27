310da82f1b1aa146ead00b9ba26edc1149c63392
Robert Smith <master_schmadoodle@outlook.com>
2022-07-27 13:20:50 +0100

    Minor changes to dashboard

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -79,7 +79,7 @@
 		<div class="col-lg-9 h-100" style="border: 2px solid var(--main-color);border-radius: 15px;">
 			<?
 				if($page['section_ids'] != "") {
-					printSectionTemplates($sections, $page['']);
+					printSectionTemplates($sections, $page['section_ids']);
 				} else {
 					print("Drag an element from the left hand side to start building the website!");
 				}
