e64cde20410e22a9827081848e2350d206cd941c
Robert Smith <master_schmadoodle@outlook.com>
2022-07-27 13:58:52 +0100

    Minor changes to oxygen builder

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -54,7 +54,7 @@
 								if($type != null && $type != $row['section_type']) {
 									print('
 										</div>
-										<div class="accordion-item container row">
+										<div class="accordion-item container row builderElements">
 											<h2 class="accordion-header p-0" id="headingOne">
 												<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#'.$row['section_type'].'" aria-expanded="true" aria-controls="'.$row['section_type'].'">
 													'.$row['section_type'].'
