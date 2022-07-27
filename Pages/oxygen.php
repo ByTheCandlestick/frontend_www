90c4f33c4e0d7d14c196a560395506cb0d01e25b
Robert Smith <master_schmadoodle@outlook.com>
2022-07-27 14:14:44 +0100

    Minor changes to oxygen builder

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -44,7 +44,7 @@
 		</style>
 		<div class="col-lg-3">
 			<div class="accordion accordion-flush" id="SectionElements">
-				<div class="accordion-item container row">
+				<div class="accordion-item container row templateBuilderElements">
 					<?
 						$query = DB_Query("SELECT * FROM `page_sections` ORDER BY `section_type`");
 						$type = null;
@@ -102,26 +102,26 @@
 		document.querySelector('.templateBuilderElements')
 	], {
 		isContainer: function (el) {
-			return false; // only elements in drake.containers will be taken into account
+			return false;				// only elements in drake.containers will be taken into account
 		},
 		moves: function (el, source, handle, sibling) {
-			return true; // elements are always draggable by default
+			return true;				// elements are always draggable by default
 		},
 		accepts: function (el, target, source, sibling) {
-			return true; // elements can be dropped in any of the `containers` by default
+			return true;				// elements can be dropped in any of the `containers` by default
 		},
 		invalid: function (el, handle) {
-			return false; // don't prevent any drags from initiating by default
+			return false;				// don't prevent any drags from initiating by default
 		},
-		direction: 'vertical',             // Y axis is considered when determining where an element would be dropped
-		copy: true,                       // elements are moved by default, not copied
-		copySortSource: false,             // elements in copy-source containers can be reordered
-		revertOnSpill: true,              // spilling will put the element back where it was dragged from, if this is true
-		removeOnSpill: false,              // spilling will `.remove` the element, if this is true
-		mirrorContainer: document.body,    // set the element that gets mirror elements appended
-		ignoreInputTextSelection: true,     // allows users to select input text, see details below
-		slideFactorX: 0,               // allows users to select the amount of movement on the X axis before it is considered a drag instead of a click
-		slideFactorY: 0,               // allows users to select the amount of movement on the Y axis before it is considered a drag instead of a click
+		direction: 'vertical',			// Y axis is considered when determining where an element would be dropped
+		copy: false,					// elements are moved by default, not copied
+		copySortSource: false,			// elements in copy-source containers can be reordered
+		revertOnSpill: true,			// spilling will put the element back where it was dragged from, if this is true
+		removeOnSpill: false,			// spilling will `.remove` the element, if this is true
+		mirrorContainer: document.body,	// set the element that gets mirror elements appended
+		ignoreInputTextSelection: true,	// allows users to select input text, see details below
+		slideFactorX: 0,				// allows users to select the amount of movement on the X axis before it is considered a drag instead of a click
+		slideFactorY: 0,				// allows users to select the amount of movement on the Y axis before it is considered a drag instead of a click
 	});
 </script>
 <?
