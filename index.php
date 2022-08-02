98489de24a64995a742e69abb8fd045e23969bda
RSmith <master_schmadoodle@outlook.com>
2022-08-02 13:56:07 +0100

    Minor changes to subdomain checking PHP

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	require_once('./Classes/vars.php');
 	// Set www. as subdomain if no subdomain exists
 		if($website_info['Subdomain'] == null) {
-			header('Location: '($_SERVER['HTTPS']?'https://www.':'http://www.').$_SERVER['HTTP_HOST'].'/');
+			header('Location: '.($_SERVER['HTTPS']?'https://www.':'http://www.').$_SERVER['HTTP_HOST'].'/');
 		}
 	//
 	if(isset($_GET['file']) && isset($_GET['ext'])) {
