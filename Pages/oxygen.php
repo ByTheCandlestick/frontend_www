8d1e98029548334b04c03d4fbc3e2c0f09e32163
Robert Smith <master_schmadoodle@outlook.com>
2022-08-03 17:29:23 +0100

    Minor changes to oxygen editor - Removed printSectionTemplate Function

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -98,6 +98,8 @@
 						}
 					}
 				?>
+				</div>
+			</div>
 		</div>
 		
 		<div class="col-lg-9 h-100 templateBuilder" style="border: 2px solid var(--main-color);border-radius: 15px;"> <!-- Template -->
