4eaf2e6509ef73fe85dba418ec0d642590372776
RSmith <master_schmadoodle@outlook.com>
2022-08-03 23:08:43 +0100

    Minor changes to subdomain checking PHP

--- index.php
+++ index.php
@@ -6,24 +6,25 @@
 		if($website_info['Subdomain'] == null) {
 			header('Location: '.($_SERVER['HTTPS']?'https://www.':'http://www.').$_SERVER['HTTP_HOST'].'/');
 		}
-	//
-	if(isset($_GET['file']) && isset($_GET['ext'])) {
-		if(file_exists($path = sprintf("%s/Themes/%s/Assets/%s/%s.%s", __ROOT__, __THEME__, $_GET['ext'], $_GET['file'], $_GET['ext']))) {
-			header('Content-Type: text/'.$_GET['ext']);
-			print(file_get_contents($path));
-		} else {
-			header('Content-Type: text/json');
-			print(json_encode(array("error"=>"File not found")));
-		}
-	} else {
-		if($website_info['Maintenance']) {
-			require_once('./Pages/maintenance.php');
-		} else {
-			if(getThemepage(false)) {
-				getThemepage(true);
+	// Display file if get[file] and get[ext] is set, else display theme index
+		if(isset($_GET['file']) && isset($_GET['ext'])) {
+			if(file_exists($path = sprintf("%s/Themes/%s/Assets/%s/%s.%s", __ROOT__, __THEME__, $_GET['ext'], $_GET['file'], $_GET['ext']))) {
+				header('Content-Type: text/'.$_GET['ext']);
+				print(file_get_contents($path));
 			} else {
-				print('The website you are looking for does not exist');
+				header('Content-Type: text/json');
+				print(json_encode(array("error"=>"File not found")));
+			}
+		} else {
+			if($website_info['Maintenance']) {
+				require_once('./Pages/maintenance.php');
+			} else {
+				if(getThemepage(false)) {
+					getThemepage(true);
+				} else {
+					print('The website you are looking for does not exist');
+				}
 			}
 		}
-	}
+	//
 ?>
