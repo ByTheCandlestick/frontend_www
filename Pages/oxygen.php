054c05002b1601fce63953553fdb8a73c7281cfd
RSmith <master_schmadoodle@outlook.com>
2022-08-03 23:43:40 +0100

    Minor changes to oxygen website builder

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -45,14 +45,14 @@
 					border-radius: 15px;
 					padding: unset;
 				}
-				.element {
+				.templateElements {
 					background: var(--section);
 					border-radius: 15px;
 					margin: 10px;
 					padding: 10px;
 				}
-				.accordion-item .element input,
-				.accordion-item .element#Columns div {
+				.accordion-item .templateElements input,
+				.accordion-item .templateElements#Columns div {
 					display: none;
 				}
 			</style>
@@ -64,14 +64,14 @@
 								<h5>Columns</h5>
 							</button>
 						</h2>
-						<div id="Columns" class="accordion-collapse collapse element" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
+						<div id="Columns" class="accordion-collapse collapse templateElements" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
 							<div class="accordion-body">
 								<h6>2</h6>
 								<div class="templateGrid col-6"></div>
 								<div class="templateGrid col-6"></div>
 							</div>
 						</div>
-						<div id="Columns" class="accordion-collapse collapse element" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
+						<div id="Columns" class="accordion-collapse collapse templateElements" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
 							<div class="accordion-body">
 								<h6>3</h6>
 								<div class="templateGrid col-4"></div>
@@ -79,7 +79,7 @@
 								<div class="templateGrid col-4"></div>
 							</div>
 						</div>
-						<div id="Columns" class="accordion-collapse collapse element" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
+						<div id="Columns" class="accordion-collapse collapse templateElements" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
 							<div class="accordion-body">
 								<h6>4</h6>
 								<div class="templateGrid col-3"></div>
@@ -88,7 +88,7 @@
 								<div class="templateGrid col-3"></div>
 							</div>
 						</div>
-						<div id="Columns" class="accordion-collapse collapse element" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
+						<div id="Columns" class="accordion-collapse collapse templateElements" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
 							<div class="accordion-body">
 								<h6>5</h6>
 								<div class="templateGrid col-2 offset-1"></div>
@@ -114,7 +114,7 @@
 											</h2>
 									');
 									print('
-											<div id="'.$row['section_type'].'" class="accordion-collapse collapse col-12 col-md-5 element" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
+											<div id="'.$row['section_type'].'" class="accordion-collapse collapse col-12 col-md-5 templateElements" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
 												<div class="accordion-body">
 													<h6>'.$row['short_description'].'</h6>
 													<input type="text" value="">
