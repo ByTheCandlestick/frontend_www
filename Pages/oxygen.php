4bcca296681aa78d4950a19f8e89c8a885b893de
Robert Smith <master_schmadoodle@outlook.com>
2022-08-03 15:43:57 +0100

    Minor changes

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -52,7 +52,7 @@
 							$sections[$row['id']] = $row;
 							if($type == null) {
 								print('
-									<div class="accordion-item container row templateBuilderElements">
+									<div class="accordion-item container row templateBuilderElements" (dragover)="changeMethod()">
 										<h2 class="accordion-header p-0" id="headingOne">
 											<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#'.$row['section_type'].'" aria-expanded="true" aria-controls="'.$row['section_type'].'">
 												'.$row['section_type'].'
@@ -63,7 +63,7 @@
 							if($type != null && $type != $row['section_type']) {
 								print('
 									</div>
-									<div class="accordion-item container row templateBuilderElements">
+									<div class="accordion-item container row templateBuilderElements" (dragover)="changeMethod()">
 										<h2 class="accordion-header p-0" id="headingOne">
 											<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#'.$row['section_type'].'" aria-expanded="true" aria-controls="'.$row['section_type'].'">
 												'.$row['section_type'].'
@@ -133,6 +133,11 @@
 		slideFactorX: 0,				// allows users to select the amount of movement on the X axis before it is considered a drag instead of a click
 		slideFactorY: 0,				// allows users to select the amount of movement on the Y axis before it is considered a drag instead of a click
 	});
+	changeMethod(){
+		// here write logic for change option for target.
+		this.copyOption = false;
+	}
+
 </script>
 <?
 	} else {
