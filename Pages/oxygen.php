caa5c829e217cd6057b058355a11faddb2b73d29
Robert Smith <master_schmadoodle@outlook.com>
2022-08-03 17:42:47 +0100

    Minor changes to oxygen editor - Removed printSectionTemplate Function

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -73,24 +73,24 @@
 							while($row = mysqli_fetch_array($query)) {
 								$sections[$row['id']] = $row;
 								if($type != $row['section_type']) print('
-										<div class="accordion-item container row templateBuilderElements">
-											<h2 class="accordion-header p-0" id="headingOne">
-												<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#'.$row['section_type'].'" aria-expanded="true" aria-controls="'.$row['section_type'].'">
-													'.$row['section_type'].'
-												</button>
-											</h2>
-									');
-								print('
-									<div id="'.$row['section_type'].'" class="accordion-collapse collapse col-12 col-md-5 element" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
-										<div class="accordion-body">
-											'.$row['short_description'].'
-										</div>
-									</div>
-								');
-								if($type != $row['section_type']) print('
-										</div>
+									<div class="accordion-item container row templateBuilderElements">
+										<h2 class="accordion-header p-0" id="headingOne">
+											<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#'.$row['section_type'].'" aria-expanded="true" aria-controls="'.$row['section_type'].'">
+												'.$row['section_type'].'
+											</button>
+										</h2>
 								');
 								$type = $row['section_type'];
+								print('
+										<div id="'.$row['section_type'].'" class="accordion-collapse collapse col-12 col-md-5 element" aria-labelledby="headingOne" data-bs-parent="#SectionElements">
+											<div class="accordion-body">
+												'.$row['short_description'].'
+											</div>
+										</div>
+								');
+								if($type != $row['section_type']) print('
+									</div>
+								');
 							}
 						}
 					?>
