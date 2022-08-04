1587e1394e6ab29b30bc5c76f6474c00b630a937
RSmith <master_schmadoodle@outlook.com>
2022-08-04 01:41:08 +0100

    Minor changes to oxygen website builder

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -228,7 +228,7 @@ if(QS_SUBPAGE != "") {
 			return source.classList.contains('templateElement');
 		},
 		accepts: function (el, target, source, sibling) {
-			return console.log(target.classList.contains('dragulaContainer'));
+			return target.classList.contains('dragulaContainer');
 		},
 		invalid: function (el, handle) {
 			return false // source === document.querySelector('.dragDisabled')
