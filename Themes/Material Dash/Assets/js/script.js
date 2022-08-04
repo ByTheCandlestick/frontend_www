b3273c0adc1f5ad5ae8c84a270a148c08ab342a3
RSmith <master_schmadoodle@outlook.com>
2022-08-04 22:45:40 +0100

    Minor changes to oxygen website builder

--- Themes/Material Dash/Assets/js/script.js
+++ Themes/Material Dash/Assets/js/script.js
@@ -359,7 +359,8 @@ $(document).ready(() => {
 						} else {
 							elementIds.push($(this).attr('element-id'))
 						}
-					})
+					});
+					console.log(elementIds.join(','))
 					for(let i = 0; i < elementIds.length; i++) {
 						if(elementString.endsWith(';') || elementIds[i].startsWith('#') || elementString == "") {
 							elementString += elementIds[i];
