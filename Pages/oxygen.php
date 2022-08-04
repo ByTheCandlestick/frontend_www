4836ca0f31fc130b69560f01ded8edef62c96d08
RSmith <master_schmadoodle@outlook.com>
2022-08-04 23:01:38 +0100

    Minor changes to oxygen website builder

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -339,7 +339,7 @@ if(QS_SUBPAGE != "") {
 				elementIds.push($(this).attr('element-id'))
 			}
 		});
-		console.log(elementIds.join(','));
+		console.log(elementString+elementIds.join(','));
 /*
 		for(let i = 0; i < elementIds.length; i++) {
 			if(elementString.endsWith('|') || elementIds[i].startsWith('#') || elementString == "") {
