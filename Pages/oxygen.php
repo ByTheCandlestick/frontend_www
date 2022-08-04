3000e12af2c706cd4f204c9bf6491caf6e0590e1
Robert Smith <master_schmadoodle@outlook.com>
2022-08-04 16:15:15 +0100

    Minor changes to oxygen editor

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -298,11 +298,10 @@ if(QS_SUBPAGE != "") {
 			return true;
 		},
 		copy: function(el, source) {
-			$('.templateBase').find('.templateElement#'+$(el).attr('id')).removeAttr('id');
+			$('.templateBase').find('.templateElement#'+$(el).attr('data-bs-parent')).removeAttr('data-bs-parent');
 			return el.parentNode.classList.contains('dragulaCopy');
 		},
 		accepts: function (el, target, source, sibling) {
-			console.log($(target));
 			return target.classList.contains('dragulaContainer');
 		},
 		invalid: function (el, handle) {
