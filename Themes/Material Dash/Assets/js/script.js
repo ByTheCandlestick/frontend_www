924ba2817d9f705d2396b780c5771d9b042cd148
RSmith <master_schmadoodle@outlook.com>
2022-08-04 23:20:25 +0100

    Minor changes to oxygen website builder

--- Themes/Material Dash/Assets/js/script.js
+++ Themes/Material Dash/Assets/js/script.js
@@ -355,6 +355,7 @@ $(document).ready(() => {
 					elementIds = [];
 					elementString = "#12;";
 					$('.templateBase .templateGrid, .templateBase .templateElement').each(function() {
+						console.log($(this).find('input').val());
 						if($(this).find('input')>0 && $(this).find('input').val() != '') {
 							elementIds.push($(this).attr('element-id')+':'+$(this).find('input').val())
 						} else {
