b7660353df10c9ebed5e890e63fce9e5bb16300e
RSmith <master_schmadoodle@outlook.com>
2022-08-04 19:22:05 +0100

    Minor changes to oxygen website builder

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -95,7 +95,7 @@ if(QS_SUBPAGE != "") {
 					<div id="columns" class="accordion-collapse collapse templateElementGrid" data-bs-parent="#SectionElements">
 						<div class="accordion-body row position-relative">
 							<div class="templateElementMenu">
-								<a onclick="$(this).closest('.templateElement, .templateElementGrid').remove()">
+								<a onclick="$(this).closest('.templateElement, .templateElementGrid').remove('explode', 500)">
 									<i class="fas fa-trash-alt"></i>
 								</a>
 							</div>
@@ -111,7 +111,7 @@ if(QS_SUBPAGE != "") {
 					<div id="columns" class="accordion-collapse collapse templateElementGrid" data-bs-parent="#SectionElements">
 						<div class="accordion-body row position-relative">
 							<div class="templateElementMenu">
-								<a onclick="$(this).closest('.templateElement, .templateElementGrid').remove()">
+								<a onclick="$(this).closest('.templateElement, .templateElementGrid').remove('explode', 500)">
 									<i class="fas fa-trash-alt"></i>
 								</a>
 							</div>
@@ -130,7 +130,7 @@ if(QS_SUBPAGE != "") {
 					<div id="columns" class="accordion-collapse collapse templateElementGrid" data-bs-parent="#SectionElements">
 						<div class="accordion-body row position-relative">
 							<div class="templateElementMenu">
-								<a onclick="$(this).closest('.templateElement, .templateElementGrid').remove()">
+								<a onclick="$(this).closest('.templateElement, .templateElementGrid').remove('explode', 500)">
 									<i class="fas fa-trash-alt"></i>
 								</a>
 							</div>
@@ -152,7 +152,7 @@ if(QS_SUBPAGE != "") {
 					<div id="columns" class="accordion-collapse collapse templateElementGrid" data-bs-parent="#SectionElements">
 						<div class="accordion-body row position-relative">
 							<div class="templateElementMenu">
-								<a onclick="$(this).closest('.templateElement, .templateElementGrid').remove()">
+								<a onclick="$(this).closest('.templateElement, .templateElementGrid').remove('explode', 500)">
 									<i class="fas fa-trash-alt"></i>
 								</a>
 							</div>
@@ -177,7 +177,7 @@ if(QS_SUBPAGE != "") {
 					<div id="columns" class="accordion-collapse collapse templateElementGrid" data-bs-parent="#SectionElements">
 						<div class="accordion-body row position-relative">
 							<div class="templateElementMenu">
-								<a onclick="$(this).closest('.templateElement, .templateElementGrid').remove()">
+								<a onclick="$(this).closest('.templateElement, .templateElementGrid').remove('explode', 500)">
 									<i class="fas fa-trash-alt"></i>
 								</a>
 							</div>
@@ -221,7 +221,7 @@ if(QS_SUBPAGE != "") {
 										<div id="'.$row['section_type'].'" class="accordion-collapse collapse templateElement" element-id="'.$row['id'].'" data-bs-parent="#SectionElements">
 											<div class="accordion-body row position-relative">
 												<div class="templateElementMenu">
-													<a onclick="$(this).closest(\'.templateElement, .templateElementGrid\').remove()">
+													<a onclick="$(this).closest(\'.templateElement, .templateElementGrid\').remove("explode", 500)">
 														<i class="fas fa-trash-alt"></i>
 													</a>
 												</div>
