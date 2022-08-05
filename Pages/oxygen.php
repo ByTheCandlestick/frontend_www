1ac8ef3237cc52d43ceeb811862da174c057f5c2
Robert Smith <master_schmadoodle@outlook.com>
2022-08-05 17:42:30 +0100

    Minor changes to oxygen editor

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -304,7 +304,7 @@ if(QS_SUBPAGE != "") {
 			columns = sectionString.split('$')
 			columns.shift();
 			console.log(columns);
-			columns.each(function(column) {
+			columns.forEach(function(column) {
 				console.log(column);
 			})
 		} else {
