c366f211ddf872968c67ffd263398134d89a67c1
Robert Smith <master_schmadoodle@outlook.com>
2022-08-05 15:56:10 +0100

    Minor changes to oxygen editor

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -300,7 +300,8 @@ if(QS_SUBPAGE != "") {
 		var sectionString = $('section .sections').attr('data-original-sections');
 		if(sectionString != "") {
 			var seccode, secext, column = null;
-			(sectionString.split('$')).shift();
+			columns = sectionString.split('$')
+			columns.shift();
 		} else {
 			$('.templateBase').html("Drag an element from the left hand side to start building the website!");
 		}
