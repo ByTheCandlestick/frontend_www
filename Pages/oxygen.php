e094b0b1ff2ffc8f3e79448d9df19c072b014b37
RSmith <master_schmadoodle@outlook.com>
2022-08-08 22:02:58 +0100

    Minor changes to oxygen website builder

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -212,10 +212,10 @@ if(QS_SUBPAGE != "") {
 							</div>
 						</div>
 					</div>
+				</div>
 					<? 
 						$query = DB_Query("SELECT * FROM `page_sections` ORDER BY `section_type`");
 						$type = null; $elementCategories = array('columns');
-						print_r($query);
 						if(mysqli_num_rows($query) > 0) {
 							while($row = mysqli_fetch_array($query)) {
 								$sections[$row['id']] = $row;
@@ -245,6 +245,7 @@ if(QS_SUBPAGE != "") {
 								$type = $row['section_type'];
 							}
 						}
+
 					?>
 				</div>
 			</div>
