836938a3918e80cc6fb4a35ce4598ccc776c3e84
Robert Smith <master_schmadoodle@outlook.com>
2022-08-08 19:35:36 +0100

    Minor changes to oxygen editor

--- Pages/index.php
+++ Pages/index.php
@@ -33,7 +33,7 @@
 		$dailySales = array_reverse($dailySales);
 	// Gets all sales data from the last 12 months
 		$monthlySales = array();
-		$monthlySales_raw = mysqli_fetch_all(DB_QUERY("SELECT date_format(`Created`,'%Y-%m'), SUM(`Deposit`) FROM `Transactions` GROUP BY 1"));
+		$monthlySales_raw = mysqli_fetch_all(DB_QUERY("SELECT date_format(`Created`,'%Y-%m'), SUM(`Deposit`) FROM `Transactions` GROUP BY 1 ORDER BY 1 DESC LIMIT 12"));
 		for($i=1; $i<13; $i++) {
 			if(isset($monthlySales_raw[$i])) {
 				array_push($monthlySales, $monthlySales_raw[$i][1]);
