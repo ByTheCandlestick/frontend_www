80c248e9395c19b06cc068e2f3476805aa94f9ba
Robert Smith <master_schmadoodle@outlook.com>
2022-08-09 14:51:43 +0100

    Minor changes to oxygen editor

--- Pages/websites_new.php
+++ Pages/websites_new.php
@@ -7,7 +7,7 @@
 		<div class="col-12 col-md-6 text-md-end">
 			<div class="row">
 				<div class="col-12 d-flex justify-content-end align-items-center p-0">
-					<a href="javascript:website.create();" class="btn btn-outline-primary m-1">
+					<a href="javascript:website.domain.create();" class="btn btn-outline-primary m-1">
 						<i class="fa fa-save"></i>
 					</a>
 				</div>
