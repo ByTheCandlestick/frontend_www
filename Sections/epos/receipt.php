3fab683fadd36e6d37fb2858601593d34f7c1fdc
Robert Smith <master_schmadoodle@outlook.com>
2022-08-10 11:44:39 +0100

    Minor changes to the ePOS ui

---
+++ Sections/epos/receipt.php
@@ -0,0 +1,11 @@
+<div class="row">
+	<div class="col-9">
+		<input type="text" class="productSearch bar" plaeholder="Scan barcode or enter item code">
+	</div>
+	<div class="col-3">
+		<input type="button" class="productSearch submit">
+	</div>
+	<div class="col-12">
+
+	</div>
+</div>
