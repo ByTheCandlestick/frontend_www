5523dec9a66704cebde277861e14b86781241950
Robert Smith <master_schmadoodle@outlook.com>
2022-08-10 17:40:09 +0100

    minor changes to ePOS header

--- Sections/epos/header.php
+++ Sections/epos/header.php
@@ -7,8 +7,8 @@
 	</div>
 	<div class="col-3 right">
 		<p>
-			<span class="date">--/--/----</span>
-			<span class="time">--:--</span>
+			<span class="date">00/00/0000</span>
+			<span class="time">00:00</span>
 		</p>
 	</div>
 </div>
