c8eba483d8fd7b155c51afe1f9456b9e7f1f7b52
Robert Smith <master_schmadoodle@outlook.com>
2022-08-12 23:05:56 +0100

    Minor changes to ePOS theme

--- Themes/Simple EPOS/Assets/js/script.js
+++ Themes/Simple EPOS/Assets/js/script.js
@@ -1,42 +1,55 @@
 $(document).ready(function() {
-    let selectedDeviceId;
-    const codeReader = new ZXing.BrowserMultiFormatReader()
-    console.log('ZXing reader initialized')
-    codeReader.listVideoInputDevices().then((videoInputDevices) => {
-        const sourceSelect = document.getElementById('sourceSelect');
-        selectedDeviceId = videoInputDevices[0].deviceId;
-        if(videoInputDevices.length >= 1) {
-            videoInputDevices.forEach((element) => {
-                const sourceOption = document.createElement('option')
-                sourceOption.text = element.label;
-                sourceOption.value = element.deviceId;
-                sourceSelect.appendChild(sourceOption);
-            })
-            sourceSelect.onchange = () => {
-                selectedDeviceId = sourceSelect.value;
-            };
-            const sourceSelectPanel = document.getElementById('sourceSelectPanel');
-            sourceSelectPanel.style.display = 'block';
-        }
-        document.getElementById('startButton').addEventListener('click', () => {
-            codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
-                if (result) {
-                    console.log(result);
-                    document.getElementById('result').textContent = result.text;
-                }
-                if (err && !(err instanceof ZXing.NotFoundException)) {
-                    console.error(err);
-                    document.getElementById('result').textContent = err;
-                }
-            })
-            console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
-        })
-        document.getElementById('resetButton').addEventListener('click', () => {
-            codeReader.reset();
-            document.getElementById('result').textContent = '';
-            console.log('Reset.');
-        })
-    }).catch((err) => {
-        console.error(err);
-    })  
+	// QZing library
+		let selectedDeviceId;
+		const codeReader = new ZXing.BrowserMultiFormatReader()
+		console.log('ZXing reader initialized')
+		codeReader.listVideoInputDevices().then((videoInputDevices) => {
+			const sourceSelect = document.getElementById('sourceSelect');
+			selectedDeviceId = videoInputDevices[0].deviceId;
+			if(videoInputDevices.length >= 1) {
+				videoInputDevices.forEach((element) => {
+					const sourceOption = document.createElement('option')
+					sourceOption.text = element.label;
+					sourceOption.value = element.deviceId;
+					sourceSelect.appendChild(sourceOption);
+				})
+				sourceSelect.onchange = () => {
+					selectedDeviceId = sourceSelect.value;
+				};
+				const sourceSelectPanel = document.getElementById('sourceSelectPanel');
+				sourceSelectPanel.style.display = 'block';
+			}
+			document.getElementById('startButton').addEventListener('click', () => {
+				codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
+					if (result) {
+						console.log(result);
+						document.getElementById('result').textContent = result.text;
+					}
+					if (err && !(err instanceof ZXing.NotFoundException)) {
+						console.error(err);
+						document.getElementById('result').textContent = err;
+					}
+				})
+				console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
+			})
+			document.getElementById('resetButton').addEventListener('click', () => {
+				codeReader.reset();
+				document.getElementById('result').textContent = '';
+				console.log('Reset.');
+			})
+		}).catch((err) => {
+			console.error(err);
+		})
+	//
+		keypad = {
+			click: (btn) => {
+				if($("input[type=number]").length <= 1 && $(".productSearch").length > 0) {
+					if(/[a-z 0-9\.]/.text(btn)){
+						$(".productSearch").value($(".productSearch").value() + btn);
+					} else {
+
+					}
+				}
+			}
+		}
 });
