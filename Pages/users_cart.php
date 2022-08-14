ea1ffb48d6c7efc2a695bc32c2fa05ec4ed0d5ea
Robert Smith <master_schmadoodle@outlook.com>
2022-08-14 15:43:12 +0100

    Minor changes to cart

--- Pages/users_cart.php
+++ Pages/users_cart.php
@@ -7,7 +7,8 @@
 		<!-- Section Header -->
 		<div class="row">
 			<div class="col-12 col-md-6">
-				<h1>User info</h1>
+				<h1>User Cart</h1>
+                <p> UID: <?print(QS)?></p>
 			</div>
 		</div>
 		<hr>
