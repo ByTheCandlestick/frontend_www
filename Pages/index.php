7bd65d2fd9c314245d173f61e39975876d274f96
Robert Smith <master_schmadoodle@outlook.com>
2022-08-14 13:37:11 +0100

    Minor changes to EPOS

--- Pages/index.php
+++ Pages/index.php
@@ -100,7 +100,7 @@
 		<div class="col-12 col-md-6 col-lg-3 p-2">
 			<div class="card h-100">
 				<div class="card-body">
-					<h5 class="card-title">GROSS PROFIT YoY</h5>
+					<h5 class="card-title">INCOME YoY</h5>
 					<p class="card-text">
 						<span>
 							<?
@@ -160,7 +160,7 @@
 		<div class="col-12 col-md-6 col-lg-3 p-2">
 			<div class="card h-100">
 				<div class="card-body">
-					<h5 class="card-title">GROSS PROFIT MoM</h5>
+					<h5 class="card-title">INCOME MoM</h5>
 					<p class="card-text">
 						<span>
 							<?
