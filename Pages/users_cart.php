bddc1b023bf228a52a1f1d1163084b12bcf7d37e
Robert Smith <master_schmadoodle@outlook.com>
2022-08-14 15:41:05 +0100

    Minor changes to cart

---
+++ Pages/users_cart.php
@@ -0,0 +1,29 @@
+<?
+	$query = DB_Query(sprintf("SELECT * FROM `Users` WHERE `ID`=%s", QS));
+	if(mysqli_num_rows($query) > 0) {
+		$user = mysqli_fetch_assoc($query);
+?>
+    <section>
+        
+    </section>
+<?
+	} else {
+?>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6">
+				<h1>User not found.</h1>
+			</div>
+			<div class="col-12 col-md-6 text-md-end">
+			</div>
+		</div>
+		<hr>
+		<!-- Section Body -->
+		<div class="row">
+			<button class="btn btn-outline-primary col-12 col-md-3 col-lg-1" onclick="history.go(-1)">Go back</buton>
+		</div>
+	</section>
+<?
+	}
+?>
