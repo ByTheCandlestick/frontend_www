82cc966b45d936b12755172f2decebd5aeaba8f8
Robert Smith <master_schmadoodle@outlook.com>
2022-08-14 14:00:35 +0100

    Minor changes to prosuct Made_by

--- Sections/shopfront/product_made_by.php
+++ Sections/shopfront/product_made_by.php
@@ -1,25 +1,25 @@
 <?
-	$supplier_image = 'http://candlestick-indev.co.uk/assets/images/logos/logo.svg';
-	$supplier_title = 'By The Candlestick';
-	$supplier_shop_link = 'https://www.thecandlestick.co.uk/';
+	$partner_image = 'http://candlestick-indev.co.uk/assets/images/logos/logo.svg';
+	$partner_title = 'By The Candlestick';
+	$partner_shop_link = 'https://www.thecandlestick.co.uk/';
 	if($product['made_by_ID'] != 1) {
-		$supplier_image = '';
-		$supplier_title = 'TODO';
-		$supplier_shop_link = '';
+		$partner_image = '';
+		$partner_title = 'TODO';
+		$partner_shop_link = '';
 	}
 
 	print_r("
 		<div class=\"row\">
 			<div class=\"offset-2 col-2 / offset-md-0 col-md-3\">
-				<img class=\"mw-100 rounded-circle\" src=\"$supplier_image\">
+				<img class=\"mw-100 rounded-circle\" src=\"$partner_image\">
 			</div>
 			<div class=\"offset-0 col-6 / offset-md-0 col-md-9\">
 				<p class=\"mb-0 text-muted\">
 					Made by: 
 				</p>
 				<p class=\"fs-6\">
-					<a href=\"$supplier_shop_link\">
-						$supplier_title
+					<a href=\"$partner_shop_link\">
+						$partner_title
 					</a>
 				</p>
 			</div>
