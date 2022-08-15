2375737b60c529e847fba9a62cba322b3e70b77b
Robert Smith <master_schmadoodle@outlook.com>
2022-08-15 15:42:11 +0100

    Minor changes to oxygen editor

--- Pages/oxygen.php
+++ Pages/oxygen.php
@@ -3,6 +3,7 @@ if(QS_SUBPAGE != "") {
 	$page = mysqli_fetch_assoc(DB_Query(sprintf("SELECT * FROM `page_layouts` WHERE `ID`=%s", QS_SUBPAGE)));
 ?>
 <section>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.js"></script>
 	<!-- Section Header -->
 	<div class="row">
 		<div class="col-12 col-md-8">
