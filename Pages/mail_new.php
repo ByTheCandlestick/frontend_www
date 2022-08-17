30aa413f797be9aebfc232db8a2725d32caca92e
Robert Smith <master_schmadoodle@outlook.com>
2022-08-17 17:14:47 +0100

    Added functionality to mail_new page

--- Pages/mail_new.php
+++ Pages/mail_new.php
@@ -57,9 +57,22 @@
 		</div>
 		<div class="col-12">
 			<div class="form-floating mb-3">
-				<textarea class="form-control  mail-message" id="floatingInput" placeholder="mail@from.com" value="" style="min-height: 200px;"><?print($m)?></textarea>
+				<textarea class="form-control mail-message" id="floatingInput" placeholder="mail@from.com" value="" style="min-height: 200px;"><?print($m)?></textarea>
 				<label for="floatingInput">Message</label>
 			</div>
 		</div>
 	</div>
+	<script>
+      tinymce.init({
+        selector: '.mail-message',
+        plugins: [
+          'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+          'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+          'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
+        ],
+        toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
+          'alignleft aligncenter alignright alignjustify | ' +
+          'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
+      });
+    </script>
 </section>
