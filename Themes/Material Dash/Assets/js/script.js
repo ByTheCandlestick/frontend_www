c37a88e20c2d4a53a1669a9c1a7b65cdf6c7116f
Robert Smith <master_schmadoodle@outlook.com>
2022-08-17 11:56:13 +0100

    Minor changes to oxygen editor

--- Themes/Material Dash/Assets/js/script.js
+++ Themes/Material Dash/Assets/js/script.js
@@ -454,7 +454,7 @@ $(document).ready(() => {
 								elements = column.split(';').pop().split(',');
 								elements.forEach(function(element) {
 									[elemID, elementString] = element.split(':');
-									console.log((elemID=""||elemID===undefined?'----':elemID)+(elementString=''||elementString===undefined?'':', '+elementString));
+									console.log((elemID==''||elemID===undefined?'----':elemID)+(elementString==''||elementString===undefined?'':', '+elementString));
 									if(contained) {
 										cols = $("div[element-id='$"+colCount+"|']").last().find('.dragulaContainer');
 										$("div[element-id='"+elemID+"']").clone().appendTo(cols[currCol]).removeClass('accordion-collapse collapse show').removeAttr('data-bs-parent id').children().each(function() {
