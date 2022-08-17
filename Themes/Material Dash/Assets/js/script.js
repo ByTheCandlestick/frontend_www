473616c23a60017bbec44a2a80fa91f8632a2f31
Robert Smith <master_schmadoodle@outlook.com>
2022-08-17 12:31:25 +0100

    Minor changes to oxygen editor

--- Themes/Material Dash/Assets/js/script.js
+++ Themes/Material Dash/Assets/js/script.js
@@ -454,19 +454,21 @@ $(document).ready(() => {
 								elements = column.split(';').pop().split(',');
 								elements.forEach(function(element) {
 									[elemID, elementString] = element.split(':');
-									if(contained) {
-										cols = $("div[element-id='$"+colCount+"|']").last().find('.dragulaContainer');
-										$("div[element-id='"+elemID+"']").clone().appendTo(cols[currCol]).removeClass('accordion-collapse collapse show').removeAttr('data-bs-parent id').children().each(function() {
-											$(this).removeClass('accordion-body');
-											$(this).find('input').first().val(elementString);
-										});
-										console.log('From string: '+elemID+', '+elementString)
-										console.log('Placed'+$(cols[currCol]).find('div[element-id=9905]').last()[0]+'Within'+cols[currCol]);
-									} else {
-										$("div[element-id='"+elemID+"']").clone().appendTo('.templateBase').removeClass('accordion-collapse collapse show').removeAttr('data-bs-parent id').children().each(function() {
-											$(this).removeClass('accordion-body');
-											$(this).find('input').first().val(elementString);
-										});
+									if(elemID!==null || elemID!==undefined) {
+										if(contained) {
+											cols = $("div[element-id='$"+colCount+"|']").last().find('.dragulaContainer');
+											$("div[element-id='"+elemID+"']").clone().appendTo(cols[currCol]).removeClass('accordion-collapse collapse show').removeAttr('data-bs-parent id').children().each(function() {
+												$(this).removeClass('accordion-body');
+												$(this).find('input').first().val(elementString);
+											});
+											console.log('From string: '+elemID+', '+elementString)
+											console.log('Placed'+$(cols[currCol]).find('div[element-id=9905]').last()[0]+'Within'+cols[currCol]);
+										} else {
+											$("div[element-id='"+elemID+"']").clone().appendTo('.templateBase').removeClass('accordion-collapse collapse show').removeAttr('data-bs-parent id').children().each(function() {
+												$(this).removeClass('accordion-body');
+												$(this).find('input').first().val(elementString);
+											});
+										}
 									}
 								})
 								currCol++;
