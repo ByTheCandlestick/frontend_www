aa0c8fb0f14a2edc9f4bfae431df3a74a6940434
Robert Smith <master_schmadoodle@outlook.com>
2022-08-17 14:13:59 +0100

    Added new mail template

---
+++ Pages/mail_new.php
@@ -0,0 +1,18 @@
+<?
+
+?>
+<section>
+	<!-- Section Header -->
+	<div class="row">
+		<div class="col-12 col-md-6">
+			<h1>New mail</h1>
+		</div>
+		<div class="col-12 col-md-6 text-md-end">
+		</div>
+	</div>
+	<hr>
+	<!-- Section Body -->
+	<div class="row">
+        
+	</div>
+</section>
