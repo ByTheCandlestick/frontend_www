f0b9cc25f44d7286e52d5a483b6174d227269819
Robert Smith <master_schmadoodle@outlook.com>
2022-08-17 14:41:04 +0100

    Added new mail template

--- Pages/mail_new.php
+++ Pages/mail_new.php
@@ -13,6 +13,14 @@
 	<hr>
 	<!-- Section Body -->
 	<div class="row">
+		<style>
+			.mail input[type="text"] {
+				width: 100%;
+				margin: 5px;
+				padding: 3px;
+				border: solid thin grey;
+			}
+		</style>
         <div class="mail">
 			<div class="col-12">
 				<input type="text"></input>
