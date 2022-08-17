eddc3ffa9b708b634d6b478759fb62a8f67e7ec3
Robert Smith <master_schmadoodle@outlook.com>
2022-08-17 15:10:27 +0100

    Added new mail template

--- Pages/mail_new.php
+++ Pages/mail_new.php
@@ -53,7 +53,7 @@
 		</div>
 		<div class="col-12">
 			<div class="form-floating mb-3">
-				<textarea class="form-control" id="floatingInput" placeholder="mail@from.com" value="">
+				<textarea class="form-control" id="floatingInput" placeholder="mail@from.com" value=""></textarea>
 				<label for="floatingInput"></label>
 			</div>
 		</div>
