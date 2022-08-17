81af5b6fc314a6152972965bf37e4634f4e31f76
Robert Smith <master_schmadoodle@outlook.com>
2022-08-17 14:31:25 +0100

    Added new mail template

--- Pages/mail.php
+++ Pages/mail.php
@@ -8,11 +8,14 @@
 			<h1>Mail</h1>
 		</div>
 		<div class="col-12 col-md-6 text-md-end">
+			<a href="mailto:<?print($invoice['Email'])?>;" class="btn btn-outline-primary m-1">
+				<i class="fa fa-at"></i>
+			</a>
 		</div>
 	</div>
 	<hr>
 	<!-- Section Body -->
 	<div class="row">
-        
+		
 	</div>
 </section>
