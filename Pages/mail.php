519e2f3a42a43d4280cb3aa1263a1ea3eaa52c58
Robert Smith <master_schmadoodle@outlook.com>
2022-08-19 16:15:57 +0100

    Minor changes to mail page

--- Pages/mail.php
+++ Pages/mail.php
@@ -26,46 +26,145 @@
 	<hr>
 	<!-- Section Body -->
 		<style>
-			sidebar {
-				background-color: var(--app-container);
-				width: 60px;
-				transition-duration: 0.5s;
-				border-radius: 0 5px 5px 0;
-				margin: 1rem 0 0 0!important;
-				top: 1rem;
+			.s-sidebar__trigger {
+				z-index: 2;
+				position: fixed;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 4em;
+				background: #192b3c;
 			}
-			sidebar div {
-				list-style: none;
-				display: block;
-				color: black;
+			.s-sidebar__trigger > i {
+				display: inline-block;
+				margin: 1.5em 0 0 1.5em;
+				color: #f07ab0;
+			}
+			.s-sidebar__nav {
+				position: fixed;
+				top: 0;
+				left: -15em;
 				overflow: hidden;
-				padding: 20px;
-				trasintion: all;
-				transition-duration: 0.5s;
+				transition: all .3s ease-in;
+				width: 15em;
+				height: 100%;
+				background: #243e56;
+				color: rgba(255, 255, 255, 0.7);
 			}
-			sidebar div:before {
-				content: ">>"
+			.s-sidebar__nav:hover,
+			.s-sidebar__nav:focus,
+			.s-sidebar__trigger:focus + .s-sidebar__nav,
+			.s-sidebar__trigger:hover + .s-sidebar__nav {
+				left: 0;
 			}
-			sidebar div.active {
-				background: gainsboro;
+			.s-sidebar__nav ul {
+				position: absolute;
+				top: 4em;
+				left: 0;
+				margin: 0;
+				padding: 0;
+				width: 15em;
 			}
-			sidebar div span {
-				margin-left: 30px;
-				color: black;
-				font-size: 20px;
+			.s-sidebar__nav ul li {
+				width: 100%;
 			}
-			sidebar div:hover {
-				background-color: silver;
+			.s-sidebar__nav-link {
+				position: relative;
+				display: inline-block;
+				width: 100%;
+				height: 4em;
 			}
-			sidebar:hover {
-				width: 250px;
+			.s-sidebar__nav-link em {
+				position: absolute;
+				top: 50%;
+				left: 4em;
+				transform: translateY(-50%);
 			}
+			.s-sidebar__nav-link:hover {
+				background: #4d6276;
+			}
+			.s-sidebar__nav-link > i {
+				position: absolute;
+				top: 0;
+				left: 0;
+				display: inline-block;
+				width: 4em;
+				height: 4em;
+			}
+			.s-sidebar__nav-link > i::before {
+				position: absolute;
+				top: 50%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+			}
+
+			/* Mobile First */
+			@media (min-width: 42em) {
+				.s-layout__content {
+					margin-left: 4em;
+				}
+				
+				/* Sidebar */
+				.s-sidebar__trigger {
+					width: 4em;
+				}
+				
+				.s-sidebar__nav {
+					width: 4em;
+					left: 0;
+				}
+				
+				.s-sidebar__nav:hover,
+				.s-sidebar__nav:focus,
+				.s-sidebar__trigger:hover + .s-sidebar__nav,
+				.s-sidebar__trigger:focus + .s-sidebar__nav {
+					width: 15em;
+				}
+			}
+
+			@media (min-width: 68em) {
+				.s-layout__content {
+					margin-left: 15em;
+				}
+				
+				/* Sidebar */
+				.s-sidebar__trigger {
+					display: none
+				}
+				
+				.s-sidebar__nav {
+					width: 15em;
+				}
+				
+				.s-sidebar__nav ul {
+					top: 1.3em;
+				}
+			}
+
 		</style>
 		<sidebar>
-			<a href="/Mail/Inbox/"><div class="<?=(strtolower(QS_SUBPAGE) == "inbox")?"active":"";?>"><span>Inbox</span></div></a>
-			<a href="/Mail/Sent/"><div class="<?=(strtolower(QS_SUBPAGE) == "sent")?"active":"";?>"><span>Sent</span></div></a>
-			<a href="/Mail/Junk/"><div class="<?=(strtolower(QS_SUBPAGE) == "junk")?"active":"";?>"><span>Junk</span></div></a>
-			<a href="/Mail/Deleted/"><div class="<?=(strtolower(QS_SUBPAGE) == "deleted")?"active":"";?>"><span>Deleted</span></div></a>
+			<a class="s-sidebar__trigger" href="#0">
+				<i class="fa fa-bars"></i>
+			</a>
+			<nav class="s-sidebar__nav">
+				<ul>
+					<li>
+					<a class="s-sidebar__nav-link" href="#0">
+						<i class="fa fa-home"></i><em>Home</em>
+					</a>
+					</li>
+					<li>
+					<a class="s-sidebar__nav-link" href="#0">
+						<i class="fa fa-user"></i><em>My Profile</em>
+					</a>
+					</li>
+					<li>
+					<a class="s-sidebar__nav-link" href="#0">
+						<i class="fa fa-camera"></i><em>Camera</em>
+					</a>
+					</li>
+				</ul>
+			</nav>
 		</sidebar>
 		<div class="row">
 
