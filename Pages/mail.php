ce69f7eb3572c263134305c0013e19f294eb2761
Robert Smith <master_schmadoodle@outlook.com>
2022-08-19 11:52:17 +0100

    Minor changes to pages/sections to clear up any minor bugs

--- Pages/mail.php
+++ Pages/mail.php
@@ -1,11 +1,8 @@
-<?
-
-?>
 <section>
 	<!-- Section Header -->
 	<div class="row">
 		<div class="col-12 col-md-6">
-			<h1>Mail</h1>
+			<h1>Inbox</h1>
 		</div>
 		<div class="col-12 col-md-6 text-md-end">
 			<a href="/Mail/New/" class="btn btn-outline-primary m-1">
@@ -15,7 +12,24 @@
 	</div>
 	<hr>
 	<!-- Section Body -->
+		<div id="sidebar">
+			<div><span><a href="/Mail/Inbox/">Inbox</a></span></div>
+			<div><span><a href="/Mail/Sent/">Sent</a></span></div>
+			<div><span><a href="/Mail/Junk/">Junk</a></span></div>
+			<div><span><a href="/Mail/Deleted/">Deleted</a></span></div>
+		</div>
+<?
+	if( strtolower(QS_SUBPAGE) == "inbox" ) {
+?>
+		<div class="row">
+			
+		</div>
+<?
+	} else {
+?>
 	<div class="row">
-		
 	</div>
+<?
+	}
+?>
 </section>
