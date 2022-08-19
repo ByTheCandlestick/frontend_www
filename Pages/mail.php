e934e4b87b4c3447ffb2698cdb18f125aa7ce0bc
Robert Smith <master_schmadoodle@outlook.com>
2022-08-19 12:49:57 +0100

    Minor changes to pages/sections to clear up any minor bugs

--- Pages/mail.php
+++ Pages/mail.php
@@ -4,8 +4,7 @@
 	} elseif(strtolower(QS_SUBPAGE) == "junk") {
 	} elseif(strtolower(QS_SUBPAGE) == "deleted") {
 	} else {
-		header("Location: ".URL_CURR."/Mail/Inbox/");
-		echo URL_CURR."/Mail/Inbox/";
+		header("Location: ${URL_CURR}/Mail/Inbox/");
 	}
 ?>
 <section>
