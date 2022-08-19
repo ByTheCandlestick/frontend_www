9ea5326c6374734c97bcb9e7621c59c8a6335d60
Robert Smith <master_schmadoodle@outlook.com>
2022-08-19 15:49:21 +0100

    Minor changes to mail page

--- Pages/mail.php
+++ Pages/mail.php
@@ -30,9 +30,9 @@
 				background-color: var(--app-container);
 				width: 60px;
 				transition-duration: 0.5s;
-				border-radius: 5px 0px 0px 5px;
+				border-radius: 0px 5px 5px 0px;
 				position: fixed;
-				right: 0.5rem;
+				left: 0.5rem;
 				margin-top: 6rem!important;
 			}
 			sidebar div {
