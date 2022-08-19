381005ef1cf7ae15824527799fe1e6e8daa8c14d
Robert Smith <master_schmadoodle@outlook.com>
2022-08-19 11:52:54 +0100

    Minor changes to pages/sections to clear up any minor bugs

--- Pages/mail.php
+++ Pages/mail.php
@@ -12,6 +12,55 @@
 	</div>
 	<hr>
 	<!-- Section Body -->
+		<style>
+			#wrap {
+				margin: 10px auto;
+				width: 500px;
+				overflow: hidden;
+				border: 1px solid grey;
+				text-align: center;
+			}
+			#wrap .title {
+				font-size: 30px;
+				padding: 10px;
+				border-bottom: 1px solid grey;
+			}
+			#sidebar {
+				position: fixed;
+				top: 20%;
+				left: 0px;
+				background-color: white;
+				width: 60px;
+				trasintion: all;
+				transition-duration: 0.5s;
+				border-bottom-right-radius: 5px;
+				border-top-right-radius: 5px;
+			}
+			#sidebar:hover {
+				-webkit-box-shadow: 10px 4px 34px -1px rgba(0,0,0,0.95);
+				-moz-box-shadow: 10px 4px 34px -1px rgba(0,0,0,0.95);
+				box-shadow: 10px 4px 34px -1px rgba(0,0,0,0.95);
+			}
+			#sidebar div {
+				list-style: none;
+				display: block;
+				color: black;
+				overflow: hidden;
+				padding: 20px;
+				trasintion: all;
+				transition-duration: 0.5s;
+			}
+			#sidebar div:before { content: ">>" }
+			#sidebar div span {
+				margin-left: 30px;
+				color: black;
+				font-size: 20px;
+			}
+			#sidebar div:hover {
+				background-color: #C9C9C9;
+			}
+			#sidebar:hover { width: 250px }
+		</style>
 		<div id="sidebar">
 			<div><span><a href="/Mail/Inbox/">Inbox</a></span></div>
 			<div><span><a href="/Mail/Sent/">Sent</a></span></div>
@@ -22,7 +71,7 @@
 	if( strtolower(QS_SUBPAGE) == "inbox" ) {
 ?>
 		<div class="row">
-			
+
 		</div>
 <?
 	} else {
