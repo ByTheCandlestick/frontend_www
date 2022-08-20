77c55fde9d027ff530887133703b4c0ad9b9a584
Robert Smith <master_schmadoodle@outlook.com>
2022-08-20 13:11:46 +0100

    Minor changes to login section to enable enter keypress

--- Pages/mail.php
+++ Pages/mail.php
@@ -26,47 +26,76 @@
 	<hr>
 	<!-- Section Body -->
 		<style>
-			sidebar {
-				background-color: var(--app-container);
-				width: 60px;
-				transition-duration: 0.5s;
-				border-radius: 0 5px 5px 0;
-				margin: 1rem 0 0 0!important;
-				top: 1rem;
+			.side-menu {
+				width:14em;
+				height: auto;
+				margin:4em auto 0;
+				font-size: 1em;
+				line-height: 2em;
+				color: #fff;
+				font-weight:700;
+				text-transform:uppercase;
+				position:absolute;
+				top:0; 
+				bottom:0;
+				left: -10em;
 			}
-			sidebar div {
+			.side-menu ul {
 				list-style: none;
-				display: block;
-				color: black;
-				overflow: hidden;
-				padding: 20px;
-				trasintion: all;
-				transition-duration: 0.5s;
+				margin: 0;
+				padding: 0;
 			}
-			sidebar div:before {
-				content: ">>"
+			.side-menu ul li {
+			display:block;
+				background-color: #333;
+				height: 2em;
+				padding: 1em 1.5em;
+				position: relative;
+				-webkit-transition: -webkit-transform 0.5s,     background-color .5s, color .5s;
+				transition: transform .5s, background-color .5s, color .5s;
 			}
-			sidebar div.active {
-				background: gainsboro;
+			/*the colors of the different columns*/
+			.side-menu ul li:nth-child(1) { background-color: #00aced;}
+			.side-menu ul li:nth-child(2) { background-color: #3b5998;}
+			.side-menu ul li:nth-child(3) { background-color: #00a300;}
+			.side-menu ul li:nth-child(4) { background-color: #1e7145;}
+			.side-menu ul li:nth-child(5) { background-color: #ffc40d;}
+			.side-menu ul li:nth-child(6) { background-color: #cb2027;}
+
+			.side-menu ul li:hover {
+				background-color: #339966; /*you can make different colors depending on the nth-child like above*/
+				-webkit-transform: translateX(10em);
+				transform: translateX(10em);/*equal to left in the .side-menu*/
 			}
-			sidebar div span {
-				margin-left: 30px;
-				color: black;
-				font-size: 20px;
+			.side-menu ul li a {
+			display:block;
+				color: #FFF;
+				text-decoration: none;
 			}
-			sidebar div:hover {
-				background-color: silver;
-			}
-			sidebar:hover {
-				width: 250px;
+			.side-menu ul li span {
+				display:block;
+				position: absolute;
+				font-size:1em;
+				line-height: 2em;
+				height:2em;
+				top:0; 
+				bottom:0;
+				margin:0 auto;
+				padding:1em 1.5em;
+				right: 0.16666666666667em;
+				color: #F8F6FF;
 			}
 		</style>
-		<sidebar>
-			<a href="/Mail/Inbox/"><div class="<?=(strtolower(QS_SUBPAGE) == "inbox")?"active":"";?>"><span>Inbox</span></div></a>
-			<a href="/Mail/Sent/"><div class="<?=(strtolower(QS_SUBPAGE) == "sent")?"active":"";?>"><span>Sent</span></div></a>
-			<a href="/Mail/Junk/"><div class="<?=(strtolower(QS_SUBPAGE) == "junk")?"active":"";?>"><span>Junk</span></div></a>
-			<a href="/Mail/Deleted/"><div class="<?=(strtolower(QS_SUBPAGE) == "deleted")?"active":"";?>"><span>Deleted</span></div></a>
-		</sidebar>
+		<nav class="side-menu">
+			<ul>
+				<li><a href="#">What to do?<span><i class="fa fa-map-marker"></i></span></a></li>
+				<li><a href="#">Where to go?<span><i class="fa fa-compass"></i></span></a></li>
+				<li><a href="#">Services<span><i class="fa fa-bicycle"></i></span></a></li>
+				<li><a href="#">Accomodation<span><i class="fa fa-bed"></i></span></a></li>
+				<li><a href="#">Free Time<span><i class="fa fa-book"></i></span></a></li>
+				<li><a href="#">Food&Drnk<span><i class="fa fa-beer"></i></span></a></li>
+			</ul>
+		</nav>
 		<div class="row">
 			
 		</div>
