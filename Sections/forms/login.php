28f2d5f9161a94acb6067597f1a0fd20765b93e6
Robert Smith <master_schmadoodle@outlook.com>
2022-08-20 10:57:12 +0100

    Minor changes to login section to enable enter keypress

--- Sections/forms/login.php
+++ Sections/forms/login.php
@@ -13,7 +13,7 @@
 		</div>
 	</div>
 	<script>
-		$("div[name=username], div[name=password]").keypress((e) => {
+		$("div[name=username] div[name=password]").keypress((e) => {
 			console.log("Key pressed");
 			if (!e) e = window.event;
 			var keyCode = e.code || e.key;
