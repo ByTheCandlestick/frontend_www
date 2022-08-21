b03a734b42704df8cddd7a7734d8f93ab151f17f
Robert Smith <master_schmadoodle@outlook.com>
2022-08-21 12:24:40 +0100

    Minor changes to mail page

--- Pages/mail.php
+++ Pages/mail.php
@@ -27,73 +27,78 @@
 			</a>
 		</div>
 	</div>
+	<!-- Seperator -->
 	<hr>
 	<!-- Section Body -->
-		<style>
-			.side-menu {
-				width: 10rem;
-				height: auto;
-				margin: 4em auto 0;
-				font-size: 1em;
-				line-height: 2em;
-				color: var(--main-color);
-				font-weight: 700;
-				text-transform: uppercase;
-				position: relative;
-				top: 0;
-				bottom: 0;
-				left: -7rem;
-			}
-			.side-menu ul {
-				list-style: none;
-				margin: 0;
-				padding: 0;
-			}
-			.side-menu ul li {
-				display: block;
-				background-color: var(--app-container);
-				padding: 1em 1.5em;
-				position: relative;
-				-webkit-transition: -webkit-transform 0.5s, background-color .5s, color .5s;
-				   -moz-transition: -moz-transform 0.5s, background-color .5s, color .5s;
-					 -o-transition: -o-transform 0.5s, background-color .5s, color .5s;
-						transition: transform .5s, background-color .5s, color .5s;
-			}
-			.side-menu ul li.active {
-				background-color: gainsboro;
-			}
-			.side-menu ul li:hover {
-				background-color: silver;
-				-webkit-transform: translateX(6rem);
-				   -moz-transform: translateX(6rem);
-					 -o-transform: translateX(6rem);
-						transform: translateX(6rem);
-			}
-			.side-menu ul li a:hover {
-				color: var(--link-color-hover);
-			}
-			.side-menu ul li span {
-				display: block;
-				position: absolute;
-				font-size: 1em;
-				line-height: 2em;
-				height: 2em;
-				top: 0;
-				bottom: 0;
-				margin: 0 auto;
-				padding: 1em 1.5em;
-				right: 0.16666666666667em;
-			}
-		</style>
-		<nav class="side-menu">
-			<ul>
-				<li class="<?=(strtolower(QS_SUBPAGE) == "inbox")?"active":"";?>"><a href="#">Inbox<span><i class="fad fa-inbox"></i></span></a></li>
-				<li class="<?=(strtolower(QS_SUBPAGE) == "sent")?"active":"";?>"><a href="#">Sent<span><i class="fad fa-inbox-out"></i></span></a></li>
-				<li class="<?=(strtolower(QS_SUBPAGE) == "junk")?"active":"";?>"><a href="#">Junk<span><i class="fad fa-ban"></i></span></a></li>
-				<li class="<?=(strtolower(QS_SUBPAGE) == "deleted")?"active":"";?>"><a href="#">Deleted<span><i class="fad fa-trash-alt"></i></span></a></li>
-			</ul>
-		</nav>
-		<div class="row">
+	<style>
+		.side-menu {
+			width: 10rem;
+			height: auto;
+			margin: 4em auto 0;
+			font-size: 1em;
+			line-height: 2em;
+			color: var(--main-color);
+			font-weight: 700;
+			text-transform: uppercase;
+			position: relative;
+			top: 0;
+			bottom: 0;
+			left: -7rem;
+		}
+		.side-menu ul {
+			list-style: none;
+			margin: 0;
+			padding: 0;
+		}
+		.side-menu ul li {
+			display: block;
+			background-color: var(--app-container);
+			padding: 1em 1.5em;
+			position: relative;
+			-webkit-transition: -webkit-transform 0.5s, background-color .5s, color .5s;
+				-moz-transition: -moz-transform 0.5s, background-color .5s, color .5s;
+					-o-transition: -o-transform 0.5s, background-color .5s, color .5s;
+					transition: transform .5s, background-color .5s, color .5s;
+		}
+		.side-menu ul li.active {
+			background-color: gainsboro;
+		}
+		.side-menu ul li:hover {
+			background-color: silver;
+			-webkit-transform: translateX(6rem);
+				-moz-transform: translateX(6rem);
+					-o-transform: translateX(6rem);
+					transform: translateX(6rem);
+		}
+		.side-menu ul li a:hover {
+			color: var(--link-color-hover);
+		}
+		.side-menu ul li span {
+			display: block;
+			position: absolute;
+			font-size: 1em;
+			line-height: 2em;
+			height: 2em;
+			top: 0;
+			bottom: 0;
+			margin: 0 auto;
+			padding: 1em 1.5em;
+			right: 0.16666666666667em;
+		}
+	</style>
+	<div class="row">
+		<div class="col-1">
+			<nav class="side-menu">
+				<ul>
+					<li class="<?=(strtolower(QS_SUBPAGE) == "inbox")?"active":"";?>"><a href="#">Inbox<span><i class="fad fa-inbox"></i></span></a></li>
+					<li class="<?=(strtolower(QS_SUBPAGE) == "sent")?"active":"";?>"><a href="#">Sent<span><i class="fad fa-inbox-out"></i></span></a></li>
+					<li class="<?=(strtolower(QS_SUBPAGE) == "junk")?"active":"";?>"><a href="#">Junk<span><i class="fad fa-ban"></i></span></a></li>
+					<li class="<?=(strtolower(QS_SUBPAGE) == "deleted")?"active":"";?>"><a href="#">Deleted<span><i class="fad fa-trash-alt"></i></span></a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="col-11">
 
 		</div>
+	</div>
 </section>
