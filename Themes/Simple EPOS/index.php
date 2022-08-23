3d51589995afc317de38a539b0783ce36734a85a
Robert Smith <master_schmadoodle@outlook.com>
2022-08-23 10:47:24 +0100

    Minor changes to Permissions

--- Themes/Simple EPOS/index.php
+++ Themes/Simple EPOS/index.php
@@ -23,7 +23,7 @@
 	// get the page information
 		if(QS_PAGE!=null && mysqli_num_rows($layout_results = DB_Query($query)) > 0) {
 			while($layout_row = mysqli_fetch_assoc($layout_results)) {
-				print_r($layout_row['Permission']);
+				print_r($layout_row);
 				if($userperm[$layout_row['Permission']] != 1) {
 					if($_SERVER['REQUEST_URI'] != '/') {
 						header("Location: /");
