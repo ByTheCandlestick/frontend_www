6c59b29ed0208a96eb4792c60c5d01b398ddc13e
Robert Smith <master_schmadoodle@outlook.com>
2022-08-23 13:23:55 +0100

    Minor changes to thew in-built API

--- Themes/Swagger API/index.php
+++ Themes/Swagger API/index.php
@@ -6,8 +6,8 @@
 				require_once('./API/'.QS_PAGE.'/base/BaseController.php');
 				require_once('./API/'.QS_PAGE.'/base/BaseModel.php');
 			// API Functions
-				require_once('./API/'.QS_PAGE.'/model/'.QS_SUBPAGE'.Model.php');
-				require_once('./API/'.QS_PAGE.'/controller/'.QS_SUBPAGE'.Controller.php');
+				require_once('./API/'.QS_PAGE.'/model/'.QS_SUBPAGE.'Model.php');
+				require_once('./API/'.QS_PAGE.'/controller/'.QS_SUBPAGE.'Controller.php');
 				require_once('./API/'.QS_PAGE.'/context/'.QS_SUBPAGE.'Context.php');
 		} else {
 			// Display the swagger api dashboard
