134270fea557b23239bfa5d3eecf61c7c2f3cdf5
Robert Smith <master_schmadoodle@outlook.com>
2022-08-23 15:30:05 +0100

    Added auto update functionality

---
+++ Themes/Swagger API/Assets/php/auto-update.php
@@ -0,0 +1,39 @@
+<?php
+	if($_SERVER['REQUEST_METHOD'] == 'POST') {
+		if($_POST['payload']) { // Only respond to POST requests from Github
+			$payload = json_decode($_POST['payload'], true);
+			define("GIT_PROT",		"https");
+			define("GIT_USER",		"azurrr");
+			define("GIT_PKEY",		"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");
+			define("GIT_ADDR",		"github.com");
+			define("COMPANY",		$payload['repository']['owner']['name']);
+			define("REPO",			$payload['repository']['name']);
+			define("BRANCH",		substr($payload['ref'], 11));
+			define("REMOTE_LINK",	GIT_PROT."://".GIT_USER.":".GIT_PKEY."@". GIT_ADDR."/".COMPANY."/".REPO.".git");
+				// http(s)://username:password@git.address/company/repository.git
+			define("ROOT",			"/kunden/homepages/36/d908228976/htdocs");
+			define("REPOSITORY",	ROOT."/".BRANCH."/".REPO);
+				// /directory/to/htdocs/branch/reponame
+			define("BRANCH_DIR",	ROOT."/".BRANCH);
+			// /directory/to/htdocs/branch
+
+			// Check if ROOT exists
+			if(!file_exists(ROOT)) die("ERROR: ROOT does not exist.");
+			// Check if the branch folder exists
+			if(!file_exists(BRANCH_DIR)) shell_exec("cd ".ROOT." && mkdir ".BRANCH);
+			// Check if REPOSITORY exists
+			if(!file_exists(REPOSITORY)) {
+				shell_exec("cd ".BRANCH_DIR." && git clone ".REMOTE_LINK);
+				shell_exec("cd ".BRANCH_DIR." && git checkout -b ".BRANCH);
+				shell_exec("cd ".REPOSITORY." && git remote add upstream ".REMOTE_LINK);
+				die("Cloned. ".date(DateTime::ISO8601, strtotime('-2 hour')));
+			} else {
+				print_r("cd ".REPOSITORY." && git pull upstream ".BRANCH);
+				shell_exec("cd ".REPOSITORY." && git pull upstream ".BRANCH);
+				die("Updated. ".date(DateTime::ISO8601, strtotime('-2 hour')));
+			}
+		}
+	}
+	header("HTTP/1.0 404 Not Found");
+	die();
+?>
