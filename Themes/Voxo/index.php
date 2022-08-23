9f22902b06f1e70ab36a3e079f8310f8fa810516
Robert Smith <master_schmadoodle@outlook.com>
2022-08-23 10:15:16 +0100

    Minor changes to Permissions

--- Themes/Voxo/index.php
+++ Themes/Voxo/index.php
@@ -24,6 +24,7 @@
 	// get the page information
 		if(QS_PAGE!=null && mysqli_num_rows($layout_results = DB_Query($query)) > 0) {
 			while($layout_row = mysqli_fetch_assoc($layout_results)) {
+				print_r($layout_results);
 ?>
 <!DOCTYPE html>
 	<html lang="en">
