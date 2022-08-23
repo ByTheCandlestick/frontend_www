1ec83ab111e22d05f35433f1ef808245a5f57e60
Robert Smith <master_schmadoodle@outlook.com>
2022-08-23 13:03:17 +0100

    Minor changes to thew in-built API

--- Themes/Swagger API/index.php
+++ Themes/Swagger API/index.php
@@ -1,14 +1,14 @@
 <?
-	if(QS != "" && file_exists($dash = "./API/${QS}/dashboard.php")) {
-		if(QS_PAGE != "") {
+	if(QS_PAGE != "" && file_exists($dash = "./API/${QS_PAGE}/dashboard.php")) {
+		if(QS_SUBPAGE != "") {
 			// Include the base files
-				require_once("./API/${QS}/base/Bootstrap.php");
-				require_once("./API/${QS}/base/BaseController.php");
-				require_once("./API/${QS}/base/BaseModel.php");
+				require_once("./API/${QS_PAGE}/base/Bootstrap.php");
+				require_once("./API/${QS_PAGE}/base/BaseController.php");
+				require_once("./API/${QS_PAGE}/base/BaseModel.php");
 			// API Functions
-				require_once("./API/${QS}/model/${QS_PAGE}Model.php");
-				require_once("./API/${QS}/controller/${QS_PAGE}Controller.php");
-				require_once("./API/${QS}/context/${QS_PAGE}Context.php");
+				require_once("./API/${QS_PAGE}/model/${QS_SUBPAGE}Model.php");
+				require_once("./API/${QS_PAGE}/controller/${QS_SUBPAGE}Controller.php");
+				require_once("./API/${QS_PAGE}/context/${QS_SUBPAGE}Context.php");
 		} else {
 			// Display the swagger api dashboard
 				require_once($dash);
