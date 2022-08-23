af37251e9c9d0208675edaa16abd39c6c693d8f7
Robert Smith <master_schmadoodle@outlook.com>
2022-08-23 14:22:11 +0100

    Minor tweaks to folder names

--- Themes/Swagger API/index.php
+++ Themes/Swagger API/index.php
@@ -2,13 +2,13 @@
 	if(QS_PAGE != "" && file_exists($dash = './API/'.QS_PAGE.'/dashboard.php')) {
 		if(QS_SUBPAGE != "") {
 			// Include the base files
-				require_once('./API/'.QS_PAGE.'/base/Bootstrap.php');
-				require_once('./API/'.QS_PAGE.'/base/BaseController.php');
-				require_once('./API/'.QS_PAGE.'/base/BaseModel.php');
+				require_once('./API/'.QS_PAGE.'/Base/Bootstrap.php');
+				require_once('./API/'.QS_PAGE.'/Base/BaseController.php');
+				require_once('./API/'.QS_PAGE.'/Base/BaseModel.php');
 			// API Functions
-				require_once('./API/'.QS_PAGE.'/models/'.ucwords(QS_SUBPAGE).'Model.php');
-				require_once('./API/'.QS_PAGE.'/controllers/'.ucwords(QS_SUBPAGE).'Controller.php');
-				require_once('./API/'.QS_PAGE.'/contexts/'.ucwords(QS_SUBPAGE).'Context.php');
+				require_once('./API/'.QS_PAGE.'/Models/'.ucwords(QS_SUBPAGE).'Model.php');
+				require_once('./API/'.QS_PAGE.'/Controllers/'.ucwords(QS_SUBPAGE).'Controller.php');
+				require_once('./API/'.QS_PAGE.'/Contexts/'.ucwords(QS_SUBPAGE).'Context.php');
 		} else {
 			// Display the swagger api dashboard
 				require_once($dash);
