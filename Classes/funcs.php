74f244399f86883f1b0419e5e013b1aaa00cca65
Robert Smith <master_schmadoodle@outlook.com>
2022-08-24 13:55:34 +0100

    Minor changes related to SQL table renaming

--- Classes/funcs.php
+++ Classes/funcs.php
@@ -31,7 +31,7 @@
 	 */
 		function getThemepage(bool $require) {
 			global $userdata, $userperm, $website_info, $user_ok, $product, $partner;
-			print($q=sprintf("SELECT `page_type` FROM `Website domains` WHERE `Domain`='%s'", $_SERVER['HTTP_HOST']));
+			print($q = sprintf("SELECT `page_type` FROM `Website domains` WHERE `Domain`='%s'", $_SERVER['HTTP_HOST']));
 			$page_type = mysqli_fetch_array(DB_Query($q))[0];
 			$theme_location = mysqli_fetch_array(DB_Query(sprintf("SELECT `Location` FROM `Website themes` WHERE `ID`='%s'", $page_type)))[0];
 			if($theme_location != "") {
