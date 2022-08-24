424d4dbdf8ad77190de7acf8ea237f54ff3e60a7
Robert Smith <master_schmadoodle@outlook.com>
2022-08-24 10:43:47 +0100

    Minor changes to define API keys

--- Classes/vars.php
+++ Classes/vars.php
@@ -12,9 +12,8 @@
 	}
 	$query = DB_Query("SELECT `ID`, `Key` FROM `API Keys` WHERE `Active?`=1 AND `Created`<now()");
 	$keys = [];
-	while($key = mysqli_fetch_array($query)) {
+	while($key = mysqli_fetch_array($query)) {}
 //		$keys['ID'] =+ $key['Key'];
-	}
 	define('__API_KEYS__',	$keys);
 	define('__ROOT__',		$_SERVER["DOCUMENT_ROOT"]);
 	define('__THEME__',		getThemepage(false));
