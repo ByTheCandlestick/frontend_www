eef003ef2736003e50f8c6116e4a20d8b7c341b4
Robert Smith <master_schmadoodle@outlook.com>
2022-08-24 12:14:48 +0100

    Minor changes to API validation

--- API/v1/Base/Bootstrap.php
+++ API/v1/Base/Bootstrap.php
@@ -1,6 +1,5 @@
 <?
 	// Define all vars
-		define('API_KEY',	'iwdk5xYYMyUbyKuHMB8UuA5R2pbqgYLvjzzKQFCeJzKbAkg2qAJGWunzJPZFxvaCvue5xHJEwrhG3b9Ye5mn3UYBT7ZE46crHkgenvY4LaUSgb3Jcj8T67tUuyVtD6nRTQxvurPZ6E96WiQKep7G8kUjJhxHchEZk6KrWqZ2Tf2B9ZgtErZ4UMNNSJWE9DV8gM3YMkzmraACBxd9nPBteJKPx3SFdBMHQGBAL5bzSmJtCfezQJ7Ed3hk4CBnhda3');
 		(isset($_SERVER["HTTP_REFERER"]))?$referer=$_SERVER["HTTP_REFERER"] : $referer=$_SERVER["SERVER_NAME"];
 		if(str_contains($referer, "indev")) {	#	THE WESBITE IN IN DEVELOPMENT MODE
 			define('STRIPE_API',	'sk_test_51JKqfVFDFLz8LpozmlliBbv92XkspmRyy2O7G6IMk2IccfP9ZnimCZ8rJHHCVfIGupLx5FJZafa92igVC2HFWPkz00umY4pOUm');
@@ -11,7 +10,8 @@
 			define('ANALYTICS',		['db5007301242.hosting-data.io',	'dbu235049',	'CandleStick2603',	'dbs6015868']);
 			define('ADMIN', 		['db5007320590.hosting-data.io',	'dbu1278426',	'CandleStick2603',	'dbs6031251']);
 		}
-		define('__ROOT__',	$_SERVER['DOCUMENT_ROOT']);
+		define('__ROOT__',			$_SERVER['DOCUMENT_ROOT']);
+	//	Get the query string for each REST type
 		if($_SERVER['REQUEST_METHOD'] == "GET") {				// 
 			$keys = array_keys($_GET);
 			$vals = $_GET;
@@ -34,49 +34,91 @@
 			define('QUERY_STRING',	file_get_contents("php://input"));
 		}
 	//	Functions
-		/**	Return an invalid request.
+		/**	invalid_request
+		 *	Return an invalid request.
 		 *	@return	exit	Ends the API and displays an error
-		*/
-		function invalid_request(int $loc) {
-			exit( json_encode( array(
-				'error' => 'Valid API request, api_key or OAuth parameters missing',
-				'location' => $loc
-			) ) );
-		};
-		/**	Return uri array of string.
+		 */
+			function invalid_request(int $loc) {
+				exit( json_encode( array(
+					'error' => 'Valid API request, api_key or OAuth parameters missing',
+					'location' => $loc
+				) ) );
+			};
+		/**	get_uri
+		 *	Return uri array of string.
 		 *	@return	array	Ends the API and displays an error
-		*/
-		function get_uri() {
-			return explode( '/', parse_url(substr($_SERVER['REQUEST_URI'], 1), PHP_URL_PATH) );
-		};
-		/**	Confirm the API_KEY is set.
+		 */
+			function get_uri() {
+				return explode( '/', parse_url(substr($_SERVER['REQUEST_URI'], 1), PHP_URL_PATH) );
+			};
+		/**	confApiKey
+		 *	Confirm the API_KEY is set.
 		 *	@return	array	Ends the API and displays an error
-		*/
-		function confApiKey() {
-			parse_str(QUERY_STRING, $query);
-			if(isset($query['api_key'])) {
-				if(in_array($query['api_key'], __API_KEYS__)) {
-					$active = true;
+		 */
+			function confApiKey() {
+				parse_str(QUERY_STRING, $query);
+				if(isset($query['api_key'])) {
+					if(in_array($query['api_key'], __API_KEYS__)) {
+						$active = true;
+					}
 				}
-			}
-			if(!$active) invalid_request(1);
-		};
-	//	Get URI Var
+				if(!$active) invalid_request(1);
+			};
+		/**	checkVersion
+		 *	Confirm the Controller is accepted.
+		 *	@return	array	Ends the API and displays an error
+		 */
+			  function checkVersion() {
+			   $versions = [];
+			   $query = DB_Query("SELECT `ID`, `Version` FROM `API Versions` WHERE `Active?`=1 AND `Created`<now()");
+			   while($controller = mysqli_fetch_array($query)) {
+				   $versions[$controller['ID']] = $controller['Version'];
+			   }
+			   if(isset($uri[0])) {
+				   if(in_array(strtolower($uri[0]), $versions)) {
+					   $active = true;
+				   }
+			   }
+			   return (!$active)?invalid_request(2): $uri[0];
+			};
+		/**	checkController
+		 *	Confirm the Controller is accepted.
+		 *	@return	array	Ends the API and displays an error
+		 */
+			function checkController() {
+				$controllers = [];
+				$query = DB_Query("SELECT `ID`, `Controller` FROM `API Controllers` WHERE `Active?`=1 AND `Created`<now()");
+				while($controller = mysqli_fetch_array($query)) {
+					$controllers[$controller['ID']] = $controller['Key'];
+				}
+				if(isset($uri[1])) {
+					if(in_array(strtolower($uri[1]), $controllers)) {
+						$active = true;
+					}
+				}
+				return (!$active)?invalid_request(3): $uri[1];
+			};
+		/**	checkHost
+		 *	Confirm the host is accepted.
+		 *	@return	array	Ends the API and displays an error
+		 */
+			function checkHost() {
+				$controllers = [];
+				$query = DB_Query("SELECT `ID`, `Controller` FROM `API Controllers` WHERE `Active?`=1 AND `Created`<now()");
+				while($controller = mysqli_fetch_array($query)) {
+					$controllers[$controller['ID']] = $controller['Key'];
+				}
+				if(isset($uri[1])) {
+					if(in_array(strtolower($uri[1]), $controllers)) {
+						$active = true;
+					}
+				}
+				return (!$active)?invalid_request(3): $uri[1];
+			};
+	//	Get URI Vars
+//		checkHost();
 		$uri = get_uri();
-		$version = $uri[0];
-		$controller = $uri[1];
-		$controllers = Array(
-			'tasks',
-			'cart',
-			'images',
-			'product',
-			'stripe',
-			'users',
-			'website',
-			'page',
-			'supplier',
-			'mail',
-		);
+		$version = checkVersion();
+		$controller = checkController();
 	//	Confirm Controller exists
-		if($controller === "" || !in_array(strtolower($controller), $controllers) ) invalid_request(2);
 ?>
