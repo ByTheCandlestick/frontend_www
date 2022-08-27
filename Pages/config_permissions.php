8d05e2cb1d9a48a92becf9eb82fbb0f3449fddc3
Robert Smith <master_schmadoodle@outlook.com>
2022-08-27 15:53:39 +0100

    Minor changes

--- Pages/config_permissions.php
+++ Pages/config_permissions.php
@@ -112,7 +112,7 @@
             <div class="col-12 col-md-6 text-md-end">
                 <div class="row">
                     <div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
-                        <a href="javascript:config.permissions.save('<?=$permission[QS]['Name']?>', '<?=$permission[QS]['Null']?>', '<?=$permission[QS]['Type']?>')" class="btn btn-outline-primary m-1">
+                        <a href="javascript:config.permissions.save('<?=$permission[QS]['Name']?>')" class="btn btn-outline-primary m-1">
                             <i class="fa fa-save"></i>
                         </a>
                     </div>
