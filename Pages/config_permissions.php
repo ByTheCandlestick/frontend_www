5baff814071cc8b5bd9be4b2f4778158dc87c0f6
Robert Smith <master_schmadoodle@outlook.com>
2022-08-27 15:27:07 +0100

    Minor changes

--- Pages/config_permissions.php
+++ Pages/config_permissions.php
@@ -70,23 +70,14 @@
         <!-- Section Header -->
         <div class="row">
             <div class="col-12 col-md-6">
-                <h1>Users</h1>
+                <h1>New permission</h1>
             </div>
             <div class="col-12 col-md-6 text-md-end">
                 <div class="row">
-                    <div class="col-12 col-lg-6 d-block d-md-flex justify-content-end align-items-center p-0">
-                        <a href="/Users/Perms/" class="btn btn-outline-warning m-1">
-                            <i class="fa fa-key"></i>
-                        </a>
-                        <a href="/Users/New/" class="btn btn-outline-primary m-1">
+                    <div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
+                        <a href="/Config/Permissions/New/" class="btn btn-outline-primary m-1">
                             <i class="fa fa-plus"></i>
                         </a>
-                    </div>
-                    <div class="col-12 col-lg-6">
-                        <div class="form-floating">
-                            <input type="text" class="form-control tableFilter" id="tableSearch" placeholder=" ">
-                            <label for="tableSearch" class="ps-5">Search</label>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -101,23 +92,14 @@
         <!-- Section Header -->
         <div class="row">
             <div class="col-12 col-md-6">
-                <h1>Users</h1>
+                <h1>Edit permission</h1>
             </div>
             <div class="col-12 col-md-6 text-md-end">
                 <div class="row">
-                    <div class="col-12 col-lg-6 d-block d-md-flex justify-content-end align-items-center p-0">
-                        <a href="/Users/Perms/" class="btn btn-outline-warning m-1">
-                            <i class="fa fa-key"></i>
+                    <div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
+                        <a href="javascript:config.permissions.save()" class="btn btn-outline-primary m-1">
+                            <i class="fa fa-save"></i>
                         </a>
-                        <a href="/Users/New/" class="btn btn-outline-primary m-1">
-                            <i class="fa fa-plus"></i>
-                        </a>
-                    </div>
-                    <div class="col-12 col-lg-6">
-                        <div class="form-floating">
-                            <input type="text" class="form-control tableFilter" id="tableSearch" placeholder=" ">
-                            <label for="tableSearch" class="ps-5">Search</label>
-                        </div>
                     </div>
                 </div>
             </div>
