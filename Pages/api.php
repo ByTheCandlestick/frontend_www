6ffbc0f6f224ce92c5c284eb5474193c8f32579a
Robert Smith <master_schmadoodle@outlook.com>
2022-08-27 11:26:46 +0100

    Minor changes to api page

--- Pages/api.php
+++ Pages/api.php
@@ -1,3 +1,89 @@
-<section>
+<?
 
+?>
+<section>
+	<!-- Section Header -->
+	<div class="row">
+		<div class="col-12 col-md-6">
+			<h1>API</h1>
+		</div>
+		<div class="col-12 col-md-6 text-md-end">
+			<div class="row">
+				<div class="col-12 col-lg-6 d-block d-md-flex justify-content-end align-items-center p-0">
+				</div>
+				<div class="col-12 col-lg-6">
+				</div>
+			</div>
+		</div>
+	</div>
+	<hr>
+	<!-- Section Body -->
+	<div class="row overflow-scroll">
+        <div class="col-12 col-md-6 col-lg-4 p-2">
+            <div class="card h-auto h-md-100">
+                <div class="card-header row m-0">
+                    <div class="col-8">
+                        <h4>Allowed Hosts</h4>
+                    </div>
+                    <div class="col-4 text-end">
+                        <?  if($userperm['api_access-hosts-edit']==1) {?>
+                            <a href="/API/allowed_hosts/New/" class="btn btn-outline-primary">
+                                <i class="fa fa-plus"></i>
+                            </a>
+                        <?}?>
+                    </div>
+                </div>
+                <div class="card-body p-0 overflow-auto">
+                    <table class="containersTable table table-striped table-hover m-0">
+                        <thead class="sticky-top">
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Hostname</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?
+                                $query = DB_Query("SELECT * FROM `API Allowed hosts` WHERE `Active?`=1 LIMIT 7");
+                                if(mysqli_num_rows($query) > 0) {
+                                    while ($row = mysqli_fetch_array($query)) {
+                                        print('
+                                            <tr>
+                                                <td>'.$row['Name'].'</td>
+                                                <td>'.$row['Hostname'].'</td>
+                                                <td>
+                                        ');
+                                        if($userperm['api_access-hosts-edit']==1) {
+                                            print('
+                                                    <a href="/API/allowed_hosts/'.$row['ID'].'">
+                                                        <i class="fa fa-pencil"></i>
+                                                    </a>
+                                            ');
+                                        }
+                                        print('
+                                                </td>
+                                            </tr>
+                                        ');
+                                    }
+                                } else {
+                                    print('
+                                        <tr>
+                                            <th scope="row"></th>
+                                            <td>No data found</td>
+                                            <td></td>
+                                        </tr>
+                                    ');
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer text-muted">
+                    <a href="/Products/Containers/">
+                        See more
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
