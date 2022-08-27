834a0febbb27e7b66ab44eac4c2cb52f846cbb1a
Robert Smith <master_schmadoodle@outlook.com>
2022-08-27 14:45:27 +0100

    Minor changes to website permission control

--- Pages/users.php
+++ Pages/users.php
@@ -7,6 +7,9 @@
 		<div class="col-12 col-md-6 text-md-end">
 			<div class="row">
 				<div class="col-12 col-lg-6 d-block d-md-flex justify-content-end align-items-center p-0">
+					<a href="/Users/Perms/" class="btn btn-outline-warn m-1">
+						<i class="fa fa-key"></i>
+					</a>
 					<a href="/Users/New/" class="btn btn-outline-primary m-1">
 						<i class="fa fa-plus"></i>
 					</a>
