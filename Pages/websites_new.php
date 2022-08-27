002d2b5497e87b05d823565a04d6c584f2a91be3
Robert Smith <master_schmadoodle@outlook.com>
2022-08-27 12:40:33 +0100

    Minor changes to api page

--- Pages/websites_new.php
+++ Pages/websites_new.php
@@ -17,43 +17,114 @@
 	<hr>
 	<!-- Section Body -->
 	<div class="row">
-		<div class="col-12">
-			<div class="row">
-				<h5>Site info</h5>
-				<div class="col-12 col-md-6 col-lg-3" name="name">
-					<div class="form-floating mb-3">
-						<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
-						<label for="floatingInput">Name</label>
-					</div>
+		<h5>Site info</h5>
+		<div class="col-12 col-md-6 col-lg-3" name="name">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Name</label>
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-3" name="domain">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Domain</label>
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-3" name="page_type">
+			<div class="form-floating mb-3">
+				<select class="form-select" id="floatingSelect">
+					<option value="-1" selected>Please select</option>
+					<?
+						$query = DB_Query("SELECT * FROM `Website themes` WHERE `Active`=1");
+						while ($row = mysqli_fetch_array($query)) {
+							print_r('<option value="'.$row['ID'].'">'.$row['Name'].'</option>');
+						}
+					?>
+				</select>
+				<label for="floatingInput">Page type</label>
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-3" name="status">
+			<div class="form-floating mb-3">
+				<div class="form-check form-switch">
+					<input class="form-check-input" type="checkbox" name="maintenance" id="flexCheck">
+					<label class="form-check-label" for="flexCheck"> Maintenance? </label>
 				</div>
-				<div class="col-12 col-md-6 col-lg-3" name="domain">
-					<div class="form-floating mb-3">
-						<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
-						<label for="floatingInput">Domain</label>
-					</div>
-				</div>
-				<div class="col-12 col-md-6 col-lg-3" name="page_type">
-					<div class="form-floating mb-3">
-						<select class="form-select" id="floatingSelect">
-							<option value="-1" selected>Please select</option>
-							<?
-								$query = DB_Query("SELECT * FROM `Website themes` WHERE `Active`=1");
-								while ($row = mysqli_fetch_array($query)) {
-									print_r('<option value="'.$row['ID'].'">'.$row['Name'].'</option>');
-								}
-							?>
-						</select>
-						<label for="floatingInput">Page type</label>
-					</div>
-				</div>
-				<div class="col-12 col-md-6 col-lg-3" name="status">
-						<div class="form-floating mb-3">
-							<div class="form-check form-switch">
-								<input class="form-check-input" type="checkbox" name="maintenance" id="flexCheck">
-								<label class="form-check-label" for="flexCheck"> Maintenance? </label>
-							</div>
-						</div>
-					</div>
+			</div>
+		</div>
+		
+		<div class="col-12 col-md-6 col-lg-3" name="title">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Title</label>
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-3" name="slogan">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Slogan</label>
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-3" name="email">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Email</label>
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-3" name="phone">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Phone</label>
+			</div>
+		</div>
+		
+		<div class="col-12 col-md-6 col-lg-3" name="meta_title">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Meta title</label>
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-3" name="meta_keywords">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Meta keywords</label>
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-3" name="meta_description">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Meta description</label>
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-3" name="meta_colour">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Meta colour</label>
+			</div>
+		</div>
+		
+		<div class="col-12 col-md-6 col-lg-3" name="primary_colour">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Primary colour</label>
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-3" name="secondary_colour">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Secondary colour</label>
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-3" name="logo">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Logo</label>
+			</div>
+		</div>
+		<div class="col-12 col-md-6 col-lg-3" name="favicon">
+			<div class="form-floating mb-3">
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<label for="floatingInput">Favicon</label>
 			</div>
 		</div>
 	</div>
