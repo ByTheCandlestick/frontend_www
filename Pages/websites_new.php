2dcaa7f6e61b4a7bd3553d47d3bd3d80cf77a97a
Robert Smith <master_schmadoodle@outlook.com>
2022-08-27 13:41:32 +0100

    Minor changes to website permission control

--- Pages/websites_new.php
+++ Pages/websites_new.php
@@ -130,7 +130,17 @@
 		<hr>
 		<div class="col-12 col-md-6 col-lg-3" name="permission">
 			<div class="form-floating mb-3">
-				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+				<select class="form-select" id="floatingSelect">
+					<option value="-1" selected>Please select</option>
+					<?
+						$query = DB_Query("DESCRIBE `Users_permissions`");
+						while($row = mysqli_fetch_array($query)) {
+							if(preg_match("([a-z]+\_[a-z\-]+)", $row['Field'])) {
+								print_r('<option value="'.$row['Field'].'">'.$row['Field'].'</option>');
+							}
+						}
+					?>
+				</select>
 				<label for="floatingInput">Permission</label>
 			</div>
 		</div>
