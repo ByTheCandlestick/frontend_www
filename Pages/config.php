d95b3399c784eb940bab1113c6f02a465994161d
Robert Smith <master_schmadoodle@outlook.com>
2022-08-28 15:21:06 +0100

    Minor changes to permissions

--- Pages/config.php
+++ Pages/config.php
@@ -12,16 +12,17 @@
 	<!-- Section Body -->
 	<div class="row overflow-scroll">
         <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <h2> General </h2>
             <a href="/Config/Permissions/">Edit permissions</a>
         </div>
         <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            .
+            <h2>  </h2>
         </div>
         <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            .
+            <h2>  </h2>
         </div>
         <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            .
+            <h2>  </h2>
         </div>
     </div>
 </section>
