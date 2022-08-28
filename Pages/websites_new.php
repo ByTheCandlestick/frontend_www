2f042704cd3b82c1565ca4727e3fd416c7b1f203
Robert Smith <master_schmadoodle@outlook.com>
2022-08-28 16:02:08 +0100

    Minor changes to permissions

--- Pages/websites_new.php
+++ Pages/websites_new.php
@@ -130,23 +130,8 @@
 		<hr>
 		<div class="col-12 col-md-6 col-lg-3" name="permission">
 			<div class="form-floating mb-3">
-				<select class="form-select" id="floatingSelect">
-					<option value="-1" selected>Please select</option>
-					<?
-						$query = DB_Query("DESCRIBE `Users_permissions`");
-						while($row = mysqli_fetch_array($query)) {
-							if(preg_match("([a-z]+\_[a-z\-]+)", $row['Field'])) {
-								print_r('<option value="'.$row['Field'].'">'.$row['Field'].'</option>');
-							}
-						}
-					?>
-				</select>
+				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
 				<label for="floatingInput">Permission</label>
-			</div>
-		</div>
-		<div class="col-12 col-lg-6" name="permission">
-			<div class="form-floating mb-3">
-				<a class="btn btn-outline-primary" href="/Config/Permissions/New/"><i class="fa fa-plus"></i> New permission</a>
 			</div>
 		</div>
 	</div>
