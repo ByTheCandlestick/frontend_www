ec19d92512afda190866c7a9db2cf515b7d3aceb
Robert Smith <master_schmadoodle@outlook.com>
2022-08-28 15:17:02 +0100

    Minor changes to permissions

--- Pages/config.php
+++ Pages/config.php
@@ -1,3 +1,15 @@
-<section>
+<?
 
+?>
+<section>
+	<!-- Section Header -->
+	<div class="row">
+		<div class="col-12">
+			<h1>Config</h1>
+		</div>
+	</div>
+	<hr>
+	<!-- Section Body -->
+	<div class="row overflow-scroll">
+    </div>
 </section>
