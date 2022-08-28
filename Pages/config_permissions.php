2a36ce5ec353832345c1d11f2ecfa83e2cfafaf0
Robert Smith <master_schmadoodle@outlook.com>
2022-08-28 13:17:17 +0100

    Minor changes to permissions

--- Pages/config_permissions.php
+++ Pages/config_permissions.php
@@ -85,6 +85,32 @@
         <hr>
         <!-- Section Body -->
         <div class="row overflow-scroll">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="floatingInput" value="">
+                        <label for="floatingInput">Name</label>
+                    </div>
+                </div>
+                <div class="col-8 col-md-4 col-lg-2 d-none">
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="floatingInput" value="tinyint(1)">
+                        <label for="floatingInput">Type</label>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="floatingInput" value="0">
+                        <label for="floatingInput">Default</label>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3 d-none">
+					<div class="form-check form-switch">
+						<input class="form-check-input" type="checkbox" name="maintenance" id="flexCheck">
+						<label class="form-check-label" for="flexCheck">Nullable?</label>
+					</div>
+                </div>
+            </div>
         </div>
     </section>
 <?} else {?>
