3338174ea42b0ceddae40fe8ff131273c8e0d161
Robert Smith <master_schmadoodle@outlook.com>
2022-08-29 17:06:45 +0100

    Minor changes to permissions

--- API/v1/Models/ProductModel.php
+++ API/v1/Models/ProductModel.php
@@ -199,10 +199,10 @@
 				$productCode = ($q = $this->Execute("SELECT `SKU` FROM `products` ORDER BY SKU DESC LIMIT 1", 3) > 9999)? $q+1: "10001";
 				$odds = preg_split("/([0-9])[0-9]/", $manufacturer.$productCode);
 				$evens = preg_split("/[0-9]([0-9])/", $manufacturer.$productCode);
-				print_r($numberSystem);
-				print_r($manufacturer);
-				print_r($productCode);
-				print_r($checkCode);
+				print_r('n: '.$numberSystem);
+				print_r('m: '.$manufacturer);
+				print_r('p: '.$productCode);
+				print_r('c: '.$checkCode);
 				print_r($evens);
 				print_r($odds);
 				$checkCode = '';
