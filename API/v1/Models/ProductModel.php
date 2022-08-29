3643e1fe7f2cf8ab56ca97e86993e23477acdc61
Robert Smith <master_schmadoodle@outlook.com>
2022-08-29 22:18:02 +0100

    Minor changes to API

--- API/v1/Models/ProductModel.php
+++ API/v1/Models/ProductModel.php
@@ -195,7 +195,7 @@
 			*/
 			public function createSKU() {
 				if($this->Execute("SELECT `SKU` FROM `products` ORDER BY SKU DESC LIMIT 1", 5)>0) {
-					return print_r(intval($this->Execute("SELECT `SKU` FROM `products` ORDER BY SKU DESC LIMIT 1", 4)['SKU']));
+					return print_r(intval($this->Execute("SELECT `SKU` FROM `products` ORDER BY SKU DESC LIMIT 1", 3)['SKU']));
 				} else {
 					return 10001;
 				}
