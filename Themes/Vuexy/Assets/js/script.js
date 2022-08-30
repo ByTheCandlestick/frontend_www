55ed7cc82a863873f978648c938919f01c45e578
Robert Smith <master_schmadoodle@outlook.com>
2022-08-30 16:43:36 +0100

    Minor changes to script for admin vuexy

--- Themes/Vuexy/Assets/js/script.js
+++ Themes/Vuexy/Assets/js/script.js
@@ -930,7 +930,7 @@
 				data[name] = isChecked;
 			});
 			$.ajax({
-				url: api_url + '/User/Perms/' + uid + '/',
+				url: api_url + '/Users/Perms/' + uid + '/',
 				data: data,
 				type: 'POST',
 				xhrFields: {
