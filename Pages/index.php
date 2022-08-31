d3d28631fd130e764cea6f5dc28f5f99412653ed
Robert Smith <master_schmadoodle@outlook.com>
2022-08-31 21:25:28 +0100

    Minor changes ot repair the dashboard

--- Pages/index.php
+++ Pages/index.php
@@ -71,8 +71,8 @@
 			<div class="card h-100">
 				<div class="card-body">
 					<h5 class="card-title">SALES YoY</h5>
-					<p class="card-text row">
 						<div class="col-8">
+						<p class="card-text">
 							<span>
 								<?
 									if($currYearIncome[1] == 0) {
@@ -103,10 +103,10 @@
 		</div>
 		<div class="col-12 col-md-6 col-lg-3 p-2">
 			<div class="card h-100">
-				<div class="card-body">
-					<h5 class="card-title">INCOME YoY</h5>
-					<p class="card-text row">
-						<div class="col-8">
+				<div class="card-body row">
+					<h5 class="col-12 card-title">INCOME YoY</h5>
+					<div class="col-8">
+						<p class="card-text">
 							<span>
 								<?
 									if($currYearIncome[1] == 0 && $currYearExpences[1] == 0) {
@@ -128,10 +128,10 @@
 									}
 								?>
 							</span>
-						</div>
-						<div class="col-4">
-						</div>
-					</p>
+						</p>
+					</div>
+					<div class="col-4">
+					</div>
 				</div>
 			</div>
 		</div>
