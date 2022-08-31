41c17c402a317701f6816217b2cc3576f27fb50c
Robert Smith <master_schmadoodle@outlook.com>
2022-08-31 22:31:09 +0100

    Minor cahnges and additional files leading to the API pages

--- Pages/api_keys.php
+++ Pages/api_keys.php
@@ -2,13 +2,13 @@
 	<!-- Section Header -->
 	<div class="row">
 		<div class="col-12 col-md-6">
-			<h1>Controllers</h1>
+			<h1>Keys</h1>
 		</div>
 		<div class="col-12 col-md-6 text-md-end">
 			<div class="row">
 				<div class="col-12 d-flex justify-content-end align-items-center p-0">
-					<?  if($userperm['api_access-controllers-edit']==1) {?>
-						<a href="/API/Controller/New/" class="btn btn-outline-primary m-1">
+					<?  if($userperm['api_access-keys-edit']==1) {?>
+						<a href="/API/Key/New/" class="btn btn-outline-primary m-1">
 							<i class="fa fa-plus"></i>
 						</a>
 					<?}?>
