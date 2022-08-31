03358d14bb1e8f9376820443ddc9395f2be53097
Robert Smith <master_schmadoodle@outlook.com>
2022-08-31 18:23:43 +0100

    Minor changes to website mode

--- Themes/Vuexy/Assets/js/script.js
+++ Themes/Vuexy/Assets/js/script.js
@@ -67,7 +67,6 @@
 			let date = new Date();
 			date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
 			document.cookie = name + "=" + value + "; " + "expires=" + date.toUTCString() + "; path="+path;
-
 		},
 		/** @final */
 		read(name) {
