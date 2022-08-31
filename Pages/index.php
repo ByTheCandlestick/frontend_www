56bd7805e1003ba5b293ea2268dab76fcbc2d5c9
Robert Smith <master_schmadoodle@outlook.com>
2022-08-31 21:26:11 +0100

    Minor changes ot repair the dashboard

--- Pages/index.php
+++ Pages/index.php
@@ -69,8 +69,8 @@
 	<div class="row" name="Sales snippets">
 		<div class="col-12 col-md-6 col-lg-3 p-2">
 			<div class="card h-100">
-				<div class="card-body">
-					<h5 class="card-title">SALES YoY</h5>
+				<div class="card-body row">
+					<h5 class="col-12 card-title">SALES YoY</h5>
 					<div class="col-8">
 						<p class="card-text">
 							<span>
