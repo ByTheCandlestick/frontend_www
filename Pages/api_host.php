a641ca581ddf3ce993d06311b18f815b2d2901c9
Robert Smith <master_schmadoodle@outlook.com>
2022-09-01 15:50:44 +0100

    Minor changes to website mode

---
+++ Pages/api_host.php
@@ -0,0 +1,73 @@
+<?
+	if(strToLower(QS) == "new"){
+?>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6">
+				<h1>New Host</h1>
+			</div>
+			<div class="col-12 col-md-6 text-md-end">
+			<div class="row">
+				<div class="col-12 d-flex justify-content-end align-items-center p-0">
+					<a href="javascript:api.host.create();" class="btn btn-outline-primary m-1">
+						<i class="fa fa-save"></i>
+					</a>
+				</div>
+			</div>
+			</div>
+		</div>
+		<hr>
+		<!-- Section Body -->
+		<div class="row">
+		</div>
+	</section>
+<?
+	} else if($query = DB_Query(sprintf("SELECT * FROM `API Hosts` WHERE `ID`=%s", QS))) {
+		if(mysqli_num_rows($query) > 0) {
+			$host = mysqli_fetch_assoc($query);
+?>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6">
+				<h1>Edit Host</h1>
+			</div>
+			<div class="col-12 col-md-6 text-md-end">
+				<div class="row">
+					<div class="col-12 d-flex justify-content-end align-items-center p-0">
+						<a href="javascript:api.host.update(<?print(QS)?>);" class="btn btn-outline-primary m-1">
+							<i class="fa fa-save"></i>
+						</a>
+						<a href="javascript:api.host.delete(<?print(QS)?>);" class="btn btn-outline-danger m-1">
+							<i class="fa fa-trash-alt"></i>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr>
+		<!-- Section Body -->
+		<div class="row">
+			<div class="col-12 col-md-6 col-lg-4" name="name">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?=$host['Host']?>">
+					<label for="floatingInput">Name</label>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-1" name="status">
+				<div class="form-floating mb-3">
+					<div class="form-check form-switch">
+						<input class="form-check-input" type="checkbox" name="active" id="flexCheck" <?=($host['Active?']==1)? "checked": ""?>>
+						<label class="form-check-label" for="flexCheck"> Active? </label>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+<?
+		}
+	} else {
+		header('location: /Error/404/');
+	}
+?>
