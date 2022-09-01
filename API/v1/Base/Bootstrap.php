c9bc14db076c7f32b3113db36bb65f8d396ca277
Robert Smith <master_schmadoodle@outlook.com>
2022-09-01 20:47:44 +0100

    Minor changes to API script

--- API/v1/Base/Bootstrap.php
+++ API/v1/Base/Bootstrap.php
@@ -1,25 +1,7 @@
 <?
 	print($_SERVER['REMOTE_ADDR']);
-	// Define all vars
-		/* */if(isset($_SERVER["HTTP_REFERER"])) {
-			$referrer = $_SERVER["HTTP_REFERER"];
-		}elseif(isset($_SERVER['REMOTE_ADDR'])) {
-			$referrer = $_SERVER['REMOTE_ADDR'];
-		}else {
-			$referrer = $_SERVER["SERVER_NAME"];
-		}
-		if(str_contains($referrer, "indev")) {	#	THE WESBITE IN IN DEVELOPMENT MODE
-			define('STRIPE_API',	'sk_test_51JKqfVFDFLz8LpozmlliBbv92XkspmRyy2O7G6IMk2IccfP9ZnimCZ8rJHHCVfIGupLx5FJZafa92igVC2HFWPkz00umY4pOUm');
-			define('ANALYTICS',		['db5007323454.hosting-data.io',	'dbu557431',	'CandleStick2603',	'dbs6034000']);
-			define('ADMIN',			['db5007323432.hosting-data.io',	'dbu3023777',	'CandleStick2603',	'dbs6033983']);
-		} else {												#	THE WEBSITE IS LAUNCHED AND IS FULLY RELEASED
-			define('STRIPE_API',	'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');
-			define('ANALYTICS',		['db5007301242.hosting-data.io',	'dbu235049',	'CandleStick2603',	'dbs6015868']);
-			define('ADMIN', 		['db5007320590.hosting-data.io',	'dbu1278426',	'CandleStick2603',	'dbs6031251']);
-		}
-		define('__ROOT__',			$_SERVER['DOCUMENT_ROOT']);
 	//	Get the query string for each REST type
-		if($_SERVER['REQUEST_METHOD'] == "GET") {				// 
+		/* */if($_SERVER['REQUEST_METHOD'] == "GET") {				// 
 			$keys = array_keys($_GET);
 			$vals = $_GET;
 			$queries = array();
@@ -27,9 +9,9 @@
 				array_push($queries, $keys[$i].'='.$vals[$keys[$i]]);
 			}
 			define('QUERY_STRING',	implode('&', $queries));
-		} elseif($_SERVER['REQUEST_METHOD'] == "PUT") {			// DONE
+		}elseif($_SERVER['REQUEST_METHOD'] == "PUT") {			// DONE
 			define('QUERY_STRING',	file_get_contents("php://input"));
-		} elseif($_SERVER['REQUEST_METHOD'] == "POST") {		//
+		}elseif($_SERVER['REQUEST_METHOD'] == "POST") {		//
 			$keys = array_keys($_POST);
 			$vals = $_POST;
 			$queries = array();
@@ -37,7 +19,7 @@
 				array_push($queries, $keys[$i].'='.$vals[$keys[$i]]);
 			}
 			define('QUERY_STRING',	implode('&', $queries));
-		} elseif($_SERVER['REQUEST_METHOD'] == "DELETE") {		// 
+		}elseif($_SERVER['REQUEST_METHOD'] == "DELETE") {		// 
 			define('QUERY_STRING',	file_get_contents("php://input"));
 		}
 	//	Functions
@@ -81,6 +63,17 @@
 				}
 				if(!$active) invalid_request(1);
 			};
+		/**	denyHost
+		 * 	Checks whether the API key allows for the function to be completed on any host
+		 *	@return bool
+		 */
+			function denyHost() {
+				parse_str(QUERY_STRING, $query);
+				if(isset($query['api_key'])) {
+					return (mysqli_fetch_array(DB_Query(sprintf("SELECT `Deny hosts?` FROM `API Keys` WHERE `Key`='%s'", $query['api_key'])))[0])? true: false;
+				}
+				if(!$active) invalid_request(1);
+			}
 		/**	checkVersion
 		 *	Confirm the Controller is accepted.
 		 *	@param	array	$uri
@@ -124,10 +117,10 @@
 		 *	@return	array	Ends the API and displays an error
 		 */
 			function checkHost(string $origin) {
-				return (!mysqli_fetch_array(DB_Query("SELECT COUNT(*) FROM `API Allowed hosts` WHERE `Active?`=1 AND `Created`<now() AND (`Hostname`='$origin' OR `Remote address`='$origin')"))[0])?invalid_request(4): true;
+				return if(!mysqli_fetch_array(DB_Query("SELECT COUNT(*) FROM `API Allowed hosts` WHERE `Active?`=1 AND `Created`<now() AND (`Hostname`='$origin' OR `Remote address`='$origin')"))[0]) invalid_request(4);
 			};
 	//	Get URI Vars
-		if(checkHost($referrer)) {
+		if((denyHost() && checkHost($referrer)) || !denyHost()) {
 			header('Access-Control-Allow-Credentials: true');
 			header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
 			header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
