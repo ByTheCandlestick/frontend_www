26dd208c8998e5e8f0ae848e3ac26df743bfeb38
Robert Smith <master_schmadoodle@outlook.com>
2022-09-01 15:55:00 +0100

    Minor changes to website mode

--- Pages/api_host.php
+++ Pages/api_host.php
@@ -20,6 +20,26 @@
 		<hr>
 		<!-- Section Body -->
 		<div class="row">
+			<div class="col-12 col-md-6 col-lg-4" name="name">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+					<label for="floatingInput">Name</label>
+				</div>
+			</div>
+			<div class="col-1
+			<div class="col-12 col-md-6 col-lg-4" name="name">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+					<label for="floatingInput">Name</label>
+				</div>
+			</div>2 col-md-6 col-lg-1" name="status">
+				<div class="form-floating mb-3">
+					<div class="form-check form-switch">
+						<input class="form-check-input" type="checkbox" name="active" id="flexCheck" >
+						<label class="form-check-label" for="flexCheck"> Active? </label>
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 <?
