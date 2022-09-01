93442101b83d4b788ffd53c6e5a2692062eb5bf5
Robert Smith <master_schmadoodle@outlook.com>
2022-09-01 21:49:41 +0100

    Minor changes to API script

--- Pages/websites_pages.php
+++ Pages/websites_pages.php
@@ -1,12 +1,19 @@
 <?
-	$page = mysqli_fetch_assoc(DB_Query(sprintf("SELECT * FROM `Website domains` WHERE `ID`=%s", QS)));
+	$website = mysqli_fetch_assoc(DB_Query(sprintf("SELECT * FROM `Website domains` WHERE `ID`=%s", QS)));
+    $pages = array();
+	$pages_per_page = 100;
+	$total_pages = mysqli_fetch_row(DB_Query("SELECT COUNT(*) FROM `Website pages`"))[0];
+	$offset = (QS_SUBPAGE !== null)?(intval(QS_SUBPAGE)-1)*$pages_per_page :0;
+    $q = DB_Query("SELECT * FROM `Website domains` ORDER BY `ID` DESC LIMIT $pages_per_page OFFSET $offset");
+	while($page = mysqli_fetch_assoc($q)) { array_push($pages, $page); }
 ?>
 <section>
 	<!-- Section Header -->
 	<div class="row">
 		<div class="col-12 col-md-6">
 			<h1>Pages</h1>
-			<h3>url: <?print($page['Domain'])?></h3>
+			<h3>url: <?print($website['Domain'])?></h3>
+			<p>Displaying: <?=($offset > 1)? ($offset + 1).'-'.($offset + count($pages)): count($pages);?>/<?=$total_pages?> Rows</p>
 		</div>
 		<div class="col-12 col-md-6 text-md-end">
 			<div class="row">
@@ -34,27 +41,49 @@
 					<th scope="col">URL</th>
 					<th scope="col">Sub URL</th>
 					<th scope="col">Menu order</th>
-					<th scope="col"></th>
 				</tr>
 			</thead>
 			<tbody>
 				<?
-					$query = DB_Query(sprintf("SELECT * FROM `Website pages` WHERE `domain_id`=%s ORDER BY CASE WHEN `menu_order`=0 THEN 99+`ID` ELSE `menu_order` END, `ID` ASC", QS));
-					while($row = mysqli_fetch_array($query)) {
+					if(count($pages) > 0) {
+						foreach($pages as $x) {
+							print('
+								<tr>
+									<th scope="row"><a href="/Websites/Page/'.$row['ID'].'">'.$row['page_name'].'<a/></th>
+									<td>'.$row['page_url'].'</td>
+									<td>'.$row['subpage_url'].'</td>
+									<td>'.(($row['menu_order']=='0')?null:$row['menu_order']).'</td>
+								</tr>
+							');
+						}
+					} else {
 						print('
 							<tr>
-								<th scope="row"><a href="/Websites/Page/'.$row['ID'].'">'.$row['page_name'].'<a/></th>
-								<td>'.$row['page_url'].'</td>
-								<td>'.$row['subpage_url'].'</td>
-								<td>'.(($row['menu_order']=='0')?null:$row['menu_order']).'</td>
-								<td>
-								</td>
+								<th scope="row"></th>
+								<td>No data found</td>
+								<td></td>
+								<td></td>
 							</tr>
 						');
 					}
 				?>
 			</tbody>
 		</table>
+		<?
+			(intval(QS_SUBPAGE) > 1)? $prev_status = '': $prev_status = ' disabled';
+			($prev_status == '')? $prev_page = "/Websites/Pages/".(intval(QS_SUBPAGE) - 1).'/' : $prev_page = "";
+			(($offset + $pages_per_page) < $total_pages)? $next_status = '': $next_status = ' disabled';
+			($next_status == '')? $next_page = "/Websites/Pages/".(intval(QS_SUBPAGE) + 1).'/' : $next_page = "";
+			// Previous/Next page button
+			print("
+				<div class=\"row\">
+					<div class=\"col-12 col-md-4 offset-md-4 d-flex\">
+						<a class=\"col-4 offset-1 col-md-5 offset-md-0 mt-2 mb-3 d-block btn btn-secondary$prev_status\" href=\"$prev_page\" role=\"button\">Previous</a>
+						<a class=\"col-4 offset-2 col-md-5 offset-md-2 mt-2 mb-3 d-block btn btn-secondary$next_status\" href=\"$next_page\" role=\"button\">Next</a>
+					</div>
+				</div>
+			");
+		?>
 	</div>
 </section>
 <script>
