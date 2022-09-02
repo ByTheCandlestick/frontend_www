1a83b7b5c7a4de711cfc77a057688d04c5754342
Robert Smith <master_schmadoodle@outlook.com>
2022-09-02 08:41:55 +0100

    Minor changes to List layout

--- Pages/api_hosts.php
+++ Pages/api_hosts.php
@@ -41,7 +41,7 @@
 				<?
 					if(count($hosts) > 0) {
 						foreach($hosts as $x) {
-							$editable = ($userperm['adm_access-hosts-edit']==1)?'<a href="/Hosts/View/'.$x['Invoice ID'].'">'.$x['Invoice ID'].'</a>':$x['Invoice ID'];
+							$editable = ($userperm['adm_access-hosts-edit']==1)?'<a href="/API/Host/'.$x['ID'].'">'.$x['Name'].'</a>':$x['Name'];
 							print('
 								<tr>
 									<th scope="row">'.$editable.'</th>
