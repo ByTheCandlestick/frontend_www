1d561e856e7b76b16e36ed6bdf582135a5cb6243
Robert Smith <master_schmadoodle@outlook.com>
2022-09-05 14:21:50 +0100

    Minor changes to analytics

--- Pages/analytics.php
+++ Pages/analytics.php
@@ -21,113 +21,119 @@
 	<hr>
 	<!-- Section Body -->
 	<div class="row">
-		<div class="row col-12 col-md-6">
-			<h3>Pages</h3>
-			<div class="col-12 col-md-6 h-100">
-				<h5>Page referrers</h5>
-				<chart class="ct-page-referrers" />
-				<script>
-					new Chartist.Line('.ct-page-referrers', {
-						labels: ['<?print(implode('\', \'', $days))?>'],
-						series: [
-							[0,1,2,3,4,5,6],
-							[1,2,3,4,5,6,7],
-							[2,3,4,5,6,7,8],
-						]
-					}, {
-						fullWidth: true,
-						showArea: true,
-						showLine: false,
-						chartPadding: {
-							right: 40
-						}
-					});
-				</script>
-			</div>
-			<div class="col-12 col-md-6 h-100">
-				<h5>Interactions</h5>
-				<chart class="ct-interactions" />
-				<script>
-					new Chartist.Line('.ct-interactions', {
-						labels: ['L0', 'L1', 'L2', 'L3'],
-						series: [
-							[0,1,2,3]
-						]
-					}, {
-						fullWidth: true,
-						showArea: true,
-						showLine: false,
-						chartPadding: {
-							right: 40
-						}
-					});
-				</script>
-			</div>
-		</div>
-		<div class="row col-12 col-md-6">
-			<h3>Products</h3>
-			<div class="col-12 col-md-6 h-100">
-				<h5>Product sales / day</h5>
-				<chart class="ct-prod-sales-day" />
-				<script>
-					new Chartist.Line('.ct-prod-sales-day', {
-						labels: ['L0', 'L1', 'L2', 'L3'],
-						series: [
-							[0,1,2,3]
-						]
-					}, {
-						fullWidth: true,
-						showArea: true,
-						showLine: false,
-						chartPadding: {
-							right: 40
-						}
-					});
-				</script>
-			</div>
-			<div class="col-12 col-md-6 h-100">
-				<h5>Product popularity</h5>
-				<chart class="ct-prod-popularity" />
-				<script>
-					new Chartist.Line('.ct-prod-popularity', {
-						labels: ['L0', 'L1', 'L2', 'L3'],
-						series: [
-							[0,1,2,3]
-						]
-					}, {
-						fullWidth: true,
-						showArea: true,
-						showLine: false,
-						chartPadding: {
-							right: 40
-						}
-					});
-				</script>
+		<div class="col-12 col-md-6">
+			<div class="row">
+				<h3>Pages</h3>
+				<div class="col-12 col-md-6 h-100">
+					<h5>Page referrers</h5>
+					<chart class="ct-page-referrers" />
+					<script>
+						new Chartist.Line('.ct-page-referrers', {
+							labels: ['<?print(implode('\', \'', $days))?>'],
+							series: [
+								[0,1,2,3,4,5,6],
+								[1,2,3,4,5,6,7],
+								[2,3,4,5,6,7,8],
+							]
+						}, {
+							fullWidth: true,
+							showArea: true,
+							showLine: false,
+							chartPadding: {
+								right: 40
+							}
+						});
+					</script>
+				</div>
+				<div class="col-12 col-md-6 h-100">
+					<h5>Interactions</h5>
+					<chart class="ct-interactions" />
+					<script>
+						new Chartist.Line('.ct-interactions', {
+							labels: ['L0', 'L1', 'L2', 'L3'],
+							series: [
+								[0,1,2,3]
+							]
+						}, {
+							fullWidth: true,
+							showArea: true,
+							showLine: false,
+							chartPadding: {
+								right: 40
+							}
+						});
+					</script>
+				</div>
 			</div>
 		</div>
-		<div class="row col-12 col-md-6">
-			<h3>Users</h3>
-			<div class="col-12 col-md-6">
-				<h5>Users created / day</h5>
-				<chart class="ct-users-created-day" />
-				<script>
-					new Chartist.Line('.ct-users-created-day', {
-						labels: ['L0', 'L1', 'L2', 'L3'],
-						series: [
-							[0,1,2,3]
-						]
-					}, {
-						fullWidth: true,
-						showArea: true,
-						showLine: false,
-						chartPadding: {
-							right: 40
-						}
-					});
-				</script>
+		<div class="col-12 col-md-6">
+			<div class="row">
+				<h3>Products</h3>
+				<div class="col-12 col-md-6 h-100">
+					<h5>Product sales / day</h5>
+					<chart class="ct-prod-sales-day" />
+					<script>
+						new Chartist.Line('.ct-prod-sales-day', {
+							labels: ['L0', 'L1', 'L2', 'L3'],
+							series: [
+								[0,1,2,3]
+							]
+						}, {
+							fullWidth: true,
+							showArea: true,
+							showLine: false,
+							chartPadding: {
+								right: 40
+							}
+						});
+					</script>
+				</div>
+				<div class="col-12 col-md-6 h-100">
+					<h5>Product popularity</h5>
+					<chart class="ct-prod-popularity" />
+					<script>
+						new Chartist.Line('.ct-prod-popularity', {
+							labels: ['L0', 'L1', 'L2', 'L3'],
+							series: [
+								[0,1,2,3]
+							]
+						}, {
+							fullWidth: true,
+							showArea: true,
+							showLine: false,
+							chartPadding: {
+								right: 40
+							}
+						});
+					</script>
+				</div>
 			</div>
-			<div class="col-12 col-md-6">
-				<h5></h5>
+		</div>
+		<div class="col-12 col-md-6">
+			<div class="row">
+				<h3>Users</h3>
+				<div class="col-12 col-md-6">
+					<h5>Users created / day</h5>
+					<chart class="ct-users-created-day" />
+					<script>
+						new Chartist.Line('.ct-users-created-day', {
+							labels: ['L0', 'L1', 'L2', 'L3'],
+							series: [
+								[0,1,2,3]
+							]
+						}, {
+							fullWidth: true,
+							showArea: true,
+							showLine: false,
+							chartPadding: {
+								right: 40
+							}
+						});
+					</script>
+				</div>
+				<div class="col-12 col-md-6">
+					<h5></h5>
+				</div>
 			</div>
 		</div>
 	</div>
