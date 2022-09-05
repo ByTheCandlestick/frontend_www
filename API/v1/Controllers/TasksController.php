8abc052221f1373c04df3a207207e4e1c905c4a5
Robert Smith <master_schmadoodle@outlook.com>
2022-09-05 13:48:03 +0100

    Added CronLogs

--- API/v1/Controllers/TasksController.php
+++ API/v1/Controllers/TasksController.php
@@ -72,6 +72,42 @@
                 );
             // End of function
         }
+        /** "/Tasks/Biannually/" Endpoint - Executes all Biannual tasks
+		 *	@final Complete
+		 *	@return JSON
+		 */
+		public function Biweekly() {
+			// Vars
+				$mdl_tasks = new TasksModel();
+				$requestMethod = $_SERVER['REQUEST_METHOD'];
+				$arr_user_info = $this->getQueryStringParams();
+				$str_response = "";
+			// Functions									☐ Incomplete / 🗹 Complete / 🗷 VOID
+				/**/if(strtoupper($requestMethod) == "PUT"):	// (C)REATE	-- 🗹 --	Unknown
+                    $this->throwError("Unknown Request type for this function", "HTTP/1.1 404 Not Found");
+                elseif(strtoupper($requestMethod) == "GET"):	// (R)EAD	-- 🗹 --	Executes all tasks
+                    // Functions
+                    try {
+
+                    } catch(Error $er) {
+                        exit($this->throwError($er->getMessage(), "HTTP/1.1 500 Internal Server Error"));
+                    }
+                    // Submit application
+                    $str_response = json_encode(array('status'=>'Successfully run \'Biannual\' tasks;'));
+                elseif(strtoupper($requestMethod) == "POST"):	// (U)PDATE	-- 🗷 --	Unknown
+                    $this->throwError("Unknown Request type for this function", "HTTP/1.1 404 Not Found");
+                elseif(strtoupper($requestMethod) == "DELETE"):	// (D)ELETE	-- 🗷 --	Unknown
+                    $this->throwError("Unknown Request type for this function", "HTTP/1.1 404 Not Found");
+                else:
+                    $this->throwError("Unknown Request type for this function", "HTTP/1.1 404 Not Found");
+                endif;
+            // Send output
+                $this->sendOutput(
+                    $str_response,
+                    array("Content-Type: application/json", "HTTP/1.1 200 OK")
+                );
+            // End of function
+        }
 		/** "/Tasks/Monthly/" Endpoint - Executes all monthly tasks
 		 *	@final Complete
 		 *	@return JSON
