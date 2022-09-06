e32550cddc31da960b825c44342187a4dd579cdd
Robert Smith <master_schmadoodle@outlook.com>
2022-09-06 16:57:55 +0100

    Minor changes to editing product Colour

--- Pages/products_colour.php
+++ Pages/products_colour.php
@@ -159,12 +159,6 @@
 					<label for="floatingInput">Price (g)</label>
 				</div>
 			</div>
-			<div class="col-12 col-md-6 col-lg-3" name="price_e">
-				<div class="form-floating mb-3">
-					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?=($cont['Price (cl)'])?>" disabled>
-					<label for="floatingInput">Price (cl)</label>
-				</div>
-			</div>
 			<div class="col-12 col-md-6 col-lg-2" name="status">
 				<div class="form-floating mb-3">
 					<div class="form-check form-switch">
