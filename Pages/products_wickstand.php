eafa7082da63d1ab8f85f5dade622e042dcc1414
Robert Smith <master_schmadoodle@outlook.com>
2022-09-06 12:25:35 +0100

    Minor changes to editing product Wick Stands

--- Pages/products_wickstand.php
+++ Pages/products_wickstand.php
@@ -5,7 +5,7 @@
 	<!-- Section Header -->
 	<div class="row">
 		<div class="col-12 col-md-6">
-			<h1>Edit wickstand</h1>
+			<h1>Edit wick stand</h1>
 		</div>
 		<div class="col-12 col-md-6 text-md-end">
 			<div class="row">
@@ -88,7 +88,7 @@
 		<!-- Section Header -->
 		<div class="row">
 			<div class="col-12 col-md-6">
-				<h1>Edit wickstand</h1>
+				<h1>Edit wick stand</h1>
 			</div>
 			<div class="col-12 col-md-6 text-md-end">
 				<div class="row">
