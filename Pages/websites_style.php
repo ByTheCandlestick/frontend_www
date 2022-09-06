488e7479a176fdb131ea8f910cc1b876e9dcb80e
Robert Smith <master_schmadoodle@outlook.com>
2022-09-06 23:14:48 +0100

    Minor changes to edit script page

--- Pages/websites_style.php
+++ Pages/websites_style.php
@@ -35,7 +35,7 @@
             </div>
             <div class="col-12 col-md-6 col-lg-4" name="importance">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+                    <input type="number" class="form-control" id="floatingInput" placeholder="" value="">
                     <label for="floatingInput">Importance</label>
                 </div>
             </div>
@@ -94,7 +94,7 @@
             </div>
             <div class="col-12 col-md-6 col-lg-4" name="importance">
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="<? print(($style['Importance']=='')?'No base URL was set':'')?>" value="<? print(($style['Importance']=='')?'':$style['Importance'])?>">
+                    <input type="number" class="form-control" id="floatingInput" placeholder="<? print(($style['Importance']=='')?'No base URL was set':'')?>" value="<? print(($style['Importance']=='')?'':$style['Importance'])?>">
                     <label for="floatingInput">Importance</label>
                 </div>
             </div>
