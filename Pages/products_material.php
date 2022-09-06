c04d1526a1392b29ff3016d3480b25a0caff1fab
Robert Smith <master_schmadoodle@outlook.com>
2022-09-06 13:13:09 +0100

    Minor changes to editing product Wick Stands

--- Pages/products_material.php
+++ Pages/products_material.php
@@ -134,13 +134,13 @@
 					<label for="floatingInput">Supplier Reference</label>
 				</div>
 			</div>
-			<div class="col-12 col-md-6 col-lg-2" name="price_b">
+			<div class="col-12 col-md-6 col-lg-3" name="price_b">
 				<div class="form-floating mb-3">
 					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($material['Price (bulk)'])?>">
 					<label for="floatingInput">Price (bulk)</label>
 				</div>
 			</div>
-			<div class="col-12 col-md-6 col-lg-3" name="quantity">
+			<div class="col-12 col-md-6 col-lg-1" name="quantity">
 				<div class="form-floating mb-3">
 					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($material['Size (kg)'])?>">
 					<label for="floatingInput">Size (kg)</label>
@@ -152,7 +152,7 @@
 					<label for="floatingInput">Price (g)</label>
 				</div>
 			</div>
-			<div class="col-12 col-md-6 col-lg-2" name="quantity">
+			<div class="col-12 col-md-6 col-lg-1" name="quantity">
 				<div class="form-floating mb-3">
 					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($material['Size (cl)'])?>" disabled>
 					<label for="floatingInput">Size (cl)</label>
