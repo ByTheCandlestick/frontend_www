7b8a14a69537bb1332d89a90b6f49da37e2181e7
Robert Smith <master_schmadoodle@outlook.com>
2022-09-06 12:17:52 +0100

    Minor changes to editing product Wick

--- Pages/products_container.php
+++ Pages/products_container.php
@@ -82,7 +82,7 @@
 		</div>
 	</section>
 <?  } elseif(mysqli_num_rows($query = DB_Query(sprintf("SELECT * FROM `Product containers` WHERE `ID`=%s", QS))) > 0) {
-        $wick = mysqli_fetch_assoc($query);
+        $cont = mysqli_fetch_assoc($query);
 ?>
 	<section>
 		<!-- Section Header -->
@@ -108,7 +108,7 @@
 		<div class="row containers">
 			<div class="col-12 col-md-6 col-lg-3" name="name">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Name'])?>">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Name'])?>">
 					<label for="floatingInput">Name</label>
 				</div>
 			</div>
@@ -120,7 +120,7 @@
 						<?
 							$query = DB_Query("SELECT * FROM `Suppliers` WHERE `Active`=1");
 							while ($row = mysqli_fetch_array($query)) {
-								($row['Reference'] == $wick['Supplier'])? $selected=' selected' : $selected='';
+								($row['Reference'] == $cont['Supplier'])? $selected=' selected' : $selected='';
 								print_r('<option value="'.$row['Reference'].'"'.$selected.'>'.$row['Name'].'</option>');
 							}
 						?>
@@ -130,50 +130,38 @@
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="supplierref">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['ItemRef'])?>">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['ItemRef'])?>">
 					<label for="floatingInput">Supplier Reference</label>
 				</div>
 			</div>
-			<div class="col-12 col-md-6 col-lg-3" name="width">
+			<div class="col-12 col-md-6 col-lg-3" name="size">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Width (mm)'])?>">
-					<label for="floatingInput">Width (mm)</label>
-				</div>
-			</div>
-			<div class="col-12 col-md-6 col-lg-3" name="height">
-				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Height (mm)'])?>">
-					<label for="floatingInput">Height (cl)</label>
-				</div>
-			</div>
-			<div class="col-12 col-md-6 col-lg-3" name="thickness">
-				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Thickness (mm)'])?>">
-					<label for="floatingInput">Thickness (mm)</label>
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Size (cl)'])?>">
+					<label for="floatingInput">size (cl)</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="price_b">
 				<div class="form-floating mb-3">
-					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Price (bulk)'])?>">
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Price (bulk)'])?>">
 					<label for="floatingInput">Price (bulk)</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="quantity">
 				<div class="form-floating mb-3">
-					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Quantity'])?>">
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Quantity'])?>">
 					<label for="floatingInput">Quantity</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="price_e">
 				<div class="form-floating mb-3">
-					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Price (ea)'])?>" disabled>
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Price (ea)'])?>" disabled>
 					<label for="floatingInput">Price (ea)</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-2" name="status">
 				<div class="form-floating mb-3">
 					<div class="form-check form-switch">
-						<input class="form-check-input" type="checkbox" name="active" id="flexCheck" <?($wick['Active']==1)?print("checked"):print("")?>>
+						<input class="form-check-input" type="checkbox" name="active" id="flexCheck" <?($cont['Active']==1)?print("checked"):print("")?>>
 						<label class="form-check-label" for="flexCheck"> Active? </label>
 					</div>
 				</div>
--- Pages/products_wick.php
+++ Pages/products_wick.php
@@ -5,12 +5,12 @@
 		<!-- Section Header -->
 		<div class="row">
 			<div class="col-12 col-md-6">
-				<h1>Edit wick</h1>
+				<h1>Edit container</h1>
 			</div>
 			<div class="col-12 col-md-6 text-md-end">
 				<div class="row">
 					<div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
-						<a href="javascript:product.comodities.wick.create();" class="btn btn-outline-primary m-1">
+						<a href="javascript:product.comodities.container.create();" class="btn btn-outline-primary m-1">
 							<i class="fa fa-save"></i>
 						</a>
 					</div>
@@ -19,7 +19,7 @@
 		</div>
 		<hr>
 		<!-- Section Body -->
-		<div class="row wicks">
+		<div class="row containers">
 			<div class="col-12 col-md-6 col-lg-3" name="name">
 				<div class="form-floating mb-3">
 					<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
@@ -81,22 +81,22 @@
 			</div>
 		</div>
 	</section>
-<?  } elseif(mysqli_num_rows($query = DB_Query(sprintf("SELECT * FROM `Product wicks` WHERE `ID`=%s", QS))) > 0) {
-        $cont = mysqli_fetch_assoc($query);
+<?  } elseif(mysqli_num_rows($query = DB_Query(sprintf("SELECT * FROM `Product containers` WHERE `ID`=%s", QS))) > 0) {
+        $wick = mysqli_fetch_assoc($query);
 ?>
 	<section>
 		<!-- Section Header -->
 		<div class="row">
 			<div class="col-12 col-md-6">
-				<h1>Edit wick</h1>
+				<h1>Edit container</h1>
 			</div>
 			<div class="col-12 col-md-6 text-md-end">
 				<div class="row">
 					<div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
-						<a href="javascript:product.comodities.wick.delete(<?print(QS)?>);" class="btn btn-outline-danger m-1">
+						<a href="javascript:product.comodities.container.delete(<?print(QS)?>);" class="btn btn-outline-danger m-1">
 							<i class="fa fa-trash-alt"></i>
 						</a>
-						<a href="javascript:product.comodities.wick.update(<?print(QS)?>);" class="btn btn-outline-primary m-1">
+						<a href="javascript:product.comodities.container.update(<?print(QS)?>);" class="btn btn-outline-primary m-1">
 							<i class="fa fa-save"></i>
 						</a>
 					</div>
@@ -105,10 +105,10 @@
 		</div>
 		<hr>
 		<!-- Section Body -->
-		<div class="row wicks">
+		<div class="row containers">
 			<div class="col-12 col-md-6 col-lg-3" name="name">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Name'])?>">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Name'])?>">
 					<label for="floatingInput">Name</label>
 				</div>
 			</div>
@@ -120,7 +120,7 @@
 						<?
 							$query = DB_Query("SELECT * FROM `Suppliers` WHERE `Active`=1");
 							while ($row = mysqli_fetch_array($query)) {
-								($row['Reference'] == $cont['Supplier'])? $selected=' selected' : $selected='';
+								($row['Reference'] == $wick['Supplier'])? $selected=' selected' : $selected='';
 								print_r('<option value="'.$row['Reference'].'"'.$selected.'>'.$row['Name'].'</option>');
 							}
 						?>
@@ -130,38 +130,50 @@
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="supplierref">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['ItemRef'])?>">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['ItemRef'])?>">
 					<label for="floatingInput">Supplier Reference</label>
 				</div>
 			</div>
-			<div class="col-12 col-md-6 col-lg-3" name="size">
+			<div class="col-12 col-md-6 col-lg-3" name="width">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Size (cl)'])?>">
-					<label for="floatingInput">size (cl)</label>
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Width (mm)'])?>">
+					<label for="floatingInput">Width (mm)</label>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3" name="height">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Height (mm)'])?>">
+					<label for="floatingInput">Height (cl)</label>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3" name="thickness">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Thickness (mm)'])?>">
+					<label for="floatingInput">Thickness (mm)</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="price_b">
 				<div class="form-floating mb-3">
-					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Price (bulk)'])?>">
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Price (bulk)'])?>">
 					<label for="floatingInput">Price (bulk)</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="quantity">
 				<div class="form-floating mb-3">
-					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Quantity'])?>">
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Quantity'])?>">
 					<label for="floatingInput">Quantity</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="price_e">
 				<div class="form-floating mb-3">
-					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Price (ea)'])?>" disabled>
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($wick['Price (ea)'])?>" disabled>
 					<label for="floatingInput">Price (ea)</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-2" name="status">
 				<div class="form-floating mb-3">
 					<div class="form-check form-switch">
-						<input class="form-check-input" type="checkbox" name="active" id="flexCheck" <?($cont['Active']==1)?print("checked"):print("")?>>
+						<input class="form-check-input" type="checkbox" name="active" id="flexCheck" <?($wick['Active']==1)?print("checked"):print("")?>>
 						<label class="form-check-label" for="flexCheck"> Active? </label>
 					</div>
 				</div>
@@ -175,7 +187,7 @@
 		<!-- Section Header -->
 		<div class="row">
 			<div class="col-12 col-md-6">
-				<h1>Wick not found.</h1>
+				<h1>Container not found.</h1>
 			</div>
 			<div class="col-12 col-md-6 text-md-end">
 			</div>
