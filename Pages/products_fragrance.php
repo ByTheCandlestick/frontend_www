27999d938ae1b31901ea961c199dbb4985289888
Robert Smith <master_schmadoodle@outlook.com>
2022-09-06 13:18:46 +0100

    Minor changes to editing product Wick Stands

--- Pages/products_fragrance.php
+++ Pages/products_fragrance.php
@@ -1,24 +1,19 @@
 <?
-    $query = DB_Query(sprintf("SELECT * FROM `Product fragrances` WHERE `ID`=%s", QS));
     if(strtolower(QS) == "new") {
-?>
-
-<?  } elseif(mysqli_num_rows($query) > 0) {
-        $cont = mysqli_fetch_assoc($query);
 ?>
 	<section>
 		<!-- Section Header -->
 		<div class="row">
 			<div class="col-12 col-md-6">
-				<h1>Edit container</h1>
+				<h1>Edit fragrance</h1>
 			</div>
 			<div class="col-12 col-md-6 text-md-end">
 				<div class="row">
 					<div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
-						<a href="javascript:product.container.delete(<?print(QS)?>);" class="btn btn-outline-danger m-1">
+						<a href="javascript:product.fragrance.delete(<?print(QS)?>);" class="btn btn-outline-danger m-1">
 							<i class="fa fa-trash-alt"></i>
 						</a>
-						<a href="javascript:product.container.update(<?print(QS)?>);" class="btn btn-outline-primary m-1">
+						<a href="javascript:product.fragrance.update(<?print(QS)?>);" class="btn btn-outline-primary m-1">
 							<i class="fa fa-save"></i>
 						</a>
 					</div>
@@ -30,7 +25,7 @@
 		<div class="row ">
 			<div class="col-12 col-md-6 col-lg-3" name="name">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Name'])?>">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($fragrance['Name'])?>">
 					<label for="floatingInput">Name</label>
 				</div>
 			</div>
@@ -42,7 +37,7 @@
 						<?
 							$query = DB_Query("SELECT * FROM `Suppliers` WHERE `Active`=1");
 							while ($row = mysqli_fetch_array($query)) {
-								($row['Reference'] == $cont['Supplier'])? $selected=' selected' : $selected='';
+								($row['Reference'] == $fragrance['Supplier'])? $selected=' selected' : $selected='';
 								print_r('<option value="'.$row['Reference'].'"'.$selected.'>'.$row['Name'].'</option>');
 							}
 						?>
@@ -52,38 +47,125 @@
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="supplierref">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['ItemRef'])?>" disabled>
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($fragrance['ItemRef'])?>" disabled>
 					<label for="floatingInput">Supplier Reference</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="size">
 				<div class="form-floating mb-3">
-					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Size (cl)'])?>">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($fragrance['Size (cl)'])?>">
 					<label for="floatingInput">size (cl)</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="price_b">
 				<div class="form-floating mb-3">
-					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Price (bulk)'])?>">
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($fragrance['Price (bulk)'])?>">
 					<label for="floatingInput">Price (bulk)</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="quantity">
 				<div class="form-floating mb-3">
-					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Quantity'])?>">
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($fragrance['Quantity'])?>">
 					<label for="floatingInput">Quantity</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-3" name="price_e">
 				<div class="form-floating mb-3">
-					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($cont['Price (ea)'])?>" disabled>
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($fragrance['Price (ea)'])?>" disabled>
 					<label for="floatingInput">Price (ea)</label>
 				</div>
 			</div>
 			<div class="col-12 col-md-6 col-lg-2" name="status">
 				<div class="form-floating mb-3">
 					<div class="form-check form-switch">
-						<input class="form-check-input" type="checkbox" name="active" id="flexCheck" <?($cont['Active']==1)?print("checked"):print("")?>>
+						<input class="form-check-input" type="checkbox" name="active" id="flexCheck" <?($fragrance['Active']==1)?print("checked"):print("")?>>
+						<label class="form-check-label" for="flexCheck"> Active? </label>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+<?  } elseif(mysqli_num_rows($query = DB_Query(sprintf("SELECT * FROM `Product fragrances` WHERE `ID`=%s", QS))) > 0) {
+        $fragrance = mysqli_fetch_assoc($query);
+?>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6">
+				<h1>Edit fragrance</h1>
+			</div>
+			<div class="col-12 col-md-6 text-md-end">
+				<div class="row">
+					<div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
+						<a href="javascript:product.fragrance.delete(<?print(QS)?>);" class="btn btn-outline-danger m-1">
+							<i class="fa fa-trash-alt"></i>
+						</a>
+						<a href="javascript:product.fragrance.update(<?print(QS)?>);" class="btn btn-outline-primary m-1">
+							<i class="fa fa-save"></i>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr>
+		<!-- Section Body -->
+		<div class="row ">
+			<div class="col-12 col-md-6 col-lg-3" name="name">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($fragrance['Name'])?>">
+					<label for="floatingInput">Name</label>
+				</div>
+			</div>
+			<!-- Type -->
+			<div class="col-12 col-md-6 col-lg-3" name="supplier">
+				<div class="form-floating mb-3">
+					<select class="form-select" id="floatingSelect">
+						<option value="-1" selected>Please select</option>
+						<?
+							$query = DB_Query("SELECT * FROM `Suppliers` WHERE `Active`=1");
+							while ($row = mysqli_fetch_array($query)) {
+								($row['Reference'] == $fragrance['Supplier'])? $selected=' selected' : $selected='';
+								print_r('<option value="'.$row['Reference'].'"'.$selected.'>'.$row['Name'].'</option>');
+							}
+						?>
+					</select>
+					<label for="floatingInput">Supplier</label>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3" name="supplierref">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($fragrance['ItemRef'])?>" disabled>
+					<label for="floatingInput">Supplier Reference</label>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3" name="size">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="<?print($fragrance['Size (cl)'])?>">
+					<label for="floatingInput">size (cl)</label>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3" name="price_b">
+				<div class="form-floating mb-3">
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($fragrance['Price (bulk)'])?>">
+					<label for="floatingInput">Price (bulk)</label>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3" name="quantity">
+				<div class="form-floating mb-3">
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($fragrance['Quantity'])?>">
+					<label for="floatingInput">Quantity</label>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3" name="price_e">
+				<div class="form-floating mb-3">
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($fragrance['Price (ea)'])?>" disabled>
+					<label for="floatingInput">Price (ea)</label>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-2" name="status">
+				<div class="form-floating mb-3">
+					<div class="form-check form-switch">
+						<input class="form-check-input" type="checkbox" name="active" id="flexCheck" <?($fragrance['Active']==1)?print("checked"):print("")?>>
 						<label class="form-check-label" for="flexCheck"> Active? </label>
 					</div>
 				</div>
@@ -97,7 +179,7 @@
 		<!-- Section Header -->
 		<div class="row">
 			<div class="col-12 col-md-6">
-				<h1>Container not found.</h1>
+				<h1>Fragrance not found.</h1>
 			</div>
 			<div class="col-12 col-md-6 text-md-end">
 			</div>
