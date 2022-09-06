7cf5499a491a22c1df9d60e0e14d8a57771240b4
Robert Smith <master_schmadoodle@outlook.com>
2022-09-06 13:12:02 +0100

    Minor changes to editing product Wick Stands

--- Pages/products_material.php
+++ Pages/products_material.php
@@ -148,7 +148,7 @@
 			</div>
 			<div class="col-12 col-md-6 col-lg-2" name="quantity">
 				<div class="form-floating mb-3">
-					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($material['Size (cl)'])?>">
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="<?print($material['Size (cl)'])?>" disabled>
 					<label for="floatingInput">Size (cl)</label>
 				</div>
 			</div>
