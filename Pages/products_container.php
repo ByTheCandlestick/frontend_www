2c850157846900990a1d7fc4ef700bc67c66fea5
Robert Smith <master_schmadoodle@outlook.com>
2022-09-06 12:11:24 +0100

    Minor changes to editing product comodities

--- Pages/products_container.php
+++ Pages/products_container.php
@@ -1,88 +1,88 @@
 <?
-    $query = DB_Query(sprintf("SELECT * FROM `Product containers` WHERE `ID`=%s", QS));
     if(strtolower(QS) == "new") {
 ?>
-<section>
-	<!-- Section Header -->
-	<div class="row">
-		<div class="col-12 col-md-6">
-			<h1>Edit container</h1>
-		</div>
-		<div class="col-12 col-md-6 text-md-end">
-			<div class="row">
-				<div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
-					<a href="javascript:product.container.create(<?print(QS)?>);" class="btn btn-outline-danger m-1">
-						<i class="fa fa-save"></i>
-					</a>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6">
+				<h1>Edit container</h1>
+			</div>
+			<div class="col-12 col-md-6 text-md-end">
+				<div class="row">
+					<div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
+						<a href="javascript:product.comodities.container.create();" class="btn btn-outline-danger m-1">
+							<i class="fa fa-save"></i>
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<hr>
-	<!-- Section Body -->
-	<div class="row containers">
-		<div class="col-12 col-md-6 col-lg-3" name="name">
-			<div class="form-floating mb-3">
-				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
-				<label for="floatingInput">Name</label>
+		<hr>
+		<!-- Section Body -->
+		<div class="row containers">
+			<div class="col-12 col-md-6 col-lg-3" name="name">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+					<label for="floatingInput">Name</label>
+				</div>
 			</div>
-		</div>
-		<!-- Type -->
-		<div class="col-12 col-md-6 col-lg-3" name="supplier">
-			<div class="form-floating mb-3">
-				<select class="form-select" id="floatingSelect">
-					<option value="-1" selected>Please select</option>
-					<?
-						$query = DB_Query("SELECT * FROM `Suppliers` WHERE `Active`=1");
-						while ($row = mysqli_fetch_array($query)) {
-							print_r('<option value="'.$row['Reference'].'">'.$row['Name'].'</option>');
-						}
-					?>
-				</select>
-				<label for="floatingInput">Supplier</label>
+			<!-- Type -->
+			<div class="col-12 col-md-6 col-lg-3" name="supplier">
+				<div class="form-floating mb-3">
+					<select class="form-select" id="floatingSelect">
+						<option value="-1" selected>Please select</option>
+						<?
+							$query = DB_Query("SELECT * FROM `Suppliers` WHERE `Active`=1");
+							while ($row = mysqli_fetch_array($query)) {
+								print_r('<option value="'.$row['Reference'].'">'.$row['Name'].'</option>');
+							}
+						?>
+					</select>
+					<label for="floatingInput">Supplier</label>
+				</div>
 			</div>
-		</div>
-		<div class="col-12 col-md-6 col-lg-3" name="supplierref">
-			<div class="form-floating mb-3">
-				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
-				<label for="floatingInput">Supplier Reference</label>
+			<div class="col-12 col-md-6 col-lg-3" name="supplierref">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+					<label for="floatingInput">Supplier Reference</label>
+				</div>
 			</div>
-		</div>
-		<div class="col-12 col-md-6 col-lg-3" name="size">
-			<div class="form-floating mb-3">
-				<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
-				<label for="floatingInput">size (cl)</label>
+			<div class="col-12 col-md-6 col-lg-3" name="size">
+				<div class="form-floating mb-3">
+					<input type="text" class="form-control" id="floatingInput" placeholder="" value="">
+					<label for="floatingInput">size (cl)</label>
+				</div>
 			</div>
-		</div>
-		<div class="col-12 col-md-6 col-lg-3" name="price_b">
-			<div class="form-floating mb-3">
-				<input type="number" class="form-control" id="floatingInput" placeholder="" value="">
-				<label for="floatingInput">Price (bulk)</label>
+			<div class="col-12 col-md-6 col-lg-3" name="price_b">
+				<div class="form-floating mb-3">
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="">
+					<label for="floatingInput">Price (bulk)</label>
+				</div>
 			</div>
-		</div>
-		<div class="col-12 col-md-6 col-lg-3" name="quantity">
-			<div class="form-floating mb-3">
-				<input type="number" class="form-control" id="floatingInput" placeholder="" value="">
-				<label for="floatingInput">Quantity</label>
+			<div class="col-12 col-md-6 col-lg-3" name="quantity">
+				<div class="form-floating mb-3">
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="">
+					<label for="floatingInput">Quantity</label>
+				</div>
 			</div>
-		</div>
-		<div class="col-12 col-md-6 col-lg-3" name="price_e">
-			<div class="form-floating mb-3">
-				<input type="number" class="form-control" id="floatingInput" placeholder="" value="" disabled>
-				<label for="floatingInput">Price (ea)</label>
+			<div class="col-12 col-md-6 col-lg-3" name="price_e">
+				<div class="form-floating mb-3">
+					<input type="number" class="form-control" id="floatingInput" placeholder="" value="" disabled>
+					<label for="floatingInput">Price (ea)</label>
+				</div>
 			</div>
-		</div>
-		<div class="col-12 col-md-6 col-lg-2" name="status">
-			<div class="form-floating mb-3">
-				<div class="form-check form-switch">
-					<input class="form-check-input" type="checkbox" name="active" id="flexCheck">
-					<label class="form-check-label" for="flexCheck"> Active? </label>
+			<div class="col-12 col-md-6 col-lg-2" name="status">
+				<div class="form-floating mb-3">
+					<div class="form-check form-switch">
+						<input class="form-check-input" type="checkbox" name="active" id="flexCheck">
+						<label class="form-check-label" for="flexCheck"> Active? </label>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
 <?  } elseif(mysqli_num_rows($query) > 0) {
+    	$query = DB_Query(sprintf("SELECT * FROM `Product containers` WHERE `ID`=%s", QS));
         $cont = mysqli_fetch_assoc($query);
 ?>
 	<section>
@@ -94,10 +94,10 @@
 			<div class="col-12 col-md-6 text-md-end">
 				<div class="row">
 					<div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
-						<a href="javascript:product.container.delete(<?print(QS)?>);" class="btn btn-outline-danger m-1">
+						<a href="javascript:product.comodities.container.delete(<?print(QS)?>);" class="btn btn-outline-danger m-1">
 							<i class="fa fa-trash-alt"></i>
 						</a>
-						<a href="javascript:product.container.update(<?print(QS)?>);" class="btn btn-outline-primary m-1">
+						<a href="javascript:product.comodities.container.update(<?print(QS)?>);" class="btn btn-outline-primary m-1">
 							<i class="fa fa-save"></i>
 						</a>
 					</div>
