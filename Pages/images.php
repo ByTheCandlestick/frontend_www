9e440e7edb11f2868295437886b4897948c52387
Robert Smith <master_schmadoodle@outlook.com>
2022-09-07 22:09:09 +0100

    Minor changes to add image control pages

--- Pages/images.php
+++ Pages/images.php
@@ -2,58 +2,29 @@
 .imageContainer {
 	position: relative;
 }
-
-.overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0);
-  transition: background 0.5s ease;
+.imageContainer .overlay {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: rgba(0, 0, 0, 0);
+	transition: background 0.5s ease;
 }
-
 .imageContainer:hover .overlay {
-  display: block;
-  background: rgba(0, 0, 0, .3);
+	display: block;
+	background: rgba(0, 0, 0, .3);
 }
-
-img {
-  position: absolute;
-  width: 500px;
-  height: 300px;
-  left: 0;
+.imageContainer .button {
+	position: absolute;
+	width: 500px;
+	left:0;
+	top: 180px;
+	text-align: center;
+	opacity: 0;
+	transition: opacity .35s ease;
 }
-
-.title {
-  position: absolute;
-  width: 500px;
-  left: 0;
-  top: 120px;
-  font-weight: 700;
-  font-size: 30px;
-  text-align: center;
-  text-transform: uppercase;
-  color: white;
-  z-index: 1;
-  transition: top .5s ease;
-}
-
-.imageContainer:hover .title {
-  top: 90px;
-}
-
-.button {
-  position: absolute;
-  width: 500px;
-  left:0;
-  top: 180px;
-  text-align: center;
-  opacity: 0;
-  transition: opacity .35s ease;
-}
-
-.button a {
+.imageContainer .button a {
   width: 200px;
   padding: 12px 48px;
   text-align: center;
@@ -61,9 +32,30 @@ img {
   border: solid 2px white;
   z-index: 1;
 }
-
 .imageContainer:hover .button {
-  opacity: 1;
+	opacity: 1;
+}
+.imageContainer .title {
+	position: absolute;
+	width: 500px;
+	left: 0;
+	top: 120px;
+	font-weight: 700;
+	font-size: 30px;
+	text-align: center;
+	text-transform: uppercase;
+	color: white;
+	z-index: 1;
+	transition: top .5s ease;
+}
+.imageContainer:hover .title {
+  top: 90px;
+}
+.imageContainer img {
+  position: absolute;
+  width: 500px;
+  height: 300px;
+  left: 0;
 }
 
 </style>
