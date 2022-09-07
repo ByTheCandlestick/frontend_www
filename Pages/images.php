9f2d69f34c421929ae9c64d19d0b0b6c58ba7d7c
Robert Smith <master_schmadoodle@outlook.com>
2022-09-07 22:19:16 +0100

    Minor changes to add image control pages

--- Pages/images.php
+++ Pages/images.php
@@ -3,59 +3,56 @@
 	position: relative;
 }
 .imageContainer .overlay {
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	background: rgba(0, 0, 0, 0);
-	transition: background 0.5s ease;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0);
+    transition: background 0.5s ease;
 }
 .imageContainer:hover .overlay {
-	display: block;
-	background: rgba(0, 0, 0, .3);
+    display: block;
+    background: rgba(0, 0, 0, .3);
 }
 .imageContainer .button {
-	position: absolute;
-	width: 500px;
-	left:0;
-	top: 180px;
-	text-align: center;
-	opacity: 0;
-	transition: opacity .35s ease;
+    position: absolute;
+    top: 50%;
+    opacity: 0;
+    transition: opacity .35s ease;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
 .imageContainer .button a {
-  width: 200px;
-  padding: 12px 48px;
-  text-align: center;
-  color: white;
-  border: solid 2px white;
-  z-index: 1;
+    width: 200px;
+    padding: 12px 48px;
+    text-align: center;
+    color: white;
+    border: solid 2px white;
+    z-index: 1;
 }
 .imageContainer:hover .button {
 	opacity: 1;
 }
 .imageContainer .title {
-	position: absolute;
-	width: 500px;
-	left: 0;
-	top: 120px;
-	font-weight: 700;
-	font-size: 30px;
-	text-align: center;
-	text-transform: uppercase;
-	color: white;
-	z-index: 1;
-	transition: top .5s ease;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    font-weight: 700;
+    font-size: 17px;
+    text-align: center;
+    text-transform: uppercase;
+    color: white;
+    z-index: 1;
+    transition: top .5s ease;
+    width: 50%;
+    transform: translate(-50%, -50%);
 }
 .imageContainer:hover .title {
-  top: 90px;
+  top: 25%;
 }
 .imageContainer img {
-  position: absolute;
-  width: 500px;
-  height: 300px;
-  left: 0;
+  width: 100%;
 }
 
 </style>
