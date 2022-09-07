ac70bdc7c9555d46dd7c02c6bda8bd3be7cb0380
Robert Smith <master_schmadoodle@outlook.com>
2022-09-07 21:59:17 +0100

    Minor changes to add image control pages

---
+++ Pages/images.php
@@ -0,0 +1,22 @@
+<section>
+	<!-- Section Header -->
+	<div class="row">
+		<div class="col-12 col-md-6">
+			<h1>Images</h1>
+		</div>
+		<div class="col-12 col-md-6 text-md-end">
+			<div class="row">
+				<div class="col-12 d-flex justify-content-end align-items-center p-0">
+					<a href="javascript:images.upload();" class="btn btn-outline-primary m-1">
+						<i class="fa fa-upload"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<hr>
+	<!-- Section Body -->
+	<div class="row">
+		
+	</div>
+</section>
