ff4363d215f30738cecde9e27c7721939a8e67e9
Robert Smith <master_schmadoodle@outlook.com>
2022-09-07 22:23:25 +0100

    Minor changes to add image control pages

--- Pages/images.php
+++ Pages/images.php
@@ -85,18 +85,18 @@
 	<!-- Section Body -->
 	<div class="row">
 		<?
-			print_r($images);
 			foreach($images as $image) {
-				
+				print(sprintf('
+					<div class="col-6 col-md-4 col-lg-3">
+						<div class="imageContainer">
+							<img src="%s" alt="" />
+							<p class="title">card title</p>
+							<div class="overlay"></div>
+							<div class="button"><a href="#"> BUTTON </a></div>
+						</div>
+					</div>
+				'), $image['location']);
 			}
 		?>
-		<div class="col-6 col-md-4 col-lg-3">
-			<div class="imageContainer">
-				<img src="https://images.unsplash.com/photo-1488628075628-e876f502d67a?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=" alt="" />
-				<p class="title">card title</p>
-				<div class="overlay"></div>
-				<div class="button"><a href="#"> BUTTON </a></div>
-			</div>
-		</div>
 	</div>
 </section>
