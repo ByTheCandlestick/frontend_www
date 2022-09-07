35d5666ef6164a7842110c91a555fbaafa47c9ca
Robert Smith <master_schmadoodle@outlook.com>
2022-09-07 12:42:58 +0100

    Minor cahnges to switch for light mode / dark mode

--- Themes/Vuexy/index.php
+++ Themes/Vuexy/index.php
@@ -142,7 +142,7 @@
 							<!-- Profile, Notifications and Dark mode -->
 							<div class="app-header-right">
 								<button class="mode-switch" title="Switch Theme">
-									<i class="fad fa-light-switch-on fa-lg"></i>
+									<i class="fad fa-light-switch-on fa-xl"></i>
 								</button>
 								<button class="profile-btn">
 									<span><?=($userdata['First_name'].' '.$userdata['Last_name'])?></span>
