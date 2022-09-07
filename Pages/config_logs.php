2256047c6f96ceffae662dd14ef1e79faad1eb06
Robert Smith <master_schmadoodle@outlook.com>
2022-09-07 15:16:16 +0100

    Minor changes to config > logs

--- Pages/config_logs.php
+++ Pages/config_logs.php
@@ -1,14 +1,28 @@
 
-<div class="input-group mb-3">
-  <div class="input-group-prepend">
-    <label class="input-group-text" for="LogSelect">Logs</label>
-  </div>
-  <select class="" id="LogSelect">
-    <option selected>Choose...</option>
-	<?
-		foreach(getDirContents(__ROOT__, '.log') as $log) {
-			printf('<option value="%s">%s</option>', $log['Path'], $log['File']);
-		}
-	?>
-  </select>
-</div>
+<section>
+	<!-- Section Header -->
+	<div class="row">
+		<div class="col-12 col-md-6">
+			<h1>Logs</h1>
+		</div>
+		<div class="col-12 col-md-6 text-md-end">
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<label class="input-group-text" for="LogSelect">Logs</label>
+				</div>
+				<select class="" id="LogSelect">
+					<option selected>Choose...</option>
+					<?
+						foreach(getDirContents(__ROOT__, '.log') as $log) {
+							printf('<option value="%s">%s</option>', $log['Path'], $log['File']);
+						}
+					?>
+				</select>
+			</div>
+		</div>
+	</div>
+	<hr>
+	<!-- Section Body -->
+	<div class="row overflow-scroll">
+	</div>
+</section>
