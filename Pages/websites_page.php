869dfb98239560c80125e9f21d2ce3c9b3501b35
Robert Smith <master_schmadoodle@outlook.com>
2022-09-07 21:46:35 +0100

    Minor changes to website page editor

--- Pages/websites_page.php
+++ Pages/websites_page.php
@@ -152,6 +152,7 @@
 	} else if($query = DB_Query(sprintf("SELECT * FROM `Website pages` WHERE `ID`=%s", QS))) {
 		if(mysqli_num_rows($query) > 0) {
 			(isset($_GET['domainID']) && $_GET['domainID']!=="")? $domainID=$_GET['domainID']:$domainID=-1;
+			print_r('domain ID: '.$domainID);
 			$page = mysqli_fetch_assoc($query);
 			$styles = explode(',', $page['style_ids']);
 			$scripts = explode(',', $page['script_ids']);
