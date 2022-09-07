a851d9b9fc492396e7f9f808e890c4965f977b7f
Robert Smith <master_schmadoodle@outlook.com>
2022-09-07 22:03:10 +0100

    Minor changes to add image control pages

--- Pages/images.php
+++ Pages/images.php
@@ -90,7 +90,7 @@ img {
 	<!-- Section Body -->
 	<div class="row">
 		<div class="col-6 col-md-4 col-lg-3">
-			<div class="container">
+			<div class="p-relative">
 				<img src="https://images.unsplash.com/photo-1488628075628-e876f502d67a?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=" alt="" />
 				<p class="title">card title</p>
 				<div class="overlay"></div>
