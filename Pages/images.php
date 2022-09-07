467639bb225d57318436b514c91875e0561a43c1
Robert Smith <master_schmadoodle@outlook.com>
2022-09-07 22:22:55 +0100

    Minor changes to add image control pages

--- Pages/images.php
+++ Pages/images.php
@@ -84,7 +84,12 @@
 	<hr>
 	<!-- Section Body -->
 	<div class="row">
-		<?print_r($images);?>
+		<?
+			print_r($images);
+			foreach($images as $image) {
+				
+			}
+		?>
 		<div class="col-6 col-md-4 col-lg-3">
 			<div class="imageContainer">
 				<img src="https://images.unsplash.com/photo-1488628075628-e876f502d67a?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=" alt="" />
