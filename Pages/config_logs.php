64dcb6fe750e992446477441289cb0b011c67258
Robert Smith <master_schmadoodle@outlook.com>
2022-09-07 14:10:51 +0100

    Minor cahnges to switch for light mode / dark mode

--- Pages/config_logs.php
+++ Pages/config_logs.php
@@ -1,5 +1,5 @@
 <?
-	function getDirContents(string $dir, bool $includeBase, string &$remove="", array &$results = array()) {
+	function getDirContents(string $dir, array &$results = array()) {
 		if(!$includeBase && $remove!="") $remove=$dir;
 		foreach (scandir($dir) as $key => $value) {
 			$path = str_replace($remove, "", realpath($dir . DIRECTORY_SEPARATOR . $value));
@@ -9,12 +9,11 @@
 				getDirContents($path, $includeBase, $remove, $results);
 			}
 		}
-
 		return $results;
 	}
 ?>
 <pre>
-<?
-	print_r(getDirContents(__ROOT__, false));
-?>
+	<?
+		print_r(getDirContents(__ROOT__));
+	?>
 </pre>
