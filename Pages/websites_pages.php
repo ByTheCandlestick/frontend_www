95af3982376b4481c334bf1fa8ab4c5979dd5b40
Robert Smith <master_schmadoodle@outlook.com>
2022-09-07 21:43:03 +0100

    Minor changes to log viewer

--- Pages/websites_pages.php
+++ Pages/websites_pages.php
@@ -24,7 +24,7 @@
 		<div class="col-12 col-md-6 text-md-end">
 			<div class="row">
 				<div class="col-12 col-lg-6 d-block d-md-flex justify-content-end align-items-center p-0">
-					<a href="/Websites/Page/New/" class="btn btn-outline-primary m-1">
+					<a href="/Websites/Page/New/?domainID=<?=($domainID)?>" class="btn btn-outline-primary m-1">
 						<i class="fa fa-plus"></i>
 					</a>
 				</div>
