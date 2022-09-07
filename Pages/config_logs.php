623ea791533fa7f1f11ba36f6c666396d1655f04
Robert Smith <master_schmadoodle@outlook.com>
2022-09-07 13:17:42 +0100

    Minor cahnges to switch for light mode / dark mode

--- Pages/config_logs.php
+++ Pages/config_logs.php
@@ -3,10 +3,10 @@
 		foreach (scandir($dir) as $key => $value) {
 			$path = realpath($dir . DIRECTORY_SEPARATOR . $value);
 			if (!is_dir($path)) {
-				($includeBase)? $results[]='PATH: '.$path: $results[]='PATH: '.$value;
+				($includeBase)? $results[]='FILE: '.$path: $results[]='FILE: '.$value;
 			} else if ($value != "." && $value != "..") {
 				getDirContents($path, true, $results);
-				($includeBase)? $results[]='FILE: '.$path: $results[]='FILE: '.$value;
+				($includeBase)? $results[]='PATH: '.$path: $results[]='PATH: '.$value;
 			}
 		}
 
