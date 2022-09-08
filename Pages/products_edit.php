cdd6154b441f457205f6a5d8127610d1cae39af0
Robert Smith <master_schmadoodle@outlook.com>
2022-09-08 10:31:44 +0100

    Minor changes to product edit

--- Pages/products_edit.php
+++ Pages/products_edit.php
@@ -249,7 +249,7 @@
 									$query = DB_Query("SELECT * FROM `Product packagings` WHERE `Active`=1");
 									while ($row = mysqli_fetch_array($query)) {
 										($row['ID'] == $prod['Packaging_ID'])? $selected=' selected' : $selected='';
-										print_r('<option value="'.$row['ID'].'" price="'.$row['Price (cl)'].'"'.$selected.'>'.$row['Name'].'</option>');
+										print_r('<option value="'.$row['ID'].'" price="'.$row['Price (g)'].'"'.$selected.'>'.$row['Name'].'</option>');
 									}
 								?>
 							</select>
