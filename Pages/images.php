0502aef8b66d9846167b6c757c15eb6c2bef0314
Robert Smith <master_schmadoodle@outlook.com>
2022-09-08 10:03:11 +0100

    Minor changes to images page

--- Pages/images.php
+++ Pages/images.php
@@ -56,7 +56,7 @@
     transition: top .5s ease;
     width: 70%;
     transform: translate(-50%, -50%);
-	-webkit-text-stroke-width: 2px;
+	-webkit-text-stroke-width: 1px;
     -webkit-text-stroke-color: black;
 }
 .imageContainer:hover .title {
