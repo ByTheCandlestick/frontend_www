6aeb4468887c5081217d0e68abc27475487b0a9f
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 14:37:10 +0100

    Minor changes to analytics JS

--- CDN/analytics/1.0.0/csa.js
+++ CDN/analytics/1.0.0/csa.js
@@ -11,11 +11,11 @@ window.onload = function () {
 	vars.timing.domComplete = window.performance.timing.domComplete;
 	vars.timing.complete = Date.now();
 
-	console.log("Domain lookup took "+funcs.ms_to_hms(analytics.timings.DOMLookup));
-	console.log("DOM interactive in "+funcs.ms_to_hms(analytics.timings.DOMInteractive));
-	console.log("DOM loaded in "+funcs.ms_to_hms(analytics.timings.DOMLoaded));
-	console.log("DOM complete in "+funcs.ms_to_hms(analytics.timings.DOMComplete));
-	console.log("Fulliy loaded in "+funcs.ms_to_hms(analytics.timings.Loaded));
+	console.log("Domain lookup took "+funcs.ms_to_hms(analytics.timing.DOMLookup));
+	console.log("DOM interactive in "+funcs.ms_to_hms(analytics.timing.DOMInteractive));
+	console.log("DOM loaded in "+funcs.ms_to_hms(analytics.timing.DOMLoaded));
+	console.log("DOM complete in "+funcs.ms_to_hms(analytics.timing.DOMComplete));
+	console.log("Fulliy loaded in "+funcs.ms_to_hms(analytics.timing.Loaded));
 }
 window.onbeforeunload = function(){
 	myfun();
@@ -23,5 +23,5 @@ window.onbeforeunload = function(){
 };
 function myfun(){
 	vars.timing.navigationEnd = Date.now();
-	console.log("Stayed on page for "+funcs.ms_to_hms());
+	console.log("Stayed on page for "+funcs.ms_to_hms(analytics.timing.Total));
 }
