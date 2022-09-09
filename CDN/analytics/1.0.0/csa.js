698c38a81bd8fffc37b4d8b35e49cfd80641de8e
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 14:01:58 +0100

    Minor changes to analytics JS

--- CDN/analytics/1.0.0/csa.js
+++ CDN/analytics/1.0.0/csa.js
@@ -5,9 +5,9 @@ console.log(fw.init());
 window.onload = function () {
 	console.log(window.performance.timing);
 	
-	console.log("Domain lookup took "+fw.ms_to_hms(window.performance.timing.domainLookupEnd - window.performance.timing.domainLookupStart));
-	console.log("DOM interactive in "+fw.ms_to_hms(window.performance.timing.domInteractive - window.performance.timing.navigationStart));
-	console.log("DOM loaded in "+fw.ms_to_hms(window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart));
-	console.log("DOM complete in "+fw.ms_to_hms(window.performance.timing.domComplete - window.performance.timing.navigationStart));
-	console.log("Fulliy loaded in "+fw.ms_to_hms(Date.now() - window.performance.timing.navigationStart));
+	console.log("Domain lookup took "+fw.funcs.ms_to_hms(window.performance.timing.domainLookupEnd - window.performance.timing.domainLookupStart));
+	console.log("DOM interactive in "+fw.funcs.ms_to_hms(window.performance.timing.domInteractive - window.performance.timing.navigationStart));
+	console.log("DOM loaded in "+fw.funcs.ms_to_hms(window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart));
+	console.log("DOM complete in "+fw.funcs.ms_to_hms(window.performance.timing.domComplete - window.performance.timing.navigationStart));
+	console.log("Fulliy loaded in "+fw.funcs.ms_to_hms(Date.now() - window.performance.timing.navigationStart));
 }
