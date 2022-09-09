becfe9e73bf196a6b11c5c53006e69bb5409c17b
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 10:35:44 +0100

    Minor changes to config page

--- Pages/config.php
+++ Pages/config.php
@@ -11,21 +11,21 @@
 	<hr>
 	<!-- Section Body -->
 	<div class="row overflow-scroll">
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2">
             <h2> General </h2>
             <p><a href="/Config/timezone/">Change default timezone</a></p>
             <p><a href="/Config/default_perms/">Edit permissions default</a></p>
             <p><a href="/Config/permissions/">Edit permissions</a></p>
         </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2">
             <h2> Tables </h2>
             <p><a href="/Config/tbl_rows/">Set max table rows</a></p>
         </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2">
             <h2> Charts </h2>
             <p><a href="/Config/chrt_colours/">Set chart colours</a></p>
         </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2">
             <h2> Admin </h2>
             <p><a href="/Config/logs/">Open logs</a></p>
         </div>
