065875ead6a739823dbcef7bce0c05ffea63fd95
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 12:40:51 +0100

    Minor changes to analytics JS

--- CDN/analytics/1.0.0/csa.js
+++ CDN/analytics/1.0.0/csa.js
@@ -6,9 +6,9 @@ fw.init();
 window.onload = function () {
 	console.log(window.performance.timing);
 	
-	console.log("Domain lookup took "+fw.convertmilliseconds(window.performance.timing.domainLookupEnd - window.performance.timing.domainLookupStart)+"ms");
-	console.log("DOM interactive in "+fw.convertmilliseconds(window.performance.timing.domInteractive - window.performance.timing.navigationStart)+"ms");
-	console.log("DOM loaded in "+fw.convertmilliseconds(window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart)+"ms");
-	console.log("DOM complete in "+fw.convertmilliseconds(window.performance.timing.domComplete - window.performance.timing.navigationStart)+"ms");
-	console.log("Fulliy loaded in "+fw.convertmilliseconds(Date.now() - window.performance.timing.navigationStart)+"ms");
+	console.log("Domain lookup took "+fw.convertms(window.performance.timing.domainLookupEnd - window.performance.timing.domainLookupStart)+"ms");
+	console.log("DOM interactive in "+fw.convertms(window.performance.timing.domInteractive - window.performance.timing.navigationStart)+"ms");
+	console.log("DOM loaded in "+fw.convertms(window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart)+"ms");
+	console.log("DOM complete in "+fw.convertms(window.performance.timing.domComplete - window.performance.timing.navigationStart)+"ms");
+	console.log("Fulliy loaded in "+fw.convertms(Date.now() - window.performance.timing.navigationStart)+"ms");
 }
--- CDN/analytics/1.0.0/framework.js
+++ CDN/analytics/1.0.0/framework.js
@@ -2,14 +2,14 @@ function init() {
 	console.log('analytics initializing');
 }
 
-function convertmilliseconds(millis) {
-	var minutes = Math.floor(millis / 60000);
-	var seconds = ((millis % 60000) / 1000).toFixed(0);
-	var hours = ((millis % 3600000) / 1000).toFixed(0);
+function convertms(ms) {
+	var minutes = Math.floor(ms / 60000);
+	var seconds = ((ms % 60000) / 1000).toFixed(0);
+	var hours = ((ms % 3600000) / 1000).toFixed(0);
 	return (hours < 10 ? '0' : '') + hours + ":" + (minutes < 10 ? '0' : '') + minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
   }
   
 export {
 	init,
-	convertmilliseconds,
+	convertms,
 };
