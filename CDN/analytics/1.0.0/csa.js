bc6c8760ddfe466830b8c8e14b8a3e28ea7d18c6
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 14:57:51 +0100

    Minor changes to analytics JS

--- CDN/analytics/1.0.0/csa.js
+++ CDN/analytics/1.0.0/csa.js
@@ -1,12 +1,10 @@
-import { fw, f, v, a } from "./framework.js";
+import { fw, f, a } from "./framework.js";
 
 
 window.onload = function () {
-	fw.initialize()
 	fw.saveLoadMetrics();
 }
 window.onbeforeunload = function(){
-	fw.immobilize();
 	fw.saveUnloadMetrics();
 	return 'Are you sure you want to leave?'
 }
--- CDN/analytics/1.0.0/framework.js
+++ CDN/analytics/1.0.0/framework.js
@@ -1,29 +1,17 @@
 var fw = {};
-	fw.initialize = () => {
+	fw.saveLoadMetrics = () => {
 		console.log('analytics initializing');
-		v.timing.navigationStart = window.performance.timing.navigationStart;
-		v.timing.domainLookupStart = window.performance.timing.domainLookupStart;
-		v.timing.domainLookupEnd = window.performance.timing.domainLookupEnd;
-		v.timing.domInteractive = window.performance.timing.domInteractive;
-		v.timing.domContentLoadedEventEnd = window.performance.timing.domContentLoadedEventEnd;
-		v.timing.domComplete = window.performance.timing.domComplete;
-		v.timing.complete = Date.now();
+		a.timing.DOMLookup = (window.performance.timing.domainLookupEnd - window.performance.timing.domainLookupStart);
+		a.timing.DOMInteractive = (window.performance.timing.domInteractive - window.performance.timing.navigationStart);
+		a.timing.DOMLoaded = (window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart);
+		a.timing.DOMComplete = (window.performance.timing.domComplete - window.performance.timing.navigationStart);
+		a.timing.DOMFinished = (Date.now() - window.performance.timing.navigationStart);
 		console.log('analytics initialized');
 	}
-	fw.immobilize = () => {
-		console.log('analytics immobilizing');
-		v.timing.navigationEnd = Date.now();
-		console.log('analytics immobilized');
-	}
-	fw.saveLoadMetrics = () => {
-		a.timing.DOMLookup = (v.timing.domainLookupEnd - v.timing.domainLookupStart);
-		a.timing.DOMInteractive = (v.timing.domInteractive - v.timing.navigationStart);
-		a.timing.DOMLoaded = (v.timing.domContentLoadedEventEnd - v.timing.navigationStart);
-		a.timing.DOMComplete = (v.timing.domComplete - v.timing.navigationStart);
-		a.timing.DOMFinished = (v.timing.complete - v.timing.navigationStart);
-	}
 	fw.saveUnloadMetrics = () => {
-		a.timing.TimeSpent = (v.timing.navigationEnd - v.timing.navigationStart);
+		console.log('analytics immobilizing');
+		a.timing.TimeSpent = (Date.now() - window.performance.timing.navigationStart);
+		console.log('analytics immobilized');
 	}
 var f = {};
 	f.ms_to_hms = (ms) => {
@@ -32,16 +20,6 @@ var f = {};
 			scs = (ms / 1000) % 60;
 		return ((hrs>0)?(hrs < 10 ? '0' : '') + hrs + ":": "") + (mns < 10 ? '0' : '') + mns + ":" + (scs < 10 ? '0' : '') + scs;
 	}
-var v = {};
-	v.timing = {};
-		v.timing.navigationStart = null;
-		v.timing.navigationEnd = null;
-		v.timing.domainLookupStart = null;
-		v.timing.domainLookupEnd = null;
-		v.timing.domInteractive = null;
-		v.timing.domContentLoadedEventEnd = null;
-		v.timing.domComplete = null;
-		v.timing.complete = null;
 var a = {}
 	a.timing = {};
 		a.timing.DOMLookup = null;
@@ -54,4 +32,4 @@ var a = {}
 
 	}
 
-export { fw, f };
+export { fw, f, a };
