adb5e4b822e27d10a8df9806c5aeef196464a7b6
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 12:36:16 +0100

    Minor changes to analytics JS

--- CDN/analytics/1.0.0/csa.js
+++ CDN/analytics/1.0.0/csa.js
@@ -6,8 +6,8 @@ fw.init();
 window.onload = function () {
 	console.log(window.performance.timing);
 	
-	console.log("DOM loaded in "+(window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart)+"ms");
 	console.log("DOM interactive in "+(window.performance.timing.domInteractive - window.performance.timing.navigationStart)+"ms");
+	console.log("DOM loaded in "+(window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart)+"ms");
 	console.log("DOM complete in "+(window.performance.timing.domComplete - window.performance.timing.navigationStart)+"ms");
 	console.log("Fulliy loaded in "+(Date.now() - window.performance.timing.navigationStart)+"ms");
 }
