704095bc2e2ea27c56f934471863f10f0a64c146
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 11:46:35 +0100

    Minor changes to analytics JS

--- CDN/analytics/1.0.0/csa.js
+++ CDN/analytics/1.0.0/csa.js
@@ -1,4 +1,10 @@
 import { init, loadTime } from "./framework.js";
 
 init();
-loadTime()
+var pageRequested = Date.now();
+$(document).ready(function() {
+	console.log("Time until DOMready: ", Date.now() - pageRequested);
+});
+$(window).load(function() {
+	console.log("Time until everything loaded: ", Date.now()-pageRequested);
+});
--- CDN/analytics/1.0.0/framework.js
+++ CDN/analytics/1.0.0/framework.js
@@ -3,13 +3,7 @@ function init() {
 }
 
 function loadTime() {
-	var pageRequested = Date.now();
-	$(document).ready(function() {
-		console.log("Time until DOMready: ", Date.now() - pageRequested);
-	});
-	$(window).load(function() {
-		console.log("Time until everything loaded: ", Date.now()-pageRequested);
-	});
+
 }
 export {
 	init,
