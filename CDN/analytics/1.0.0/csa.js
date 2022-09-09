170c2f4343faa64f97c6817ea7e29a84fa1111bb
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 12:07:39 +0100

    Minor changes to analytics JS

--- CDN/analytics/1.0.0/csa.js
+++ CDN/analytics/1.0.0/csa.js
@@ -1,12 +1,8 @@
-import * as tti from "./vendors/tti-polyfill.js";
+//import * as tti from "./vendors/tti-polyfill.js";
 import * as fw from "./framework.js";
 
 fw.init();
 
 window.onload = function () {
 	console.log(window.performance.timing);
-    var DOMloadTime = window.performance.timing.domContentLoadedEventStart-window.performance.timing.navigationStart;
-	var FINloadTime = window.performance.timing.connectStart-window.performance.timing.navigationStart;
-    console.log('Page load time is '+ DOMloadTime);
-    console.log('Page load time is '+ FINloadTime);
 }
