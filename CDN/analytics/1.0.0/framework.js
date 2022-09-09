734c00dea065fcccf0b0ed9c3269f914d22606f9
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 12:55:03 +0100

    Minor changes to analytics JS

--- CDN/analytics/1.0.0/framework.js
+++ CDN/analytics/1.0.0/framework.js
@@ -1,15 +1,16 @@
 function init() {
 	console.log('analytics initializing');
 }
-
-function convertms(ms) {
-	var hrs = Math.floor(ms / 3600000),
-		mns = ((ms % 60000) / 6000).toFixed(0),
-		scs = ((ms % 3600000) / 1000).toFixed(0);
-	return ((hrs>0)?(hrs < 10 ? '0' : '') + hrs + ":": "") + (mns < 10 ? '0' : '') + mns + ":" + (scs < 10 ? '0' : '') + scs + " (" + ms + "ms)";
-  }
+convert = {
+	ms_to_MHS(ms) {
+		var hrs = Math.floor(ms / 3600000),
+			mns = ((ms % 60000) / 6000).toFixed(0),
+			scs = ((ms % 3600000) / 1000).toFixed(0);
+		return ((hrs>0)?(hrs < 10 ? '0' : '') + hrs + ":": "") + (mns < 10 ? '0' : '') + mns + ":" + (scs < 10 ? '0' : '') + scs + " (" + ms + "ms)";
+	}
+}
   
 export {
 	init,
-	convertms,
+	convert,
 };
