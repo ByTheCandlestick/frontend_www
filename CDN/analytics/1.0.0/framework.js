d17be57cd2b4f3b66853a9e9a2a1eae779e0d9d9
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 12:43:20 +0100

    Minor changes to analytics JS

--- CDN/analytics/1.0.0/framework.js
+++ CDN/analytics/1.0.0/framework.js
@@ -3,8 +3,8 @@ function init() {
 }
 
 function convertms(ms) {
-	var hours = Math.floor(ms / 60000),
-		minutes = ((ms % 60000) / 1000).toFixed(0),
+	var hours = Math.floor(ms / 3600000),
+		minutes = ((ms % 60000) / 6000).toFixed(0),
 		seconds = ((ms % 3600000) / 1000).toFixed(0);
 	return (hours < 10 ? '0' : '') + hours + ":" + (minutes < 10 ? '0' : '') + minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
   }
