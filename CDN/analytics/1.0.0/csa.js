d3f0bb133b730d72f0a15ec4e97da19621983205
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 14:04:48 +0100

    Minor changes to analytics JS

--- CDN/analytics/1.0.0/csa.js
+++ CDN/analytics/1.0.0/csa.js
@@ -11,3 +11,8 @@ window.onload = function () {
 	console.log("DOM complete in "+fw.funcs.ms_to_hms(window.performance.timing.domComplete - window.performance.timing.navigationStart));
 	console.log("Fulliy loaded in "+fw.funcs.ms_to_hms(Date.now() - window.performance.timing.navigationStart));
 }
+window.onbeforeunload = function(){
+	var end = window.performance.now();
+	console.log(`Execution time: ${end - start} ms`);
+	return 'Are you sure you want to leave?';
+};
