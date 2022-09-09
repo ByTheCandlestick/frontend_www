29360ab436def6f507b017e4c87c55792b011cda
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 14:09:27 +0100

    Minor changes to analytics JS

--- CDN/analytics/1.0.0/csa.js
+++ CDN/analytics/1.0.0/csa.js
@@ -3,13 +3,19 @@ import { fw } from "./framework.js";
 console.log(fw.init());
 
 window.onload = function () {
-	console.log(window.performance.timing);
-	fw.vars.initTime = window.performance.timing.navigationStart;
-	console.log("Domain lookup took "+fw.funcs.ms_to_hms(window.performance.timing.domainLookupEnd - window.performance.timing.domainLookupStart));
-	console.log("DOM interactive in "+fw.funcs.ms_to_hms(window.performance.timing.domInteractive - window.performance.timing.navigationStart));
-	console.log("DOM loaded in "+fw.funcs.ms_to_hms(window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart));
-	console.log("DOM complete in "+fw.funcs.ms_to_hms(window.performance.timing.domComplete - window.performance.timing.navigationStart));
-	console.log("Fulliy loaded in "+fw.funcs.ms_to_hms(Date.now() - window.performance.timing.navigationStart));
+	fw.vars.timing.navigationStart = window.performance.timing.navigationStart;
+	fw.vars.timing.domainLookupStart = window.performance.timing.domainLookupStart;
+	fw.vars.timing.domainLookupEnd = window.performance.timing.domainLookupEnd;
+	fw.vars.timing.domInteractive = window.performance.timing.domInteractive;
+	fw.vars.timing.domContentLoadedEventEnd = window.performance.timing.domContentLoadedEventEnd;
+	fw.vars.timing.domComplete = window.performance.timing.domComplete;
+	fw.vars.timing.complete = Date.now();
+
+	console.log("Domain lookup took "+fw.funcs.ms_to_hms(fw.vars.timing.domainLookupEnd - fw.vars.timing.domainLookupStart));
+	console.log("DOM interactive in "+fw.funcs.ms_to_hms(fw.vars.timing.domInteractive - fw.vars.timing.navigationStart));
+	console.log("DOM loaded in "+fw.funcs.ms_to_hms(fw.vars.timing.domContentLoadedEventEnd - fw.vars.timing.navigationStart));
+	console.log("DOM complete in "+fw.funcs.ms_to_hms(fw.vars.timing.domComplete - fw.vars.timing.navigationStart));
+	console.log("Fulliy loaded in "+fw.funcs.ms_to_hms(fw.vars.timing.complete - fw.vars.timing.navigationStart));
 }
 window.onbeforeunload = function(){
 	var end = window.performance.now();
