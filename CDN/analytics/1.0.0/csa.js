ccf687987091caa65156c398d8d6427ba3193cd4
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 12:06:00 +0100

    Minor changes to analytics JS

--- CDN/analytics/1.0.0/csa.js
+++ CDN/analytics/1.0.0/csa.js
@@ -4,6 +4,7 @@ import * as fw from "./framework.js";
 fw.init();
 
 window.onload = function () {
+	console.log(window.performance.timing);
     var DOMloadTime = window.performance.timing.domContentLoadedEventStart-window.performance.timing.navigationStart;
 	var FINloadTime = window.performance.timing.connectStart-window.performance.timing.navigationStart;
     console.log('Page load time is '+ DOMloadTime);
