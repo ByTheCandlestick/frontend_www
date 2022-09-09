fead91099d0abff755a2faaed494ee36f5e89081
Robert Smith <master_schmadoodle@outlook.com>
2022-09-09 12:08:56 +0100

    Minor changes to analytics JS

--- CDN/analytics/1.0.0/csa.js
+++ CDN/analytics/1.0.0/csa.js
@@ -4,5 +4,5 @@ import * as fw from "./framework.js";
 fw.init();
 
 window.onload = function () {
-	console.log(window.performance.timing);
+	console.log("Dom loaded in "+window.performance.timing.domContentLoadedEventEnd - window.performance.timing.connectStart+"ms");
 }
