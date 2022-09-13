66404112162493fb19f5534c1daab08879db9f01
Robert Smith <master_schmadoodle@outlook.com>
2022-09-13 15:22:27 +0100

    Minor changes to framework classes

--- CDN/analytics/1.0.0/framework.js
+++ CDN/analytics/1.0.0/framework.js
@@ -43,9 +43,9 @@ var f = {};
 		}
 	}
 	f.registerClick = () => {
-		a.clicks[a.clicks.length] = [];
-		a.clicks[a.clicks.length]["X"] = event.screenX + window.scrollX;
-		a.clicks[a.clicks.length]["Y"] = event.screenY + window.scrollY;
+		a.clicks[Object.keys(a.clicks).length] = [];
+		a.clicks[Object.keys(a.clicks)]["X"] = event.screenX + window.scrollX;
+		a.clicks[Object.keys(a.clicks)]["Y"] = event.screenY + window.scrollY;
 	}
 	f.saveLoadMetrics = () => {
 		//console.log('analytics initializing');
