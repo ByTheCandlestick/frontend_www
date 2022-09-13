0f838d9aacfda80ce9e4b38473d01e529f3b8141
Robert Smith <master_schmadoodle@outlook.com>
2022-09-13 17:17:16 +0100

    Added lifecycle.js functionality

--- Themes/Voxo/index.php
+++ Themes/Voxo/index.php
@@ -112,7 +112,6 @@
 						}
 					?>
 			<!-- ===== Javascript ===== -->
-				<script defer src="/CDN/lifecycle/0.1.1/lifecycle.mjs"></script>
 				<script src="/CDN/analytics/1.0.0/csa.js" type="module"></script>
 				<?
 					if($layout_row['script_ids'] != NULL) {
