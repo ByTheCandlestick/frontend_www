7eace5d3deeb99012450a4f3d61cc313f6f29b85
Robert Smith <master_schmadoodle@outlook.com>
2022-09-13 11:02:15 +0100

    Minor changes to framework classes

--- CDN/analytics/1.0.0/csa.js
+++ CDN/analytics/1.0.0/csa.js
@@ -4,7 +4,7 @@ window.onload = function () {
 	f.registerAnalyticsID();
 	f.saveLoadMetrics();
 	f.saveBrowserMetrics();
-	f.saveSystemMetrics();
+	f.saveSystemMetrics();S
 	console.log(a);
 }
 window.onbeforeunload = function(){
