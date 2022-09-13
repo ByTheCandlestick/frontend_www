8c72ac5f93b49d3c479caf121f1fcf09379fd494
Robert Smith <master_schmadoodle@outlook.com>
2022-09-13 17:38:48 +0100

    Added lifecycle.js functionality

--- CDN/analytics/1.0.0/csa.js
+++ CDN/analytics/1.0.0/csa.js
@@ -1,5 +1,5 @@
 import { f, a } from "./framework.js";
-import "../../lifecycle/0.1.1/lifecycle.js";
+import * as lifecycle from "../../lifecycle/0.1.1/lifecycle.js";
 
 window.onload = () => {
 	f.registerAnalyticsID();
