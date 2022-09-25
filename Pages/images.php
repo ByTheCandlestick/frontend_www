eca8dde376758e6516af717ca23ee6504adb8bcc
Robert Smith <master_schmadoodle@outlook.com>
2022-09-25 16:12:37 +0100

    Minor changes to image edit page

--- Pages/images.php
+++ Pages/images.php
@@ -126,8 +126,38 @@
 
 				</div>
 				<script>
-					import Cropper from 'cropperjs';
-					const cropper = new Cropper($('img#cropperImage'));
+					var image = container.getElementById('cropperImage');
+					var options = {
+						aspectRatio: 16 / 9,
+						preview: '.img-preview',
+						ready: function (e) {
+							console.log(e.type);
+						},
+						cropstart: function (e) {
+							console.log(e.type, e.detail.action);
+						},
+						cropmove: function (e) {
+							console.log(e.type, e.detail.action);
+						},
+						cropend: function (e) {
+							console.log(e.type, e.detail.action);
+						},
+						crop: function (e) {
+							var data = e.detail;
+							console.log(e.type);
+							dataX.value = Math.round(data.x);
+							dataY.value = Math.round(data.y);
+							dataHeight.value = Math.round(data.height);
+							dataWidth.value = Math.round(data.width);
+							dataRotate.value = typeof data.rotate !== 'undefined' ? data.rotate : '';
+							dataScaleX.value = typeof data.scaleX !== 'undefined' ? data.scaleX : '';
+							dataScaleY.value = typeof data.scaleY !== 'undefined' ? data.scaleY : '';
+						},
+						zoom: function (e) {
+							console.log(e.type, e.detail.ratio);
+						}
+					};
+					const cropper = new Cropper(image, options);
 				</script>
 			</div>
 			<div class="modal-footer">
