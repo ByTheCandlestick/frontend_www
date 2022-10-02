202f693fd2265a73957f6e4dc1b707df29a69cf5
Robert Smith <master_schmadoodle@outlook.com>
2022-10-02 13:15:34 +0100

    Minor changes to image editor

--- Pages/images.php
+++ Pages/images.php
@@ -118,85 +118,21 @@
 			</div>
 			<div class="modal-body row">
 				<div class="col-12 col-md-8 bg-primary">
-					<img id="cropperImage" src="images/partners/fizzlux/logo.jpeg">
+					<div id="image-editor">
+						<canvas></canvas>
+					</div>
 				</div>
 				<div class="col-12 col-md-4 bg-secondary">
 
 				</div>
 				<script>
-					$(document).ready(function() {
-						var image = document.getElementById('cropperImage');
-						var cropper = new Cropper(image, {
-							// The view mode of the cropper
-							viewMode: 0, // 0, 1, 2, 3
-							// The dragging mode of the cropper
-							//dragMode: DRAG_MODE_CROP, // 'crop', 'move' or 'none'
-							// The initial aspect ratio of the crop box
-							initialAspectRatio: NaN,
-							// The aspect ratio of the crop box
-							aspectRatio: NaN,
-							// An object with the previous cropping result data
-							data: null,
-							// A selector for adding extra containers to preview
-							preview: '',
-							// Re-render the cropper when resize the window
-							responsive: true,
-							// Restore the cropped area after resize the window
-							restore: true,
-							// Check if the current image is a cross-origin image
-							checkCrossOrigin: true,
-							// Check the current image's Exif Orientation information
-							checkOrientation: true,
-							// Show the black modal
-							modal: true,
-							// Show the dashed lines for guiding
-							guides: true,
-							// Show the center indicator for guiding
-							center: true,
-							// Show the white modal to highlight the crop box
-							highlight: true,
-							// Show the grid background
-							background: true,
-							// Enable to crop the image automatically when initialize
-							autoCrop: true,
-							// Define the percentage of automatic cropping area when initializes
-							autoCropArea: 0.8,
-							// Enable to move the image
-							movable: true,
-							// Enable to rotate the image
-							rotatable: true,
-							// Enable to scale the image
-							scalable: true,
-							// Enable to zoom the image
-							zoomable: true,
-							// Enable to zoom the image by dragging touch
-							zoomOnTouch: true,
-							// Enable to zoom the image by wheeling mouse
-							zoomOnWheel: true,
-							// Define zoom ratio when zoom the image by wheeling mouse
-							wheelZoomRatio: 0.1,
-							// Enable to move the crop box
-							cropBoxMovable: true,
-							// Enable to resize the crop box
-							cropBoxResizable: true,
-							// Toggle drag mode between "crop" and "move" when click twice on the cropper
-							toggleDragModeOnDblclick: true,
-							// Size limitation
-							minCanvasWidth: 0,
-							minCanvasHeight: 0,
-							minCropBoxWidth: 0,
-							minCropBoxHeight: 0,
-							minContainerWidth: 200,
-							minContainerHeight: 100,
-							// Shortcuts of events
-							ready: null,
-							cropstart: null,
-							cropmove: null,
-							cropend: null,
-							crop: null,
-							zoom: null
-						});
+					var imageEditor = new tui.component.ImageEditor('#image-editor canvas', {
+						cssMaxWidth: 1000,
+						cssMaxHeight: 1000,
 					});
+					function loadImage() {
+						imageEditor.loadImageFromURL('img/sampleImage.jpg', 'My sample image');
+					}
 				</script>
 			</div>
 			<div class="modal-footer">
