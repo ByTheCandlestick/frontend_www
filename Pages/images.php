a0c37a93f00e36fb898983489cb04b962d8502a5
Robert Smith <master_schmadoodle@outlook.com>
2022-10-02 15:46:39 +0100

    Minor changes to image editor

--- Pages/images.php
+++ Pages/images.php
@@ -119,7 +119,7 @@
 			<div class="modal-body row">
 				<div class="col-12 col-md-8 bg-primary">
 					<div id="image-editor">
-						<canvas>
+						<canvas></canvas>
 					</div>
 				</div>
 				<div class="col-12 col-md-4 bg-secondary">
@@ -127,7 +127,10 @@
 				</div>
 				<script>
 					$( document ).ready(function() {
-						images.modify.imageEditor = new tui.ImageEditor('#image-editor canvas');
+						images.modify.imageEditor = new tui.ImageEditor('#image-editor canvas', {
+							cssMaxWidth: 1000, // Component default value: 1000
+							cssMaxHeight: 800, // Component default value: 800
+						});
 					})
 				</script>
 			</div>
