4040eb291ee94d2af7ad7412258d1db1b5a9bc5e
Robert Smith <master_schmadoodle@outlook.com>
2023-01-29 15:14:49 +0000

    minor changes

--- Classes/vars.php
+++ Classes/vars.php
@@ -69,7 +69,7 @@
 				//
 			}
 		// GET ALL USERS
-			$users = [];
+			$	 = [];
 			$tmp = DB_Query("SELECT * FROM `User accounts` WHERE `Active`='1'");
 			while($u = mysqli_fetch_assoc($tmp)) { $users[$u['ID']] = $u; }
 ?>
--- Pages/blog.php
+++ Pages/blog.php
@@ -45,7 +45,7 @@
                                             <tr>
                                                 <td>'.$row['ID'].'</td>
                                                 <td>'.$row['Title'].'</td>
-                                                <td>'.$users[$row['UID']]['Username'].'</td>
+                                                <td>'.$users[$row['UID']].'</td>
                                                 <td>
                                         ');
                                         if($userperm['api_access-hosts-edit']==1) {
