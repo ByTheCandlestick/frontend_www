a15a0c82f813781f550f2c73517906432d18e5cd
Robert Smith <master_schmadoodle@outlook.com>
2023-01-30 13:10:30 +0000

    Minor changes to work on oauth

--- Themes/Vuexy/Assets/php/OauthCallback.php
+++ Themes/Vuexy/Assets/php/OauthCallback.php
@@ -1,8 +1,10 @@
 <?
     print_r($_GET);
-    print_r($_POST);    
+    print_r($_POST);
 
-    print_r($userdata);
+    if($userdata['Zoho Mail Access Code'] == null) {
+        print('hello');
+    }
 ?>
 
 <html>
