5fdb437a8fbb6bad70c263bba53bb456a857297a
Robert Smith <master_schmadoodle@outlook.com>
2023-01-30 17:09:56 +0000

    Minor changes to work on oauth

--- Themes/Vuexy/Assets/js/script.js
+++ Themes/Vuexy/Assets/js/script.js
@@ -1820,7 +1820,7 @@
 $(document).ready(function() {
 	// -----========== Re-auth Zoho ==========----- // @wip //
 	$('.zoho-status.error').click(()=>{
-		var win = window.open('https://accounts.zoho.eu/oauth/v2/auth?scope=ZohoMail.messages.CREATE,ZohoMail.messages.READZohoMail.messages.UPDATEZohoMail.messages.DELETE&client_id='+$('.zoho-status').attr('cid')+'&response_type=code&access_type=online&redirect_uri=http://admin.candlestick-indev.co.uk/OauthCallback.php', 'Zoho OAuth', 'scrollbars=no,resizable=yes,status=no,location=no,toolbar=no,menubar=no,width=0,height=0,left=0,top=0');
+		var win = window.open('https://accounts.zoho.eu/oauth/v2/auth?scope=ZohoMail.messages.CREATE,ZohoMail.messages.READ,ZohoMail.messages.UPDATE,ZohoMail.messages.DELETE&client_id='+$('.zoho-status').attr('cid')+'&response_type=code&access_type=online&redirect_uri=http://admin.candlestick-indev.co.uk/OauthCallback.php', 'Zoho OAuth', 'scrollbars=no,resizable=yes,status=no,location=no,toolbar=no,menubar=no,width=0,height=0,left=0,top=0');
 		var timer = setInterval(function() {   
 			if(win.closed) {  
 				clearInterval(timer);  
