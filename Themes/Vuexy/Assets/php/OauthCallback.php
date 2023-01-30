d41b69147c4d3bc25feefd40d6f978109f916925
Robert Smith <master_schmadoodle@outlook.com>
2023-01-30 16:53:57 +0000

    Minor changes to work on oauth

--- Themes/Vuexy/Assets/php/OauthCallback.php
+++ Themes/Vuexy/Assets/php/OauthCallback.php
@@ -8,17 +8,22 @@
     </head>
     <body>
         <script>
-		    const api_url = '<?print(__API__)?>/Users/OAuth/';
-            const xhr = new XMLHttpRequest();
-            // configure a `POST` request
-            xhr.open('POST', api_url)
-            // create a JSON object
-            params= 'api_key=s999MUtbgk44pSsXClHLmlT7M0MV6zXZr8CXRJzRP1uU75CnyKUJwiAdK3vwcCDsOIfTEMGuDFqCegQd8ySZ7qIuZyOhEEsbqvo1lejs2qOU8J3bleZV9PG6GQoJsp6nv45c4CQINEswrkLvLUjafTBCkti8migCD94azeA9uWkH7PwlVjLjXprYQ1AbGbQwX54PLezf3XsqwVQVZccZUUeasavhlwK8nwCDuSjLUrdMefTjS0ZH09SO5qeKBn3s&oauth=<?print($userdata['Zoho Mail Access Code'])?>&uid=<?print($userdata['ID'])?>'
-            // set `Content-Type` header
-            xhr.setRequestHeader('Content-Type', 'application/json')
-            // pass `params` to `send()` method
-            xhr.send(params)
-            // listen for `load` event
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", '<?print(__API__)?>/Users/OAuth/');
+
+            xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                console.log(xhr.status);
+                console.log(xhr.responseText);
+            }};
+
+            let data = {
+                "api_key": 's999MUtbgk44pSsXClHLmlT7M0MV6zXZr8CXRJzRP1uU75CnyKUJwiAdK3vwcCDsOIfTEMGuDFqCegQd8ySZ7qIuZyOhEEsbqvo1lejs2qOU8J3bleZV9PG6GQoJsp6nv45c4CQINEswrkLvLUjafTBCkti8migCD94azeA9uWkH7PwlVjLjXprYQ1AbGbQwX54PLezf3XsqwVQVZccZUUeasavhlwK8nwCDuSjLUrdMefTjS0ZH09SO5qeKBn3s',
+                "oauth": "<?print($userdata['Zoho Mail Access Code'])?>",
+                "uid": "<?print($userdata['ID'])?>"
+            };
+
+            xhr.send(data);
             xhr.onload = () => {
                 console.log(xhr.responseText)
             }
