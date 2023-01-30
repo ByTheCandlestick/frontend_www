e5feddcd3122955da66265bd9572a46c65554937
Robert Smith <master_schmadoodle@outlook.com>
2023-01-30 11:23:52 +0000

    Minor changes to email script

--- Themes/Vuexy/Assets/php/email.php
+++ Themes/Vuexy/Assets/php/email.php
@@ -1,26 +1,39 @@
 <?
-    $url = 'https://mail.zoho.eu/api/accounts/20081772349/messages';
-    $data = array(
-        'fromAddress'=>'noreply@thecandlestick.co.uk',
-        'toAddress'=>'rsmith_20@outlook.com',
-        'subject'=>'subject',
-        'content'=>'content',
-        'askReceipt'=>'yes');
+    // Get user account
+        $url = " http://mail.zoho.com/api/accounts";
+        $options = array(
+            'http' => array(
+                'method'  => 'GET',
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE) { 
+            echo 'Email failled to get account. Var dump:';
+            die(var_dump($result));
+        }
 
-    // use key 'http' even if you send the request to https://...
-    $options = array(
-        'http' => array(
-            'method'  => 'POST',
-            'content' => http_build_query($data)
-        )
-    );
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { 
-        echo 'Email failled to send. Var dump:';
-        var_dump($result);
-    } else {
-        echo 'Email sent successfully';
-    }
+    // Send Email
+        $url = 'https://mail.zoho.eu/api/accounts/20081772349/messages';
+        $data = array(
+            'fromAddress'=>'noreply@thecandlestick.co.uk',
+            'toAddress'=>'rsmith_20@outlook.com',
+            'subject'=>'subject',
+            'content'=>'content',
+            'askReceipt'=>'yes');
+        $options = array(
+            'http' => array(
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE) { 
+            echo 'Email failled to send. Var dump:';
+            var_dump($result);
+        } else {
+            echo 'Email sent successfully';
+        }
 
 ?>
