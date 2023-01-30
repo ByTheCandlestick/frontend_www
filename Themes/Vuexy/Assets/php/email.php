077f26c1e0393542cad92a0d82e84d27d3873c33
Robert Smith <master_schmadoodle@outlook.com>
2023-01-30 12:08:42 +0000

    Minor changes to email script

--- Themes/Vuexy/Assets/php/email.php
+++ Themes/Vuexy/Assets/php/email.php
@@ -1,12 +1,17 @@
 <?
     // Auth
-        $client_id='1000.XN45OX38ZOV31OELINSLXW5NJBM09X';
-        $client_secret='fede0b187e3e93608c2fd2bf8cf214267f0b172c9a';
+        $client_id='1000.GMJG9U2N95YAZGNK6ZN871Q944ASIH';
+        $client_secret='e6a2b1e622c987501425712fb1e6193a41eebf38f3';
         $scope='messages';
         $response_type='code';
         $rediredt_url='http://candlestick-indev.co.uk/';
 
-        $url = 'https://accounts.zoho.eu/oauth/v2/auth?scope='.$scope.'&client_id='.$client_id.'&response_type='.$response_type.'&access_type=online&redirect_uri='.$redirect_uri;
+        $url = 'https://accounts.zoho.com/oauth/v2/auth?
+            response_type=code&
+            client_id='.$client_id.'&
+            scope=ZohoMail.folders.READ&
+            redirect_uri=http://candlestick-indev.co.uk/&
+        ';
         $options = array(
             'http' => array(
                 'content' => 'application/x-www-form-urlencoded',
@@ -19,41 +24,4 @@
             echo 'Failled to auth. Var dump: ';
             die(var_dump($result));
         }
-    // Get user account
-        $url = " http://mail.zoho.com/api/accounts";
-        $options = array(
-            'http' => array(
-                'method'  => 'GET',
-            )
-        );
-        $context  = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
-        if ($result === FALSE) { 
-            echo 'Email failled to get account. Var dump: ';
-            die(var_dump($result));
-        }
-
-    // Send Email
-        $url = 'https://mail.zoho.eu/api/accounts/20081772349/messages';
-        $data = array(
-            'fromAddress'=>'noreply@thecandlestick.co.uk',
-            'toAddress'=>'rsmith_20@outlook.com',
-            'subject'=>'subject',
-            'content'=>'content',
-            'askReceipt'=>'yes');
-        $options = array(
-            'http' => array(
-                'method'  => 'POST',
-                'content' => http_build_query($data)
-            )
-        );
-        $context  = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
-        if ($result === FALSE) { 
-            echo 'Email failled to send. Var dump: ';
-            var_dump($result);
-        } else {
-            echo 'Email sent successfully';
-        }
-
 ?>
