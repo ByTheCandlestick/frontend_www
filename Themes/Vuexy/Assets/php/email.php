f14bb2dd533ab304610c13c703153c51e15bd8c1
Robert Smith <master_schmadoodle@outlook.com>
2023-01-30 12:22:01 +0000

    Minor changes to email script

--- Themes/Vuexy/Assets/php/email.php
+++ Themes/Vuexy/Assets/php/email.php
@@ -35,9 +35,7 @@
         <input type="button" value="Send Email" onclick="submit();">
         <script>
             function submit() {
-                var url = "https://accounts.zoho.com/oauth/v2/auth";
-                var params = "\
-                    scope=ZohoMail.folders.READ&\
+                var url = "https://accounts.zoho.com/oauth/v2/auth?scope=ZohoMail.folders.READ&\
                     client_id=1000.GMJG9U2N95YAZGNK6ZN871Q944ASIH&\
                     response_type=code&\
                     access_type=online&\
