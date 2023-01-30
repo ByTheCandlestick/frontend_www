d37233fdeb7ffec9c62d7507095bc38aa1fcbffc
Robert Smith <master_schmadoodle@outlook.com>
2023-01-30 16:44:16 +0000

    Minor changes to work on oauth

--- Themes/Vuexy/Assets/php/OauthCallback.php
+++ Themes/Vuexy/Assets/php/OauthCallback.php
@@ -8,13 +8,13 @@
     </head>
     <body>
         <script>
-		    const api_url = '<?print(__API__)?>/Users/OAuth/';
+		    const api_url = '<?print(__API__)?>/Users/OAuth/?api_key=s999MUtbgk44pSsXClHLmlT7M0MV6zXZr8CXRJzRP1uU75CnyKUJwiAdK3vwcCDsOIfTEMGuDFqCegQd8ySZ7qIuZyOhEEsbqvo1lejs2qOU8J3bleZV9PG6GQoJsp6nv45c4CQINEswrkLvLUjafTBCkti8migCD94azeA9uWkH7PwlVjLjXprYQ1AbGbQwX54PLezf3XsqwVQVZccZUUeasavhlwK8nwCDuSjLUrdMefTjS0ZH09SO5qeKBn3s';
             const xhr = new XMLHttpRequest()
             // configure a `POST` request
             xhr.open('POST', api_url)
             // create a JSON object
             const params = {
-                api_key: 's999MUtbgk44pSsXClHLmlT7M0MV6zXZr8CXRJzRP1uU75CnyKUJwiAdK3vwcCDsOIfTEMGuDFqCegQd8ySZ7qIuZyOhEEsbqvo1lejs2qOU8J3bleZV9PG6GQoJsp6nv45c4CQINEswrkLvLUjafTBCkti8migCD94azeA9uWkH7PwlVjLjXprYQ1AbGbQwX54PLezf3XsqwVQVZccZUUeasavhlwK8nwCDuSjLUrdMefTjS0ZH09SO5qeKBn3s',
+                api_key: '',
                 oauth: '<?print($userdata['Zoho Mail Access Code'])?>',
                 uid: '<?print($userdata['ID'])?>'
             }
