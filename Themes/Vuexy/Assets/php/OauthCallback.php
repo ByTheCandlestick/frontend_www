b5d07623a4fa6b95c4138c35b8d6013ab539bf94
Robert Smith <master_schmadoodle@outlook.com>
2023-01-30 16:30:06 +0000

    Minor changes to work on oauth

--- Themes/Vuexy/Assets/php/OauthCallback.php
+++ Themes/Vuexy/Assets/php/OauthCallback.php
@@ -16,7 +16,7 @@
             const params = {
                 api_key: 'iwdk5xYYMyUbyKuHMB8UuA5R2pbqgYLvjzzKQFCeJzKbAkg2qAJGWunzJPZFxvaCvue5xHJEwrhG3b9Ye5mn3UYBT7ZE46crHkgenvY4LaUSgb3Jcj8T67tUuyVtD6nRTQxvurPZ6E96WiQKep7G8kUjJhxHchEZk6KrWqZ2Tf2B9ZgtErZ4UMNNSJWE9DV8gM3YMkzmraACBxd9nPBteJKPx3SFdBMHQGBAL5bzSmJtCfezQJ7Ed3hk4CBnhda3',
                 oauth: '<?print($userdata['Zoho Mail Access Code'])?>',
-                uid: '<?print($userdata['Uid'])?>'
+                uid: '<?print($userdata['ID'])?>'
             }
             // set `Content-Type` header
             xhr.setRequestHeader('Content-Type', 'application/json')
