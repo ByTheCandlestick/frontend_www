1ffeed1d5c0c0cf55964ef8704ab29ef293fe7a7
Robert Smith <master_schmadoodle@outlook.com>
2023-01-30 13:11:57 +0000

    Minor changes to work on oauth

--- Themes/Vuexy/Assets/php/OauthCallback.php
+++ Themes/Vuexy/Assets/php/OauthCallback.php
@@ -1,10 +1,6 @@
 <?
     print_r($_GET);
     print_r($_POST);
-
-    if($userdata['Zoho Mail Access Code'] == null) {
-        print('hello');
-    }
 ?>
 
 <html>
@@ -12,8 +8,16 @@
 
     </head>
     <body>
-        <script>
 
+        <script>
+<?
+    if($userdata['Zoho Mail Access Code'] == null) {
+?>
+    window.open('https://accounts.zoho.eu/oauth/v2/auth?scope=messages&client_id=1000.GMJG9U2N95YAZGNK6ZN871Q944ASIH&response_type=code&access_type=online&redirect_uri=http://admin.candlestick-indev.co.uk/OathCallback.php')
+
+<?
+    }
+?>
         </script>
     </body>
 </html>
