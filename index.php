8699e528cfa50c39037a31ec2f58946976ff654e
Robert Smith <master_schmadoodle@outlook.com>
2023-01-30 13:05:41 +0000

    Fixed get requests

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
 		if(isset($_GET['file']) && isset($_GET['ext'])) {
 			if(file_exists($path = sprintf("%s/Themes/%s/Assets/%s/%s.%s", __ROOT__, __THEME__, $_GET['ext'], $_GET['file'], $_GET['ext']))) {
 				if($_GET['ext'] == "php") {
-					empty($_GET);
+					unset($_GET);
 					include_once($path);
 				} else {
 					header('Content-Type: text/'.$_GET['ext']);
-					empty($_GET);
+					unset($_GET);
 					print(file_get_contents($path));
 				}
 			} else {
