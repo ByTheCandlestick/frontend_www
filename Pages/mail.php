71dc1a3df631eb8a77523ff7351cb3c64b3f4b21
Robert Smith <master_schmadoodle@outlook.com>
2023-02-01 11:10:16 +0000

    Minor changes to admin email page

--- Pages/mail.php
+++ Pages/mail.php
@@ -1,34 +1,22 @@
 <?
 	$query = "";
 	if(strtolower(QS_SUBPAGE) == "inbox") {
-		//The url you wish to send the POST request to
 		$url = "https://accounts.zoho.eu/oauth/v2/token/";
-
-		//The data you want to send via POST
-		$fields = [
+		$fields_string = http_build_query([
 			'code' => $userdata['Zoho Mail Auth Code'],
 			'grant_type' => 'authorization_code',
 			'client_id' => $config['Zoho Client ID'],
 			'client_secret' => $config['Zoho Client Secret'],
 			'redirect_uri' => 'http://admin.candlestick-indev.co.uk/OauthCallback.php'
-		];
+		]);
+		
+		$conn = curl_init();
+		curl_setopt($conn, CURLOPT_URL,				$url);
+		curl_setopt($conn, CURLOPT_POST,			true);
+		curl_setopt($conn, CURLOPT_POSTFIELDS,		$fields_string);
+		curl_setopt($conn, CURLOPT_RETURNTRANSFER,	true); 
+		$result = curl_exec($conn);
 
-		//url-ify the data for the POST
-		$fields_string = http_build_query($fields);
-
-		//open connection
-		$ch = curl_init();
-
-		//set the url, number of POST vars, POST data
-		curl_setopt($ch,CURLOPT_URL, $url);
-		curl_setopt($ch,CURLOPT_POST, true);
-		curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-
-		//So that curl_exec returns the contents of the cURL; rather than echoing it
-		curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
-
-		//execute post
-		$result = curl_exec($ch);
 		echo $result;
 	?>
 		<!--
