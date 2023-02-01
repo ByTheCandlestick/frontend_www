e118baaebdd798ba6859862f2d111e0e1ff2c8c9
Robert Smith <master_schmadoodle@outlook.com>
2023-02-01 11:07:45 +0000

    Added refund old orders daily function

--- Pages/mail.php
+++ Pages/mail.php
@@ -1,9 +1,38 @@
 <?
 	$query = "";
 	if(strtolower(QS_SUBPAGE) == "inbox") {
+		//The url you wish to send the POST request to
+		$url = "https://accounts.zoho.eu/oauth/v2/token/";
+
+		//The data you want to send via POST
+		$fields = [
+			'code' => $userdata['Zoho Mail Auth Code'],
+			'grant_type' => 'authorization_code',
+			'client_id' => $config['Zoho Client ID'],
+			'client_secret' => $config['Zoho Client Secret'],
+			'redirect_uri' => 'http://admin.candlestick-indev.co.uk/OauthCallback.php'
+		];
+
+		//url-ify the data for the POST
+		$fields_string = http_build_query($fields);
+
+		//open connection
+		$ch = curl_init();
+
+		//set the url, number of POST vars, POST data
+		curl_setopt($ch,CURLOPT_URL, $url);
+		curl_setopt($ch,CURLOPT_POST, true);
+		curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+		//So that curl_exec returns the contents of the cURL; rather than echoing it
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+
+		//execute post
+		$result = curl_exec($ch);
+		echo $result;
 	?>
+		<!--
 		<script>
-			
 			var OAuth_Access;
             $.ajax({
                 url: 'https://accounts.zoho.eu/oauth/v2/token/',
@@ -21,6 +50,7 @@
                 }
             });
 		</script>
+		-->
 	<?
 	} elseif(strtolower(QS_SUBPAGE) == "sent") {
 		$query = DB_Query("SELECT * FROM `Mail` WHERE `Direction`='Outbound' AND `Status`='Sent'");
