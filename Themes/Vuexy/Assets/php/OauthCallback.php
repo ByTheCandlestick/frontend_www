8bd493fb81380b3ff2d946ca06ff75349e3b4993
Robert Smith <master_schmadoodle@outlook.com>
2023-02-05 12:23:29 +0000

    Minor changes regarding oauth pages

--- Themes/Vuexy/Assets/php/OauthCallback.php
+++ Themes/Vuexy/Assets/php/OauthCallback.php
@@ -17,7 +17,7 @@
             console.log(url);
 
 
-
+/*
                     data  = 'api_key=iwdk5xYYMyUbyKuHMB8UuA5R2pbqgYLvjzzKQFCeJzKbAkg2qAJGWunzJPZFxvaCvue5xHJEwrhG3b9Ye5mn3UYBT7ZE46crHkgenvY4LaUSgb3Jcj8T67tUuyVtD6nRTQxvurPZ6E96WiQKep7G8kUjJhxHchEZk6KrWqZ2Tf2B9ZgtErZ4UMNNSJWE9DV8gM3YMkzmraACBxd9nPBteJKPx3SFdBMHQGBAL5bzSmJtCfezQJ7Ed3hk4CBnhda3';
                     data += '&uid=<?print($userdata['ID'])?>';
                     data += '&access=<?print($userdata['Zoho Mail Access Code'])?>';
@@ -36,6 +36,7 @@
                             alert('Error authorizing.')
                         }
                     });
+*/
         </script>
     </body>
 </html>
