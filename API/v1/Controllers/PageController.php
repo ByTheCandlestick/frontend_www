a93dd0888b3a167d973e1b1c9ab148697cff0abf
Robert Smith <master_schmadoodle@outlook.com>
2023-02-06 14:56:32 +0000

    Minor changes with the API to improve usability

--- API/v1/Controllers/PageController.php
+++ API/v1/Controllers/PageController.php
@@ -38,7 +38,7 @@
 						try{
 							if($mdl_page->CreatePage($arr_page_info)) {	// Success
 								$resp = array('status'=>'success');
-								$arr_page_info['id'] = $mdl_page->ReadPage($arr_page_info);
+								$arr_page_info['id'] = $mdl_page->ReadPage($arr_page_info)['ID'];
 								print_r($arr_page_info['id']);
 								$resp['info'] = $arr_page_info;
 								$str_response = json_encode($resp);
