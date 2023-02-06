e9f9bc991cb846ed126c2fe00a6df5f841cbe892
Robert Smith <master_schmadoodle@outlook.com>
2023-02-06 13:49:34 +0000

    Minor changes with creating/updating a partner

--- API/v1/Models/PartnerModel.php
+++ API/v1/Models/PartnerModel.php
@@ -11,8 +11,7 @@
 		public function Create(string $name, string $public, string $desc_s, string $desc_l, string $logo, string $link, string $email, string $phone, string $slug, string $active) {
 			try {
 				// Add line to Partner
-				$this->Execute("INSERT INTO `Partner accounts` (`Name`, `Public`, `About short`, `About long`, `Logo image`, `Shop link`, `Email`, `Phone`, `Slug`, `Active`)
-										VALUES ('$name', '$public', '$desc_s', '$desc_l', '$logo', '$link', '$email', '$phone', '$slug', '$active')", 1);
+				$this->Execute("INSERT INTO `Partner accounts` (`Name`, `Public`, `About short`, `About long`, `Logo image`, `Shop link`, `Email`, `Phone`, `Slug`, `Active`) VALUES ('$name', '$public', '$desc_s', '$desc_l', '$logo', '$link', '$email', '$phone', '$slug', '$active')", 1);
 				return true;
 			} catch(Error $er) {
 				return false;
@@ -26,10 +25,10 @@
          *  @param string $opt
          *  @return
          */
-		public function Update(string $id, string $name, string $percentage, string $start, string $end, string $voucher, string $active, string $type, string $description, string $categories, string $collections) {
+		public function Update(string $id, string $name, string $public, string $desc_s, string $desc_l, string $logo, string $link, string $email, string $phone, string $slug, string $active) {
 			try {
 				// Update Partners line
-				$this->Execute("UPDATE `Partners` SET `Name`='$name', `Description`='$description', `Voucher`='$voucher',`Type`='$type',`Category IDs`='$categories',`Collection IDs`='$collections',`Percentage discount`='$percentage',`Scheduled start`='$start',`Scheduled end`='$end',`Active`='$active' WHERE `ID`=$id", 1);
+				$this->Execute("UPDATE `Partners` SET `Name`='$name', `public`='$public', `About short`='$desc_s',`About long`='$desc_l',`Logo image`='$logo',`Shop link`='$link',`Email`='$email',`Phone`='$phone',`Slug`='$slug',`Active`='$active' WHERE `ID`=$id", 1);
 				return true;
 			} catch(Error $er) {
 				return false;
