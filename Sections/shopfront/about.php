767be52f3e5d03161a85d4fa18140d8103fef37d
Robert Smith <master_schmadoodle@outlook.com>
2023-02-06 17:12:12 +0000

    Minor changes to partner sections

--- Sections/shopfront/about.php
+++ Sections/shopfront/about.php
@@ -1,7 +1,7 @@
 <div class="row px-4 py-2 text-center">
 	<h5>
 		<?
-			$text = $partner['about_long'];
+			$text = $partner['About long'];
 			$text = str_replace('/n', '</br>', $text);
 			$text = str_replace('/r', '</br>', $text);
 			print($text);
