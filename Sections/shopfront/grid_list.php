c324c698c1c9f61a9e9effdb2b9cd61366498153
Robert Smith <master_schmadoodle@outlook.com>
2023-02-06 18:18:08 +0000

    Minor changes making more variables globally ajustable

--- Sections/shopfront/grid_list.php
+++ Sections/shopfront/grid_list.php
@@ -1,5 +1,6 @@
 <div class="row boutique">
 	<?
+	print_r($config['Shopfront products per page']);
 		(isset($_GET['p']))? $page = $_GET['p']: $page = 1;
 		if(isset($secext) && $secext == 'products') {
 			//
