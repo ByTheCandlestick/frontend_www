72527afcff76b5f44b0117e0f30bc4a9fdaa7eaa
Robert Smith <master_schmadoodle@outlook.com>
2023-02-06 13:47:41 +0000

    Minor changes with creating/updating a partner

--- API/v1/Models/PartnerModel.php
+++ API/v1/Models/PartnerModel.php
@@ -8,10 +8,11 @@
          *  @param string $opt
          *  @return
          */
-		public function Create(string $name, string $percentage, string $start, string $end, string $voucher, string $active, string $type, string $description, string $categories, string $collections) {
+		public function Create(string $name, string $public, string $desc_s, string $desc_l, string $logo, string $link, string $email, string $phone, string $slug, string $active) {
 			try {
 				// Add line to Partner
-				$this->Execute("INSERT INTO `Partners` (`Name`, `Description`, `Percentage discount`, `Voucher`, `Type`, `Category IDs`, `Collection IDs`, `Scheduled start`, `Scheduled end`, `Active`) VALUES ('$name', '$description', '$percentage', '$voucher', '$type', '$categories', '$collections', '$start', '$end', '$active')", 1);
+				$this->Execute("INSERT INTO `Partner accounts` (`Name`, `Public`, `About short`, `About long`, `Logo image`, `Shop link`, `Email`, `Phone`, `Slug`, `Active`)
+										VALUES ('$name', '$public', '$desc_s', '$desc_l', '$logo', '$link', '$email', '$phone', '$slug', '$active')", 1);
 				return true;
 			} catch(Error $er) {
 				return false;
