1a942aac630bdb4f52a67007683898b0f9661850
Robert Smith <master_schmadoodle@outlook.com>
2023-02-06 12:39:32 +0000

    Minor changes to index to enable counters

--- Pages/partners_edit.php
+++ Pages/partners_edit.php
@@ -31,13 +31,13 @@
 					<label for="floatingInput">Name</label>
 				</div>
 			</div>
-			<div class="col-12 col-md-6 col-lg-2" name="name">
+			<div class="col-12 col-md-6 col-lg-4" name="name">
 				<div class="form-floating mb-3">
 					<textarea type="text" class="form-control" id="floatingInput" placeholder=""><?= $partner['About short']?></textarea>
 					<label for="floatingInput">Short description</label>
 				</div>
 			</div>
-			<div class="col-12 col-md-6 col-lg-2" name="name">
+			<div class="col-12 col-md-6 col-lg-4" name="name">
 				<div class="form-floating mb-3">
 					<textarea type="text" class="form-control" id="floatingInput" placeholder=""><?= $partner['About long']?></textarea>
 					<label for="floatingInput">About us</label>
