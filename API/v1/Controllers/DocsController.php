aa7a8933f16016291348fcd7a3f237d26f29f71d
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 19:48:37 +0000

    Minor changes to make PDF invoices

--- API/v1/Controllers/DocsController.php
+++ API/v1/Controllers/DocsController.php
@@ -60,13 +60,13 @@
 								$pdf->SetFont('Raleway', '', $pdf->fs_h1);
 								$pdf->SetXY(0, 10);
 								$pdf->SetTextColor(28, 92, 147);
-								$pdf->Cell($pdf->GetPageWidth(), $pdf->GetStringHeight($pdf->fs_h1), $tx_company, 0, 1, "R");
+								$pdf->Cell($pdf->GetPageWidth()-10, $pdf->GetStringHeight($pdf->fs_h1), $tx_company, 0, 1, "R");
 
 								$pdf->SetFont('Raleway', '', $pdf->fs_h6);
 								$pdf->SetXY(0, 25);
 								$pdf->SetTextColor(255, 127, 0);
-								$pdf->Cell($pdf->GetPageWidth()/2, $pdf->GetStringHeight($pdf->fs_h1), $tx_company, 0, 0, "L");
-								$pdf->Cell($pdf->GetPageWidth()/2, $pdf->GetStringHeight($pdf->fs_h1), $tx_company, 0, 0, "R");
+								$pdf->Cell(($pdf->GetPageWidth()-10)/2, $pdf->GetStringHeight($pdf->fs_h1), $tx_company, 0, 0, "L");
+								$pdf->Cell(($pdf->GetPageWidth()-10)/2, $pdf->GetStringHeight($pdf->fs_h1), $tx_company, 0, 0, "R");
 							// Output document
 							$pdf->Output();
 						} catch(Error $er) {
