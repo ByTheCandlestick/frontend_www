92e8ecb8db506edcd04a438c5c3690ae124607aa
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 10:37:20 +0000

    Minor changes to transactions page

--- Pages/transactions_view.php
+++ Pages/transactions_view.php
@@ -1,19 +1,42 @@
-<section>
-	<!-- Section Header -->
-	<div class="row">
-		<div class="col-12 col-md-6">
-			<h1>Transaction</h1>
-			<p></p>
-		</div>
-		<div class="col-12 col-md-6 text-md-end">
+<?
+	$query = DB_Query(sprintf("SELECT * FROM `Transactions` WHERE `Transaction ID`=%s", urldecode(QS)));
+	if(mysqli_num_rows($query) > 0) {
+?>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6">
+				<h1>Transaction</h1>
+			</div>
+			<div class="col-12 col-md-6 text-md-end">
 			<div class="row">
-				<div class="col-12 col-lg-6 d-block d-md-flex justify-content-end align-items-center p-0">
+				<div class="col-12 d-block d-md-flex justify-content-end align-items-center p-0">
 				</div>
 			</div>
 		</div>
-	</div>
-	<hr>
-	<!-- Section Body -->
-	<div class="row overflow-scroll">
-	</div>
-</section>
+		<hr>
+		<!-- Section Body -->
+		<div class="row">
+		</div>
+	</section>
+<?
+	} else {
+?>
+	<section>
+		<!-- Section Header -->
+		<div class="row">
+			<div class="col-12 col-md-6">
+				<h1>Transaction not found.</h1>
+			</div>
+			<div class="col-12 col-md-6 text-md-end">
+			</div>
+		</div>
+		<hr>
+		<!-- Section Body -->
+		<div class="row">
+			<button class="btn btn-outline-primary col-12 col-md-3 col-lg-1" onclick="history.go(-1)">Go back</buton>
+		</div>
+	</section>
+<?
+	}
+?>
