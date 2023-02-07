c07c464cc8e3a2f124206b5ce7188f2ccbedb770
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 18:58:30 +0000

    Minor changes to make PDF invoices

--- API/v1/Controllers/DocsController.php
+++ API/v1/Controllers/DocsController.php
@@ -2,7 +2,7 @@
 	// Require fpdf PHP library
 	require_once(__ROOT__ . '/Vendor/fpdf/1.85/init.php');
 	class PDF Extends FPDF {
-		public $company = "The Candlestick";
+		public $tx_company = "The Candlestick";
 		public $fs_h1 = 32;
 		public $fs_h2 = 24;
 		public $fs_h3 = 20.8;
