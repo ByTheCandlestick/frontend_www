cbadc13af6fab473477b0cfe7f02f6f8ba5a2e34
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 17:13:55 +0000

    Minor changes to cart functionality

--- Themes/Voxo/Assets/js/script.js
+++ Themes/Voxo/Assets/js/script.js
@@ -679,7 +679,7 @@ $(document).ready(function() {
 										$(this).hide(500)
 									});
 								}
-								(quantity == 1)? $(".cartSubtract").val("&#x2d;"): $(".cartSubtract").val("&#xf1f8;");
+								(quantity == 1)? $(".cartSubtract").val("\u2d"): $(".cartSubtract").val("\uf1f8");
 								$("cart-item[prod-sku="+sku+"] .priceTotal").val(currency + newTotal);
 								$("p.subtotal.h4").text(currency + newSubtotal);
 							}
