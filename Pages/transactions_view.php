4b9544ff18602dd4cde05e2a94aa81c982fdfe23
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 11:19:03 +0000

    Minor changes to transaction view

--- Pages/transactions_view.php
+++ Pages/transactions_view.php
@@ -3,7 +3,7 @@
 	if(mysqli_num_rows($query) > 0) {
 		$charge = mysqli_fetch_array($query)['Charge ID'];
 		require_once(__ROOT__ . '/Vendor/StripeSecure/init.php');
-		$stripe = new \Stripe\StripeClient(STRIPE_API);
+		$stripe = new \Stripe\StripeClient(STRIPE_API[0]);
 		$charge_info = $stripe->charges->retrieve(
 			$charge,
 			[]
