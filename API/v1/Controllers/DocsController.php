6537f6d4ef572105196561ed92fb26a70c7e6b57
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 18:58:14 +0000

    Minor changes to make PDF invoices

--- API/v1/Controllers/DocsController.php
+++ API/v1/Controllers/DocsController.php
@@ -44,7 +44,7 @@
 
 							// Initialize
 							$pdf = new PDF();
-							$pdf->SetFont('Arial', 'B', 32);
+							$pdf->SetFont('Arial', 'B', $pdf->fs_h1);
 							$pdf->AliasNbPages();
 							// Create first page
 							$pdf->AddPage();
@@ -53,7 +53,7 @@
 								$pdf->Image(__ROOT__.'/images/partners/candlestick/logo.png', 10, 10, 30);
 								$pdf->SetXY(50, 10);
 								$pdf->SetTextColor(28, 92, 147);
-								$pdf->Cell($pdf->GetStringWidth($company), 10, $company, 1, 0, "C");
+								$pdf->Cell($pdf->GetStringWidth($pdf->tx_company), 10, $pdf->tx_company, 1, 0, "C");
 							// Output document
 							$pdf->Output();
 						} catch(Error $er) {
