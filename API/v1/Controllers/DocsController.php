6c2474e967f0989b18edef485b50bb0e06e1abf1
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 18:35:20 +0000

    Minor changes to make PDF invoices

--- API/v1/Controllers/DocsController.php
+++ API/v1/Controllers/DocsController.php
@@ -38,7 +38,7 @@
 							// Initialize
 							$pdf = new PDF();
 							if(file_exists(__ROOT__."/CDN/Fonts/Raleway/makefont/Raleway-Regular.php")) {
-								$pdf->AddFont("Raleway", __ROOT__."/CDN/Fonts/Raleway/makefont/Raleway-Regular.php");
+								$pdf->AddFont("", __ROOT__."/CDN/Fonts/Raleway/makefont/Raleway-Regular.php");
 								$pdf->SetFont('Raleway', 16);
 							} else {
 								$pdf->SetFont('Arial', 16);
