2b4a878255d07c5a95cd04cb13669bc8a56d5d07
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 13:32:08 +0000

    Minor changes to grid_list

--- Sections/shopfront/grid_list.php
+++ Sections/shopfront/grid_list.php
@@ -59,9 +59,6 @@
 										<div class="product-info row">
 											<span class="product-price col-12 col-lg-6">' . $prod_currency . $prod_price .'</span>
 											<div class="product-btn-group col-12 col-lg-6">
-												<a href="javascript:cart.add('.$userdata['ID'].', \''.$prod_sku.'\');" class="product-btn product-btn--add">
-													<i class="fad fa-cart-plus"></i>
-												</a>
 											</div>
 										</div>
 									</div>
