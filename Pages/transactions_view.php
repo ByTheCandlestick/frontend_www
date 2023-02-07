017d7069a513d9c431f2a6386e38116758a017d4
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 10:25:55 +0000

    Minor changes to transactions page

---
+++ Pages/transactions_view.php
@@ -0,0 +1,19 @@
+<section>
+	<!-- Section Header -->
+	<div class="row">
+		<div class="col-12 col-md-6">
+			<h1>Transaction</h1>
+			<p></p>
+		</div>
+		<div class="col-12 col-md-6 text-md-end">
+			<div class="row">
+				<div class="col-12 col-lg-6 d-block d-md-flex justify-content-end align-items-center p-0">
+				</div>
+			</div>
+		</div>
+	</div>
+	<hr>
+	<!-- Section Body -->
+	<div class="row overflow-scroll">
+	</div>
+</section>
