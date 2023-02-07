666025352834a68745c8b147110b2bae94a706ab
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 17:53:22 +0000

    Minor changes to make PDF invoices

--- API/v1/Controllers/DocsController.php
+++ API/v1/Controllers/DocsController.php
@@ -6,7 +6,7 @@
 			$this->Image(__ROOT__.'/images/partners/candlestick/logo.png',10,6,30);
 			$this->SetFont('Arial','B',15);
 			$this->Cell(50);
-			$this->Cell(30,10,'By The Candlestick',1,0,'C');
+			$this->Cell(30,10,'By The Candlestick',0,0,'C');
 			$this->Ln(20);
 		}
 		function Footer() {	// Page footer
