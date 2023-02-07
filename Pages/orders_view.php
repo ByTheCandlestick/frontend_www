0d359fee74d4d2a2d6d34c43c1f4acef86b7d3cb
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 11:57:12 +0000

    Minor changes to order View

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -13,88 +13,88 @@
 		$incomeAfterRefunds = (($invoice['Deposit'] - $invoice['Processing Fees']) - $invoice['tax']) - $refundsValue;
 		$depositAfterRefunds = $invoice['Deposit'] - $refundsValue;
 ?>
-<style>
-	/* --=== standard ===-- */
-		.order-progress-stepper {
-			width: 100%;
-			display: flex;
-			flex-direction: row;
-			flex-wrap: nowrap;
-			justify-content: space-between;
-			background: transparent;
-		}
-		.order-progress-stepper .step {
-			padding: 0;
-			margin: 0;
-			flex-grow: 0;
-			width: 100%;
-			text-align: center;
-			position: relative;
-			font-weight: bold;
-		}
-		.order-progress-stepper .step::before {
-			font-family: "Font Awesome 5 Pro";
-			content: "\f128";
-			font-weight: 900;
-		}
-	/* --=== Before and after ===-- */
-		.order-progress-stepper .step::before {
-			background-color: white;
-			display: block;
-			border-radius: 50%;
-			width: 35px;
-			height: 35px;
-			line-height: 35px;
-			margin: 0 auto 10px auto;
-			text-align: center;
-			position: relative;
-			z-index: 1;
-		}
-		.order-progress-stepper .step::after {
-			content: "";
-			display: block;
-			width: 100%;
-			background-color: #ddd;
-			height: 5px;
-			position: relative;
-			z-index: 0;
-			top: -70%;
-		}
-	/* --=== Contents ===-- */
-		.order-progress-stepper .step.confirmed::before { content: "\f00c"; }
-		.order-progress-stepper .step.awaDelivery::before { content: "\f007"; }
-		.order-progress-stepper .step.dispatched::before { content: "\f0d1"; }
-		.order-progress-stepper .step.delivered::before { content: "\f466"; }
-		.order-progress-stepper .step.completed::before { content: "\f00c"; }
-		.order-progress-stepper .step.caution::before { content: "\f12a"; }
-		.order-progress-stepper .step.error::before { content: "\f00d"; }
-	/* --=== Font colours ===-- */
-		.order-progress-stepper .step::before { color: #ffffff; }
-		.order-progress-stepper .step.active { color: #009cde; }
-		.order-progress-stepper .step.active { color: #009cde; }
-		.order-progress-stepper .step.caution { color: #ff8200; }
-		.order-progress-stepper .step.error { color: #d50032; }
-	/* --=== Background colours ===-- */
-		.order-progress-stepper .step::before,
-		.order-progress-stepper .step::after { background-color: #dadada; }
-		.order-progress-stepper .step.completed::before,
-		.order-progress-stepper .step.completed::after { background-color: #4caf50; }
-		.order-progress-stepper .step.active::before,
-		.order-progress-stepper .step.active::after { background-color: #009cde; }
-		.order-progress-stepper .step.caution::before,
-		.order-progress-stepper .step.caution::after { background-color: #ff8200; }
-		.order-progress-stepper .step.error::before,
-		.order-progress-stepper .step.error::after { background-color: #d50032; }
-	/* --=== First and last ===-- */
-		.order-progress-stepper .step:first-child::after {
-			width: 50%;
-			left: 50%;
-		}
-		.order-progress-stepper .step:last-child::after {
-			width: 50%;
-		}
-	/* --===  ===--*/
-</style>
+	<style>
+		/* --=== standard ===-- */
+			.order-progress-stepper {
+				width: 100%;
+				display: flex;
+				flex-direction: row;
+				flex-wrap: nowrap;
+				justify-content: space-between;
+				background: transparent;
+			}
+			.order-progress-stepper .step {
+				padding: 0;
+				margin: 0;
+				flex-grow: 0;
+				width: 100%;
+				text-align: center;
+				position: relative;
+				font-weight: bold;
+			}
+			.order-progress-stepper .step::before {
+				font-family: "Font Awesome 5 Pro";
+				content: "\f128";
+				font-weight: 900;
+			}
+		/* --=== Before and after ===-- */
+			.order-progress-stepper .step::before {
+				background-color: white;
+				display: block;
+				border-radius: 50%;
+				width: 35px;
+				height: 35px;
+				line-height: 35px;
+				margin: 0 auto 10px auto;
+				text-align: center;
+				position: relative;
+				z-index: 1;
+			}
+			.order-progress-stepper .step::after {
+				content: "";
+				display: block;
+				width: 100%;
+				background-color: #ddd;
+				height: 5px;
+				position: relative;
+				z-index: 0;
+				top: -70%;
+			}
+		/* --=== Contents ===-- */
+			.order-progress-stepper .step.confirmed::before { content: "\f00c"; }
+			.order-progress-stepper .step.awaDelivery::before { content: "\f007"; }
+			.order-progress-stepper .step.dispatched::before { content: "\f0d1"; }
+			.order-progress-stepper .step.delivered::before { content: "\f466"; }
+			.order-progress-stepper .step.completed::before { content: "\f00c"; }
+			.order-progress-stepper .step.caution::before { content: "\f12a"; }
+			.order-progress-stepper .step.error::before { content: "\f00d"; }
+		/* --=== Font colours ===-- */
+			.order-progress-stepper .step::before { color: #ffffff; }
+			.order-progress-stepper .step.active { color: #009cde; }
+			.order-progress-stepper .step.active { color: #009cde; }
+			.order-progress-stepper .step.caution { color: #ff8200; }
+			.order-progress-stepper .step.error { color: #d50032; }
+		/* --=== Background colours ===-- */
+			.order-progress-stepper .step::before,
+			.order-progress-stepper .step::after { background-color: #dadada; }
+			.order-progress-stepper .step.completed::before,
+			.order-progress-stepper .step.completed::after { background-color: #4caf50; }
+			.order-progress-stepper .step.active::before,
+			.order-progress-stepper .step.active::after { background-color: #009cde; }
+			.order-progress-stepper .step.caution::before,
+			.order-progress-stepper .step.caution::after { background-color: #ff8200; }
+			.order-progress-stepper .step.error::before,
+			.order-progress-stepper .step.error::after { background-color: #d50032; }
+		/* --=== First and last ===-- */
+			.order-progress-stepper .step:first-child::after {
+				width: 50%;
+				left: 50%;
+			}
+			.order-progress-stepper .step:last-child::after {
+				width: 50%;
+			}
+		/* --===  ===--*/
+	</style>
 	<section>
 		<!-- Section Header -->
 		<div class="row">
@@ -126,20 +126,20 @@
 	<div class="mb-2 row p-2 border text-center">
 		<div class="col-6 col-md-3 pb-3">
 			<h5>Order date: </h5>
-			<p><?= (date('d M Y', strtotime($invoice['Created'])))?></p>
+			<p><?=(date('d M Y', strtotime($invoice['Created'])))?></p>
 		</div>
 		<hr class="d-block d-md-none" style="border-top: dashed 1px #dddddd;"/>
 		<div class="col-6 col-md-3 pb-3">
 			<h5>Estimated delivery date: </h5>
-			<p><?= (date('d M Y', strtotime($invoice['Created'].'+ 7 days')))?></p>
+			<p><?=(date('d M Y', strtotime($invoice['Created'].'+ 7 days')))?></p>
 		</div>
 		<div class="col-6 col-md-3 pb-3">
-			<h5>Order ID:</h5>
-			<p><?= ($invoice['Invoice ID']) ?></p>
+			<h5>Shipped:</h5>
+			<p><?=()?></p>
 		</div>
 		<div class="col-6 col-md-3 pb-3">
 			<h5>Shipped by: </h5>
-			<p><?= ($invoice['Shipping by']) ?></p>
+			<p><?=($invoice['Shipping by'])?></p>
 		</div>
 	</div>
 	<div class="col-12 order-progress-stepper">
