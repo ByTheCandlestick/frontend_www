9d5cc56c564db5729d60ad98b12de1acd7e3cc1e
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 15:44:25 +0000

    Minor changes to cart functionality

--- API/v1/Models/CartModel.php
+++ API/v1/Models/CartModel.php
@@ -1,11 +1,17 @@
-<?php
+<?
 	class CartModel extends BaseModel {
-		public function checkUser(int $uid) {
-			return $this->Execute(sprintf("SELECT * FROM `User accounts` WHERE `ID`=%s", $uid), 5) > 0 ;
-		}
-		public function checkItem(int $sku) {
-			return $this->Execute(sprintf("SELECT * FROM `Product` WHERE `SKU`='%s'", $sku), 5) > 0;
-		}
+		/** checkUser
+		 * 
+		 */
+			public function checkUser(int $uid) {
+				return $this->Execute(sprintf("SELECT * FROM `User accounts` WHERE `ID`=%s", $uid), 5) > 0 ;
+			}
+		/** checkItem
+		 * 
+		 */
+			public function checkItem(int $sku) {
+				return $this->Execute(sprintf("SELECT * FROM `Product` WHERE `SKU`='%s'", $sku), 5) > 0;
+			}
         /** add
          *  Adds the item SKU and options to a users cart
          *  @param int $uid
@@ -14,18 +20,18 @@
          *  @param string $opt
          *  @return
          */
-		public function add(int $uid, int $sku, int $qty, string $opt) {
-			// check if line already exists,
-			if($this->Execute(sprintf("SELECT * FROM `User carts` WHERE `UID`=%s AND `SKU`='%s' AND `Options`='%s'", $uid, $sku, $opt), 5) > 0) {
-				// Get qty on existing line
-				$new_qty = $this->Execute(sprintf("SELECT `Quantity` FROM `User carts` WHERE `UID`=%s AND `SKU`='%s' AND `Options`='%s'", $uid, $sku, $opt), 2)[0] + $qty;
-				// Add quantities and update line
-				return $this->Execute(sprintf("UPDATE `User carts` SET `Quantity`=%s WHERE `Options`='%s' AND `SKU`='%s'", $new_qty, $opt, $sku), 1);
-			} else {
-				// Create a new line
-				return $this->Execute("INSERT INTO `User carts` (`UID`, `SKU`, `Quantity`, `Options`) VALUES ('$uid', '$sku', '$qty', '$opt')", 1);
+			public function add(int $uid, int $sku, int $qty, string $opt) {
+				// check if line already exists,
+				if($this->Execute(sprintf("SELECT * FROM `User carts` WHERE `UID`=%s AND `SKU`='%s' AND `Options`='%s'", $uid, $sku, $opt), 5) > 0) {
+					// Get qty on existing line
+					$new_qty = $this->Execute(sprintf("SELECT `Quantity` FROM `User carts` WHERE `UID`=%s AND `SKU`='%s' AND `Options`='%s'", $uid, $sku, $opt), 2)[0] + $qty;
+					// Add quantities and update line
+					return $this->Execute(sprintf("UPDATE `User carts` SET `Quantity`=%s WHERE `Options`='%s' AND `SKU`='%s'", $new_qty, $opt, $sku), 1);
+				} else {
+					// Create a new line
+					return $this->Execute("INSERT INTO `User carts` (`UID`, `SKU`, `Quantity`, `Options`) VALUES ('$uid', '$sku', '$qty', '$opt')", 1);
+				}
 			}
-		}
 		/** remove
 		 * Removed an item completely from the users cart
 		 * @todo All
@@ -36,7 +42,7 @@
 		 * @return
 		 */
 		public function remove(int $uid, int $sku, string $opt, int $qty) {
-
+			return $this->Execute(sprintf("UPDATE `User carts` SET `Quantity`=(`Quantity`-%s) WHERE `UID`=%s AND `Options`='%s' AND `SKU`='%s'", $qty, $uid, $opt, $sku), 1)
 		}
 	}
 ?>
