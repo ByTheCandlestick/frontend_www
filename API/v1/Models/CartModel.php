ea10c464a9c833f3fac554c8443bc0499f9d4afb
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 14:55:21 +0000

    Minor changes to cart functionality

--- API/v1/Models/CartModel.php
+++ API/v1/Models/CartModel.php
@@ -1,10 +1,10 @@
 <?php
 	class CartModel extends BaseModel {
 		public function checkUser(int $uid) {
-			return $this->Execute("SELECT * FROM `User accounts` WHERE `UID`='$uid'", 1)
+			return $this->Execute("SELECT * FROM `User accounts` WHERE `UID`='$uid'", 1);
 		}
 		public function checkItem(int $sku) {
-			return $this->Execute("SELECT * FROM `Product` WHERE `sku`='$sku'", 1)
+			return $this->Execute("SELECT * FROM `Product` WHERE `sku`='$sku'", 1);
 		}
         /** add
          *  Adds the item SKU and options to a users cart
