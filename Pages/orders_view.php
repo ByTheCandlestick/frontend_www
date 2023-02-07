29787b4e8ddf9da8a2cf952a41e728c70553c5b0
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 11:29:48 +0000

    Minor changes to transaction view

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -77,7 +77,7 @@
 				<h5>Stripe</h5>
 				<div class="row">
 					<div class="col-12 col-md-6 col-lg-3 form-floating mb-3">
-						<a name="charge_id" class="form-control border-0" id="floatingInput" href="/Transactions/View/<? print($invoice['Transaction ID'])?>"><? print($invoice['Charge ID'])?></a>
+						<a name="charge_id" class="form-control border-0" id="floatingInput" href="/Transactions/View/<? print($invoice['Transaction ID'])?>"><?=$invoice['Transaction ID']?></a>
 						<label for="floatingInput">Transaction ID</label>
 					</div>
 				</div>
