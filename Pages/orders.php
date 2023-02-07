a062277b0bf43097a4a18ae604996d7573f14773
Robert Smith <master_schmadoodle@outlook.com>
2023-02-07 12:32:55 +0000

    Minor changes to counter display

--- Pages/orders.php
+++ Pages/orders.php
@@ -3,7 +3,7 @@
 ?><?
 	$total_orders = mysqli_fetch_row(DB_Query("SELECT COUNT(*) FROM `Transactions` WHERE `Type`='Order'"))[0];
 	$offset = (QS_SUBPAGE !== null)?(intval(QS_SUBPAGE)-1)*$config['Maximum list size']: 0;
-    $q = DB_Query(sprintf("SELECT * FROM `Transactions` WHERE `Type`='Order' ORDER BY `Invoice ID` DESC LIMIT %s OFFSET %s", $config['Maximum list size'], $offset));
+    $q = DB_Query(sprintf("SELECT * FROM `Transactions` WHERE `Type`='Order' ORDER BY `Shipping Status` ASC, `Invoice ID` DESC LIMIT %s OFFSET %s", $config['Maximum list size'], $offset));
 	while($order = mysqli_fetch_assoc($q)) { array_push($orders, $order); }
 ?>
 <section>
