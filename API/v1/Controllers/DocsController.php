281daf9e14c093d481ba932c110b133b95f5764e
Robert Smith <master_schmadoodle@outlook.com>
2023-02-08 00:48:54 +0000

    Minor changes to improve PDF invoices

--- API/v1/Controllers/DocsController.php
+++ API/v1/Controllers/DocsController.php
@@ -66,23 +66,21 @@
 								// Logo
 								$pdf->Image(__ROOT__.'/images/partners/candlestick/logo.png', 10, 5, 20);
 								// Company Name
-								$tx_company_strlen = $pdf->GetStringWidth($tx_company);
-								$align_right = $pdf->GetPageWidth()-($tx_company_strlen+5);
-								$pdf->SetXY($align_right, 10);
+								$tx_company_strlen = $pdf->GetStringWidth($pdf->tx_company);
+								$left = $pdf->GetPageWidth()-($tx_company_strlen+5);
+								$pdf->SetXY(5, 10);
 								$pdf->SetFont('Raleway', '', $pdf->fs_h1);
 								$pdf->SetTextColor(28, 92, 147);
-								$pdf->Cell($pdf->GetStringWidth($tx_company), $pdf->GetStringHeight($pdf->fs_h1), $tx_company, $pdf->dev_outline, 1, "R");
+								$pdf->Cell($pdf->GetPageWidth()-10, $pdf->GetStringHeight($pdf->fs_h1), $tx_company, $pdf->dev_outline, 1, "R");
 								// Website
-								$tx_website_strlen = $pdf->GetStringWidth($pdf->tx_website);
-								$align_right = $pdf->GetPageWidth()-($tx_website_strlen+5);
 								$pdf->SetXY(5, 25);
 								$pdf->SetFont('Raleway', '', $pdf->fs_h6);
 								$pdf->SetTextColor(255, 127, 0);
 								$pdf->Cell(($pdf->GetPageWidth()-10)/2, $pdf->GetStringHeight($pdf->fs_h1), $tx_website, $pdf->dev_outline, 0, "L");
 								// Sale items
 								$tx_elems_strlen = $pdf->GetStringWidth($tx_elems_str = join(" | ", $tx_elems));
-								$align_right = $pdf->GetPageWidth()-($tx_elems_strlen+5);
-								$pdf->SetXY($align_right, 25);
+								$left = $pdf->GetPageWidth()-($tx_elems_strlen+5);
+								$pdf->SetXY($left, 25);
 								$pdf->SetFont('Raleway', '', $pdf->fs_h6);
 								$c = count($tx_elems);
 								for($i=0; $i<$c; $i++) {
