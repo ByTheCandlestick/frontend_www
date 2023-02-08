3e58a51621e088784015158818b692f3bd792f52
Robert Smith <master_schmadoodle@outlook.com>
2023-02-08 00:49:17 +0000

    Minor changes to improve PDF invoices

--- API/v1/Controllers/DocsController.php
+++ API/v1/Controllers/DocsController.php
@@ -68,10 +68,10 @@
 								// Company Name
 								$tx_company_strlen = $pdf->GetStringWidth($pdf->tx_company);
 								$left = $pdf->GetPageWidth()-($tx_company_strlen+5);
-								$pdf->SetXY(5, 10);
+								$pdf->SetXY($left, 10);
 								$pdf->SetFont('Raleway', '', $pdf->fs_h1);
 								$pdf->SetTextColor(28, 92, 147);
-								$pdf->Cell($pdf->GetPageWidth()-10, $pdf->GetStringHeight($pdf->fs_h1), $tx_company, $pdf->dev_outline, 1, "R");
+								$pdf->Cell($tx_company_strlen, $pdf->GetStringHeight($pdf->fs_h1), $tx_company, $pdf->dev_outline, 1, "R");
 								// Website
 								$pdf->SetXY(5, 25);
 								$pdf->SetFont('Raleway', '', $pdf->fs_h6);
