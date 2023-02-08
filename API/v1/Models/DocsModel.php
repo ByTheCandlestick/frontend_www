f4b850d730b6a00288961abe24774aeb0f15bde5
Robert Smith <master_schmadoodle@outlook.com>
2023-02-08 11:36:43 +0000

    Minor changes to the PDF invoice api

--- API/v1/Models/DocsModel.php
+++ API/v1/Models/DocsModel.php
@@ -11,7 +11,7 @@
 		 */
 			public function getItemInfo(string $itemStr) {
 				$i=0;
-				foreach(explode(';', $itemStr) as $itemInfo){
+				foreach(array_filter(explode(';', $itemStr)) as $itemInfo){
 					$item = explode(',', $itemInfo);
 					$inf = $this->Execute(sprintf("SELECT `Title`, `RetailPrice` FROM `Product` WHERE `SKU`='%s'", $item[0]), 3);
 					$arr[$i] = array($inf[0], $inf[$item[1]], $inf[1], $inf[1]*$item[1]);
