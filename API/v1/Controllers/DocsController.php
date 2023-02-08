7962d2ec7757d1478db34a0608b8f0c0212bbf5d
Robert Smith <master_schmadoodle@outlook.com>
2023-02-08 01:37:59 +0000

    Minor changes to improve PDF invoices

--- API/v1/Controllers/DocsController.php
+++ API/v1/Controllers/DocsController.php
@@ -16,16 +16,20 @@
 		}
 		function InvoiceTable($header, $w, $data, $posx, $posy) {
 			// Header
+			$this->SetXY($posx, $posy);
 			for($i=0;$i<count($header);$i++)
 				$this->Cell($w[$i],7,$header[$i],1,0,'C');
 			$this->Ln();
 			// Data
+			$posy+10;
 			foreach($data as $row) {
+				$this->SetXY($posx, $posy);
 				$this->Cell($w[0],6,$row[0],'LR');
 				$this->Cell($w[1],6,$row[1],'LR');
 				$this->Cell($w[2],6,number_format($row[2]),'LR',0,'R');
 				$this->Cell($w[3],6,number_format($row[3]),'LR',0,'R');
 				$this->Ln();
+				$posy+10;
 			}
 			// Closing line
 			$this->Cell(array_sum($w),0,'','T');
