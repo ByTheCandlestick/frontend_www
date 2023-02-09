d9b73188516809002384e782f297d085f8987661
Robert Smith <master_schmadoodle@outlook.com>
2023-02-09 01:11:10 +0000

    Minor changes to improve PDF invoices

--- API/v1/Controllers/DocsController.php
+++ API/v1/Controllers/DocsController.php
@@ -151,7 +151,7 @@
 									$pdf->SetXY($pdf->GetPageWidth()-($pdf->GetStringWidth($invoice['Invoice ID'])+10), 40);
 									$pdf->Cell($pdf->GetStringWidth($invoice['Invoice ID']), $pdf->GetStringHeight($pdf->fs_p), $invoice['Invoice ID'], $pdf->dev_outline, 2, "R");
 									$dt = new DateTime($invoice['Created']);
-									$date = $dt->format('d/m/Y');
+									$date = $dt->format('dS F Y');
 									$pdf->SetFont('Raleway', '', $pdf->fs_h5);
 									$pdf->SetXY($pdf->GetPageWidth()-($pdf->GetStringWidth($invoice['Created'])+10), 46);
 									$pdf->Cell($pdf->GetStringWidth($date), $pdf->GetStringHeight($pdf->fs_h5), $date, $pdf->dev_outline, 2, "R");
