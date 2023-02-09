54425f6290c6574eb570cbbfe3ae7693d580a5bd
Robert Smith <master_schmadoodle@outlook.com>
2023-02-09 00:52:51 +0000

    Minor changes to improve PDF invoices

--- API/v1/Controllers/DocsController.php
+++ API/v1/Controllers/DocsController.php
@@ -145,22 +145,22 @@
 
 									$pdf->SetXY(10, 40);
 									$pdf->SetFont('Raleway', 'B', $pdf->fs_p);
-									$pdf->Cell($pdf->GetStringWidth($invoice['Name']), $pdf->GetStringHeight($pdf->fs_p), $invoice['Name'], $pdf->dev_outline, 1, "L");
+									$pdf->Cell($pdf->GetStringWidth($invoice['Name']), $pdf->GetStringHeight($pdf->fs_p), $invoice['Name'], $pdf->dev_outline, 2, "L");
 									$pdf->SetFont('Raleway', '', $pdf->fs_h5);
-									$pdf->Cell($pdf->GetStringWidth($address['number_name'].' '.$address['line_1']), $pdf->GetStringHeight($pdf->fs_h5), $address['number_name'].' '.$address['line_1'], $pdf->dev_outline, 1, "L");
-									$pdf->Cell($pdf->GetStringWidth($address['line_2']), $pdf->GetStringHeight($pdf->fs_h5), $address['line_2'], $pdf->dev_outline, 1, "L");
-									$pdf->Cell($pdf->GetStringWidth($address['town']), $pdf->GetStringHeight($pdf->fs_h5), $address['town'], $pdf->dev_outline, 1, "L");
-									$pdf->Cell($pdf->GetStringWidth($address['county']), $pdf->GetStringHeight($pdf->fs_h5), $address['county'], $pdf->dev_outline, 1, "L");
-									$pdf->Cell($pdf->GetStringWidth($address['country']), $pdf->GetStringHeight($pdf->fs_h5), $address['country'], $pdf->dev_outline, 1, "L");
-									$pdf->Cell($pdf->GetStringWidth($address['postcode']), $pdf->GetStringHeight($pdf->fs_h5), $address['postcode'], $pdf->dev_outline, 1, "L");
-									$pdf->Cell($pdf->GetStringWidth($invoice['Email']), $pdf->GetStringHeight($pdf->fs_h5), $invoice['Email'], $pdf->dev_outline, 1, "L");
-									$pdf->Cell($pdf->GetStringWidth($invoice['Phone']), $pdf->GetStringHeight($pdf->fs_h5), $invoice['Phone'], $pdf->dev_outline, 1, "L");
+									$pdf->Cell($pdf->GetStringWidth($address['number_name'].' '.$address['line_1']), $pdf->GetStringHeight($pdf->fs_h5), $address['number_name'].' '.$address['line_1'], $pdf->dev_outline, 2, "L");
+									$pdf->Cell($pdf->GetStringWidth($address['line_2']), $pdf->GetStringHeight($pdf->fs_h5), $address['line_2'], $pdf->dev_outline, 2, "L");
+									$pdf->Cell($pdf->GetStringWidth($address['town']), $pdf->GetStringHeight($pdf->fs_h5), $address['town'], $pdf->dev_outline, 2, "L");
+									$pdf->Cell($pdf->GetStringWidth($address['county']), $pdf->GetStringHeight($pdf->fs_h5), $address['county'], $pdf->dev_outline, 2, "L");
+									$pdf->Cell($pdf->GetStringWidth($address['country']), $pdf->GetStringHeight($pdf->fs_h5), $address['country'], $pdf->dev_outline, 2, "L");
+									$pdf->Cell($pdf->GetStringWidth($address['postcode']), $pdf->GetStringHeight($pdf->fs_h5), $address['postcode'], $pdf->dev_outline, 2, "L");
+									$pdf->Cell($pdf->GetStringWidth($invoice['Email']), $pdf->GetStringHeight($pdf->fs_h5), $invoice['Email'], $pdf->dev_outline, 2, "L");
+									$pdf->Cell($pdf->GetStringWidth($invoice['Phone']), $pdf->GetStringHeight($pdf->fs_h5), $invoice['Phone'], $pdf->dev_outline, 2, "L");
 								// Invoice Date / ID
 									$pdf->SetFont('Raleway', 'B', $pdf->fs_p);
 									$pdf->SetXY($pdf->GetPageWidth()-($pdf->GetStringWidth($invoice['Invoice ID'])+10), 40);
 									$pdf->Cell($pdf->GetStringWidth($invoice['Invoice ID']), $pdf->GetStringHeight($pdf->fs_p), $invoice['Invoice ID'], $pdf->dev_outline, 2, "R");
 									$pdf->SetFont('Raleway', '', $pdf->fs_h5);
-									//$pdf->SetXY($pdf->GetPageWidth()-($pdf->GetStringWidth($invoice['Created'])+10), 46);
+									$pdf->SetXY($pdf->GetPageWidth()-($pdf->GetStringWidth($invoice['Created'])+10), 46);
 									$pdf->Cell($pdf->GetStringWidth($invoice['Created']), $pdf->GetStringHeight($pdf->fs_h5), $invoice['Created'], $pdf->dev_outline, 2, "R");
 								// invoice Table
 									$pdf->InvoiceTable($headers, $widths, $items, 10, 90, $tableColour, $textColor, $this->fs_h6);
