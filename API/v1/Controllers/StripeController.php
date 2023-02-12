45f0ac2db6a2280b4e3c893954d7305f0284d369
Robert Smith <master_schmadoodle@outlook.com>
2023-02-12 15:33:45 +0000

    Minor changes to estimated delivery date

--- API/v1/Controllers/StripeController.php
+++ API/v1/Controllers/StripeController.php
@@ -91,7 +91,7 @@
 										$invoice_number .= '0';
 									}
 									$invoice_number .=$inv_counts;
-									$mdl_stripe->uploadSalesOrder($invoice_number, $uid, $name, $email, $phone, $items, $notes, $shipping = 0, $address_id, $price, $chargeJson['amount'], $chargeJson['application_fee_amount'], $chargeJson['currency'], $status = 1, $chargeJson['balance_transaction'], $chargeJson['id'], $chargeJson['status'], $chargeJson['payment_method_details']['card']['network'], $chargeJson['payment_method_details']['card']['last4'], $chargeJson['payment_method_details']['card']['exp_month'], $chargeJson['payment_method_details']['card']['exp_year']);
+									$mdl_stripe->uploadSalesOrder($invoice_number, $uid, $name, $email, $phone, $items, $notes, $shipping = 1, $address_id, $price, $chargeJson['amount'], $chargeJson['application_fee_amount'], $chargeJson['currency'], $status = 1, $chargeJson['balance_transaction'], $chargeJson['id'], $chargeJson['status'], $chargeJson['payment_method_details']['card']['network'], $chargeJson['payment_method_details']['card']['last4'], $chargeJson['payment_method_details']['card']['exp_month'], $chargeJson['payment_method_details']['card']['exp_year']);
 									$payment_id = $conn->insert_id;
 								// Empty users cart
 									if(!$mdl_stripe->emptyCart($uid)){
