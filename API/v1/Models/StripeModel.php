a8b89763340378bcc20c6226727aab562f7dd198
Robert Smith <master_schmadoodle@outlook.com>
2023-02-12 01:16:46 +0000

    Updated Stripe payment api

--- API/v1/Models/StripeModel.php
+++ API/v1/Models/StripeModel.php
@@ -66,13 +66,7 @@
 		 * 
 		 */
 			public function uploadAddress($uid, $number,  $address1, $address2, $town, $county, $country, $postcode) {
-				$this->Execute(print("	INSERT INTO `User addresses`(`uid`, `number_name`, `line_1`, `line_2`, `town`, `county`, `country`, `postcode`)
-											SELECT NewRow
-											FROM ( VALUES ($uid, '$number', '$address1', '$address2', '$town', '$county', '$country', '$postcode'))
-											AS NewRow
-										WHERE NOT EXISTS(	SELECT 1
-															FROM   `User addresses` AS Tbl
-															WHERE  Tbl.number_name = NewNames.number_name );"), 1);
+				$this->Execute(print(sprintf("SELECT COUNT(*) FROM `User addresses` WHERE `uid`=%s AND `number_name`=%s AND `nine_1`=%s AND `line_2`=%s AND `town`=%s AND `county`=%s AND `country`=%s AND `postcode`=%s", $uid, $number, $address1, $address2, $town, $county, $country, $postcode)), 2);
 			}
 		/** getAddressID
 		 * 
