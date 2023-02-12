712cf83f4cf0a965ffa88ddb0f504be0e2814de1
Robert Smith <master_schmadoodle@outlook.com>
2023-02-12 15:28:17 +0000

    Minor changes to estimated delivery date

--- Pages/orders_view.php
+++ Pages/orders_view.php
@@ -99,7 +99,7 @@
 		<!-- Section Header -->
 		<div class="row">
 			<div class="col-12 col-md-6">
-				<h1></h1>
+				<h1> Order information</h1>
 			</div>
 			<div class="col-12 col-md-6 text-md-end">
 				<div class="row">
