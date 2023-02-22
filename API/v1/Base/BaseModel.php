a0848f0dd9f6dcdce6e9b7a14564ec751132690f
Robert Smith <master_schmadoodle@outlook.com>
2023-02-22 23:21:55 +0000

    Minor changes to audio trail uploader

--- API/v1/Base/BaseModel.php
+++ API/v1/Base/BaseModel.php
@@ -1,37 +1,38 @@
 <?
 	class BaseModel {
-		/**
+		/** CreateCookie
 		 * 
+		 *	@param	string	$name
+		 *	@param	string	$value
+		 *	@return	void
 		 */
-		public function CreateCookie(string $name, string $value) {
-			$options = array (
-				'expires'	=>  strtotime('+365 days'),	// Epiry date in string
-				'domain'	=> '.thecandlestick.co.uk',	// leading dot for compatibility or use subdomain
-				'httponly'	=>  true,					// true or false
-				'secure'	=>  true,					// true or false
-				'samesite'	=> 'none',					// None || Lax  || Strict
-			);
-			try {
-				
-				if(isset($_COOKIE[$name])) {
-					return true;
-				} else {
-					throw new Error();
+			public function CreateCookie(string $name, string $value) {
+				$options = array (
+					'expires'	=>  strtotime('+365 days'),	// Epiry date in string
+					'domain'	=> '.thecandlestick.co.uk',	// leading dot for compatibility or use subdomain
+					'httponly'	=>  true,					// true or false
+					'secure'	=>  true,					// true or false
+					'samesite'	=> 'none',					// None || Lax  || Strict
+				);
+				try {
+					if(isset($_COOKIE[$name])) {
+						return true;
+					} else {
+						throw new Error();
+					}
+				}catch(Error $er){
+					return $er;
 				}
-			}catch(Error $er){
-				return $er;
 			}
-		}
-		/**
+		/** ConfCookie
 		 * 
+		 *	@param	string	$name
+		 *	@return	void
 		 */
-		public function ConfCookie(string $name) {
-			if(isset($_COOKIE[$name])) return true;
-			return false;
-		}
-		
-		/**
-		 *	Execute
+			public function ConfCookie(string $name) {
+				return isset($_COOKIE[$name]);
+			}
+		/** Execute
 		 *
 		 *	@param	array	$DBInfo
 		 *	@param	string	$query	"SELECT * FROM..."
@@ -40,39 +41,57 @@
 		 *	@return	Row		Fetch row as array
 		 *	@return	Array	Fetch row as array with keys
 		 *	@return	Assoc	Fetch multiple rows with keys
-		**/
-		public function Execute(string $query, int $returnType, $DBinfo = ADMIN) {
-			try {
-				if($conn = mysqli_connect($DBinfo[0], $DBinfo[1], $DBinfo[2], $DBinfo[3])) {
-					if($res = mysqli_query($conn, $query)) {
-						switch ($returnType) {
-							case 1:
-								return true;
-							case 2:
-								return mysqli_fetch_row($res);
-							case 3:
-								return mysqli_fetch_array($res);
-							case 4:
-								$result = Array();
-								while($r=mysqli_fetch_assoc($res)) {
-									array_push($result, $r);
-								}
-								return $result;
-							case 5:
-								return mysqli_num_rows($res);
+		 *	@return	void
+		 */
+			public function Execute(string $query, int $returnType, $DBinfo = ADMIN) {
+				try {
+					if($conn = mysqli_connect($DBinfo[0], $DBinfo[1], $DBinfo[2], $DBinfo[3])) {
+						if($res = mysqli_query($conn, $query)) {
+							switch ($returnType) {
+								case 1:
+									return true;
+								case 2:
+									return mysqli_fetch_row($res);
+								case 3:
+									return mysqli_fetch_array($res);
+								case 4:
+									$result = Array();
+									while($r=mysqli_fetch_assoc($res)) {
+										array_push($result, $r);
+									}
+									return $result;
+								case 5:
+									return mysqli_num_rows($res);
+							}
 						}
+					} else {
+						throw new Exception("Unable to connect to the DB, Please try again later");
 					}
-				} else {
-					throw new Exception("Unable to connect to the DB, Please try again later: " . mysqli_error($this->connection));
+				} catch (Exception $e) {
+					throw new Exception($e->getMessage());
 				}
-			} catch (Exception $e) {
-				throw new Exception($e->getMessage());
+				return false;
 			}
-			return false;
-		}
 
-		public function uploadAudit(string $func, array $args, string $string) {
-			$this->Execute(sprintf("INSERT INTO `Audit trail`(`Timestamp`, `Function`, `Args`, `String`, `User ID`) VALUES(now(), '%s', '%s', '%s', '%s')", $func, $args, $string, $uid), 1);
-		}
+		/** uploadAudit
+		 * 
+		 *	@param	string	$func
+		 *	@param	array	$args
+		 *	@param	string	$string
+		 *	@return	void
+		 */
+			public function uploadAudit(string $func, array $args, string $string, string $uid) {
+				$arr = array();
+				foreach($args as $x) {
+					array_push($arr, gettype($x)."(".strlen($x).") ".$x);
+				}
+				$this->Execute(sprintf("INSERT INTO `Audit trail`(`IP`, `Timestamp`, `Function`, `Args`, `String`, `User ID`) VALUES(`%s`, now(), '%s', '%s', '%s', '%s')",
+					getHostByName(getHostName()),
+					$func,
+					join(", ", $arr),
+					$string,
+					$uid
+				), 1);
+			}
 	}
 ?>
--- API/v1/Models/WebsiteModel.php
+++ API/v1/Models/WebsiteModel.php
@@ -7,6 +7,7 @@
 		 *	@result boolean
 		 */
 			public function CreateWebsite(array $info) {
+				$this->uploadAudit(__FUNCTION__, func_get_args(), "Created a new Website/Domain", $info['uid']);
 				return $this->Execute(sprintf("INSERT INTO `Website domains`(`Name`, `Domain`, `Page_type`, `Maintenance`, `Meta_title`, `meta_keywords`, `meta_description`, `meta_colour`, `Title`, `Slogan`, `Email`, `Phone`, `Colour_primary`, `Colour_secondary`, `Default styles`, `Default scripts`, `Logo`, `Favicon`, `Permission`) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')", $info['name'], $info['domain'], $info['page_type'], $info['maintenance'], $info['meta_title'], $info['meta_keywords'], $info['meta_description'], $info['meta_colour'], $info['title'], $info['slogan'], $info['email'], $info['phone'], $info['primary_colour'], $info['secondary_colour'], $info['styles'], $info['scripts'], $info['logo'], $info['favicon'], $info['permission']), 1);
 			}
 		/**	UpdateWebsite
@@ -17,6 +18,7 @@
 		 *	@result boolean
 		 */
 			public function UpdateWebsite(string $sid, array $info) {
+				$this->uploadAudit(__FUNCTION__, func_get_args(), "Updated a Website/Domain", $info['uid']);
 				return $this->Execute(sprintf("UPDATE `Website domains` SET `Name`='%s', `Domain`='%s', `Page_type`='%s', `Maintenance`='%s', `Meta_title`='%s', `meta_keywords`='%s', `meta_description`='%s', `meta_colour`='%s', `Title`='%s', `Slogan`='%s', `Email`='%s', `Phone`='%s', `Colour_primary`='%s', `Colour_secondary`='%s', `Default styles`='%s', `Default scripts`='%s', `Logo`='%s', `Favicon`='%s', `Permission`='%s' WHERE `ID`='%s'", $info['name'], $info['domain'], $info['page_type'], $info['maintenance'], $info['meta_title'], $info['meta_keywords'], $info['meta_description'], $info['meta_colour'], $info['title'], $info['slogan'], $info['email'], $info['phone'], $info['primary_colour'], $info['secondary_colour'], $info['styles'], $info['scripts'], $info['logo'], $info['favicon'], $info['permission'], $sid), 1);
 			}
 		/** DeleteWebsite
@@ -26,6 +28,7 @@
 		 *	@result boolean
 		 */
 			public function DeleteWebsite(string $sid) {
+				$this->uploadAudit(__FUNCTION__, func_get_args(), "Deleted a Website/Domain", "");
 				return $this->Execute(sprintf("DELETE FROM `Website domains` WHERE `ID`='%s'", $sid), 1);
 			}
 		//
@@ -36,6 +39,7 @@
 		 *	@result boolean
 		 */
 			public function CreateStyle(array $info) {
+				$this->uploadAudit(__FUNCTION__, func_get_args(), "Created a new CSS style link", $info['uid']);
 				return $this->Execute(sprintf("INSERT INTO `Website styles`(`Name`, `Location`, `Importance`, `Preload`, `Active`) VALUES('%s', '%s', '%s', '%s', '%s')", $info['name'], $info['location'], $info['importance'], $info['preload'], $info['active']), 1);
 			}
 		/**	UpdateStyle
@@ -46,6 +50,7 @@
 		 *	@result boolean
 		 */
 			public function UpdateStyle(string $id, array $info) {
+				$this->uploadAudit(__FUNCTION__, func_get_args(), "Updated a CSS style link", $info['uid']);
 				return $this->Execute(sprintf("UPDATE `Website styles` SET `Name`='%s', `Location`='%s', `Importance`='%s', `Preload`='%s', `Active`='%s' WHERE `ID`='%s'", $info['name'], $info['location'], $info['importance'], $info['preload'], $info['active'], $id), 1);
 			}
 		/**	DeleteStyle
@@ -55,6 +60,7 @@
 		 *	@result boolean
 		 */
 			public function DeleteStyle(string $id) {
+				$this->uploadAudit(__FUNCTION__, func_get_args(), "Deleted a CSS style link", "");
 				return $this->Execute(sprintf("DELETE FROM `Website styles` WHERE `ID`='%s'", $id), 1);
 			}
 		//
@@ -65,6 +71,7 @@
 		 *	@result boolean
 		 */
 			public function CreateScript(array $info) {
+				$this->uploadAudit(__FUNCTION__, func_get_args(), "Created a new JS script link", $info['uid']);
 				return $this->Execute(sprintf("INSERT INTO `Website scripts`(`Name`, `Location`, `Importance`, `Active`) VALUES('%s', '%s', '%s', '%s')", $info['name'], $info['location'], $info['importance'], $info['active']), 1);
 			}
 		/**	UpdateScript
@@ -75,6 +82,7 @@
 		 *	@result boolean
 		 */
 			public function UpdateScript(string $id, array $info) {
+				$this->uploadAudit(__FUNCTION__, func_get_args(), "Updated a JS script link", $info['uid']);
 				return $this->Execute(sprintf("UPDATE `Website scripts` SET `Name`='%s', `Location`='%s', `Importance`='%s', `Active`='%s' WHERE `ID`='%s'", $info['name'], $info['location'], $info['importance'], $info['active'], $id), 1);
 			}
 		/**	DeleteScript
@@ -84,6 +92,7 @@
 		 *	@result boolean
 		 */
 			public function DeleteScript(string $id) {
+				$this->uploadAudit(__FUNCTION__, func_get_args(), "Deleted a JS script link", "");
 				return $this->Execute(sprintf("DELETE FROM `Website scripts` WHERE `ID`='%s'", $id), 1);
 			}
 		//
@@ -94,6 +103,7 @@
 		 *	@result boolean
 		 */
 			public function CreateTheme(array $info) {
+				$this->uploadAudit(__FUNCTION__, func_get_args(), "Created a new Theme", $info['uid']);
 				return $this->Execute(sprintf("INSERT INTO `Website themes`(`Name`, `Description`, `Location`, `Active`) VALUES('%s', '%s', '%s', '%s')", $info['name'], $info['description'], $info['location'], $info['active']), 1);
 			}
 		/**	UpdateTheme
@@ -104,6 +114,7 @@
 		 *	@result boolean
 		 */
 			public function UpdateTheme(string $id, array $info) {
+				$this->uploadAudit(__FUNCTION__, func_get_args(), "Updated a Theme", $info['uid']);
 				return $this->Execute(sprintf("UPDATE `Website themes` SET `Name`='%s', `Description`='%s', `Location`='%s', `Active`='%s' WHERE `ID`='%s'", $info['name'], $info['description'], $info['location'], $info['active'], $id), 1);
 			}
 		/**	DeleteTheme
@@ -113,6 +124,7 @@
 		 *	@result boolean
 		 */
 			public function DeleteTheme(string $id) {
+				$this->uploadAudit(__FUNCTION__, func_get_args(), "Deleted a Theme", "");
 				return $this->Execute(sprintf("DELETE FROM `Website themes` WHERE `ID`='%s'", $id), 1);
 			}
 		//
