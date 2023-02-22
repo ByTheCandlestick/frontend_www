07f33d1079cfeede2da437d6315218b96a67daa0
Robert Smith <master_schmadoodle@outlook.com>
2023-02-22 23:29:35 +0000

    Minor changes to audio trail uploader

--- Themes/Vuexy/Assets/js/script.js
+++ Themes/Vuexy/Assets/js/script.js
@@ -1171,7 +1171,7 @@
 					url: api_url + '/Website/',
 					data: {
 						'api_key': api_key,
-						'uid': ($('.profile-btn').children('span')[0]).attr('user-id'),
+						'uid': $($('.profile-btn').children('span')[0]).attr('user-id'),
 						'styles': styles.join(","),
 						'scripts': scripts.join(","),
 						'name': $("div[name=name] input").val(),
@@ -1216,7 +1216,7 @@
 					url: api_url + '/Website/' + sid + '/',
 					data: {
 						'api_key': api_key,
-						'uid': ($('.profile-btn').children('span')[0]).attr('user-id'),
+						'uid': $($('.profile-btn').children('span')[0]).attr('user-id'),
 						'styles': styles.join(","),
 						'scripts': scripts.join(","),
 						'name': $("div[name=name] input").val(),
@@ -1259,7 +1259,7 @@
 					url: api_url + '/Website/' + sid + '/',
 					data: {
 						'api_key': api_key,
-						'uid': ($('.profile-btn').children('span')[0]).attr('user-id'),
+						'uid': $($('.profile-btn').children('span')[0]).attr('user-id'),
 					},
 					type: 'DELETE',
 					xhrFields: {
