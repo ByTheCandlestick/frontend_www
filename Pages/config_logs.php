6f797caa2823f9d8cfb9106e8b19c4659fe2e015
Robert Smith <master_schmadoodle@outlook.com>
2023-02-24 22:46:52 +0000

    Minor changes to admin logs

--- Pages/config_logs.php
+++ Pages/config_logs.php
@@ -6,22 +6,57 @@
 			<h1>Logs</h1>
 		</div>
 		<div class="col-12 col-md-3">
-			<div class="form-floating" name="logSelector">
-				<select class="form-control" id="LogSelect" style="padding: unset; appearance: auto; -webkit-appearance: auto; -moz-appearance: auto;">
-					<option value="">Please select a log...</option>
-					<?
-						foreach(getDirContents(__ROOT__, '.log') as $log) {
-							printf('<option value="%s">%s</option>', $log['Path'], $log['File']);
-						}
-					?>
-				</select>
-			</div>
+			<!--
+				<div class="form-floating" name="logSelector">
+					<select class="form-control" id="LogSelect" style="padding: unset; appearance: auto; -webkit-appearance: auto; -moz-appearance: auto;">
+						<option value="">Please select a log...</option>
+						<?
+							/*
+								foreach(getDirContents(__ROOT__, '.log') as $log) {
+									printf('<option value="%s">%s</option>', $log['Path'], $log['File']);
+								}
+							*/
+						?>
+					</select>
+				</div>
+			-->
 		</div>
 	</div>
 	<hr>
 	<!-- Section Body -->
 	<div class="row overflow-scroll">
-		<div name="logData" class="font-monospace mw-unset w-unset"></div>
+		<div name="logData" class="mw-unset w-unset">
+			<table>
+				<thead>
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">First</th>
+						<th scope="col">Last</th>
+						<th scope="col">Handle</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">1</th>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td>@mdo</td>
+					</tr>
+					<tr>
+						<th scope="row">2</th>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td>@fat</td>
+					</tr>
+					<tr>
+						<th scope="row">3</th>
+						<td>Larry</td>
+						<td>the Bird</td>
+						<td>@twitter</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	</div>
 </section>
 <script>
