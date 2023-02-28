1c88c7523f881925989566fa9686e542f314f225
Robert Smith <master_schmadoodle@outlook.com>
2023-02-28 22:08:34 +0000

    Minor changes to admin logs

--- Pages/config_logs.php
+++ Pages/config_logs.php
@@ -37,14 +37,20 @@
 			<thead>
 				<tr>
 					<th scope="col">#</th>
-					<th scope="col">First</th>
-					<th scope="col">Last</th>
-					<th scope="col">Handle</th>
+					<th scope="col">IP</th>
+					<th scope="col">User</th>
+					<th scope="col">Category</th>
+					<th scope="col">String</th>
+					<th scope="col">Command</th>
+					<th scope="col">Arguments</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr>
 					<th scope="row">1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
 					<td>Mark</td>
 					<td>Otto</td>
 					<td>@mdo</td>
@@ -54,9 +60,15 @@
 					<td>Jacob</td>
 					<td>Thornton</td>
 					<td>@fat</td>
+					<td>Jacob</td>
+					<td>Thornton</td>
+					<td>@fat</td>
 				</tr>
 				<tr>
 					<th scope="row">3</th>
+					<td>Larry</td>
+					<td>the Bird</td>
+					<td>@twitter</td>
 					<td>Larry</td>
 					<td>the Bird</td>
 					<td>@twitter</td>
