e70460372d4c1691e5cfbda9cb19487513f9adb6
Robert Smith <master_schmadoodle@outlook.com>
2023-02-28 22:13:17 +0000

    Minor changes to admin logs

--- Pages/config_logs.php
+++ Pages/config_logs.php
@@ -37,42 +37,34 @@
 			<thead>
 				<tr>
 					<th scope="col">#</th>
-					<th scope="col">IP</th>
 					<th scope="col">User</th>
 					<th scope="col">Category</th>
 					<th scope="col">String</th>
 					<th scope="col">Command</th>
 					<th scope="col">Arguments</th>
+					<th scope="col">IP</th>
 				</tr>
 			</thead>
 			<tbody>
-				<tr>
-					<th scope="row">1</th>
-					<td>Mark</td>
-					<td>Otto</td>
-					<td>@mdo</td>
-					<td>Mark</td>
-					<td>Otto</td>
-					<td>@mdo</td>
-				</tr>
-				<tr>
-					<th scope="row">2</th>
-					<td>Jacob</td>
-					<td>Thornton</td>
-					<td>@fat</td>
-					<td>Jacob</td>
-					<td>Thornton</td>
-					<td>@fat</td>
-				</tr>
-				<tr>
-					<th scope="row">3</th>
-					<td>Larry</td>
-					<td>the Bird</td>
-					<td>@twitter</td>
-					<td>Larry</td>
-					<td>the Bird</td>
-					<td>@twitter</td>
-				</tr>
+				<?
+					$users = array();
+					$query = DB_QUERY(sprintf("SELECT * FROM `Audit trail`"));
+					if($query) {
+						while($row = mysqli_fetch_assoc($query)){
+							?>
+								<tr>
+									<th scope="row">1</th>
+									<td>Mark</td>
+									<td>Otto</td>
+									<td>@mdo</td>
+									<td>Mark</td>
+									<td>Otto</td>
+									<td>@mdo</td>
+								</tr>
+							<?
+						}
+					}
+				?>
 			</tbody>
 		</table>
 	</div>
