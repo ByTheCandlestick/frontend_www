4d003f85610380a428aa6b3af64d4a2d785bea63
Robert Smith <master_schmadoodle@outlook.com>
2023-03-10 23:34:46 +0000

    Minor changes about the Logs page

--- Pages/config_log.php
+++ Pages/config_log.php
@@ -116,7 +116,7 @@
 					<?
 						$args = str_replace("], ", "],\n", $row['Args']);
 						$lines = ($x=count(preg_split('/\n/', $args)) > 10)? 10: $x;
-						print_r($lines);
+						print_r('lines='.$lines);
 					?>
 					<textarea type="text" class="form-control" id="floatingInput" rows="<?=($lines)?>" style="height: unset;" disabled><?=($args)?></textarea>
 					<label for="floatingInput">Args</label>
