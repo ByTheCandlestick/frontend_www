7dc9bcdef1f5d99b9f522a56968408e0ed1a3024
Robert Smith <master_schmadoodle@outlook.com>
2023-03-11 00:42:17 +0000

    Minor changes to API functions

--- API/v1/Models/OrdersModel.php
+++ API/v1/Models/OrdersModel.php
@@ -8,7 +8,7 @@
 		 */
 			public function updateStatus(string $status, string $invoice, string $uid) {
 				$vars = array(array('status', $status), array('invoice', $invoice), array('uid', $uid));
-				$this->uploadAudit(__FUNCTION__, $vars, "Changes status of order -> ".$status, "Orders", $uid);
+				$this->uploadAudit(__FUNCTION__, $vars, "Changes status of order ".$invoice." -> ".$status, "Orders", $uid);
 				return $this->Execute(sprintf("UPDATE `Transactions` SET `Shipping status`='%s' WHERE `Invoice ID`='%s' AND `Type`='Order'", $status, $invoice), 1);
 			}
 	}
