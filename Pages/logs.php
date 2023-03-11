7a1392c1a219e3199dac13f777dc8aed0d79fba9
Robert Smith <master_schmadoodle@outlook.com>
2023-03-11 01:35:55 +0000

    Minor changes to API functions

--- Pages/logs.php
+++ Pages/logs.php
@@ -17,7 +17,7 @@
 			<div class="row">
 				<div class="col-12 col-md-6 form-floating" name="logSelector">
 					<select class="form-control" id="LogSelect" style="padding: unset; appearance: auto; -webkit-appearance: auto; -moz-appearance: auto;" onChange="misc.filterLogs('user', this)">
-						<option>Please select a user...</option>
+						<option value="">Please select a user...</option>
 						<?
 							$query = DB_QUERY(sprintf("SELECT `User ID` FROM `Audit trail` GROUP BY `User ID`"));
 							if($query) {
@@ -31,7 +31,7 @@
 				</div>
 				<div class="col-12 col-md-6 form-floating" name="logSelector">
 					<select class="form-control" id="LogSelect" style="padding: unset; appearance: auto; -webkit-appearance: auto; -moz-appearance: auto;" onChange="misc.filterLogs('category', this)">
-						<option>Please select a category...</option>
+						<option value="">Please select a category...</option>
 						<?
 							$query = DB_QUERY(sprintf("SELECT `Category` FROM `Audit trail` GROUP BY `Category`"));
 							if($query) {
@@ -62,14 +62,13 @@
 			<tbody>
 				<?
 					$str = '';
-					if($_GET['category'] != '' || $_GET['user'] != '') {
-						if($_GET['category'] != '' && $_GET['user'] != '') {
-							$str .= sprintf("WHERE `Category`='%s' AND `User ID`='%s'", $_GET['category'], $_GET['user']);
-						} elseif($_GET['category'] != '') {
-							$str .= sprintf("WHERE `Category`='%s'", $_GET['category']);
-						} elseif($_GET['user'] != '') {
-							$str .= sprintf("WHERE `User ID`='%s'", $_GET['user']);
-						}
+					if($_GET['category'] != '' && $_GET['user'] != '') {
+						$str .= sprintf("WHERE `Category`='%s' AND `User ID`='%s'", $_GET['category'], $_GET['user']);
+					} elseif($_GET['category'] != '') {
+						$str .= sprintf("WHERE `Category`='%s'", $_GET['category']);
+					} elseif($_GET['user'] != '') {
+						$str .= sprintf("WHERE `User ID`='%s'", $_GET['user']);
+					
 					}
 					$query = DB_QUERY($sql = sprintf("SELECT `ID`, `User ID`, `Category`, `String`, `IP`, `Timestamp` FROM `Audit trail` %s ORDER BY `ID` DESC LIMIT %s", $str, $config['Maximum list size']));
 					print_r($sql);
