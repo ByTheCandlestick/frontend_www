14bee6abf847fab545d71d73137ae8ac5c910d7a
Robert Smith <master_schmadoodle@outlook.com>
2023-03-20 15:58:25 +0000

    Minor changes to the installer package

--- installer.php
+++ installer.php
@@ -38,8 +38,20 @@ body {
     position: relative
 }
 
+#msform fieldset span {
+    display: flex;
+	position: relative
+}
+
 #msform fieldset:not(:first-of-type) {
     display: none
+}
+
+#msform fieldset span p {
+    cursor: help;
+    position: absolute;
+    right: 10px;
+    top: 14px;
 }
 
 #msform input,
@@ -180,11 +192,11 @@ body {
 		<fieldset> <!-- Company info -->
 			<h2 class="fs-title">Create your company</h2>
 			<h3 class="fs-subtitle">Here you can set up all of the default company information.</h3>
-
-			<input type="text" name="companyName" placeholder="LexCorp" />
-			<input type="text" name="companyAddress" placeholder="LexCorp Tower, Metropolis" />
-			<input type="tel" name="companyPhone" placeholder="08320031986" />
-			<input type="email" name="companyEmail" placeholder="Lex@lexcorp.com" />
+	
+			<span><input type="text" name="companyName" placeholder="Company Name" /><p title="This will be the name of your company">?</p></span>
+			<span><input type="text" name="companyAddress" placeholder="Company Address" /><p title="">?</p></span>
+			<span><input type="tel" name="companyPhone" placeholder="Company phone" /><p title="">?</p></span>
+			<span><input type="email" name="companyEmail" placeholder="Company Email" /><p title="">?</p></span>
 
 			<input type="button" name="next" class="next action-button" value="Next" />
 		</fieldset>
@@ -192,60 +204,58 @@ body {
 			<h2 class="fs-title">Create your user</h2>
 			<h3 class="fs-subtitle">This will be the default administrator user</h3>
 
-			<input type="text" name="companyName" placeholder="Username" />
-			<input type="text" name="companyName" placeholder="Firstname" />
-			<input type="text" name="companyName" placeholder="Lastname" />
-			<input type="email" name="companyName" placeholder="Email" />
-			<input type="tel" name="companyName" placeholder="Phone" />
-			<input type="password" name="companyName" placeholder="Password" />
-			<input type="password" name="companyName" placeholder="Repeat password" />
+			<span><input type="text" name="companyName" placeholder="Username" /><p title="">?</p></span>
+			<span><input type="text" name="companyName" placeholder="Firstname" /><p title="">?</p></span>
+			<span><input type="text" name="companyName" placeholder="Lastname" /><p title="">?</p></span>
+			<span><input type="email" name="companyName" placeholder="Email" /><p title="">?</p></span>
+			<span><input type="tel" name="companyName" placeholder="Phone" /><p title="">?</p></span>
+			<span><input type="password" name="companyName" placeholder="Password" /><p title="">?</p></span>
+			<span><input type="password" name="companyName" placeholder="Repeat password" /><p title="">?</p></span>
 
 			<input type="button" name="previous" class="previous action-button" value="Previous" />
 			<input type="button" name="next" class="next action-button" value="Next" />
 		</fieldset>
-    <fieldset> <!-- Domain Info -->
+		<fieldset> <!-- Domain Info -->
 			<h2 class="fs-title">Domains</h2>
 			<h3 class="fs-subtitle">Please enter the domains you would like to use with the following websites</h3>
 
-			<input type="text" name="domain-www" placeholder="Public store" />
-			<input type="text" name="domain-admin" placeholder="Admin" />
-			<input type="text" name="domain-blog" placeholder="Blog" />
-			<input type="text" name="domain-blog" placeholder="xPos" />
-			<input type="text" name="domain-blog" placeholder="api" />
+			<span><input type="text" name="domain-www" placeholder="Public store" /><p title="">?</p></span>
+			<span><input type="text" name="domain-admin" placeholder="Admin" /><p title="">?</p></span>
+			<span><input type="text" name="domain-blog" placeholder="Blog" /><p title="">?</p></span>
+			<span><input type="text" name="domain-blog" placeholder="xPos" /><p title="">?</p></span>
+			<span><input type="text" name="domain-blog" placeholder="api" /><p title="">?</p></span>
 
 			<input type="button" name="previous" class="previous action-button" value="Previous" />
 			<input type="button" name="next" class="next action-button" value="Next" />
-
-			<a href="#" class="submit action-button" target="_top">Submit</a>
 		</fieldset>
-    <fieldset> <!-- Database Info -->
+		<fieldset> <!-- Database Info -->
 			<h2 class="fs-title">Databases</h2>
 			<h3 class="fs-subtitle">Please enter the information you would like to use with the following databases.</h3>
 
-      <p>Central Database</p>
-			<input type="text" name="DB1-address" placeholder="Address" />
-			<input type="text" name="DB1-username" placeholder="Username" />
-			<input type="text" name="DB1-password" placeholder="Password" />
-      <p>Analytics Database</p>
-			<input type="text" name="DB2-address" placeholder="Address" />
-			<input type="text" name="DB2-username" placeholder="Username" />
-			<input type="text" name="DB2-password" placeholder="Password" />
+			<p>Central Database</p>
+			<span><input type="text" name="DB1-address" placeholder="Address" /><p title="">?</p></span>
+			<span><input type="text" name="DB1-username" placeholder="Username" /><p title="">?</p></span>
+			<span><input type="text" name="DB1-password" placeholder="Password" /><p title="">?</p></span>
+			<p>Analytics Database</p>
+			<span><input type="text" name="DB2-address" placeholder="Address" /><p title="">?</p></span>
+			<span><input type="text" name="DB2-username" placeholder="Username" /><p title="">?</p></span>
+			<span><input type="text" name="DB2-password" placeholder="Password" /><p title="">?</p></span>
 
 			<input type="button" name="previous" class="previous action-button" value="Previous" />
 			<input type="button" name="next" class="next action-button" value="Next" />
-
-			<a href="#" class="submit action-button" target="_top">Submit</a>
 		</fieldset>
 		<fieldset> <!-- Security Info -->
 			<h2 class="fs-title">Security Details</h2>
 			<h3 class="fs-subtitle">Here you should enter security information to keep the website as secure as possible. DO NOT SHARE THIS INFORMATION.</h3>
 
-			<input type="text" name="salt" placeholder="Salt" />
-			<input type="text" name="pepper" placeholder="Pepper" />
-			<input type="text" name="encryption" placeholder="Password Encryption" />
+			<span><input type="text" name="salt" placeholder="Salt" /><p title="">?</p></span>
+			<span><input type="text" name="pepper" placeholder="Pepper" /><p title="">?</p></span>
+			<span><input type="text" name="encryption" placeholder="Password Encryption" /><p title="">?</p></span>
 
 			<input type="button" name="previous" class="previous action-button" value="Previous" />
 			<input type="button" name="next" class="next action-button" value="Next" />
+
+			<a href="#" class="submit action-button" target="_top">Submit</a>
 		</fieldset>
 	</form>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
