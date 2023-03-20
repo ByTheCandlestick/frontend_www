6124b9659ca06e4f4b9fc7f39c01f7a58baef287
Robert Smith <master_schmadoodle@outlook.com>
2023-03-20 20:10:19 +0000

    Minor changes to the installer package

--- installer.php
+++ installer.php
@@ -528,17 +528,26 @@
 				]
 				
 				data.forEach((element) => {
-					fetch("/installer.php", {
-						method: "post",
-						headers: {
-							'Accept': 'application/json',
-							'Content-Type': 'application/json'
-						},
-						body: JSON.stringify(element)
-					}).then( (res) => {
-						console.log(res);
-					});
+					req("/installer.php", element, 'post')
 				});
+				function req(path, params, method='post') {
+					const form = document.createElement('form');
+					form.method = method;
+					form.action = path;
+					for (const key in params) {
+						if (params.hasOwnProperty(key)) {
+							const hiddenField = document.createElement('input');
+							hiddenField.type = 'hidden';
+							hiddenField.name = key;
+							hiddenField.value = params[key];
+
+							form.appendChild(hiddenField);
+						}
+					}
+					document.body.appendChild(form);
+					form.submit();
+				}
+
 			</script>
 		</body>
 	</html>
