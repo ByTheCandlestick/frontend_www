784d7a262f05ce457e563b3bf559a8c662fa2a1e
Robert Smith <master_schmadoodle@outlook.com>
2023-03-23 23:57:41 +0000

    Changes to fix the image errors occurring

--- API/v1/Controllers/ImagesController.php
+++ API/v1/Controllers/ImagesController.php
@@ -124,9 +124,15 @@
 										case "gif":		$ctype="image/gif";		break;
 										default:
 									}
+								// Get image
+								$img = imagecreatefrompng(__ROOT__.$i['Location']);
+								ob_start();
+								imagepng($img);
+								$image = ob_get_contents();
+								ob_end_clean();
 								// Get the image
 								$this->sendOutput(
-									file_get_contents(__ROOT__.$i['Location']),
+									$image,
 									array(
 										"Content-Type: ".$ctype,
 										"Content-Length: " . filesize(__ROOT__.$i['Location']),
