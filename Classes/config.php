4b931120dc6450d19e9b235d4f40b2e1e46b2220
Robert Smith <master_schmadoodle@outlook.com>
2023-03-26 13:44:15 +0100

    Minor changes to the main index file

--- Classes/config.php
+++ Classes/config.php
@@ -1,5 +1,4 @@
 <?
-	define('DOMAIN_TYPE',	'indev');
 	define('STRIPE_API',	['pk_test_51JKqfVFDFLz8LpozviM6GdhHOMvQJh75VMcH0CVomCXtA9gbTRR7tKRvfjLnWQuXEedTNvoD2O6Gj6hmhDRktH2I00hk6jMnBJ', 'sk_test_51JKqfVFDFLz8LpozmlliBbv92XkspmRyy2O7G6IMk2IccfP9ZnimCZ8rJHHCVfIGupLx5FJZafa92igVC2HFWPkz00umY4pOUm']);
 	define('ADMIN',			['db5007323432.hosting-data.io',	'dbu3023777',	'CandleStick2603',	'dbs6033983']);
 	define('ANALYTICS',		['db5007323454.hosting-data.io',	'dbu557431',	'CandleStick2603',	'dbs6034000']);
