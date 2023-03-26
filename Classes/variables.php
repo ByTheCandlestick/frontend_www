7cee8ea344ff3943842041f8499bb7adc960a3f7
Robert Smith <master_schmadoodle@outlook.com>
2023-03-26 14:14:40 +0100

    Minor changes to the Class files

--- Classes/variables.php
+++ Classes/variables.php
@@ -2,6 +2,9 @@
 	$keys = [];
 	$query = DB_Query("SELECT `ID`, `Key` FROM `API Keys` WHERE `Active?`=1 AND `Created`<now()");
 	while($key = mysqli_fetch_array($query)) { $keys[$key['ID']] = $key['Key']; }
+	define('STRIPE_API',	['pk_test_51JKqfVFDFLz8LpozviM6GdhHOMvQJh75VMcH0CVomCXtA9gbTRR7tKRvfjLnWQuXEedTNvoD2O6Gj6hmhDRktH2I00hk6jMnBJ', 'sk_test_51JKqfVFDFLz8LpozmlliBbv92XkspmRyy2O7G6IMk2IccfP9ZnimCZ8rJHHCVfIGupLx5FJZafa92igVC2HFWPkz00umY4pOUm']);
+	define('ADMIN',			['db5007323432.hosting-data.io',	'dbu3023777',	'CandleStick2603',	'dbs6033983']);
+	define('ANALYTICS',		['db5007323454.hosting-data.io',	'dbu557431',	'CandleStick2603',	'dbs6034000']);
 	define('__API_KEYS__',	$keys);
 	define('__ROOT__',		$_SERVER["DOCUMENT_ROOT"]);
 	define('__THEME__',		getThemepage(false));
