124b69a6b15ac63f8e23ccf6c643d038742ad86d
Robert Smith <master_schmadoodle@outlook.com>
2023-03-26 11:47:58 +0100

    Minor changes to the the images API endpoint

--- index.php
+++ index.php
@@ -7,27 +7,21 @@
 		if($website_info['Subdomain'] == null) {
 			header('Location: '.($_SERVER['HTTPS']?'https://www.':'http://www.').$_SERVER['HTTP_HOST'].'/');
 		}
-	// Display file if get[file] and get[ext] is set, else display theme index file for the eheme
+	// Display file if get[file] and get[ext] is set, else display index file for the teheme
 		if(isset($_GET['file']) && isset($_GET['ext'])) {
 			ob_start();
-			if(file_exists($path = sprintf("%s/Themes/%s/Assets/%s/%s.%s", __ROOT__, __THEME__, $_GET['ext'], $_GET['file'], $_GET['ext']))) {
-				if($_GET['ext'] == "php") {
-					include_once($path);
-				} else {
-
+				if(file_exists($path = sprintf("%s/Themes/%s/Assets/%s/%s.%s", __ROOT__, __THEME__, $_GET['ext'], $_GET['file'], $_GET['ext']))) {
+					if($_GET['ext'] == "php") {
+						include_once($path);
+					} else {
 						header('Content-Type: text/'.$_GET['ext']);
 						print(file_get_contents($path));
-
+					}
+				} else {
+					header('Content-Type: text/json');
+					print(json_encode(array("error"=>"File not found")));
 				}
-			} else {
-				header('Content-Type: text/json');
-				print(json_encode(array("error"=>"File not found")));
-			}
-			if (ob_get_length() > 0) {
-				ob_end_flush();
-			} else {
-				ob_end_clean();
-			}
+			ob_end_clean();
 		} else {
 			if($website_info['Maintenance']) {
 				require_once('./Pages/maintenance.php');
